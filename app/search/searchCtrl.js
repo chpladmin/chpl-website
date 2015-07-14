9cfaf4b9fe81873ed51ebbf3c3ff312fec6597f5
Andrew Larned <andrew.larned@gmail.com>
2015-07-14 12:37:36 -0400

    Attempts to do search in-place

--- app/search/searchCtrl.js
+++ app/search/searchCtrl.js
@@ -2,14 +2,19 @@
     'use strict';
 
     angular.module('app.search')
-        .controller('SearchController', ['$scope', '$log', 'searchService', function ($scope, $log, searchService) {
+        .controller('SearchController', ['$scope', '$log', '$filter', 'searchService', function ($scope, $log, $filter, searchService) {
             var self = this;
             $scope.searchResults = [];
             $scope.displayedResults = [];
+            this.filteredResults = [];
 
             self.search = function () {
                 $scope.searchResults = searchService.search();
-                $scope.displayedResults = [].concat(self.searchResults);
+                this.filteredResults = $filter('filter')($scope.searchResults, $scope.searchTerm);
+                $scope.displayedResults = [].concat(this.filteredResults);
+                $log.info($scope.searchResults.length);
+                $log.info(this.filteredResults.length);
+                $log.info($scope.displayedResults.length);
             };
             $scope.search = self.search;
 
--- app/search/searchService.js
+++ app/search/searchService.js
@@ -4,7 +4,7 @@
     /*****************************************************
      * This section is dummy data until the API can be wired up
      */
-    var max_ishResults = 300;
+    var max_ishResults = 200;
     var words = ['Aliquam', 'Erat', 'Volutpat', 'Nunc', 'Eleifend', 'Leo', 'Vitae', 'Magna', 'In', 'Id', 'Erat', 'Non', 'Orci', 'Commodo', 'Lobortis', 'Proin', 'Neque', 'Massa', 'Cursus', 'Ut', 'Gravida', 'Ut', 'Lobortis', 'Eget', 'Lacus', 'Sed', 'Diam', 'Praesent', 'Fermentum', 'Tempor', 'Tellus', 'Nullam', 'Tempus', 'Mauris', 'Ac', 'Felis', 'Vel', 'Velit', 'Tristique', 'Imperdiet', 'Donec', 'At', 'Pede', 'Etiam', 'Vel', 'Neque', 'Nec', 'Dui', 'Dignissim', 'Bibendum', 'Vivamus', 'Id', 'Enim', 'Phasellus', 'Neque', 'Orci', 'Porta', 'A', 'Aliquet', 'Quis', 'Semper', 'A', 'Massa', 'Phasellus', 'Purus', 'Pellentesque', 'Tristique', 'Imperdiet', 'Tortor', 'Nam', 'Euismod', 'Tellus', 'Id', 'Erat', 'Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet', 'Consectetuer', 'Adipiscing', 'Elit', 'Donec', 'Hendrerit', 'Tempor', 'Tellus', 'Donec', 'Pretium', 'Posuere', 'Tellus', 'Proin', 'Quam', 'Nisl', 'Tincidunt', 'Et', 'Mattis', 'Eget', 'Convallis', 'Nec', 'Purus', 'Sociis', 'Natoque', 'Penatibus', 'Et', 'Magnis', 'Dis', 'Parturient', 'Montes', 'Nascetur', 'Ridiculus', 'Mus', 'Nulla', 'Posuere', 'Donec', 'Vitae', 'Dolor', 'Nullam', 'Tristique', 'Diam', 'Non', 'Turpis', 'Cras', 'Placerat', 'Accumsan', 'Nulla', 'Nullam', 'Rutrum', 'Nam', 'Vestibulum', 'Accumsan', 'Nisl'];
     var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
     var days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'];
