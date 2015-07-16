f71d0d576fcd010b4713ec900d00c4b666fbffb5
Andrew Larned <andrew.larned@gmail.com>
2015-07-16 13:56:38 -0400

    First pass at product details page done

--- app/product/productCtrl.js
+++ app/product/productCtrl.js
@@ -2,8 +2,14 @@
     'use strict';
 
     angular.module('app.product')
-        .controller('ProductController', ['$scope', '$log', '$routeParams', 'productService', function($scope, $log, $routeParams, productService) {
+        .controller('ProductController', ['$scope', '$log', '$routeParams', 'commonService', function($scope, $log, $routeParams, commonService) {
             var self = this;
-            this.productId = $routeParams.id;
+            self.productId = $routeParams.id;
+            commonService.getProduct(this.productId)
+                .then(function (data) {
+                    self.product = data;
+                }, function (error) {
+                    $log.error(error);
+                });
         }]);
 })();
--- app/product/productModule.js
+++ app/product/productModule.js
@@ -1,11 +1,36 @@
 ;(function () {
     'use strict';
 
-    angular.module('app.product', ['ngRoute'])
+    angular.module('app.product', ['ngRoute', 'app.common', 'ui.bootstrap'])
         .constant('searchAPI', 'http://ainq.com')
         .config(['$routeProvider', function($routeProvider) {
             $routeProvider.when('/product/:id', {
                 templateUrl: 'product/product.html'
             });
-        }]);
+        }])
+        .filter('splitArrayFilter', function ($log) {  // <div class="row" ng-repeat="row in filtered = (cert.certs | splitArrayFilter:2)">
+            return function(arr, lengthofsublist) {
+                if (!angular.isUndefined(arr) && arr.length > 0) {
+                    var arrayToReturn = [];
+                    var subArray = [];
+                    var pushed = true;
+                    for (var i = 0; i < arr.length; i++){
+                        if ((i + 1) % lengthofsublist === 0) {
+                            subArray.push(arr[i]);
+                            arrayToReturn.push(subArray);
+                            subArray = [];
+                            pushed = true;
+                        } else {
+                            subArray.push(arr[i]);
+                            pushed = false;
+                        }
+                    }
+                    if (!pushed)
+                        arrayToReturn.push(subArray);
+
+                    $log.info(JSON.stringify(arrayToReturn));
+                    return arrayToReturn;
+                }
+            }
+        });
 })();
