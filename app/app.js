81a46a41f9171448a161e14078c03684c1b6c707
Andrew Larned <andrew.larned@gmail.com>
2015-09-11 08:43:31 -0400

    Initial commit of GA tracking code

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 ;(function () {
     'use strict';
 
-    angular.module('app', ['ngRoute', 'angular-loading-bar', 'ngAnimate',
+    angular.module('app', ['ngRoute', 'angular-loading-bar', 'ngAnimate', 'angulartics', 'angulartics.google.analytics',
                            'app.api', 'app.nav', 'app.compare', 'app.product', 'app.search', 'app.admin'])
         .config(['$routeProvider', function($routeProvider) {
             $routeProvider
