8586cffbfe6cfdf5b30107924de202b290c3d043
Andrew Larned <andrew.larned@gmail.com>
2016-02-15 08:27:30 -0500

    OCD-549: Added 'skip to main content' link

--- app/app.js
+++ app/app.js
@@ -8,11 +8,23 @@
             $routeProvider
                 .otherwise({redirectTo: '/search'});
         }])
-        .run(['$location', '$rootScope', function($location, $rootScope) {
+        .run(['$location', '$rootScope', '$anchorScroll', '$timeout', '$window', function($location, $rootScope, $anchorScroll, $timeout, $window) {
             $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
                 if(current.$$route) {
                     $rootScope.title = current.$$route.title;
+                    $rootScope.currentPage = $location.path();
                 }
-            })
+                if($location.hash()) {
+                    $anchorScroll();
+                    $timeout(function () {
+                        var element = $window.document.getElementById('mainContent');
+                        var elementAng = angular.element($window.document.getElementById('mainContent'));
+                        if (element && elementAng) {
+                            elementAng.attr('tabindex', '-1');
+                            element.focus();
+                        }
+                    });
+                }
+            });
         }]);
 })();
