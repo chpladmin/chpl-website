7ae47c4da3de7dd6122ac56da6ccd05aedd42f9d
Noby Nobriga <nnobriga@deloitte.com>
2016-09-01 14:24:13 -0500

    Changed Cert ID lookup invalid format error message to include the ID

--- app/resources/resourcesCtrl.js
+++ app/resources/resourcesCtrl.js
@@ -7,7 +7,7 @@
 
 			vm.lookupCertIds = lookupCertIds;
 			vm.download = download;
-			vm.lookupProductsFormatInvalid = false;
+			vm.lookupProductsFormatInvalidIds = [];
 			vm.viewProduct = viewProduct;
 
             activate();
@@ -15,6 +15,7 @@
 			// Restore lookup IDs and results
 			vm.certIds = $localStorage.lookupCertIds;
 			vm.lookupProducts = $localStorage.lookupProducts;
+			vm.lookupProductsFormatInvalidIds = $localStorage.lookupProductsFormatInvalidIds;
 
 			if ($localStorage.lookupCertIds && !$localStorage.lookupProducts) {
 				lookupCertIds();
@@ -103,7 +104,7 @@
 
 			function lookupCertIds () {
 				vm.lookupProducts = null;
-				vm.lookupProductsFormatInvalid = false;
+				vm.lookupProductsFormatInvalidIds = [];
 
 				if ((vm.lookup !== "undefined") && (vm.certIds !== "undefined")) {
 					vm.certIds = vm.certIds.replace(/[;,\s]+/g, " ");
@@ -111,43 +112,54 @@
 
 					// Check format of input
 					if ("" === vm.certIds.trim()) {
-						vm.lookupProductsFormatInvalid = false;
 						clearLookup();
-					} else if (null !== vm.certIds.match(/^([0-9A-Z]{15}([ ][0-9A-Z]{15})*)$/i)) {
+					} else {
 
 						// Split IDs
 						var idArray = vm.certIds.split(/ /);
 						vm.lookupProducts = null;
 
 						$localStorage.lookupCertIds = vm.certIds;
-
+						
+						var preventDuplicationIds = {};
+						
 						// Call LookupAPI
 						idArray.forEach(function (id) {
-							commonService.lookupCertificationId(id)
-								.then(function (data) {
-									if (vm.lookupProducts === null)
-										vm.lookupProducts = [];
-									data.products.forEach(function (product) {
-										product.certificationId = data.ehrCertificationId;
-										product.certificationIdEdition = data.year;
-										vm.lookupProducts.push(product);
-									});
 
-									$localStorage.lookupProducts = vm.lookupProducts;
+							// Check if we've already checked this ID in case the user entered duplicates
+							if (typeof preventDuplicationIds[id] === "undefined") {
+								preventDuplicationIds[id] = true;
+						
+								// Check if ID format is valid
+								if (!id.match(/^[0-9A-Z]{15}$/i)) {
+									// Invalid ID format
+									vm.lookupProductsFormatInvalidIds.push(id);
+								} else {
+									// Valid ID format
+									commonService.lookupCertificationId(id)
+										.then(function (data) {
+											if (vm.lookupProducts === null)
+												vm.lookupProducts = [];
+											data.products.forEach(function (product) {
+												product.certificationId = data.ehrCertificationId;
+												product.certificationIdEdition = data.year;
+												vm.lookupProducts.push(product);
+											});
 
-								}, function (error) {
-									console.debug("Error: " + error);
-									clearLookupResults();
-								});
+											$localStorage.lookupProducts = vm.lookupProducts;
+											$localStorage.lookupProductsFormatInvalidIds = vm.lookupProductsFormatInvalidIds;
+											
+										}, function (error) {
+											console.debug("Error: " + error);
+											clearLookupResults();
+										});
+								}
+							}
 						});
-					} else {
-						$localStorage.lookupCertIds = vm.certIds;
-						vm.lookupProductsFormatInvalid = true;
-						clearLookupResults();
 					}
 				}
 			}
-
+			
 			function clearLookup() {
 				delete $localStorage.lookupCertIds;
 				vm.certIds = null;
@@ -156,6 +168,7 @@
 
 			function clearLookupResults() {
 				delete $localStorage.lookupProducts;
+				delete $localStorage.lookupProductsFormatInvalidIds
 				vm.lookupProducts = null;
 			}
 
