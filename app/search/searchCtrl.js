6e3f3223c686bf9e1c243637a519cb5ea56a5e38
Andrew Larned <andrew.larned@gmail.com>
2016-11-14 09:09:16 -0500

    Fixed product merge w/previous developer; updated filters
     * OCD-977 for Product merge
     * OCD-997 for Filter update

--- app/search/searchCtrl.js
+++ app/search/searchCtrl.js
@@ -14,6 +14,7 @@
             vm.clearPreviouslyViewed = clearPreviouslyViewed;
             vm.certificationStatusFilter = certificationStatusFilter;
             vm.compare = compare;
+            vm.isChangedFromDefault = isChangedFromDefault;
             vm.populateSearchOptions = populateSearchOptions;
             vm.reloadResults = reloadResults;
             vm.restoreResults = restoreResults
@@ -37,15 +38,17 @@
                 vm.defaultRefineModel = {
                     certificationStatus: {
                         'Active': true,
+                        'Retired': false,
                         'Suspended by ONC-ACB': true,
                         'Withdrawn by Developer': true,
                         'Withdrawn by ONC-ACB': true
                     },
                     certificationEdition: {
+                        '2011': false,
                         '2014': true,
                         '2015': true
                     },
-                    certificationBody: {
+                    acb: {
                         'Drummond Group': true,
                         'ICSA Labs': true,
                         'InfoGard': true
@@ -206,6 +209,14 @@
                     }
                     $localStorage.previouslyCompared = prev;
                     $location.url(comparePath);
+                }
+            }
+
+            function isChangedFromDefault (index, data) {
+                if (!vm.defaultRefineModel[index]) {
+                    return vm.refineModel[index] && vm.refineModel[index][data];
+                } else {
+                    return (vm.defaultRefineModel[index][data] !== vm.refineModel[index][data]);
                 }
             }
 
