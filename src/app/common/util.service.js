ec51877db6826bdbaa20cad0bb6de0afee1841d9
Andrew Larned <andrew.larned@gmail.com>
2017-01-24 11:24:38 -0500

    OCD-1028: Added reports for g1/g2 macra measure activity

--- src/app/common/util.service.js
+++ src/app/common/util.service.js
@@ -8,6 +8,7 @@
     function utilService () {
         var self = this;
 
+        self.arrayCompare = arrayCompare;
         self.extendSelect = extendSelect;
         self.sortCert = sortCert;
         self.sortCqm = sortCqm;
@@ -15,6 +16,62 @@
         self.sortRequirements = sortRequirements;
 
         ////////////////////////////////////////////////////////////////////
+
+        function arrayCompare (before, after, key) {
+            var ret = {
+                added: [],
+                edited: [],
+                removed: []
+            };
+            if (angular.isUndefined(key)) { key = 'id'; }
+            var i,j;
+            var added, removed;
+            var count = Math.max(
+                angular.isArray(before) ? before.length : 0,
+                angular.isArray(after) ? after.length : 0
+            );
+
+            for (i = 0; i < count; i++) {
+                // check after[i] against before
+                if (after && angular.isDefined(after[i])) {
+                    added = true;
+                    if (before) {
+                        for (j = 0; j < before.length; j++) {
+                            // if equal, then not added
+                            if (angular.equals(after[i],before[j])) { added = false; }
+                            // if not equal, but have equal ids, then edited
+                            else if (angular.isDefined(after[i][key]) &&
+                                     angular.isDefined(before[j][key]) &&
+                                     after[i][key] === before[j][key]) {
+                                added = false;
+                                ret.edited.push({before: before[j], after: after[i]});
+                            }
+                        }
+                    }
+                    if (added) {
+                        ret.added.push(after[i]);
+                    }
+                }
+                // check before[i] against after
+                if (before && angular.isDefined(before[i])) {
+                    removed = true;
+                    if (after) {
+                        for (j = 0; j < after.length; j++) {
+                            // if equal, then not added
+                            if (angular.equals(before[i], after[j])) { removed = false; }
+                            // if not equal, but have equal ids, then edited
+                            else if (angular.isDefined(before[i][key]) &&
+                                     angular.isDefined(after[j][key]) &&
+                                     before[i][key] === after[j][key]) { removed = false; }
+                        }
+                    }
+                    if (removed) {
+                        ret.removed.push(before[i]);
+                    }
+                }
+            }
+            return ret;
+        }
 
         function extendSelect (options, value) {
             var newValue = { name: value };
