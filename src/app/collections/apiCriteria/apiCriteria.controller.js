67fa23870e61db3e8433a2b362f25449a098eaf0
Andrew Larned <andrew.larned@gmail.com>
2017-06-05 13:23:04 -0400

    OCD-1350: API Criteria page in progress; needs text/description/title/etc.

--- src/app/collections/apiCriteria/apiCriteria.controller.js
+++ src/app/collections/apiCriteria/apiCriteria.controller.js
@@ -8,27 +8,58 @@
     function ApiCriteriaController () {
         var vm = this;
 
+        vm.apiTransform = apiTransform;
+        vm.disclosuresTransform = disclosuresTransform;
+
         activate();
 
         ////////////////////////////////////////////////////////////////////
 
         function activate () {
             vm.columnSet = [
-                { predicate: 'edition', display: 'Edition' },
                 { predicate: 'developer', display: 'Developer' },
                 { predicate: 'product', display: 'Product' },
                 { predicate: 'version', display: 'Version' },
-                { predicate: 'acb', display: 'ONC-ACB' },
-                { predicate: 'certificationDate', display: 'Certification Date', isDate: true },
                 { predicate: 'chplProductNumber', display: 'CHPL ID', sortDefault: true, isLink: true },
-                { predicate: 'apiDocumentation', display: 'API Documentation', nullDisplay: 'N/A' },
+                { predicate: 'apiDocumentation', display: 'API Documentation', transformFn: vm.apiTransform },
+                { predicate: 'mandatoryDisclosures', display: 'Mandatory Disclosures URL', transformFn: vm.disclosuresTransform },
             ];
-            vm.filters = ['acb', 'certificationDate'];
-            vm.refineModel = {acb: [
-                { value: 'Drummond Group', selected: true },
-                { value: 'ICSA Labs', selected: true },
-                { value: 'Infogard', selected: true },
-            ]};
+        }
+
+        ////////////////////////////////////////////////////////////////////
+
+        function apiTransform (data) {
+            var ret = 'Unknown';
+            if (data) {
+                var apis = {};
+                var pairs = data.split('☺');
+                var key, value;
+                for (var i = 0; i < pairs.length; i++) {
+                    var items = pairs[i].split('☹');
+                    key = items[0];
+                    value = items[1];
+                    if (value) {
+                        if (!apis[value]) {
+                            apis[value] = [];
+                        }
+                        apis[value].push(key);
+                    }
+                }
+                ret = '<ul class="list-unstyled">';
+                angular.forEach(apis, function (value, key) {
+                    ret += '<li>' + value.join(', ') + ': ' + '<ai-a href="' + key + '">' + key + '</ai-a></li>';
+                });
+                ret += '</ul>';
+            }
+            return ret;
+        }
+
+        function disclosuresTransform (data) {
+            var ret = 'Unknown';
+            if (data) {
+                ret = '<ai-a href="' + data + '">' + data + '</ai-a>';
+            }
+            return ret;
         }
     }
 })();
--- src/app/collections/apiCriteria/apiCriteria.controller.spec.js
+++ src/app/collections/apiCriteria/apiCriteria.controller.spec.js
@@ -27,12 +27,48 @@
 
         afterEach(function () {
             if ($log.debug.logs.length > 0) {
-                //console.log('Debug log, ' + $log.debug.logs.length + ' length:\n Debug: ' + $log.debug.logs.join('\n Debug: '));
+                /* eslint-disable no-console,angular/log */
+                console.log('Debug:\n' + $log.debug.logs.map(function (o) { return angular.toJson(o); }).join('\n'));
+                /* eslint-enable no-console,angular/log */
             }
         });
 
         it('should exist', function () {
             expect(vm).toBeDefined();
+        });
+
+        describe('transforming API Documentation data', function () {
+            it('should return "Unknown" if no data', function () {
+                var data;
+                expect(vm.apiTransform(data)).toBe('Unknown');
+            });
+
+            it('should create a list of three elements if the APIs are all different', function () {
+                var data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☹http://example2.com☺170.315 (g)(9)☹http://example3.com';
+                expect(vm.apiTransform(data)).toBe('<ul class="list-unstyled"><li>170.315 (g)(7): <ai-a href="http://example1.com">http://example1.com</ai-a></li><li>170.315 (g)(8): <ai-a href="http://example2.com">http://example2.com</ai-a></li><li>170.315 (g)(9): <ai-a href="http://example3.com">http://example3.com</ai-a></li></ul>');
+            });
+
+            it('should combine elements a list of three elements if the APIs the same', function () {
+                var data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☹http://example1.com☺170.315 (g)(9)☹http://example1.com';
+                expect(vm.apiTransform(data)).toBe('<ul class="list-unstyled"><li>170.315 (g)(7), 170.315 (g)(8), 170.315 (g)(9): <ai-a href="http://example1.com">http://example1.com</ai-a></li></ul>');
+            });
+
+            it('should only have one element if only one api link', function () {
+                var data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☺170.315 (g)(9)☹';
+                expect(vm.apiTransform(data)).toBe('<ul class="list-unstyled"><li>170.315 (g)(7): <ai-a href="http://example1.com">http://example1.com</ai-a></li></ul>');
+            });
+        });
+
+        describe('transforming Mandatory Disclosures data', function () {
+            it('should return "Unknown" if no data', function () {
+                var data;
+                expect(vm.disclosuresTransform(data)).toBe('Unknown');
+            });
+
+            it('should a link for the data', function () {
+                var data = 'http://example.com';
+                expect(vm.disclosuresTransform(data)).toBe('<ai-a href="http://example.com">http://example.com</ai-a>');
+            });
         });
     });
 })();
