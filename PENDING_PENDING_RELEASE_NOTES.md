cb8876a918a10f2556fd7c38805f5b9742f55b0f
Andrew Larned <andrew.larned@gmail.com>
2018-05-21 09:23:35 -0400

    OCD-2104: Re-order charts; add RELEASE NOTES

---
+++ PENDING_PENDING_RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+
+## Version 9.21.0
+_Date TBD_
+
+### New Features
+* Added "New vs. Incumbent Developer" charts to Charts! page
+
+---
--- src/app/charts/charts.controller.spec.js
+++ src/app/charts/charts.controller.spec.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    fdescribe('the Charts component controller', function () {
+    describe('the Charts component controller', function () {
 
         var $controller, $log, $q, mock, networkService, scope, vm;
         mock = {
