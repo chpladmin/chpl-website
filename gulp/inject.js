6220aab2c7dfa92cdf18917009c86ad6d14a8708
Andrew Larned <andrew.larned@gmail.com>
2018-07-16 13:16:15 -0400

    Remove dependency on bower
    [#OCD-2253]

--- gulp/inject.js
+++ gulp/inject.js
@@ -5,10 +5,9 @@ var gulp = require('gulp');
 var conf = require('./conf');
 var argv = require('yargs').argv;
 
-var $ = require('gulp-load-plugins')();
+//var $ = require('gulp-load-plugins')();
 
-var wiredep = require('wiredep').stream;
-var _ = require('lodash');
+//var _ = require('lodash');
 
 var browserSync = require('browser-sync');
 var environment = argv.env || 'local';
@@ -18,31 +17,11 @@ gulp.task('inject-reload', ['inject'], function() {
 });
 
 gulp.task('inject', [], function () {
-//    var injectStyles = gulp.src([
-//        path.join(conf.paths.tmp, '/serve/app/**/*.css'),
-//        path.join('!' + conf.paths.tmp, '/serve/app/vendor.css')
-//    ], { read: false });
-
-//    var injectScripts = gulp.src([
-//        path.join(conf.paths.src, '/app/**/*.module.js'),
-//        path.join(conf.paths.src, '/app/**/*.js'),
-//        path.join('!' + conf.paths.src, '/app/**/*.spec.js'),
-//        path.join('!' + conf.paths.src, '/app/**/*.mock.js'),
-//    ])
-//        .pipe($.angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
-
-//    var injectOptions = {
-//        ignorePath: [conf.paths.src, path.join(conf.paths.tmp, '/serve')],
-//        addRootSlash: false
-//    };
 
     var htmlFiles = [path.join(conf.paths.src, '/*.html')];
     if (environment !== 'local') {
         htmlFiles.push('!**/style.html');
     }
     return gulp.src(htmlFiles)
-//        .pipe($.inject(injectStyles, injectOptions))
-  //      .pipe($.inject(injectScripts, injectOptions))
-//        .pipe(wiredep(_.extend({}, conf.wiredep)))
         .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve')));
 });
