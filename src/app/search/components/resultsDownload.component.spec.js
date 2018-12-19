ac39510f049edbc135e314fe6ba06fb4fce1be16
Andrew Larned <andrew.larned@gmail.com>
2018-12-19 10:54:27 -0500

    feat: allow download of search results
    [#OCD-111]

--- src/app/search/components/resultsDownload.component.spec.js
+++ src/app/search/components/resultsDownload.component.spec.js
@@ -1,84 +1,100 @@
-(function () {
-    'use strict';
+'use strict';
 
-    fdescribe('the search results download component', function () {
-        var $log, Mock, ctrl, el, mock, scope, utilService;
+describe('the search results download component,', () => {
+    var $log, Mock, ctrl, el, mock, scope, utilService;
 
-        let getLink = id => 'http://chpl.healthit.gov/#/product/' + id;
-        let makeDate = date => 'do the filter' + date;
-        mock = {
-            listings: [],
-            columns: [
-                { display: 'Edition', key: 'edition' },
+    mock = {
+        listings: [],
+        categories: [
+            { display: 'Edition', enabled: true, columns: [{ display: 'Edition', key: 'edition' }] },
+            { display: 'Product data', enabled: true, columns: [
                 { display: 'Developer', key: 'developer' },
                 { display: 'Product', key: 'product' },
                 { display: 'Version', key: 'version' },
-                { display: 'Certification Date', key: 'certificationDate', transform: makeDate },
-                { display: 'CHPL ID', key: 'chplProductNumber' },
-                { display: 'Status', key: 'certificationStatus' },
-                { display: 'Details', key: 'id', transform: getLink },
-            ],
-        };
+            ]},
+            { display: 'Certification Date', enabled: true, columns: [{ display: 'Certification Date', key: 'certificationDate', transform: date => 'do the filter' + date }] },
+            { display: 'CHPL ID', enabled: true, columns: [{ display: 'CHPL ID', key: 'chplProductNumber' }] },
+            { display: 'Status', enabled: true, columns: [{ display: 'Status', key: 'certificationStatus' }] },
+            { display: 'Details', enabled: true, columns: [{ display: 'Details', key: 'id', transform: id => 'http://chpl.healthit.gov/#/product/' + id }] },
+            { display: 'Criteria', enabled: false, columns: [{ display: '170.315 (a)(1)', key: '170315a1' }] },
+        ],
+    };
 
-        beforeEach(function () {
-            angular.mock.module('chpl.mock', 'chpl', function ($provide) {
-                $provide.decorator('utilService', function ($delegate) {
-                    $delegate.makeCsv = jasmine.createSpy('makeCsv');
-                    return $delegate;
-                });
-            });
-
-            inject(function ($compile, _$log_, $rootScope, _Mock_, _utilService_) {
-                $log = _$log_;
-                Mock = _Mock_;
-                utilService = _utilService_;
-                utilService.makeCsv.and.returnValue();
-
-                scope = $rootScope.$new();
-                scope.listings = Mock.allCps;
-                scope.columns = mock.columns;
-
-                el = angular.element('<ai-results-download listings="listings" columns="columns" max-size="50"></ai-results-download>');
-
-                $compile(el)(scope);
-                scope.$digest();
-                ctrl = el.isolateScope().$ctrl;
+    beforeEach(() => {
+        angular.mock.module('chpl.mock', 'chpl', ($provide) => {
+            $provide.decorator('utilService', ($delegate) => {
+                $delegate.makeCsv = jasmine.createSpy('makeCsv');
+                return $delegate;
             });
         });
 
-        afterEach(function () {
-            if ($log.debug.logs.length > 0) {
-                /* eslint-disable no-console,angular/log */
-                console.log('Debug:\n' + $log.debug.logs.map(function (o) { return angular.toJson(o); }).join('\n'));
-                /* eslint-enable no-console,angular/log */
-            }
-        });
+        inject(($compile, _$log_, $rootScope, _Mock_, _utilService_) => {
+            $log = _$log_;
+            Mock = _Mock_;
+            utilService = _utilService_;
+            utilService.makeCsv.and.returnValue();
 
-        it('should be compiled', function () {
-            expect(el.html()).not.toEqual(null);
-        });
+            scope = $rootScope.$new();
+            scope.listings = Mock.allCps;
+            scope.categories = mock.categories
+            scope.maxSize = 50;
 
-        describe('should use the util service', function () {
-            it('to make a csv', function () {
-                ctrl.getCsv();
-                expect(utilService.makeCsv).toHaveBeenCalled();
-            });
-        });
+            el = angular.element('<ai-results-download listings="listings" categories="categories" max-size="{{ scope.maxSize }}"></ai-results-download>');
 
-        describe('for the csv download', function () {
-            it('should create a data object with a name and a header row', function () {
-                expect(ctrl.csvData.name).toBe('search-results.csv');
-                expect(ctrl.csvData.values[0]).toEqual([
-                    'Edition', 'Developer', 'Product', 'Version', 'Certification Date', 'CHPL ID', 'Status', 'Details',
-                ]);
-            });
-
-            it('should have data rows', function () {
-                expect(ctrl.csvData.values.length).toBe(9);
-                expect(ctrl.csvData.values[1]).toEqual([
-                    '2014', 'Systemedx Inc', '2013 Systemedx Clinical Navigator', '2013.12', 'do the filter1396497600000', 'CHP-022218', 'Active', 'http://chpl.healthit.gov/#/product/296',
-                ]);
-            });
+            $compile(el)(scope);
+            scope.$digest();
+            ctrl = el.isolateScope().$ctrl;
         });
     });
-})();
+
+    afterEach(() => {
+        if ($log.debug.logs.length > 0) {
+            /* eslint-disable no-console,angular/log */
+            console.log('Debug:\n' + $log.debug.logs.map((o) => angular.toJson(o)).join('\n'));
+            /* eslint-enable no-console,angular/log */
+        }
+    });
+
+    it('should be compiled', () => {
+        expect(el.html()).not.toEqual(null);
+    });
+
+    describe('should use the util service', () => {
+        it('to make a csv', () => {
+            ctrl.getCsv();
+            expect(utilService.makeCsv).toHaveBeenCalled();
+        });
+    });
+
+    describe('when generating the csv download,', () => {
+        it('should create a data object with a name and a header row', () => {
+            expect(ctrl.csvData.name).toBe('search-results.csv');
+            expect(ctrl.csvData.values[0]).toEqual([
+                'Edition', 'Developer', 'Product', 'Version', 'Certification Date', 'CHPL ID', 'Status', 'Details',
+            ]);
+        });
+
+        it('should have data rows', () => {
+            expect(ctrl.csvData.values.length).toBe(9);
+            expect(ctrl.csvData.values[1]).toEqual([
+                '2014', 'Systemedx Inc', '2013 Systemedx Clinical Navigator', '2013.12', 'do the filter1396497600000', 'CHP-022218', 'Active', 'http://chpl.healthit.gov/#/product/296',
+            ]);
+        });
+    });
+
+    describe('when updating values,', () => {
+        it('should not generate a csv if there are too many listings', () => {
+            ctrl.$onChanges({ maxSize: { currentValue: 6 }});
+            expect(ctrl.csvData.values).toEqual([]);
+        });
+
+        it('should update when categories change', () => {
+            let cats = angular.copy(mock.categories);
+            cats[1].enabled = false;
+            ctrl.$onChanges({ categories: { currentValue: cats }});
+            expect(ctrl.csvData.values[0]).toEqual([
+                'Edition', 'Certification Date', 'CHPL ID', 'Status', 'Details',
+            ]);
+        });
+    });
+});
