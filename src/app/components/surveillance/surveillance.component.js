2d17162303f8815f7d268fa27cecddd3018340a3
Andrew Larned <andrew.larned@gmail.com>
2019-02-11 12:55:05 -0500

    fix: re-enable editing of surveillance
    [#OCD-2694]

--- src/app/components/surveillance/surveillance.component.js
+++ src/app/components/surveillance/surveillance.component.js
@@ -38,9 +38,7 @@ export const SurveillanceComponent = {
         editSurveillance (surveillance) {
             this._fixRequirementOptions();
             this.uibModalInstance = this.$uibModal.open({
-                templateUrl: 'chpl.admin/components/surveillance/edit.html',
-                controller: 'EditSurveillanceController',
-                controllerAs: 'vm',
+                component: 'aiSurveillanceEdit',
                 animation: false,
                 backdrop: 'static',
                 keyboard: false,
