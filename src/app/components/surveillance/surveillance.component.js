cc60dcea586428e13e66f060b0f6a948b37b22c8
Andrew Larned <andrew.larned@gmail.com>
2019-02-12 09:15:31 -0500

    fix: use correct component for surveillance
    [#OCD-2694]

--- src/app/components/surveillance/surveillance.component.js
+++ src/app/components/surveillance/surveillance.component.js
@@ -100,9 +100,7 @@ export const SurveillanceComponent = {
         initiateSurveillance () {
             this._fixRequirementOptions();
             this.uibModalInstance = this.$uibModal.open({
-                templateUrl: 'chpl.admin/components/surveillance/edit.html',
-                controller: 'EditSurveillanceController',
-                controllerAs: 'vm',
+                component: 'aiSurveillanceEdit',
                 animation: false,
                 backdrop: 'static',
                 keyboard: false,
