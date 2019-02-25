deb584a7cab5be113180810d566e14eb87262f18
Andrew Larned <andrew.larned@gmail.com>
2019-02-25 10:07:54 -0500

    test: add tests for allowing impersonation
    [#OCD-2652]

--- src/app/services/login.service.spec.js
+++ src/app/services/login.service.spec.js
@@ -9,7 +9,7 @@
                 Identity: [31, 'username', 'Full Name'],
             },
             impersonating: {
-                Authorities: [],
+                Authorities: ['ROLE_ADMIN'],
                 Identity: [31, 'username', 'Full Name', 3, 'admin'],
             },
         }
@@ -47,11 +47,6 @@
         it('should get a fullname when logged in', () => {
             auth.saveToken(buildToken(mock.user));
             expect(auth.getFullname()).toBe('Full Name');
-        });
-
-        it('should indicate when impersonating', () => {
-            auth.saveToken(buildToken(mock.impersonating));
-            expect(auth.getFullname()).toBe('Impersonating Full Name');
         });
 
         it('should not get a username when not logged in', () => {
@@ -107,6 +102,85 @@
                 auth.saveToken(buildToken(user));
                 expect(auth.hasAnyRole(['ROLE_ACB'])).toBe(true);
             });
+        });
+
+        describe('when handling impersonating', () => {
+            it('should indicate when impersonating', () => {
+                auth.saveToken(buildToken(mock.impersonating));
+                expect(auth.getFullname()).toBe('Impersonating Full Name');
+            });
+
+            it('should know when a user is not impersonating', () => {
+                auth.saveToken(buildToken(mock.user));
+                expect(auth.isImpersonating()).toBe(false);
+            });
+
+            it('should know when a user is impersonating', () => {
+                auth.saveToken(buildToken(mock.impersonating));
+                expect(auth.isImpersonating()).toBe(true);
+            });
+
+            describe('when knowing who may be be impersonated', () => {
+                let user;
+                let target;
+                beforeEach(() => {
+                    user = angular.copy(mock.user);
+                    user.Authorities = [];
+                    target = { roles: [] };
+                });
+
+                it('should let ROLE_ADMIN impersonate non ROLE_ADMIN', () => {
+                    user.Authorities.push('ROLE_ADMIN');
+                    target.roles.push('ROLE_ONC');
+                    auth.saveToken(buildToken(user));
+                    expect(auth.canImpersonate(target)).toBe(true);
+                });
+
+                it('should not let ROLE_ADMIN impersonate ROLE_ADMIN', () => {
+                    user.Authorities.push('ROLE_ADMIN');
+                    target.roles.push('ROLE_ADMIN', 'ROLE_ACB');
+                    auth.saveToken(buildToken(user));
+                    expect(auth.canImpersonate(target)).toBe(false);
+                });
+
+                it('should let ROLE_ONC impersonate non ROLE_ONC / non ROLE_ADMIN', () => {
+                    user.Authorities.push('ROLE_ONC');
+                    target.roles.push('ROLE_ACB');
+                    auth.saveToken(buildToken(user));
+                    expect(auth.canImpersonate(target)).toBe(true);
+                });
+
+                it('should not let ROLE_ONC impersonate ROLE_ONC', () => {
+                    user.Authorities.push('ROLE_ONC');
+                    target.roles.push('ROLE_ONC');
+                    auth.saveToken(buildToken(user));
+                    expect(auth.canImpersonate(target)).toBe(false);
+                });
+
+                it('should not let ROLE_ONC impersonate ROLE_ADMIN', () => {
+                    user.Authorities.push('ROLE_ONC');
+                    target.roles.push('ROLE_ADMIN');
+                    auth.saveToken(buildToken(user));
+                    expect(auth.canImpersonate(target)).toBe(false);
+                });
+
+                it('should not let ROLE_ACB impersonate anyone', () => {
+                    user.Authorities.push('ROLE_ACB');
+                    target.roles.push('ROLE_ATL');
+                    auth.saveToken(buildToken(user));
+                    expect(auth.canImpersonate(target)).toBe(false);
+                });
+
+                it('should not let anyone impersonate while already impersonating', () => {
+                    target.roles.push('ROLE_ATL');
+                    auth.saveToken(buildToken(angular.copy(mock.impersonating)));
+                    expect(auth.canImpersonate(target)).toBe(false);
+                });
+            });
+        });
+
+        it('should know what the API KEY is', () => {
+            expect(auth.getApiKey()).toBe('12909a978483dfb8ecd0596c98ae9094');
         });
 
         ////////////////////////////////////////////////////////////////////////
