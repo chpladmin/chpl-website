d3f30fe32ea70adcff536ed95130f58abca4c98a
Andrew Larned <andrew.larned@gmail.com>
2019-03-12 11:14:50 -0400

    feat: allow editing of status history
    [#OCD-2756]

--- src/app/components/developer/developer.component.js
+++ src/app/components/developer/developer.component.js
@@ -12,9 +12,11 @@ export const DeveloperComponent = {
         showProducts: '<',
     },
     controller: class DeveloperComponent {
-        constructor ($log) {
+        constructor ($filter, $log, authService) {
             'ngInject'
+            this.$filter = $filter;
             this.$log = $log;
+            this.hasAnyRole = authService.hasAnyRole;
             this.isEditing = false;
             this.isSplitting = false;
             this.valid = {
@@ -26,6 +28,10 @@ export const DeveloperComponent = {
         $onChanges (changes) {
             if (changes.developer) {
                 this.developer = angular.copy(changes.developer.currentValue);
+                this.developer.statusEvents = this.developer.statusEvents.map(e => {
+                    e.statusDateObject = new Date(e.statusDate);
+                    return e;
+                });
             }
             if (changes.canEdit) {
                 this.canEdit = angular.copy(changes.canEdit.currentValue);
@@ -48,33 +54,28 @@ export const DeveloperComponent = {
             this.canSplit = this.canSplit && this.products && this.products.length > 1;
         }
 
-        cancel () {
-            this.developer = angular.copy(this.backup);
-            this.isEditing = false;
-            this.isSplitting = false;
-        }
-
+        /*
+         * Initiate changes
+         */
         edit () {
             this.backup = angular.copy(this.developer);
             this.isEditing = true;
         }
 
-        editAddress (address, errors, validForm) {
-            this.developer.address = angular.copy(address);
-            this.valid.address = validForm;
+        split () {
+            this.backup = angular.copy(this.developer);
+            this.isSplitting = true;
         }
 
-        editContact (contact, errors, validForm) {
-            this.developer.contact = angular.copy(contact);
-            this.valid.contact = validForm;
-        }
-
-        isValid () {
-            return this.form.$valid && this.valid.address && this.valid.contact;
-        }
-
+        /*
+         * Resolve changes
+         */
         save () {
             if (this.isEditing) {
+                this.developer.statusEvents = this.developer.statusEvents.map(e => {
+                    e.statusDate = e.statusDateObject.getTime();
+                    return e;
+                });
                 this.onEdit({developer: this.developer});
                 this.isEditing = false;
             }
@@ -87,9 +88,61 @@ export const DeveloperComponent = {
             }
         }
 
-        split () {
-            this.backup = angular.copy(this.developer);
-            this.isSplitting = true;
+        cancel () {
+            this.developer = angular.copy(this.backup);
+            this.isEditing = false;
+            this.isSplitting = false;
+        }
+
+        /*
+         * Handle callbacks
+         */
+        editAddress (address, errors, validForm) {
+            this.developer.address = angular.copy(address);
+            this.valid.address = validForm;
+        }
+
+        editContact (contact, errors, validForm) {
+            this.developer.contact = angular.copy(contact);
+            this.valid.contact = validForm;
+        }
+
+        /*
+         * Form validation
+         */
+        isValid () {
+            return this.form.$valid // basic form validation
+                && this.valid.address && this.valid.contact // validation from sub-components
+                && this.developer.statusEvents && this.developer.statusEvents.length > 0 // status history exists
+                && this.developer.statusEvents.reduce((acc, e) => acc && !this.matchesPreviousStatus(e) && !this.matchesPreviousDate(e), true); // no duplicate status history data
+        }
+
+        matchesPreviousStatus (status) {
+            let orderedStatus = this.developer.statusEvents.sort((a, b) => a.statusDateObject < b.statusDateObject ? -1 : a.statusDateObject > b.statusDateObject ? 1 : 0); // earliest first
+            let idx = orderedStatus.indexOf(status);
+            return idx > 0 && status.status.status === orderedStatus[idx - 1].status.status;
+        }
+
+        matchesPreviousDate (status) {
+            let orderedStatus = this.developer.statusEvents.sort((a, b) => a.statusDateObject < b.statusDateObject ? -1 : a.statusDateObject > b.statusDateObject ? 1 : 0); // earliest first
+            let idx = orderedStatus.indexOf(status);
+            return idx > 0 && this.$filter('date')(status.statusDateObject, 'mediumDate', 'UTC') === this.$filter('date')(orderedStatus[idx - 1].statusDateObject, 'mediumDate', 'UTC');
+        }
+
+        isBeingActivatedFromOncInactiveStatus () {
+            let orderedStatus = this.developer.statusEvents.sort((a, b) => a.statusDateObject < b.statusDateObject ? 1 : a.statusDateObject > b.statusDateObject ? -1 : 0); // latest first
+            return this.developer.status.status !== 'Active' && orderedStatus[0].status.status === 'Active';
+        }
+
+        /*
+         * Form actions
+         */
+        addStatus () {
+            this.developer.statusEvents.push({statusDateObject: new Date()});
+        }
+
+        removeStatus (idx) {
+            this.developer.statusEvents.splice(idx, 1);
         }
     },
 }
