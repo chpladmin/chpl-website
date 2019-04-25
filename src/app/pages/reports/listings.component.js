21be62c04e833f9dc7c566125dc2ef9ee7ebd672
tmy1313 <tyoung@ainq.com>
2019-04-25 15:26:32 -0400

    fix: update misspelled 'angular'
    OCD-2755

--- src/app/pages/reports/listings.component.js
+++ src/app/pages/reports/listings.component.js
@@ -33,7 +33,7 @@ export const ReportsListingsComponent = {
         }
 
         onApplyFilter (filter) {
-            let f = angluar.fromJson(filter);
+            let f = angular.fromJson(filter);
             if (f.productId) {
                 this.productId = f.productId;
             } else {
