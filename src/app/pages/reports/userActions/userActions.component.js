415e07343baf6be59429f8e1b7b0548cda0f7c52
tmy1313 <tyoung@ainq.com>
2019-05-20 13:36:49 -0400

    feat: wip - add search capability to user action activity report
    OCD-2789

--- src/app/pages/reports/userActions/userActions.component.js
+++ src/app/pages/reports/userActions/userActions.component.js
@@ -59,7 +59,7 @@ export const ReportsUserActionsComponent = {
 
         prepare (results) {
             this.displayed = results.map(item => {
-                item.filterText = item.email + '|' + item.subjectName + '|' + item.description;
+                item.filterText = item.responsibleUser.fullName + '|' + item.description;
                 item.friendlyActivityDate = new Date(item.date).toISOString().substring(0, 10);
                 item.responsibleUserFullName = item.responsibleUser.fullName;
                 return item;
