1fe8c55614d802681e5588340fc39f9c68b212f9
tmy1313 <tyoung@ainq.com>
2019-07-17 10:37:35 -0400

    refactor: removed unused binding
    OCD-2921

--- src/app/components/surveillance/complaint/complaint.component.js
+++ src/app/components/surveillance/complaint/complaint.component.js
@@ -7,7 +7,6 @@ export const SurveillanceComplaintComponent = {
         certificationBodies: '<',
         criteria: '<',
         displayHeader: '<',
-        displayAdd: '<',
         editions: '<',
         errorMessages: '<',
         listings: '<',
