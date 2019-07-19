5fcef9e1e376e8d7403b3e43af373fc32261d443
tmy1313 <tyoung@ainq.com>
2019-07-19 10:11:12 -0400

    feat: wip - add ui chnages requested after demo
    OCD-2912

--- src/app/components/surveillance/complaint/complaint.component.js
+++ src/app/components/surveillance/complaint/complaint.component.js
@@ -17,8 +17,9 @@ export const SurveillanceComplaintComponent = {
         surveillances: '<',
     },
     controller: class SurveillanceComplaintComponent {
-        constructor ($filter, $log, authService, featureFlags, toaster, utilService) {
+        constructor ($anchorScroll, $filter, $log, authService, featureFlags, toaster, utilService) {
             'ngInject'
+            this.$anchorScroll = $anchorScroll;
             this.$filter = $filter;
             this.$log = $log;
             this.isOn = featureFlags.isOn;
@@ -44,8 +45,7 @@ export const SurveillanceComplaintComponent = {
                     this.currentMode = this.modes.ADD;
                 }
                 this.sortCertifications(this.complaint);
-                this.$log.info(this.complaint.receivedDate);
-                this.$log.info(this.complaint.closedDate);
+                this.$anchorScroll();
             }
             if (changes.complainantTypes) {
                 this.complainantTypes = angular.copy(changes.complainantTypes.currentValue);
