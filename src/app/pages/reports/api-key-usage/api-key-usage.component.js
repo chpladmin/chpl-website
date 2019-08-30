7ff8dd26bd4f5e554aa2a67e5f0e6be187331dab
Todd Young <tyoung@ainq.com>
2019-08-30 13:36:37 -0400

    feat: Update the API Key Usage report UI to be consistent with other reports
    OCD-2748

--- src/app/pages/reports/api-key-usage/api-key-usage.component.js
+++ src/app/pages/reports/api-key-usage/api-key-usage.component.js
@@ -12,18 +12,18 @@ export const ReportsApiKeyUsageComponent = {
 
         $onInit () {
             this.clearApiKeyFilter();
-            this.search();
+            this.search(1);
         }
 
         clearApiKeyFilter () {
-            this.apiKey = {
+            this.apiKeyReport = {
                 range: 30,
                 visiblePage: 1,
                 pageSize: 100,
                 startDate: new Date(),
                 endDate: new Date(),
             };
-            this.apiKey.startDate.setDate(this.apiKey.endDate.getDate() - this.apiKey.range + 1); // offset to account for inclusion of endDate in range
+            this.apiKeyReport.startDate.setDate(this.apiKeyReport.endDate.getDate() - this.apiKeyReport.range + 1); // offset to account for inclusion of endDate in range
         }
 
         dateAdjust (obj) {
@@ -35,25 +35,63 @@ export const ReportsApiKeyUsageComponent = {
 
         loadApiKeys () {
             let ctrl = this;
-            this.networkService.getApiUsers()
+            this.networkService.getApiUsers(true)
                 .then(result => ctrl.apiKeys = result);
         }
 
-        search () {
+        search (pageNumber) {
+            this.apiKeyReport.visiblePage = pageNumber;
+            this.getData();
+        }
+
+        getData () {
             if (!this.apiKeys) {
                 this.loadApiKeys();
             }
             let ctrl = this;
-            this.apiKey.pageNumber = this.apiKey.visiblePage - 1;
-            this.networkService.getApiActivity(this.dateAdjust(this.apiKey))
+            this.apiKeyReport.pageNumber = this.apiKeyReport.visiblePage - 1;
+            this.networkService.getApiActivity(this.dateAdjust(this.apiKeyReport))
                 .then(data => {
                     ctrl.searchedApi = data;
+                    //We are calculating this since we don't know how many "total items" there really
+                    //are.  This only doesn't work for the edge case where there would be a multiple
+                    //of 100 for the actual total number of search results.  In that case, the system
+                    //would allow the user to click 'next' and there would be no results on the page.
+                    ctrl.apiKeyReport.totalItems = (ctrl.apiKeyReport.pageNumber * ctrl.apiKeyReport.pageSize) + ctrl.searchedApi.length + 1;
                 });
         }
 
         validDates () {
-            return this.ReportService.validDates(this.activityRange.startDate, this.activityRange.endDate, this.activityRange.range, false);
+            return this.ReportService.validDates(this.apiKeyReport.startDate, this.apiKeyReport.endDate, this.apiKeyReport.range, false);
         }
+
+        onApplyFilter (filterObj) {
+            let f = angular.fromJson(filterObj);
+            this.apiKeyReport.startDate = new Date(Date.parse(f.startDate));
+            this.apiKeyReport.endDate = new Date(Date.parse(f.endDate));
+            this.apiKeyReport.dateAscending = f.dateAscending;
+            this.apiKeyReport.filter = f.apiKeyFilter;
+            this.apiKeyReport.showOnly = f.showOnly;
+            this.search(1);
+        }
+
+        createFilterDataObject () {
+            let filterData = {};
+            filterData.startDate = this.ReportService.coerceToMidnight(this.apiKeyReport.startDate);
+            filterData.endDate = this.ReportService.coerceToMidnight(this.apiKeyReport.endDate);
+            filterData.dateAscending = this.apiKeyReport.dateAscending;
+            filterData.apiKeyFilter = this.apiKeyReport.filter;
+            filterData.showOnly = this.apiKeyReport.showOnly;
+            return filterData;
+        }
+
+        onClearFilter () {
+            this.apiKeyReport.endDate = new Date();
+            this.apiKeyReport.startDate = this.utilService.addDays(this.apiKeyReport.endDate, (this.apiKeyReport.range * -1) + 1)
+            this.apiKeyReport.filter = undefined;
+            this.search(1);
+        }
+
     },
 }
 
