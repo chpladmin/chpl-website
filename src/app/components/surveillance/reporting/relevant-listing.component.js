5ed4eb8ccc38220e0dca429efd554e42b93d9637
Andrew Larned <andrew.larned@gmail.com>
2019-09-13 11:48:46 -0400

    ui: edit surveillance via modal on reporting
    [#OCD-2920]

--- src/app/components/surveillance/reporting/relevant-listing.component.js
+++ src/app/components/surveillance/reporting/relevant-listing.component.js
@@ -8,11 +8,17 @@ export const SurveillanceReportRelevantListingComponent = {
         onCancel: '&',
     },
     controller: class SurveillanceReportRelevantListingComponent {
-        constructor ($log, authService, networkService) {
+        constructor ($log, $state, $uibModal, authService, networkService) {
             'ngInject'
             this.$log = $log;
+            this.$state = $state;
+            this.$uibModal = $uibModal;
             this.networkService = networkService;
             this.hasAnyRole = authService.hasAnyRole;
+        }
+
+        $onInit () {
+            this.surveillanceTypes = this.networkService.getSurveillanceLookups();
         }
 
         $onChanges (changes) {
@@ -44,6 +50,37 @@ export const SurveillanceReportRelevantListingComponent = {
                 that.surveillances.push(that.calculateCompletion(response));
                 that.activeSurveillance = undefined;
             });
+        }
+
+        editSurveillance (relevantSurveillance) {
+            let that = this;
+            this._fixRequirementOptions();
+            this.networkService.getListing(this.listing.id, true).then(listing => {
+                let surveillance = listing.surveillance.find(s => s.id === relevantSurveillance.id);
+                that.uibModalInstance = that.$uibModal.open({
+                    component: 'aiSurveillanceEdit',
+                    animation: false,
+                    backdrop: 'static',
+                    keyboard: false,
+                    size: 'lg',
+                    resolve: {
+                        surveillance: () => { return surveillance; },
+                        surveillanceTypes: () => { return that.surveillanceTypes; },
+                        workType: () => { return 'edit'; },
+                    },
+                });
+                that.uibModalInstance.result.then(() => {
+                    that.$state.reload();
+                });
+            });
+        }
+
+        _fixRequirementOptions () {
+            if (this.listing.edition === '2015') {
+                this.surveillanceTypes.surveillanceRequirements.criteriaOptions = this.surveillanceTypes.surveillanceRequirements.criteriaOptions2015;
+            } else if (this.listing.edition === '2014') {
+                this.surveillanceTypes.surveillanceRequirements.criteriaOptions = this.surveillanceTypes.surveillanceRequirements.criteriaOptions2014;
+            }
         }
 
         cancel () {
