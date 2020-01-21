c9199e047c571a4b8ec49f3017a30e24aa456243
Dan Brown <dbrown.se@gmail.com>
2020-01-21 12:08:36 -0500

    Convert to query-based and add sorting

--- src/app/services/network.service.js
+++ src/app/services/network.service.js
@@ -392,7 +392,7 @@ export class NetworkService {
     }
 
     getScheduledSystemJobs () {
-        return this.apiGET('/schedules/triggers/scheduled_system_jobs');
+        return this.apiGET('/schedules/triggers/?jobType=system');
     }
 
     getScheduleJobs () {
