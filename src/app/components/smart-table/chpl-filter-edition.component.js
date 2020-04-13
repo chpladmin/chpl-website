89ef51f8606e19875bc6e8a74dae97c812cb074d
Andrew Larned <andrew.larned@gmail.com>
2020-04-13 15:41:44 -0400

    feat: create Certification Edition filter component
    [#OCD-3299]

--- src/app/components/smart-table/chpl-filter-edition.component.js
+++ src/app/components/smart-table/chpl-filter-edition.component.js
@@ -1,339 +1,111 @@
-(function () {
-    'use strict';
-    angular.module('chpl.components')
-        .controller('ListMultipleController', ListMultipleController)
-        .directive('stListMultiple', stListMultiple);
-
-    /** @ngInclude */
-    function stListMultiple () {
-        return {
-            bindToController: {
-                allowAnd: '@?',
-                hasChanges: '=?',
-                hidden: '@?',
-                matchFull: '@?',
-                nameSpace: '@?',
-                separator: '@?',
-                trackAnalytics: '@?',
-                triggerShowRetired: '&?',
-            },
-            controller: 'ListMultipleController',
-            controllerAs: 'vm',
-            link: stListMultipleLink,
-            restrict: 'E',
-            require: ['^stTable', 'stListMultiple'],
-            scope: {
-                collection: '=',
-                fixedItems: '=?',
-                predicate: '@',
-                predicateExpression: '=',
-                registerAllowAll: '&?',
-                registerClearFilter: '&',
-                registerRestoreState: '&',
-                registerShowRetired: '&?',
-            },
-            templateUrl: 'chpl.components/smart-table/stListMultiple.html',
+export const FilterEditionComponent = {
+    templateUrl: 'chpl.components/smart-table/chpl-filter-edition.html',
+    bindings: {
+        nameSpace: '@?',
+        onChange: '&',
+        registerClearFilter: '&?',
+        registerRestoreState: '&?',
+        registerShowRetired: '&?',
+    },
+    require: ['^stTable'],
+    controller: class FilterEditionComponent {
+        constructor ($localStorage, $log) {
+            'ngInject'
+            this.$localStorage = $localStorage;
+            this.$log = $log;
         }
-    }
 
-    function stListMultipleLink (scope, element, attr, ctrls) {
+        $onInit () {
+            this.items = [
+                { value: '2011', display: '2011', retired: true, selected: false },
+                { value: '2014', display: '2014', retired: true, selected: false },
+                { value: '2015', display: '2015', retired: false, selected: true },
+                { value: '2015 Cures Edition', display: '2015 Cures Edition', retired: false, selected: true },
+            ];
 
-        activate();
-
-        function activate () {
-            var table = ctrls[0];
-            var ctrl = ctrls[1];
-            if (scope.registerAllowAll) {
-                var allowAll = scope.registerAllowAll({
-                    allowAll: function () {
-                        ctrl.allowAll();
+            let that = this;
+            if (this.registerClearFilter) {
+                this.clearFilter = this.registerClearFilter({
+                    clearFilter: () => {
+                        that.clearFilter();
                     },
                 });
-                scope.$on('$destroy', allowAll);
             }
-            var clearFilter = scope.registerClearFilter({
-                clearFilter: function () {
-                    ctrl.clearFilter();
-                },
-            });
-            scope.$on('$destroy', clearFilter);
-            var restoreState = scope.registerRestoreState({
-                restoreState: function (state) {
-                    ctrl.restoreState(state);
-                },
-            });
-            scope.$on('$destroy', restoreState);
-            if (scope.registerShowRetired) {
-                var showRetired = scope.registerShowRetired({
-                    showRetired: function (state) {
-                        ctrl.showRetired(state);
+            if (this.registerRestoreState) {
+                this.restoreState = this.registerRestoreState({
+                    restoreState: state => {
+                        that.restoreState(state);
                     },
                 });
-                scope.$on('$destroy', showRetired);
             }
-
-            setItems();
-            setSelected();
-            scope.$watch('fixedItems', function () {
-                setItems();
-                setSelected();
-                ctrl.distinctItems = scope.distinctItems;
-                ctrl.selected = scope.selected;
-                ctrl.filterChanged();
-            });
-            ctrl.distinctItems = scope.distinctItems;
-            ctrl.predicate = getPredicate();
-            ctrl.selected = scope.selected;
-            ctrl.tableCtrl = table;
-            ctrl.activate();
-        }
-
-        function bindCollection (collection) {
-            var predicate = getPredicate();
-            var distinctItems = [];
-
-            angular.forEach(collection, function (item) {
-                var value = item[predicate];
-                fillDistinctItems(value, distinctItems);
-            });
-
-            distinctItems.sort(function (obj, other) {
-                if (obj.value > other.value) {
-                    return 1;
-                } else if (obj.value < other.value) {
-                    return -1;
-                }
-                return 0;
-            });
-            scope.distinctItems = distinctItems;
-        }
-
-        function fillDistinctItems (values, distinctItems) {
-            if (!angular.isObject(values)) {
-                values = [values];
-            }
-            var value;
-            for (var i = 0; i < values.length; i++) {
-                value = values[i] + '';
-                if (value && value.trim().length > 0 && !findItemWithValue(distinctItems, value)) {
-                    distinctItems.push({
-                        value: value,
-                        selected: true,
-                    });
-                }
-            }
-        }
-
-        function findItemWithValue (collection, value) {
-            for (var i = 0; i < collection.length; i++) {
-                if (collection[i].value === value) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        function getPredicate () {
-            var predicate = scope.predicate;
-            if (!predicate && scope.predicateExpression) {
-                predicate = scope.predicateExpression;
-            }
-            return predicate;
-        }
-
-        function setSelected () {
-            scope.selected = [];
-            angular.forEach(scope.distinctItems, function (item) {
-                item.isSelected = item.selected;
-                if (item.selected) {
-                    scope.selected.push(item.value);
-                }
-            });
-        }
-
-        function setItems () {
-            if (angular.isDefined(scope.fixedItems)) {
-                scope.distinctItems = angular.copy(scope.fixedItems);
-            } else if (angular.isDefined(scope.collection)) {
-                bindCollection(scope.collection);
-                scope.$watch('collection', function (newCollection) {
-                    bindCollection(newCollection)
+            if (this.registerShowRetired) {
+                this.showRetired = this.registerShowRetired({
+                    showRetired: state => {
+                        that.showRetired(state);
+                    },
                 });
             }
         }
-    }
 
-    /** @ngInclude */
-    function ListMultipleController ($analytics, $localStorage) {
-        var vm = this;
+        clearFilter () {
+            this.items.forEach(edition => edition.selected = edition.retired);
+            this.filterChanged();
+        }
 
-        vm.activate = activate;
-        vm.allowAll = allowAll;
-        vm.clearFilter = clearFilter;
-        vm.filterChanged = filterChanged;
-        vm.getId = getId;
-        vm.isNotDefault = isNotDefault;
-        vm.restoreState = restoreState;
-        vm.selectAll = selectAll;
-        vm.showRetired = showRetired;
-        vm.storeState = storeState;
-        vm.toggleSelection = toggleSelection;
+        filterChanged () {
+            let query = {
+                certificationEdition: {
+                    items: this.items,
+                },
+            }
+            this.stTable.search(query);
+            this.onChange({
+                hasChanges: this.isChanged(),
+            });
+            this.storeState();
+        }
 
-        ////////////////////////////////////////////////////////////////////
+        isChanged () {
+            return this.items.reduce((acc, edition) => acc || this.isNotDefault(edition.selected), false);
+        }
 
-        function activate () {
-            if (vm.distinctItems && vm.distinctItems.length > 0 && vm.selected && vm.selected.length > 0) {
-                vm.filterChanged();
+        isNotDefault (item) {
+            return item.selected !== item.retired;
+        }
+
+        restoreState (state) {
+            this.items = angular.copy(state.items);
+            this.filterChanged();
+        }
+
+        selectAll () {
+            this.items.forEach(edition => edition.selected = true);
+            this.filterChanged();
+        }
+
+        showRetired () {
+            this.items
+                .filter(edition => edition.retired)
+                .forEach(edition => edition.selected = true);
+            this.filterChanged();
+        }
+
+        storeState () {
+            if (this.nameSpace) {
+                this.$localStorage[this.nameSpace] = angular.toJson(this.stTable.tableState());
             }
         }
 
-        function allowAll () {
-            var restoreAnalytics = false;
-            if (vm.trackAnalytics) {
-                vm.trackAnalaytics = false;
-                restoreAnalytics = true;
+        toggleSelection (item) {
+            if (item.retired) {
+                this.onChange({
+                    showRetired: true,
+                });
             }
-            angular.forEach(vm.distinctItems, function (item) {
-                if (item.isSelected) {
-                    item.isSelected = false;
-                    vm.toggleSelection(item, true, true);
-                }
-            })
-            if (restoreAnalytics) {
-                vm.trackAnalaytics = true;
-            }
-            vm.matchAll = false;
-            vm.filterChanged();
-            vm.storeState();
+            this.filterChanged();
         }
+    },
+}
 
-        function clearFilter () {
-            angular.forEach(vm.distinctItems, function (item) {
-                if (item.isSelected !== item.selected) {
-                    item.isSelected = item.selected;
-                    vm.toggleSelection(item, true, true);
-                }
-            })
-            vm.matchAll = false;
-            vm.filterChanged();
-            vm.storeState();
-        }
-
-        function getId (value) {
-            return 'filter-list-' + (value + '').replace(/\s/g, '_');
-        }
-
-        function filterChanged () {
-            vm.hasChanges = getChanged();
-            var query;
-            query = { separator: vm.separator ? vm.separator : '' };
-            if (vm.matchAll) {
-                query.matchAll = {
-                    matchFull: vm.matchFull,
-                    items: vm.selected,
-                }
-            } else {
-                query.matchAny = {
-                    matchFull: vm.matchFull,
-                    items: vm.selected,
-                }
-            }
-            vm.tableCtrl.search(query, vm.predicate);
-        }
-
-        function isNotDefault (item) {
-            return (angular.isUndefined(item.selected) && item.isSelected) || (item.selected !== item.isSelected);
-        }
-
-        function restoreState (state) {
-            var predicateSearch = state.search.predicateObject[vm.predicate];
-            if (predicateSearch) {
-                if (predicateSearch.matchAny) {
-                    vm.matchAll = false;
-                    vm.selected = predicateSearch.matchAny.items;
-                } else if (predicateSearch.matchAll) {
-                    vm.matchAll = true;
-                    vm.selected = predicateSearch.matchAll.items;
-                }
-                setToTableState();
-            }
-        }
-
-        function selectAll () {
-            angular.forEach(vm.distinctItems, function (item) {
-                if (!item.isSelected) {
-                    item.isSelected = true;
-                    vm.selected.push(item.value);
-                }
-            })
-            vm.matchAll = false;
-            vm.filterChanged();
-            vm.storeState();
-        }
-
-        function showRetired () {
-            angular.forEach(vm.distinctItems, function (item) {
-                if (!item.isSelected && item.retired) {
-                    item.isSelected = true;
-                    vm.selected.push(item.value);
-                }
-            })
-            vm.matchAll = false;
-            vm.filterChanged();
-            vm.storeState();
-        }
-
-        function storeState () {
-            if (vm.nameSpace) {
-                $localStorage[vm.nameSpace] = angular.toJson(vm.tableCtrl.tableState());
-            }
-        }
-
-        function toggleSelection (item, dontSearch, dontTriggerRetired) {
-            var index = vm.selected.indexOf(item.value);
-            if (index > -1) {
-                vm.selected.splice(index, 1);
-            } else {
-                vm.selected.push(item.value);
-                if (!dontTriggerRetired && item.retired && vm.triggerShowRetired) {
-                    vm.triggerShowRetired();
-                }
-                if (vm.trackAnalytics) {
-                    var event;
-                    switch (vm.predicate) {
-                    case 'criteriaMet': event = 'Certification Criteria Filter'; break;
-                    case 'cqmsMet': event = 'CQM Filter'; break;
-                    default: event = 'Other';
-                    }
-                    $analytics.eventTrack(event, { category: 'Search', label: item.value });
-                }
-            }
-            if (!dontSearch) {
-                vm.filterChanged();
-                vm.storeState();
-            }
-        }
-
-        ////////////////////////////////////////////////////////////////////
-
-        function getChanged () {
-            var ret = vm.matchAll;
-            angular.forEach(vm.distinctItems, function (item) {
-                ret = ret || isNotDefault(item);
-            })
-            return ret;
-        }
-
-        function setToTableState () {
-            angular.forEach(vm.distinctItems, function (item) {
-                if (vm.selected.indexOf(item.value) > -1) {
-                    item.isSelected = true;
-                } else {
-                    item.isSelected = false;
-                }
-            })
-            vm.filterChanged();
-            vm.storeState();
-        }
-    }
-})();
+angular.module('chpl.components')
+    .component('chplFilterEdition', FilterEditionComponent);
