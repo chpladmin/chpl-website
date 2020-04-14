a629c4d6da65bdb21bee8bc52c57f9326bcd140f
Andrew Larned <andrew.larned@gmail.com>
2020-04-14 08:07:38 -0400

    refactor: update resources routes to current standard
    [#OCD-2996]

--- src/app/pages/resources/resources.state.js
+++ src/app/pages/resources/resources.state.js
@@ -1,39 +1,45 @@
-function resourcesStateConfig ($stateProvider) {
+let states = [
+    {
+        name: 'resources',
+        abstract: true,
+        url: '/resources',
+        template: '<ui-view/>',
+    },{
+        name: 'resources.chpl-api',
+        url: '/chpl-api',
+        template: require('./chpl-api/chpl-api.html'),
+        controller: 'ChplApiController',
+        controllerAs: 'vm',
+        data: { title: 'CHPL API' },
+    },{
+        name: 'resources.chpl_api', // state is a result of OCD-2964; should be removed when API is updated
+        url: '/chpl_api',
+        redirectTo: 'resources.chpl-api',
+    },{
+        name: 'resources.cms-lookup',
+        url: '/cms-lookup',
+        template: require('./cms-lookup/cms-lookup.html'),
+        controller: 'CmsLookupController',
+        controllerAs: 'vm',
+        data: { title: 'CMS ID Reverse Lookup' },
+    },{
+        name: 'resources.download',
+        url: '/download',
+        component: 'aiResourcesDownload',
+        data: { title: 'Download the CHPL' },
+    },{
+        name: 'resources.overview',
+        url: '/overview',
+        component: 'aiOverview',
+        data: { title: 'CHPL Overview' },
+    },
+];
+
+function resourcesStatesConfig ($stateProvider) {
     'ngInject'
-    $stateProvider
-        .state('resources', {
-            abstract: true,
-            url: '/resources',
-            template: '<ui-view/>',
-        })
-        .state('resources.chpl-api', {
-            url: '/chpl-api',
-            template: require('./chpl-api/chpl-api.html'),
-            controller: 'ChplApiController',
-            controllerAs: 'vm',
-            data: { title: 'CHPL API' },
-        })
-        .state('resources.chpl_api', { // state is a result of OCD-2964; should be removed when API is updated
-            url: '/chpl_api',
-            redirectTo: 'resources.chpl-api',
-        })
-        .state('resources.cms-lookup', {
-            url: '/cms-lookup',
-            template: require('./cms-lookup/cms-lookup.html'),
-            controller: 'CmsLookupController',
-            controllerAs: 'vm',
-            data: { title: 'CMS ID Reverse Lookup' },
-        })
-        .state('resources.download', {
-            url: '/download',
-            component: 'aiResourcesDownload',
-            data: { title: 'Download the CHPL' },
-        })
-        .state('resources.overview', {
-            url: '/overview',
-            component: 'aiOverview',
-            data: { title: 'CHPL Overview' },
-        });
+    states.forEach(state => {
+        $stateProvider.state(state);
+    });
 }
 
-module.exports = resourcesStateConfig;
+export { resourcesStatesConfig }
