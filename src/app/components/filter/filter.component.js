2e476b8f9bca9b5b47622a27ada859af74a014cf
Andrew Larned <andrew.larned@gmail.com>
2020-05-06 16:47:44 -0400

    wip: start adding new "filter" component
    [#OCD-3117]

---
+++ src/app/components/filter/filter.component.js
@@ -0,0 +1,88 @@
+export const FilterComponent = {
+    templateUrl: 'chpl.components/filter/filter.html',
+    bindings: {
+    },
+    transclude: true,
+    controller: class FilterComponent {
+        constructor ($log) {
+            'ngInject'
+            this.$log = $log;
+            this.filters = [];
+        }
+
+        select (filter) {
+            this.filters.forEach(filter => {
+                filter.selected = false;
+            });
+            filter.selected = true;
+        }
+
+        addFilter (filter) {
+            if (this.filters.length === 0) {
+                this.select(filter);
+            }
+            this.filters.push(filter);
+        }
+
+        /*
+        onApplyFilter (filter) {
+            let f = angular.fromJson(filter);
+            this.doFilter(f);
+        }
+
+        onClearFilter () {
+            let filterData = {};
+            filterData.dataFilter = '';
+            filterData.tableState = this.tableController.tableState();
+            this.clearFilterHs.forEach(handler => handler());
+            this.doFilter(filterData);
+        }
+
+        doFilter (filter) {
+            let that = this;
+            this.filterText = filter.dataFilter;
+            let filterItems = [
+                'acbName',
+                'complaintStatusTypeName',
+                'receivedDate',
+                'closedDate',
+                'complainantTypeName',
+                'complainantContacted',
+                'developerContacted',
+                'oncAtlContacted',
+            ];
+            filterItems.forEach(predicate => {
+                if (filter.tableState.search.predicateObject[predicate]) {
+                    this.tableController.search(filter.tableState.search.predicateObject[predicate], predicate);
+                } else {
+                    this.tableController.search({}, predicate);
+                }
+            });
+            this.restoreStateHs.forEach(handler => handler(that.tableController.tableState()));
+            this.tableController.sortBy(filter.tableState.sort.predicate, filter.tableState.sort.reverse);
+        }
+
+        registerClearFilter (handler) {
+            this.clearFilterHs.push(handler);
+        }
+
+        registerRestoreState (handler) {
+            this.restoreStateHs.push(handler);
+        }
+
+        getFilterData () {
+            let filterData = {};
+            filterData.dataFilter = this.filterText;
+            filterData.tableState = this.tableController.tableState();
+            return filterData;
+        }
+
+        tableStateListener (tableController) {
+            this.tableController = tableController;
+        }
+        */
+    },
+}
+
+angular.module('chpl.components')
+    .component('chplFilter', FilterComponent);
