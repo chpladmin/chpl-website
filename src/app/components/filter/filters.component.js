25505b9ab150d81f8d0092186f44dc74fbe494bb
Andrew Larned <andrew.larned@gmail.com>
2020-05-08 09:11:13 -0400

    style: remove unused code
    [#OCD-3117]

--- src/app/components/filter/filters.component.js
+++ src/app/components/filter/filters.component.js
@@ -28,64 +28,6 @@ export const FiltersComponent = {
             }
             this.filters.push(filter);
         }
-
-        /*
-        onApplyFilter (filter) {
-            let f = angular.fromJson(filter);
-            this.doFilter(f);
-        }
-
-        onClearFilter () {
-            let filterData = {};
-            filterData.dataFilter = '';
-            filterData.tableState = this.tableController.tableState();
-            this.clearFilterHs.forEach(handler => handler());
-            this.doFilter(filterData);
-        }
-
-        doFilter (filter) {
-            let that = this;
-            this.filterText = filter.dataFilter;
-            let filterItems = [
-                'acbName',
-                'complaintStatusTypeName',
-                'receivedDate',
-                'closedDate',
-                'complainantTypeName',
-                'complainantContacted',
-                'developerContacted',
-                'oncAtlContacted',
-            ];
-            filterItems.forEach(predicate => {
-                if (filter.tableState.search.predicateObject[predicate]) {
-                    this.tableController.search(filter.tableState.search.predicateObject[predicate], predicate);
-                } else {
-                    this.tableController.search({}, predicate);
-                }
-            });
-            this.restoreStateHs.forEach(handler => handler(that.tableController.tableState()));
-            this.tableController.sortBy(filter.tableState.sort.predicate, filter.tableState.sort.reverse);
-        }
-
-        registerClearFilter (handler) {
-            this.clearFilterHs.push(handler);
-        }
-
-        registerRestoreState (handler) {
-            this.restoreStateHs.push(handler);
-        }
-
-        getFilterData () {
-            let filterData = {};
-            filterData.dataFilter = this.filterText;
-            filterData.tableState = this.tableController.tableState();
-            return filterData;
-        }
-
-        tableStateListener (tableController) {
-            this.tableController = tableController;
-        }
-        */
     },
 }
 
