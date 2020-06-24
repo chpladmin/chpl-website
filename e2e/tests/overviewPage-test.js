d4dbade3ed79f5e6d36cec982a2ed7f7934af698
Andrew Larned <andrew.larned@gmail.com>
2020-06-24 13:50:00 -0400

    fix: handle capitalization in test file
    [#OCD-3408]

--- e2e/tests/overviewPage-test.js
+++ e2e/tests/overviewPage-test.js
@@ -1,22 +1,22 @@
-import overviewPage from "../page-objects/overviewPage.js"
-import homePage from "../page-objects/homePage.js";
+import OverviewPage from "../page-objects/OverviewPage.js"
+import HomePage from "../page-objects/HomePage.js";
 
 beforeEach(async () => {
-  await homePage.open();
+  await HomePage.open();
 });
 
 describe('Overview page', () => {
 
   beforeEach(function() {
-    homePage.gotoResourcePage();
+    HomePage.gotoResourcePage();
   });
   
   it('should have ONC ACB ATL table', () => {
-    assert.equal(overviewPage.acbatlTable.isDisplayed(),true);
+    assert.equal(OverviewPage.acbatlTable.isDisplayed(),true);
   })
 
   it('should have correct acb and atl in the table', () => {
-    overviewPage.compareTableData();
-    assert.equal(overviewPage.compareTableData(),true);
+    OverviewPage.compareTableData();
+    assert.equal(OverviewPage.compareTableData(),true);
    })
 })
