95617312ed1ab165e98e6ce8703f17469824902b
Andrew Larned <andrew.larned@gmail.com>
2020-07-30 09:23:44 -0400

    test: expect basic DR data to exist and be correct
    [#OCD-3364]

--- e2e/components/direct-reviews/direct-reviews.po.js
+++ e2e/components/direct-reviews/direct-reviews.po.js
@@ -1,8 +1,14 @@
 const elements = {
     root: 'div.direct-reviews',
     directReviews: '.direct-review',
-    directReviewTitle: '.direct-review__header-title',
-    directReviewSubtitle: '.direct-review__header-subtitle',
+    title: '.direct-review__header-title',
+    subtitle: '.direct-review__header-subtitle',
+    start: '.direct-review__start',
+    end: '.direct-review__end',
+    circumstances: '.direct-review__circumstances',
+    dataElement: '.direct-review__data-element',
+    nonconformities: '.nonconformity__header',
+    result: '.nonconformity__gridtable-header--last',
 }
 
 class DirectReviewsComponent {
@@ -17,11 +23,31 @@ class DirectReviewsComponent {
     }
 
     getDirectReviewTitle (directReview) {
-        return directReview.$(elements.directReviewTitle);
+        return directReview.$(elements.title);
     }
 
     getDirectReviewSubtitle (directReview) {
-        return directReview.$(elements.directReviewSubtitle);
+        return directReview.$(elements.subtitle);
+    }
+
+    getDirectReviewBegan (directReview) {
+        return directReview.$(elements.start).$(elements.dataElement);
+    }
+
+    getDirectReviewEnded (directReview) {
+        return directReview.$(elements.end).$(elements.dataElement);
+    }
+
+    getDirectReviewCircumstances (directReview) {
+        return directReview.$(elements.circumstances).$$('li');
+    }
+
+    getDirectReviewNonconformities (directReview) {
+        return directReview.$$(elements.nonconformities);
+    }
+
+    getNonconformityResult (nonconformity) {
+        return nonconformity.$(elements.result)
     }
 }
 
--- e2e/components/direct-reviews/direct-reviews.spec.js
+++ e2e/components/direct-reviews/direct-reviews.spec.js
@@ -36,5 +36,36 @@ describe('the Direct Reviews component', () => {
             expect(component.getDirectReviewSubtitle(directReviews[2]).getText()).toMatch(/Jun 29, 2020/);
             expect(component.getDirectReviewSubtitle(directReviews[3]).getText()).toMatch(/Jun 17, 2020/);
         });
+
+        it('should have basic DR data', () => {
+            let directReviews = component.directReviews;
+            directReviews[2].scrollIntoView({block: 'center', inline: 'center'});
+            browser.saveScreenshot('./test_reports/tmp.png');
+            directReviews[2].click();
+            expect(component.getDirectReviewBegan(directReviews[2]).getText()).toMatch(/Jun 10, 2020/);
+            expect(component.getDirectReviewEnded(directReviews[2]).getText()).toMatch(/Jun 29, 2020/);
+            let circumstances = component.getDirectReviewCircumstances(directReviews[2]);
+            expect(circumstances.length).toBe(2);
+            expect(circumstances[0].getText()).toBe('Patient safety issue');
+            expect(circumstances[1].getText()).toBe('Impediment to ONC-ACB');
+            expect(component.getDirectReviewNonconformities(directReviews[2]).length).toBe(3);
+        });
+
+        describe('for nonconformities', () => {
+            let nonconformities;
+            beforeEach(() => {
+                let directReviews = component.directReviews;
+                directReviews[2].scrollIntoView({block: 'center', inline: 'center'});
+                directReviews[2].click();
+                nonconformities = component.getDirectReviewNonconformities(directReviews[2]);
+            });
+
+            it('should sort them "open" first', () => {
+                expect(nonconformities.length).toBe(3);
+                expect(component.getNonconformityResult(nonconformities[0]).getText()).toBe('Open');
+                expect(component.getNonconformityResult(nonconformities[1]).getText()).toBe('Closed');
+                expect(component.getNonconformityResult(nonconformities[2]).getText()).toBe('None recorded');
+            });
+        });
     });
 });
