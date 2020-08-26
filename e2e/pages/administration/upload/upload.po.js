3a06f2010e3c842055db0731d441318136f55aa2
Palak Patel <papatel@ainq.com>
2020-08-26 09:41:00 -0400

    fix: remove api document upload related methods from upload.po
    [#OCD-3049]

--- e2e/pages/administration/upload/upload.po.js
+++ e2e/pages/administration/upload/upload.po.js
@@ -4,6 +4,7 @@ const uploadElements = {
     chooseUploadListing: '//*[@id="ngf-label-upload-button-listing"]/input[@id="ngf-upload-button-listing"]',
     uploadButton: '.btn.btn-ai-success',
     listingUploadText: '//chpl-upload/div/div/chpl-upload-listings/div/div[2]/div',
+    uploadSuccessfulText: '//*[@id="main-content"]/div/ui-view/chpl-upload/div/div/chpl-upload-listings/div/div[2]/div',
 };
 const path = require('path');
 
