ab2c8a0d0fa3c7661a0a51703164ff6355e43aa2
Palak Patel <papatel@ainq.com>
2020-08-26 09:39:50 -0400

    [#OCD-3049]

--- e2e/pages/administration/upload/upload.po.js
+++ e2e/pages/administration/upload/upload.po.js
@@ -4,11 +4,6 @@ const uploadElements = {
     chooseUploadListing: '//*[@id="ngf-label-upload-button-listing"]/input[@id="ngf-upload-button-listing"]',
     uploadButton: '.btn.btn-ai-success',
     listingUploadText: '//chpl-upload/div/div/chpl-upload-listings/div/div[2]/div',
-    chooseUploadAPIDocumentation: '//*[@id="ngf-label-upload-button-api"]/input[@id="ngf-upload-button-api"]',
-    uploadAPIDoc: 'chpl-upload-api-documentation',
-    uploadMessages: '.upload-messages',
-    uploadMessagesText: 'div.ng-binding.ng-scope',
-    uploadSuccessfulText: '//*[@id="main-content"]/div/ui-view/chpl-upload/div/div/chpl-upload-listings/div/div[2]/div',
 };
 const path = require('path');
 
@@ -31,28 +26,17 @@ class UploadPage {
         return $(uploadElements.listingUploadText);
     }
 
-    get chooseUploadAPIDocumentation () {
-        return $(uploadElements.chooseUploadAPIDocumentation);
-    }
-
-    get apiDocUploadText () {
-        return $(uploadElements.uploadAPIDoc).$(uploadElements.uploadMessages).$(uploadElements.uploadMessagesText);
+    waitForSuccessfulUpload () {
+        browser.waitUntil( () => this.uploadSuccessfulText.isDisplayed());
     }
 
     uploadListing (uploadfilePath) {
         const filePath = path.join(__dirname, uploadfilePath);
         this.chooseUploadListingButton.addValue(browser.uploadFile(filePath));
         this.uploadButton.waitAndClick();
-        browser.waitUntil( () => this.listingUploadText.isDisplayed())
+        browser.waitUntil( () => this.listingUploadText.isDisplayed());
     }
 
-    uploadAPIDocFile (uploadfilePath) {
-        const filePath = path.join(__dirname, uploadfilePath);
-        this.chooseUploadAPIDocumentation.addValue(browser.uploadFile(filePath));
-        this.uploadButton.scrollIntoView();
-        this.uploadButton.click();
-        browser.waitUntil( () => this.apiDocUploadText.isDisplayed());
-    }
 }
 
 export default UploadPage;
