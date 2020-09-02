715d016d1d995a1b8dda1f03a76aa953fb2a9774
Palak Patel <papatel@ainq.com>
2020-09-02 17:05:17 -0400

    test: Add test for rejecting a listing feature
    [#OCD-3474]

--- e2e/pages/administration/confirm/confirm.spec.js
+++ e2e/pages/administration/confirm/confirm.spec.js
@@ -6,25 +6,53 @@ import LoginComponent from '../../../components/login/login.po';
 import Hooks from '../../../utilities/hooks';
 
 let confirmPage, hooks, loginComponent, uploadPage;
-const rejectListingId = '15.04.04.1722.AQA3.03.01.1.200620';
+const rejectListingId1 = '15.04.04.1722.AQA3.03.01.1.200620';
+const rejectListingId2 = '15.04.04.1722.AQA4.03.01.1.200620';
 
-beforeEach(() => {
-    uploadPage = new UploadPage();
-    confirmPage = new ConfirmPage();
-    loginComponent = new LoginComponent();
-    hooks = new Hooks();
-    hooks.open('#/administration/upload');
-    loginComponent.loginAsACB();
-    uploadPage.uploadListing('../../../resources/2015_v19_AQA3.csv');
-    uploadPage.waitForSuccessfulUpload();
-});
-
-describe('Confirm page', () => {
-
-    it('allows user to reject a file', () => {
-        hooks.open('#/administration/confirm/listings');
-        confirmPage.rejectListing(rejectListingId);
-        assert.isFalse(confirmPage.findListingtoReject(rejectListingId).isDisplayed())
+describe('when user is on confirm listing page', () => {
+    beforeEach(() => {
+        uploadPage = new UploadPage();
+        confirmPage = new ConfirmPage();
+        loginComponent = new LoginComponent();
+        hooks = new Hooks();
+        hooks.open('#/administration/upload');
+        loginComponent.loginAsACB();
     });
 
+    afterEach(() => {
+        loginComponent.openLoginComponent();
+        loginComponent.logOut();
+    });
+
+    describe('when uploading a listing', () => {
+        beforeEach(() => {
+            uploadPage.uploadListing('../../../resources/2015_v19_AQA3.csv');
+            uploadPage.waitForSuccessfulUpload();
+        });
+
+        it('should allow user to reject a file', () => {
+            hooks.open('#/administration/confirm/listings');
+            confirmPage.rejectListing(rejectListingId1);
+            assert.isFalse(confirmPage.findListingtoReject(rejectListingId1).isDisplayed());
+        });
+    });
+
+    describe('when uploading multiple listing', () => {
+        beforeEach(() => {
+            uploadPage.uploadListing('../../../resources/2015_v19_AQA3.csv');
+            uploadPage.waitForSuccessfulUpload();
+            uploadPage.uploadListing('../../../resources/2015_v19_AQA4.csv');
+            uploadPage.waitForSuccessfulUpload();
+        });
+
+        it('should allow user to mass reject ', () => {
+            hooks.open('#/administration/confirm/listings');
+            confirmPage.rejectCheckbox(rejectListingId1).scrollAndClick();
+            confirmPage.rejectCheckbox(rejectListingId2).scrollAndClick();
+            confirmPage.rejectButton.waitAndClick();
+            confirmPage.yesConfirmation.waitAndClick();
+            assert.isFalse(confirmPage.findListingtoReject(rejectListingId1).isDisplayed());
+            assert.isFalse(confirmPage.findListingtoReject(rejectListingId2).isDisplayed());
+        });
+    });
 });
