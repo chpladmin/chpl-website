69a4d00cef6810a34181644bb77a84c75e16c885
Katy Ekey <katy.ekey@gmail.com>
2020-09-10 09:58:00 -0400

    test: add email address to credentials
    [#OCD-3406]

--- e2e/config/credentialsEXAMPLE.js
+++ e2e/config/credentialsEXAMPLE.js
@@ -5,14 +5,17 @@ module.exports = {
     passwordAdmin: 'ACB PASSWORD',
     acb: {
         username: 'insert username here',
+        email: 'email address',
         password: 'insert password here',
     },
     onc: {
         username: 'insert username here',
+        email: 'email address',
         password: 'insert password here',
     },
     admin: {
         username: 'insert username here',
+        email: 'email address',
         password: 'insert password here',
     },
 };
