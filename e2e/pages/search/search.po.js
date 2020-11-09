90a45ac2ae497ea1ad5180f9007009f3eeb72a1d
Palak Patel <papatel@ainq.com>
2020-11-09 09:41:13 -0500

    refactor: Increase time for all listings to be loaded
    [#OCD-3484]

--- e2e/pages/search/search.po.js
+++ e2e/pages/search/search.po.js
@@ -148,7 +148,7 @@ class SearchPage {
     // There is no spinner or other indication on search page to make browser wait until listing results are updating
     // Hoping with redesigning of search page, this timeout won't be needed
     waitForUpdatedListingResultsCount () {
-        browser.pause(5000);
+        browser.pause(10000);
     }
 
     homeSearchPageButtons (buttonName) {
