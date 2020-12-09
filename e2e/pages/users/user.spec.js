d231d9b13e94b02e0be5d3f54112c82b439b4b3a
Palak Patel <papatel@ainq.com>
2020-12-09 09:49:56 -0500

    refactor: Remove test which makes users locked and eventually fails other test as user is locked
    [#OCD-3554]

--- e2e/pages/users/user.spec.js
+++ e2e/pages/users/user.spec.js
@@ -5,7 +5,7 @@ import Hooks from '../../utilities/hooks';
 
 let actionBarComponent, hooks, loginComponent, page;
 
-beforeAll(async () => {
+beforeEach(async () => {
     loginComponent = new LoginComponent();
     actionBarComponent = new ActionBarComponent();
     page = new UsersPage();
@@ -14,13 +14,13 @@ beforeAll(async () => {
 });
 
 describe('ONC STAFF can ', () => {
-    beforeAll(function () {
+    beforeEach(function () {
         loginComponent.logInWithEmail('oncstaff');
         page.usersButton.click();
         page.userManagementButton.click();
     });
 
-    afterAll(function () {
+    afterEach(function () {
         loginComponent.logOut();
     });
 
@@ -41,42 +41,4 @@ describe('ONC STAFF can ', () => {
         actionBarComponent.save();
         assert.include(page.userInformation('AQA ONC STAFF').getText(),number);
     });
-
-});
-
-describe('ADMIN can ', () => {
-    beforeAll(function () {
-        loginComponent.logInWithEmail('admin');
-        page.usersButton.click();
-        page.userManagementButton.click();
-    });
-
-    afterAll(function () {
-        if (page.lockedCheckbox.isSelected()) {
-            page.lockedCheckbox.scrollAndClick();
-        }
-        if (!page.enabledCheckbox.isSelected()) {
-            page.enabledCheckbox.scrollAndClick();
-        }
-        if (page.pwChangeCheckbox.isSelected()) {
-            page.pwChangeCheckbox.scrollAndClick();
-        }
-        actionBarComponent.save();
-    });
-
-    it('change status of locked , enabled, password change on next login successfully for ROLE_ONC_STAFF', () => {
-        page.editUser('AQA ONC STAFF');
-        page.lockedCheckbox.scrollAndClick();
-        page.enabledCheckbox.scrollAndClick();
-        page.pwChangeCheckbox.scrollAndClick();
-        var isLocked = page.lockedCheckbox.isSelected();
-        var isEnabled = page.enabledCheckbox.isSelected();
-        var isPasswordChange = page.pwChangeCheckbox.isSelected();
-        actionBarComponent.save();
-        page.editUser('AQA ONC STAFF');
-        assert.equal(page.lockedCheckbox.isSelected(),isLocked);
-        assert.equal(page.enabledCheckbox.isSelected(),isEnabled);
-        assert.equal(page.pwChangeCheckbox.isSelected(),isPasswordChange);
-    });
-
 });
