2a8516f2c0c6af8b89885a98e4d96e5279d45b2d
Palak Patel <papatel@ainq.com>
2021-01-11 22:07:45 -0500

    test: Update an existing test  for download text
    [#OCD-2995]

--- e2e/pages/resources/download/download.readonly.spec.js
+++ e2e/pages/resources/download/download.readonly.spec.js
@@ -3,10 +3,9 @@ import Hooks from '../../../utilities/hooks';
 
 let hooks, page;
 const productFile2015 = 'The 2015 Edition Products file is updated nightly.';
-const productFile2014 = 'The 2014 Edition Products file is updated monthly.';
-const productFile2011 = 'The 2011 Edition Products file is updated quarterly.';
+const productFile2014And2011 = 'The 2014 Edition Products file and the 2011 Edition Products file is updated quarterly.';
 const summaryFile2015 = 'The 2015 Edition Summary file is updated nightly.';
-const summaryFile2014 = 'The 2014 Edition Summary file is updated monthly.';
+const summaryFile2014 = 'The 2014 Edition Summary file is updated quarterly.';
 const directReview = 'Entire collection of Direct Review activity reported to the CHPL. Available as a CSV file.';
 const surveillanceNonConformity = 'Collection of surveillance activities that resulted in a non-conformity. This is a subset of the data available in the above "Surveillance Activity" file. Available as a CSV file.';
 const surveillanceActivity = 'Entire collection of surveillance activity reported to the CHPL. Available as a CSV file.';
@@ -22,11 +21,8 @@ describe('the Download page - 2015/2014/2011 Edition products section', () => {
     it('should have correct information about 2015 edition products file', () => {
         assert.include(page.downloadListingText.getText(),productFile2015);
     });
-    it('should have correct information about 2014 edition products file', () => {
-        assert.include(page.downloadListingText.getText(),productFile2014);
-    });
-    it('should have correct information about 2011 edition products file', () => {
-        assert.include(page.downloadListingText.getText(),productFile2011);
+    it('should have correct information about 2014 and 2011 edition products file', () => {
+        assert.include(page.downloadListingText.getText(),productFile2014And2011);
     });
 });
 
