0f32365b55b60cd5305e236e850607448fa73d6e
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2021-01-14 09:10:08 -0500

    OCD 2995: Update test to have an updated text on download page (#844)
    * test: Update an existing test  for download text
    
    [#OCD-2995]

---
+++ e2e/pages/administration/jobs/scheduled/scheduledJob.readonly.spec.js
@@ -0,0 +1,29 @@
+import UploadListingComponent from '../../../components/upload/upload-listing/upload-listing.po';
+import UploadAPIDocumentationComponent from '../../../components/upload/upload-api-documentation/upload-api-documentation.po';
+import LoginComponent from '../../../components/login/login.po';
+import Hooks from '../../../utilities/hooks';
+
+let hooks, loginComponent, uploadAPIDocumentationComponent, uploadListingComponent;
+
+beforeAll(async () => {
+    uploadListingComponent = new UploadListingComponent();
+    uploadAPIDocumentationComponent = new UploadAPIDocumentationComponent();
+    loginComponent = new LoginComponent();
+    hooks = new Hooks();
+    await hooks.open('#/administration/upload');
+});
+
+describe('Upload Page', () => {
+    beforeAll(function () {
+        loginComponent.logIn('admin');
+    });
+
+    it('has correct title for upload listing component', () => {
+        assert.include(uploadListingComponent.title.getText(),'Upload Certified Products');
+    });
+
+    it('has correct title for upload api documentation component', () => {
+        assert.include(uploadAPIDocumentationComponent.title.getText(),'Upload API Documentation Information');
+    });
+
+});
