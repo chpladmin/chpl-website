582bd61a36d380590e1a2173a4ba1a2c13267de6
Andrew Larned <andrew.larned@gmail.com>
2021-01-27 08:19:45 -0500

    feat: add functionality to React ellipsis component
    [#OCD-3599]

--- src/app/components/util/ellipsis.jsx
+++ src/app/components/util/ellipsis.jsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
+import { bool, number, string } from 'prop-types';
 
-const ChplEllipsis = () => {
-    return <div>Hello Ellipsis</div>;
-};
+class ChplEllipsis extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            displayText: props.text,
+            isShortened: false,
+            maxLength: props.maxLength || 80,
+        };
+        let displayText = this.state.displayText;
+        let isShortened = this.state.isShortened;
+        let maxLength = this.state.maxLength;
+        if (displayText.length > maxLength) {
+            displayText = props.text.substring(0, maxLength);
+            isShortened = true;
+            if (props.wordBoundaries) {
+                let parts = displayText.split(' ');
+                parts.splice(parts.length - 1, 1);
+                if (parts.length > 0) {
+                    displayText = parts.join(' ');
+                }
+            }
+        }
+        if (isShortened) {
+            this.state.canShorten = true;
+        }
+        this.state.isShortened = isShortened;
+        this.state.displayText = displayText;
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle () {
+        this.setState(state => ({
+            isShortened: !state.isShortened,
+        }));
+    }
+
+    render () {
+        /* eslint-disable indent */
+        return (
+            <span>
+              { this.state.isShortened ? this.state.displayText : this.props.text }
+              { this.state.isShortened &&
+                <button className="btn btn-link btn-xs" data-tip data-for="ellipsis" onClick={ this.toggle }>
+                  <ReactTooltip id="ellipsis">{ this.props.text }</ReactTooltip>
+                  <i className="fa fa-ellipsis-h"></i><span className="sr-only">Expand description</span>
+                </button>
+              }
+              { !this.state.isShortened && this.state.canShorten &&
+                <button className="btn btn-link btn-xs" onClick={ this.toggle }>
+                  <i className="fa fa-arrow-left"></i><span className="sr-only">Minimize description</span>
+                </button>
+              }
+            </span>
+        );
+        /* eslint-enable indent */
+    }
+}
 
 export { ChplEllipsis };
+
+ChplEllipsis.propTypes = {
+    text: string,
+    maxLength: number,
+    wordBoundaries: bool,
+};
--- src/app/services/angular-react-helper.jsx
+++ src/app/services/angular-react-helper.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const reactToAngularComponent = (Component) => {
+const toBindings = propTypes => {
+    const bindings = {};
+    Object.keys(propTypes).forEach(key => bindings[key] = '<');
+    return bindings;
+};
+
+const toProps = (propTypes, ctrl) => {
+    const props = {};
+    Object.keys(propTypes).forEach(key => props[key] = ctrl[key]);
+    return props;
+};
+
+const reactToAngularComponent = Component => {
+    const propTypes = Component.propTypes || {};
     return {
+        bindings: toBindings(propTypes),
         controller: function ($element) {
             'ngInject';
-            this.$onInit = () => {
-                ReactDOM.render(<Component/>, $element[0]);
+            this.$onChanges = () => {
+                const props = toProps(propTypes, this);
+                ReactDOM.render(<Component {...props} />, $element[0]);
             };
             this.$onDestroy = () => {
                 ReactDOM.unmountComponentAtNode($element[0]);
