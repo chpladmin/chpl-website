3ac90d1cf9d8b276d706e7517769d451a6910a7d
Andrew Larned <andrew.larned@gmail.com>
2021-02-03 14:52:47 -0500

    refactor: convert class to function in newer React style
    [#OCD-3599]

--- src/app/components/fuzzy-type/fuzzy-type.jsx
+++ src/app/components/fuzzy-type/fuzzy-type.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-console,angular/log */
-import React from 'react';
+import React, { useState } from 'react';
 import { arrayOf, func, shape, string } from 'prop-types';
 import { getAngularService } from './';
 
@@ -7,76 +6,68 @@ const dependencies = {
     getChplLogService: () => require('../../services/services-bridge.jsx').default,
 };
 
-class ChplFuzzyType extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            isEditing: false,
-        };
-        this.$log = getAngularService('$log');
-        this.chplLog = dependencies.getChplLogService();
-        this.toggle = this.toggle.bind(this);
-    }
+function ChplFuzzyType ({fuzzyType, takeAction}) {
+    const [isEditing, setEditing] = useState(false);
+    const $log = getAngularService('$log');
+    const chplLog = dependencies.getChplLogService();
 
-    toggle () {
-        if (this.state.isEditing) {
-            this.props.takeAction(this.props.fuzzyType, 'cancel');
+    const toggle = () => {
+        setEditing(!isEditing);
+        if (isEditing) {
+            $log.info({isEditing});
+            takeAction(fuzzyType, 'cancel');
         } else {
-            this.props.takeAction(this.props.fuzzyType, 'edit');
+            chplLog.info({isEditing});
+            takeAction(fuzzyType, 'edit');
         }
-        this.setState(state => ({
-            isEditing: !state.isEditing,
-        }));
-    }
+    };
 
-    render () {
+    return (
         /* eslint-disable indent,react/jsx-indent */
-        return (
-            <div id={ 'fuzzy-type-' + this.props.fuzzyType.fuzzyType }>
-              <div className="panel panel-default">
-                <div className="panel-heading">
-                  <h4 className="panel-title">{ this.props.fuzzyType.fuzzyType }</h4>
-                </div>
-                <div className="panel-body">
-                  { !this.state.isEditing &&
-                    <>
-                      <span className="pull-right">
-                        <button className="btn btn-link btn-small" id={ 'fuzzy-type-' + this.props.fuzzyType.fuzzyType + '-edit'} onClick={ this.toggle }><i className="fa fa-pencil-square-o"></i><span className="sr-only"> Edit Fuzzy Type</span></button>
-                      </span>
-                      Choices
-                      <ul>
-                        {
-                            this.props.fuzzyType.choices
-                            .sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
-                            .map(choice => (
-                                <li key={ choice }>{ choice }</li>
-                            ))
-                        }
-                      </ul>
-                    </>
-                  }
-                  { this.state.isEditing &&
-                    <>
-                      <span className="pull-right">
-                        <button className="btn btn-link btn-small" id={ 'fuzzy-type-' + this.props.fuzzyType.fuzzyType + '-cancel'} onClick={ this.toggle }><i className="fa fa-close"></i><span className="sr-only"> Cancel Edit Fuzzy Type</span></button>
-                      </span>
-                      Choices (for editing)
-                      <ul>
-                        {
-                            this.props.fuzzyType.choices
-                            .sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
-                            .map(choice => (
-                                <li key={ choice }>{ choice }</li>
-                            ))
-                        }
-                      </ul>
-                    </>
-                  }
-                </div>
-              </div>
+        <div id={ 'fuzzy-type-' + fuzzyType.fuzzyType }>
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <h4 className="panel-title">{ fuzzyType.fuzzyType }</h4>
             </div>
-        );
-    }
+            <div className="panel-body">
+              { !isEditing &&
+                <>
+                  <span className="pull-right">
+                    <button className="btn btn-link btn-small" id={ 'fuzzy-type-' + fuzzyType.fuzzyType + '-edit'} onClick={() => toggle()}><i className="fa fa-pencil-square-o"></i><span className="sr-only"> Edit Fuzzy Type</span></button>
+                  </span>
+                  Choices
+                  <ul>
+                    {
+                        fuzzyType.choices
+                        .sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+                        .map(choice => (
+                            <li key={ choice }>{ choice }</li>
+                        ))
+                    }
+                  </ul>
+                </>
+              }
+              { isEditing &&
+                <>
+                  <span className="pull-right">
+                    <button className="btn btn-link btn-small" id={ 'fuzzy-type-' + fuzzyType.fuzzyType + '-cancel'} onClick={() => toggle()}><i className="fa fa-close"></i><span className="sr-only"> Cancel Edit Fuzzy Type</span></button>
+                  </span>
+                  Choices (for editing)
+                  <ul>
+                    {
+                        fuzzyType.choices
+                        .sort((a, b) => a < b ? -1 : a > b ? 1 : 0)
+                        .map(choice => (
+                            <li key={ choice }>{ choice }</li>
+                        ))
+                    }
+                  </ul>
+                </>
+              }
+            </div>
+          </div>
+        </div>
+    );
 }
 
 export { ChplFuzzyType };
