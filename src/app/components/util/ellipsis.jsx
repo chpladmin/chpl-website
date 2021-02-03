8c622a18d49ef1ae6e2ec876e4355908fc5173bb
Andrew Larned <andrew.larned@gmail.com>
2021-02-03 13:51:07 -0500

    refactor: use function form of component
    [#OCD-3599]

--- src/app/components/util/ellipsis.jsx
+++ src/app/components/util/ellipsis.jsx
@@ -1,61 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactTooltip from 'react-tooltip';
 import { bool, number, string } from 'prop-types';
 
-class ChplEllipsis extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            displayText: props.text,
-            isShortened: false,
-            maxLength: props.maxLength || 80,
-        };
-        let displayText = this.state.displayText;
-        let isShortened = this.state.isShortened;
-        let maxLength = this.state.maxLength;
-        if (displayText.length > maxLength) {
-            displayText = props.text.substring(0, maxLength);
-            isShortened = true;
-            if (props.wordBoundaries) {
-                let parts = displayText.split(' ');
-                parts.splice(parts.length - 1, 1);
-                if (parts.length > 0) {
-                    displayText = parts.join(' ');
-                }
-            }
-        }
-        if (isShortened) {
-            this.state.canShorten = true;
-        }
-        this.state.isShortened = isShortened;
-        this.state.displayText = displayText;
-        this.toggle = this.toggle.bind(this);
+function ChplEllipsis ({text, maxLength, wordBoundaries}) {
+    const [isShortened, setShortened] = useState(true);
+
+    if (text.length <= maxLength) {
+        return <span>{ text }</span>;
     }
 
-    toggle () {
-        this.setState(state => ({
-            isShortened: !state.isShortened,
-        }));
+    let display = text.substr(0, maxLength).trim();
+    if (wordBoundaries) {
+        let parts = display.split(' ');
+        parts.splice(parts.length - 1, 1);
+        display = parts.join(' ');
     }
 
-    render () {
-        return (
-          <span>
-            { this.state.isShortened ? this.state.displayText : this.props.text }
-            { this.state.isShortened &&
-              <button className="btn btn-link btn-xs" data-tip data-for="ellipsis" onClick={ this.toggle }>thing
-                <ReactTooltip id="ellipsis" effect="solid">{ this.props.text }</ReactTooltip>
-                <i className="fa fa-ellipsis-h"></i><span className="sr-only">Expand description</span>
-              </button>
-            }
-            { !this.state.isShortened && this.state.canShorten &&
-              <button className="btn btn-link btn-xs" onClick={ this.toggle }>
-                <i className="fa fa-arrow-left"></i><span className="sr-only">Minimize description</span>
-              </button>
-            }
-          </span>
-        );
-    }
+    return (
+      <span>
+        { isShortened ? display : text }
+        { isShortened ? (
+          <button className="btn btn-link btn-xs" data-tip data-for="ellipsis" onClick={() => setShortened(false)}>
+            <ReactTooltip id="ellipsis" effect="solid">{ text }</ReactTooltip>
+            <i className="fa fa-ellipsis-h"></i><span className="sr-only">Expand description</span>
+          </button>
+        ) : (
+          <button className="btn btn-link btn-xs" onClick={() => setShortened(true)}>
+            <i className="fa fa-arrow-left"></i><span className="sr-only">Minimize description</span>
+          </button>
+        ) }
+      </span>
+    );
 }
 
 export { ChplEllipsis };
