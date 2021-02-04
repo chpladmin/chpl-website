c1ce613d87c174a9fc6836673c6d6038b6b385c5
Andrew Larned <andrew.larned@gmail.com>
2021-02-04 09:50:47 -0500

    style: update whitespace styles iaw React standards
    [#OCD-3599]

--- src/app/components/fuzzy-type/index.js
+++ src/app/components/fuzzy-type/index.js
@@ -1,4 +1,4 @@
-import { ChplFuzzyType } from './fuzzy-type.jsx';
-import { getAngularService } from '../../services/angular-react-helper.jsx';
+import {ChplFuzzyType} from './fuzzy-type.jsx';
+import {getAngularService} from '../../services/angular-react-helper.jsx';
 
-export { ChplFuzzyType, getAngularService };
+export {ChplFuzzyType, getAngularService};
