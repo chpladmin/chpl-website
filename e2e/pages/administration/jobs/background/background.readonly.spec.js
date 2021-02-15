619033d461fcdfcb63114d01ca02a2068748f222
Palak Patel <papatel@ainq.com>
2021-02-15 13:20:18 -0500

    fix: simplify tests
    [#OCD-3584]

--- e2e/pages/administration/jobs/background/background.readonly.spec.js
+++ e2e/pages/administration/jobs/background/background.readonly.spec.js
@@ -15,25 +15,14 @@ describe('when an ONC-Staff user is logged in', () => {
   beforeEach(function () {
     loginComponent.logInWithEmail('oncstaff');
     hooks.open('#/administration/jobs/background');
-    browser.pause(2000); //Have to add this wait as jobs gets in order in micro seconds.
   });
 
   it('should see the right set of background jobs', () => {
-    var actualResult = new Set([]);
-    var expectedResult = new Set(['Export Annual Report','Export Quarterly Report','MUU Upload','Surveillance Upload']);
-    var length = background.backgroundJobRows.length;
-    for ( var j = 0; j < length; j++ ) {
-      actualResult.push(background.backgroundJobName(j).getText());
-    }
-    var isExist;
-    assert.equal(actualResult.size,expectedResult.size);
-    for (var value of actualResult) {
-      if (!expectedResult.has(value)) {
-        return isExist = false;
-      }
-      return isExist = true;
-    }
-    assert.isTrue(isExist);
+    const expected = new Set(['Export Annual Report','Export Quarterly Report','MUU Upload','Surveillance Upload']);
+    assert.equal(background.backgroundJobRows.length,expected.size);
+    background.backgroundJobRows.forEach(row => {
+      assert.exists(expected , row.getText());
+    });
   });
 
 });
--- e2e/pages/administration/jobs/scheduled/scheduled.readonly.spec.js
+++ e2e/pages/administration/jobs/scheduled/scheduled.readonly.spec.js
@@ -16,25 +16,13 @@ describe('when an ONC-Staff user is logged in', () => {
   beforeEach(function () {
     loginComponent.logInWithEmail('oncstaff');
     hooks.open('#/administration/jobs/scheduled');
-    browser.pause(2000); //Have to add this wait as jobs gets in order in micro seconds
   });
 
   it('should see the right set of scheduled jobs', () => {
-    var actualResult = new Set([]);
-    var expectedResult = new Set(['All Broken Surveillance Rules Report','Developer Access Report','Inherited Certification Status Errors Report','Listing Validation Email Report','Overnight Broken Surveillance Rules Report','Pending "Change Request" Report','Questionable Activity Report','Questionable URL Report','Real World Testing Email Report','Summary Statistics Email','Trigger Developer Ban Notification']);
-    var length = scheduled.scheduledJobRows.length;
-    for ( var j = 0; j < length; j++ ) {
-      actualResult.add(scheduled.scheduledJobName(j).getText());
-    }
-    var isExist;
-    assert.equal(actualResult.size,expectedResult.size);
-    for (var value of actualResult) {
-      if (!expectedResult.has(value)) {
-        return isExist = false;
-      }
-      return isExist = true;
-    }
-    assert.isTrue(isExist);
+    const expected = new Set(['All Broken Surveillance Rules Report','Developer Access Report','Inherited Certification Status Errors Report','Listing Validation Email Report','Overnight Broken Surveillance Rules Report','Pending "Change Request" Report','Questionable Activity Report','Questionable URL Report','Real World Testing Email Report','Summary Statistics Email','Trigger Developer Ban Notification']);
+    assert.equal(scheduled.scheduledJobRows.length,expected.size);
+    scheduled.scheduledJobRows.forEach(row => {
+      assert.exists(expected , row.getText());
+    });
   });
-
 });
