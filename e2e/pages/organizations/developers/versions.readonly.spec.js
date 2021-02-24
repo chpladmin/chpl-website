bf54639d6adc869b67d253b8eb5771689ffbd20b
Andrew Larned <andrew.larned@gmail.com>
2021-02-24 14:56:26 -0500

    refactor: break up Developer AQA tests by category
    [#OCD-3204]

--- e2e/pages/organizations/developers/versions.readonly.spec.js
+++ e2e/pages/organizations/developers/versions.readonly.spec.js
@@ -10,8 +10,8 @@ describe('the Version part of the Developers page', () => {
   beforeEach(async () => {
     browser.setWindowSize(1600, 1024); // demo of a bigger screen (esp. useful for screenshots)
     browser.setWindowRect(0, 0, 1600, 1024); // not sure if both are required
-    login = new LoginComponent();
     page = new DevelopersPage();
+    login = new LoginComponent();
     actionBar = new ActionBarComponent();
     actionConfirmation = new ActionConfirmationComponent();
     hooks = new Hooks();
@@ -54,35 +54,20 @@ describe('the Version part of the Developers page', () => {
 
         it('should allow cancellation', () => {
           page.mergeVersion(product);
-          //page.mergeButton.click();
-          browser.saveScreenshot('./test_reports/e2e/0.png');
-          //page.versionMergeButton.click();
-          //browser.saveScreenshot('./test_reports/e2e/1.png');
           page.versionName.clearValue();
-          browser.saveScreenshot('./test_reports/e2e/2.png');
           page.versionName.addValue(newVersion);
-          browser.saveScreenshot('./test_reports/e2e/3.png');
           actionBar.cancel();
-          browser.saveScreenshot('./test_reports/e2e/4.png');
           actionConfirmation.yes.click();
-          browser.saveScreenshot('./test_reports/e2e/5.png');
           page.productsHeader.waitForDisplayed();
-          browser.saveScreenshot('./test_reports/e2e/6.png');
           product = page.getProduct(name);
-          browser.saveScreenshot('./test_reports/e2e/7.png');
           product.scrollIntoView({block: 'center', inline: 'center'});
-          browser.saveScreenshot('./test_reports/e2e/8.png');
           page.selectProduct(product);
-          browser.saveScreenshot('./test_reports/e2e/9.png');
           page.getProductInfo(product).waitForDisplayed({timeout: 55000});
-          browser.saveScreenshot('./test_reports/e2e/10.png');
           expect(page.getActiveVersion(product, productId)).toHaveTextContaining(version);
         });
 
         it('should require the version', () => {
           page.mergeVersion(product);
-          //page.mergeButton.click();
-          //page.versionMergeButton.click();
           page.moveVersionToBeMerged(versionToBeMerged);
           page.versionName.clearValue();
           expect(page.errorMessage.getText()).toBe('Field is required');
