d0bc6a370e7c349fec758ee1aecc66653012cf46
todd.young <tyoung@ainq.com>
2021-04-09 11:17:27 -0400

    refactor: update based on linting rules
    OCD-3596

--- src/app/components/api-key/api-key-confirm.jsx
+++ src/app/components/api-key/api-key-confirm.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import {string} from 'prop-types';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from '../../themes/theme';
-import {makeStyles} from '@material-ui/core/styles';
+import { string } from 'prop-types';
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
+
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
-import {getAngularService} from './';
+import theme from '../../themes/theme';
+import { getAngularService } from '.';
 
 const useStyles = makeStyles(() => ({
   grid: {
@@ -20,7 +20,7 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-function ChplApiKeyConfirm (props) {
+function ChplApiKeyConfirm(props) {
   const networkService = getAngularService('networkService');
   const [apiKey, setApiKey] = useState({});
   const [confirmError, setConfirmError] = useState(false);
@@ -37,38 +37,42 @@ function ChplApiKeyConfirm (props) {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={ theme }>
-      <div className={ classes.grid }>
-        <div className={ classes.centeredGridItem }>
+    <ThemeProvider theme={theme}>
+      <div className={classes.grid}>
+        <div className={classes.centeredGridItem}>
           <Card>
-            <CardHeader title="API Key Confirmation"/>
+            <CardHeader title="API Key Confirmation" />
             <CardContent>
-              { confirmError ?
-                <>
-                  <Typography variant="error" component={'div'}>
-                    There was an error confirming your email!
-                  </Typography>
-                  <Typography variant="body1" component={'div'}>
-                    Possible reasons:
-                    <ul>
-                      <li>This email confirmation request has already been processed.  Be sure to check you email for an email with your API key.</li>
-                      <li>The confirmation token in the URL is not correct.</li>
-                    </ul>
-                  </Typography>
-                </>
-                :
-                <>
-                  <Typography variant="body1" gutterBottom={ true }>
-                    Congratulations on sucessfully registering your email for use with CHPL API.  CHPL has sent you an email containing your new API key.
-                  </Typography>
-                  <Typography variant="body1" gutterBottom={ true } data-testid="api-key-display">
-                    Your API key is: &nbsp; <strong>{ apiKey.apiKey }</strong>
-                  </Typography>
-                  <Typography variant="body1" gutterBottom={ true }>
-                    Your API key must be included in all API calls via either a header with the name 'API-Key' or as a URL parameter named 'api_key'.
-                  </Typography>
-                </>
-              }
+              { confirmError
+                ? (
+                  <>
+                    <Typography variant="error" component="div">
+                      There was an error confirming your email!
+                    </Typography>
+                    <Typography variant="body1" component="div">
+                      Possible reasons:
+                      <ul>
+                        <li>This email confirmation request has already been processed.  Be sure to check you email for an email with your API key.</li>
+                        <li>The confirmation token in the URL is not correct.</li>
+                      </ul>
+                    </Typography>
+                  </>
+                )
+                : (
+                  <>
+                    <Typography variant="body1" gutterBottom>
+                      Congratulations on sucessfully registering your email for use with CHPL API.  CHPL has sent you an email containing your new API key.
+                    </Typography>
+                    <Typography variant="body1" gutterBottom data-testid="api-key-display">
+                      Your API key is: &nbsp;
+                      {' '}
+                      <strong>{ apiKey.apiKey }</strong>
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                      Your API key must be included in all API calls via either a header with the name 'API-Key' or as a URL parameter named 'api_key'.
+                    </Typography>
+                  </>
+                )}
             </CardContent>
           </Card>
         </div>
@@ -77,8 +81,8 @@ function ChplApiKeyConfirm (props) {
   );
 }
 
-export { ChplApiKeyConfirm };
+export default ChplApiKeyConfirm;
 
 ChplApiKeyConfirm.propTypes = {
-  hash: string,
+  hash: string.isRequired,
 };
