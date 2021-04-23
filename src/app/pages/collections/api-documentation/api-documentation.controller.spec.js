c7937376e3f421a58ea1516be3f3252f769ad704
Andrew Larned <andrew.larned@gmail.com>
2021-04-23 09:57:15 -0400

    style: fix eslinting rules
    [#OCD-3644]

--- src/app/pages/collections/api-documentation/api-documentation.controller.spec.js
+++ src/app/pages/collections/api-documentation/api-documentation.controller.spec.js
@@ -1,49 +1,45 @@
-(function () {
-  'use strict';
+(() => {
+  describe('chpl.collections.apiDocumentation.controller', () => {
+    let $log;
+    let $q;
+    let authService;
+    let networkService;
+    let scope;
+    let vm;
 
-  describe('chpl.collections.apiDocumentation.controller', function () {
-
-    var $log, $q, authService, networkService, scope, vm;
-
-    beforeEach(function () {
-      angular.mock.module('chpl.mock', 'chpl.collections', function ($provide) {
-        $provide.decorator('authService', function ($delegate) {
-          $delegate.getApiKey = jasmine.createSpy('getApiKey');
-          return $delegate;
-        });
-        $provide.decorator('networkService', function ($delegate) {
-          $delegate.getApiDocumentationDate = jasmine.createSpy('getApiDocumentationDate');
-          return $delegate;
-        });
+    beforeEach(() => {
+      angular.mock.module('chpl.mock', 'chpl.collections', ($provide) => {
+        $provide.decorator('authService', ($delegate) => ({ ...$delegate, getApiKey: jasmine.createSpy('getApiKey') }));
+        $provide.decorator('networkService', ($delegate) => ({ ...$delegate, getApiDocumentationDate: jasmine.createSpy('getApiDocumentationDate') }));
       });
 
-      inject(function ($controller, _$log_, _$q_, $rootScope, _authService_, _networkService_) {
+      inject(($controller, _$log_, _$q_, $rootScope, _authService_, _networkService_) => {
         $log = _$log_;
         $q = _$q_;
         authService = _authService_;
         authService.getApiKey.and.returnValue('api-key');
         networkService = _networkService_;
-        networkService.getApiDocumentationDate.and.returnValue($q.when({associatedDate: 39393939}));
+        networkService.getApiDocumentationDate.and.returnValue($q.when({ associatedDate: 39393939 }));
 
         scope = $rootScope.$new();
         vm = $controller('ApiDocumentationController', {
           $scope: scope,
-          networkService: networkService,
+          networkService,
         });
         scope.$digest();
       });
     });
 
-    afterEach(function () {
+    afterEach(() => {
       if ($log.debug.logs.length > 0) {
         /* eslint-disable no-console,angular/log */
-        console.log('Debug:\n' + $log.debug.logs.map(function (o) { return angular.toJson(o); }).join('\n'));
+        console.log(`Debug:\n${$log.debug.logs.map((o) => angular.toJson(o)).join('\n')}`);
         /* eslint-enable no-console,angular/log */
       }
     });
 
     describe('on load', () => {
-      it('should exist', function () {
+      it('should exist', () => {
         expect(vm).toBeDefined();
       });
 
@@ -64,44 +60,44 @@
       });
     });
 
-    describe('transforming API Documentation data', function () {
-      it('should return "N/A" if no data', function () {
-        var data;
+    describe('transforming API Documentation data', () => {
+      it('should return "N/A" if no data', () => {
+        let data;
         expect(vm.apiTransform(data)).toBe('N/A');
       });
 
-      it('should create a list of three elements if the APIs are all different', function () {
-        var data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☹http://example2.com☺170.315 (g)(9)☹http://example3.com';
-        let output = vm.apiTransform(data);
+      it('should create a list of three elements if the APIs are all different', () => {
+        const data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☹http://example2.com☺170.315 (g)(9)☹http://example3.com';
+        const output = vm.apiTransform(data);
         expect(output.indexOf('<dt>170.315 (g)(7)</dt>')).toBeGreaterThan(-1);
         expect(output.indexOf('<dt>170.315 (g)(8)</dt>')).toBeGreaterThan(-1);
         expect(output.indexOf('<dt>170.315 (g)(9)</dt>')).toBeGreaterThan(-1);
       });
 
-      it('should combine elements a list of three elements if the APIs the same', function () {
-        var data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☹http://example1.com☺170.315 (g)(9)☹http://example1.com';
-        let output = vm.apiTransform(data);
+      it('should combine elements a list of three elements if the APIs the same', () => {
+        const data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☹http://example1.com☺170.315 (g)(9)☹http://example1.com';
+        const output = vm.apiTransform(data);
         expect(output.indexOf('<dt>170.315 (g)(7), 170.315 (g)(8), 170.315 (g)(9)</dt>')).toBeGreaterThan(-1);
       });
 
-      it('should only have one element if only one api link', function () {
-        var data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☺170.315 (g)(9)☹';
-        let output = vm.apiTransform(data);
+      it('should only have one element if only one api link', () => {
+        const data = '170.315 (g)(7)☹http://example1.com☺170.315 (g)(8)☺170.315 (g)(9)☹';
+        const output = vm.apiTransform(data);
         expect(output.indexOf('<dt>170.315 (g)(7)</dt>')).toBeGreaterThan(-1);
         expect(output.indexOf('170.315 (g)(8)')).toBe(-1);
         expect(output.indexOf('170.315 (g)(9)')).toBe(-1);
       });
     });
 
-    describe('transforming Mandatory Disclosures data', function () {
-      it('should return "Unknown" if no data', function () {
-        var data;
+    describe('transforming Mandatory Disclosures data', () => {
+      it('should return "Unknown" if no data', () => {
+        let data;
         expect(vm.disclosuresTransform(data)).toBe('Unknown');
       });
 
-      it('should a link for the data', function () {
-        var data = 'http://example.com';
-        let output = vm.disclosuresTransform(data);
+      it('should a link for the data', () => {
+        const data = 'http://example.com';
+        const output = vm.disclosuresTransform(data);
         expect(output.indexOf('<a ai-a href="http://example.com"')).toBeGreaterThan(-1);
       });
     });
