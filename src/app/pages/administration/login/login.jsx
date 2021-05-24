0f9ad9f9d85b3ef861daf858f10d7aa4b12d07a4
Andrew Larned <andrew.larned@gmail.com>
2021-05-24 10:45:57 -0400

    feat: implement login from new component
    [#OCD-3546]

--- src/app/pages/administration/login/login.jsx
+++ src/app/pages/administration/login/login.jsx
@@ -11,16 +11,31 @@ import {
   Typography,
   makeStyles,
 } from '@material-ui/core';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 import theme from '../../../themes/theme';
 import { getAngularService } from '../../../services/angular-react-helper';
+import { ChplTextField } from '../../../components/util';
 
 const useStyles = makeStyles(() => ({
 }));
 
+const validationSchema = yup.object({
+  userName: yup.string()
+    .required('Email (or User Name) is required'),
+  password: yup.string()
+    .required('Password is required'),
+});
+
 function ChplLogin(props) {
-  console.log(props);
+  const $analytics = getAngularService('$analytics');
+  const $rootScope = getAngularService('$rootScope');
   const $state = getAngularService('$state');
+  const Idle = getAngularService('Idle');
+  const Keepalive = getAngularService('Keepalive');
+  const authService = getAngularService('authService');
+  const networkService = getAngularService('networkService');
   const classes = useStyles();
   const state = props.returnTo.state();
   const params = props.returnTo.params();
@@ -29,8 +44,32 @@ function ChplLogin(props) {
   let formik;
 
   const login = () => {
-    console.log('logging in');
+    networkService.login({userName: formik.values.userName, password: formik.values.password})
+      .then(() => {
+        networkService.getUserById(authService.getUserId())
+          .then(user => {
+            $analytics.eventTrack('Log In', { category: 'Authentication' });
+            authService.saveCurrentUser(user);
+            Idle.watch();
+            Keepalive.ping();
+            $rootScope.$broadcast('loggedIn');
+            $state.go(state, params, options);
+          });
+      });
   };
+
+  formik = useFormik({
+    validationSchema,
+    initialValues: {
+      userName: '',
+      password: '',
+    },
+    validateOnChange: false,
+    validateOnBlur: true,
+    onSubmit: () => {
+      login();
+    },
+  });
 
   return (
     <ThemeProvider theme={theme}>
@@ -38,18 +77,40 @@ function ChplLogin(props) {
         <Card>
           <CardHeader title="Login required" />
           <CardContent>
-            <Typography
-              variant="body1"
-            >
-              Log in here
+            <ChplTextField
+              id="user-name"
+              name="userName"
+              label="Email (or User Name)"
+              required
+              value={formik.values.userName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.userName && !!formik.errors.userName}
+              helperText={formik.touched.userName && formik.errors.userName}
+            />
+            <ChplTextField
+              type="password"
+              id="password"
+              name="password"
+              label="Password"
+              required
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.password && !!formik.errors.password}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+            <Typography>
+              This warning banner provides privacy and security notices consistent with applicable federal laws, directives, and other federal guidance for accessing this Government system, which includes all devices/storage media attached to this system. This system is provided for Government-authorized use only. Unauthorized or improper use of this system is prohibited and may result in disciplinary action and/or civil and criminal penalties. At any time, and for any lawful Government purpose, the government may monitor, record, and audit your system usage and/or intercept, search and seize any communication or data transiting or stored on this system. Therefore, you have no reasonable expectation of privacy. Any communication or data transiting or stored on this system may be disclosed or used for any lawful Government purpose.
             </Typography>
           </CardContent>
           <CardActions>
             <Button
               color="primary"
-              onClick={login}
+              variant="contained"
+              onClick={formik.handleSubmit}
             >
-              Login
+              Log In
             </Button>
           </CardActions>
         </Card>
