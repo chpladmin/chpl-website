c15ab97ce08afd57e1e21375e124f67b44da029f
Andrew Larned <andrew.larned@gmail.com>
2021-05-25 12:39:09 -0400

    test: expect legacy login to continue to work
    [#OCD-3546]

--- e2e/components/login/login.po.js
+++ e2e/components/login/login.po.js
@@ -6,6 +6,7 @@ class LoginComponent {
       component: '#login-component',
       loginToggle: '//*[@id="login-toggle"]',
       userName: '[name="userName"]',
+      username: '[name="username"]',
       password: '[name="password"]',
       login: 'button=Log In',
       logout: '//button[text()="Log Out"]',
@@ -28,9 +29,25 @@ class LoginComponent {
   }
 
   logIn(user) {
-    $(this.elements.component).$(this.elements.userName).addValue(credentials[user].email || credentials[user].username);
-    $(this.elements.component).$(this.elements.password).addValue(credentials[user].password);
-    $(this.elements.component).$(this.elements.login).scrollAndClick();
+    let un;
+    let pw;
+    let btn;
+    if ($(this.elements.component).isDisplayed()) {
+      un = $(this.elements.component).$(this.elements.userName);
+      pw = $(this.elements.component).$(this.elements.password);
+      btn = $(this.elements.component).$(this.elements.login);
+    } else {
+      this.toggleLoginComponent();
+      un = $(this.elements.username);
+      pw = $(this.elements.password);
+      btn = $(this.elements.login);
+    }
+    un.addValue(credentials[user].email || credentials[user].username);
+    pw.addValue(credentials[user].password);
+    btn.scrollAndClick();
+    if (!($(this.elements.component).isDisplayed())) {
+      this.toggleLoginComponent();
+    }
   }
 
   logOut() {
@@ -38,6 +55,7 @@ class LoginComponent {
       this.toggleLoginComponent();
     }
     $(this.elements.logout).scrollAndClick();
+    browser.waitUntil(() => this.getLoggedInUserName() === 'Administrator Login');
     this.toggleLoginComponent();
   }
 }
--- e2e/components/login/login.readonly.spec.js
+++ e2e/components/login/login.readonly.spec.js
@@ -4,11 +4,50 @@ import Hooks from '../../utilities/hooks';
 let component;
 let hooks;
 
-describe('the login component', () => {
+describe('the new login component', () => {
   beforeEach(async () => {
     component = new LoginComponent();
     hooks = new Hooks();
     await hooks.open('#/login');
+  });
+
+  describe('when logging in', () => {
+    afterEach(() => {
+      component.logOut();
+    });
+
+    it('should be able to log in as drummond ACB', () => {
+      component.logIn('drummond');
+      component.waitToBeLoggedIn();
+      expect(component.getLoggedInUserName()).toBe('AQA Drummond');
+    });
+
+    it('should be able to log in as ONC', () => {
+      component.logIn('onc');
+      component.waitToBeLoggedIn();
+      expect(component.getLoggedInUserName()).toBe('AQA ONC');
+    });
+
+    it('should be able to log in as ADMIN', () => {
+      component.logIn('admin');
+      component.waitToBeLoggedIn();
+      expect(component.getLoggedInUserName()).toBe('AQA Admin');
+    });
+  });
+
+  it('should be able to log out', () => {
+    component.logIn('onc');
+    component.waitToBeLoggedIn();
+    component.logOut();
+    expect(component.getLoggedInUserName()).toBe('Administrator Login');
+  });
+});
+
+describe('the legacy login component', () => {
+  beforeEach(async () => {
+    component = new LoginComponent();
+    hooks = new Hooks();
+    await hooks.open('#/resources/overview');
   });
 
   describe('when logging in', () => {
