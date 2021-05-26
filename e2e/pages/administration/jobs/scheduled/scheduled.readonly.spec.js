d1ebc6f333634273bea9553abb769a7a24ee190b
Andrew Larned <andrew.larned@gmail.com>
2021-05-26 08:47:19 -0400

    test: expect new Job to be in page
    [#OCD-3663]

--- e2e/pages/administration/jobs/scheduled/scheduled.readonly.spec.js
+++ e2e/pages/administration/jobs/scheduled/scheduled.readonly.spec.js
@@ -3,33 +3,35 @@ import ScheduledPage from './scheduled.po';
 import Hooks from '../../../../utilities/hooks';
 import { assert } from 'chai';
 
-let hooks, loginComponent, scheduled;
+let hooks;
+let loginComponent;
+let scheduled;
 
 beforeEach(async () => {
   loginComponent = new LoginComponent();
   scheduled = new ScheduledPage();
   hooks = new Hooks();
-  await hooks.open('#/search');
+  await hooks.open('#/administration/jobs/scheduled');
 });
 
 describe('when an ONC-Staff user is logged in', () => {
-  beforeEach(function () {
+  beforeEach(() => {
     loginComponent.logIn('oncstaff');
-    hooks.open('#/administration/jobs/scheduled');
+    loginComponent.waitToBeLoggedIn();
     hooks.waitForSpinnerToDisappear();
   });
 
   it('should see the right set of scheduled jobs', () => {
-    const expected = new Set(['All Broken Surveillance Rules Report','Developer Access Report','Inherited Certification Status Errors Report','Listing Validation Email Report','ONC-ACB Questionable URL Report','Overnight Broken Surveillance Rules Report','Pending "Change Request" Report','Questionable Activity Report','Questionable URL Report','Real World Testing Email Report','Summary Statistics Email','Trigger Developer Ban Notification']);
+    const expected = new Set(['All Broken Surveillance Rules Report','Cures Statistics Email','Developer Access Report','Inherited Certification Status Errors Report','Listing Validation Email Report','ONC-ACB Questionable URL Report','Overnight Broken Surveillance Rules Report','Pending "Change Request" Report','Questionable Activity Report','Questionable URL Report','Real World Testing Email Report','Summary Statistics Email','Trigger Developer Ban Notification']);
     browser.waitUntil(() => scheduled.scheduledJobTable.isDisplayed());
-    assert.equal(scheduled.scheduledJobRows.length,expected.size);
+    expect(scheduled.scheduledJobRows.length).toBe(expected.size);
     // get the existing jobs into a de-duplicated array of jobs
-    let rows = [...new Set(scheduled.scheduledJobRows.map(row => row.getText()))];
+    let rows = [...new Set(scheduled.scheduledJobRows.map((row) => row.getText()))];
     // for each of the expected jobs
-    expected.forEach(exp => {
+    expected.forEach((exp) => {
       // count how many existing jobs start with the expected job's name
-      let found = rows.filter(row => row.startsWith(exp)).length;
-      expect(found).toBe(1, 'did not find expected job: "' + exp + '"');
+      let found = rows.filter((row) => row.startsWith(exp)).length;
+      expect(found).toBe(1, `did not find expected job: "${exp}"`);
     });
   });
 });
