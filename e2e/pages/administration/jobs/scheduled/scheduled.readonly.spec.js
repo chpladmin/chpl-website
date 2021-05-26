8f1f0ce26a5cce26a4339358c51865ad23436067
Andrew Larned <andrew.larned@gmail.com>
2021-05-26 15:26:50 -0400

    refactor: clarify AQA tests for Scheduled Jobs page
    [#OCD-3546]

--- e2e/pages/administration/jobs/scheduled/scheduled.readonly.spec.js
+++ e2e/pages/administration/jobs/scheduled/scheduled.readonly.spec.js
@@ -1,4 +1,3 @@
-import { assert } from 'chai';
 import LoginComponent from '../../../../components/login/login.po';
 import ScheduledPage from './scheduled.po';
 import Hooks from '../../../../utilities/hooks';
@@ -7,23 +6,19 @@ let hooks;
 let loginComponent;
 let scheduled;
 
-beforeEach(async () => {
-  loginComponent = new LoginComponent();
-  scheduled = new ScheduledPage();
-  hooks = new Hooks();
-  await hooks.open('#/administration/jobs/scheduled');
-});
+describe('the Scheduled Jobs page', () => {
+  beforeEach(async () => {
+    loginComponent = new LoginComponent();
+    scheduled = new ScheduledPage();
+    hooks = new Hooks();
+    await hooks.open('#/administration/jobs/scheduled');
+  });
 
-describe('when an ONC-Staff user is logged in', () => {
-  beforeEach(() => {
+  it('should have specific jobs for ONC-Staff users', () => {
+    const expected = new Set(['All Broken Surveillance Rules Report', 'Cures Statistics Email', 'Developer Access Report', 'Inherited Certification Status Errors Report', 'Listing Validation Email Report', 'ONC-ACB Questionable URL Report', 'Overnight Broken Surveillance Rules Report', 'Pending "Change Request" Report', 'Questionable Activity Report', 'Questionable URL Report', 'Real World Testing Email Report', 'Summary Statistics Email', 'Trigger Developer Ban Notification']);
     loginComponent.logIn('oncstaff');
     loginComponent.waitToBeLoggedIn();
     hooks.waitForSpinnerToDisappear();
-  });
-
-  it('should see the right set of scheduled jobs', () => {
-    const expected = new Set(['All Broken Surveillance Rules Report', 'Cures Statistics Email', 'Developer Access Report', 'Inherited Certification Status Errors Report', 'Listing Validation Email Report', 'ONC-ACB Questionable URL Report', 'Overnight Broken Surveillance Rules Report', 'Pending "Change Request" Report', 'Questionable Activity Report', 'Questionable URL Report', 'Real World Testing Email Report', 'Summary Statistics Email', 'Trigger Developer Ban Notification']);
-    browser.waitUntil(() => scheduled.scheduledJobTable.isDisplayed());
     expect(scheduled.scheduledJobRows.length).toBe(expected.size);
     // get the existing jobs into a de-duplicated array of jobs
     const rows = [...new Set(scheduled.scheduledJobRows.map((row) => row.getText()))];
