df8cb9ec065d2e0595372aa347a3f4e1f02cdfb7
todd.young <tyoung@ainq.com>
2021-06-02 21:54:34 -0400

    test: add tests to surveillance-view for handling 0 non-cons
    OCD-3637

--- src/app/components/listing/details/surveillance/surveillance-view.test.jsx
+++ src/app/components/listing/details/surveillance/surveillance-view.test.jsx
@@ -85,6 +85,52 @@ const survWith1Nonconformity = {
   lastModifiedDate: 1597786978488,
 };
 
+const survWith0Nonconformity = {
+  id: 973,
+  friendlyId: 'SURV01',
+  certifiedProduct: {
+    id: 10332,
+    chplProductNumber: '15.04.04.2880.Athe.AM.07.1.200312',
+    lastModifiedDate: 1616496456285,
+    edition: '2015',
+    certificationDate: 1583985600000,
+    certificationStatus: 'Active',
+    curesUpdate: false,
+  },
+  startDate: 1584331200000,
+  endDate: 1596513600000,
+  type: {
+    id: 1,
+    name: 'Reactive',
+  },
+  randomizedSitesUsed: null,
+  requirements: [{
+    id: 1522,
+    type: {
+      id: 1,
+      name: 'Certified Capability',
+    },
+    requirement: '170.315 (c)(1)',
+    criterion: {
+      id: 25,
+      number: '170.315 (c)(1)',
+      title: 'Clinical Quality Measures - Record and Export',
+      certificationEditionId: 3,
+      certificationEdition: '2015',
+      description: null,
+      removed: false,
+    },
+    result: {
+      id: 2,
+      name: 'No Non-Conformity',
+    },
+    nonconformities: [],
+    requirementName: '170.315 (c)(1)',
+  }],
+  authority: 'ROLE_ACB',
+  lastModifiedDate: 1597786978488,
+};
+
 const dateUtilMock = {
   timestampToString: jest.fn(() => 'June 1, 2020'),
 };
@@ -95,18 +141,16 @@ angularReactHelper.getAngularService = jest.fn();
 when(angularReactHelper.getAngularService).calledWith('DateUtil').mockReturnValue(dateUtilMock);
 
 describe('the ChplSurveillanceView component', () => {
-  beforeEach(async () => {
-    render(
-      <ChplSurveillanceView
-        surveillance={survWith1Nonconformity}
-      />,
-    );
-  });
   afterEach(() => {
     cleanup();
   });
 
   it('should display the table of attributes', async () => {
+    render(
+      <ChplSurveillanceView
+        surveillance={survWith1Nonconformity}
+      />,
+    );
     const table = screen.getByTestId('surveillance-attributes-table');
 
     await waitFor(() => {
@@ -114,12 +158,17 @@ describe('the ChplSurveillanceView component', () => {
     });
   });
   it('should use the DateUtil.timestampToString to format dates', async () => {
+    render(
+      <ChplSurveillanceView
+        surveillance={survWith1Nonconformity}
+      />,
+    );
     await waitFor(() => {
       expect(dateUtilMock.timestampToString).toHaveBeenCalled();
     });
   });
 
-  describe('when the surveillance has a closed non-formity', () => {
+  describe('when the surveillance has a closed non-conformity', () => {
     /*
     it("should display 'Certified Capability : 170.315 (c)(1) : Clinical Quality Measures - Record and Export' for CertificationCriteria and Program Requirements Surveilled", async () => {
       const tableCell = screen.getByTestId('criteria-reqs-surveilled');
@@ -130,6 +179,11 @@ describe('the ChplSurveillanceView component', () => {
     })
     */
     it('should display the non-conformities header', async () => {
+      render(
+        <ChplSurveillanceView
+          surveillance={survWith1Nonconformity}
+        />,
+      );
       const header = screen.getByTestId('non-conformity-header');
 
       await waitFor(() => {
@@ -137,9 +191,42 @@ describe('the ChplSurveillanceView component', () => {
       });
     });
     it('should display 1 non-conformity component', async () => {
+      render(
+        <ChplSurveillanceView
+          surveillance={survWith1Nonconformity}
+        />,
+      );
+
       await waitFor(() => {
         const components = screen.getAllByTestId('non-conformity-component');
         expect(components.length).toEqual(1);
+      });
+    });
+  });
+
+  describe('when the surveillance has 0 non-conformities', () => {
+    it('should not display the non-conformities header', async () => {
+      render(
+        <ChplSurveillanceView
+          surveillance={survWith0Nonconformity}
+        />,
+      );
+      const header = screen.queryByTestId('non-conformity-header');
+
+      await waitFor(() => {
+        expect(header).toBeNull();
+      });
+    });
+    it('should display 0 non-conformity components', async () => {
+      render(
+        <ChplSurveillanceView
+          surveillance={survWith0Nonconformity}
+        />,
+      );
+
+      await waitFor(() => {
+        const components = screen.queryAllByTestId('non-conformity-component');
+        expect(components.length).toEqual(0);
       });
     });
   });
