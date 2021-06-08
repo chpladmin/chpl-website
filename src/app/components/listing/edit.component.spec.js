d997b970f68559121f9214744a4159d21087c842
Andrew Larned <andrew.larned@gmail.com>
2021-06-08 08:33:07 -0400

    style: fix eslinting errors
    [#OCD-3621]

--- src/app/components/listing/edit.component.spec.js
+++ src/app/components/listing/edit.component.spec.js
@@ -1,15 +1,19 @@
 (() => {
-  'use strict';
-
   describe('the Listing Edit component', () => {
-    var $compile, $log, authService, ctrl, el, mock, scope, utilService;
+    let $compile;
+    let $log;
+    let authService;
+    let ctrl;
+    let el;
+    let scope;
+    let utilService;
 
-    mock = {};
+    const mock = {};
     mock.listing = {
       accessibilityStandards: [],
-      certificationEdition: {name: '2015'},
+      certificationEdition: { name: '2015' },
       certificationEvents: [
-        { eventDate: 1498622400000, certificationStatusId: 1, status: { name: 'Active' }},
+        { eventDate: 1498622400000, certificationStatusId: 1, status: { name: 'Active' } },
       ],
       certifyingBody: [],
       chplProductNumber: 'CHP-123123',
@@ -18,42 +22,48 @@
       practiceType: [],
       product: { productId: 1 },
       qmsStandards: [
-        {id: 1, qmsStandardName: 'name1'},
-        {id: null, qmsStandardName: 'nullname'},
+        { id: 1, qmsStandardName: 'name1' },
+        { id: null, qmsStandardName: 'nullname' },
       ],
       targetedUsers: [],
       testingLabs: [],
     };
     mock.resources = {
-      accessibilityStandards: {data: [
-        {id: 1, name: 'name1'},
-      ]},
-      bodies: [{id: 1, name: 'name1'}, {id: 2, name: 'name2'}],
-      classifications: [{id: 1, name: 'name1'}],
-      practices: [{id: 1, name: 'name1'}],
-      qmsStandards: {data: [
-        {id: 1, name: 'name1'},
-        {id: 2, name: 'name2'},
-      ]},
-      testStandards: {data: [
-        {id: 1, name: 'name1', year: 2014},
-        {id: 2, name: 'name2', year: 2015},
-      ]},
-      statuses: [{id: 1, name: 'name1'}],
-      testingLabs: [{id: 1, name: 'name1'}],
+      accessibilityStandards: {
+        data: [
+          { id: 1, name: 'name1' },
+        ],
+      },
+      bodies: [{ id: 1, name: 'name1' }, { id: 2, name: 'name2' }],
+      classifications: [{ id: 1, name: 'name1' }],
+      practices: [{ id: 1, name: 'name1' }],
+      qmsStandards: {
+        data: [
+          { id: 1, name: 'name1' },
+          { id: 2, name: 'name2' },
+        ],
+      },
+      testStandards: {
+        data: [
+          { id: 1, name: 'name1', year: 2014 },
+          { id: 2, name: 'name2', year: 2015 },
+        ],
+      },
+      statuses: [{ id: 1, name: 'name1' }],
+      testingLabs: [{ id: 1, name: 'name1' }],
     };
 
     beforeEach(() => {
-      angular.mock.module('chpl.components', $provide => {
-        $provide.decorator('authService', $delegate => {
-          $delegate.hasAnyRole = jasmine.createSpy('hasAnyRole');
-          return $delegate;
-        });
-        $provide.decorator('utilService', $delegate => {
-          $delegate.certificationStatusWhenEditing = jasmine.createSpy('certificationStatusWhenEditing');
-          $delegate.extendSelect = jasmine.createSpy('extendSelect');
-          return $delegate;
-        });
+      angular.mock.module('chpl.components', ($provide) => {
+        $provide.decorator('authService', ($delegate) => ({
+          ...$delegate,
+          hasAnyRole: jasmine.createSpy('hasAnyRole'),
+        }));
+        $provide.decorator('utilService', ($delegate) => ({
+          ...$delegate,
+          certificationStatusWhenEditing: jasmine.createSpy('certificationStatusWhenEditing'),
+          extendSelect: jasmine.createSpy('extendSelect'),
+        }));
       });
 
       inject((_$compile_, _$controller_, _$log_, $rootScope, _authService_, _utilService_) => {
@@ -82,7 +92,7 @@
     afterEach(() => {
       if ($log.debug.logs.length > 0) {
         /* eslint-disable no-console,angular/log */
-        console.log('Debug:\n' + $log.debug.logs.map(o => angular.toJson(o)).join('\n'));
+        console.log(`Debug:\n${$log.debug.logs.map((o) => angular.toJson(o)).join('\n')}`);
         /* eslint-enable no-console,angular/log */
       }
     });
@@ -92,8 +102,8 @@
     });
 
     it('should not create parents if they exist', () => {
-      var cp = angular.copy(mock.listing);
-      cp.ics.parents = [{name: 'a parent'}];
+      const cp = angular.copy(mock.listing);
+      cp.ics.parents = [{ name: 'a parent' }];
       scope.listing = cp;
 
       el = angular.element('<chpl-listing-edit listing="listing" on-change="onChange(listing, messages, reason)" resources="resources" work-type="workType"></chpl-listing-edit>');
@@ -101,11 +111,11 @@
       $compile(el)(scope);
       scope.$digest();
       ctrl = el.isolateScope().$ctrl;
-      expect(ctrl.listing.ics.parents).toEqual([{name: 'a parent'}]);
+      expect(ctrl.listing.ics.parents).toEqual([{ name: 'a parent' }]);
     });
 
     it('should break the parts of the product number apart if it\'s the new style', () => {
-      var cp = angular.copy(mock.listing);
+      const cp = angular.copy(mock.listing);
       cp.chplProductNumber = '15.07.07.2713.CQ01.02.00.1.170331';
       scope.listing = cp;
 
@@ -134,16 +144,16 @@
     });
 
     xit('should attach the model for the select boxes', () => {
-      ctrl.listing.practiceType = {id: 1};
-      ctrl.listing.classificationType = {id: 1};
-      ctrl.listing.certifyingBody = {id: 2};
-      ctrl.listing.certificationStatus = {id: 1};
+      ctrl.listing.practiceType = { id: 1 };
+      ctrl.listing.classificationType = { id: 1 };
+      ctrl.listing.certifyingBody = { id: 2 };
+      ctrl.listing.certificationStatus = { id: 1 };
       ctrl.attachModel();
       expect(ctrl.listing.practiceType).toEqual(mock.resources.practices[0]);
       expect(ctrl.listing.classificationType).toEqual(mock.resources.classifications[0]);
       expect(ctrl.listing.certifyingBody).toEqual(mock.resources.bodies[1]);
       expect(ctrl.listing.testingLab).not.toEqual(mock.resources.testingLabs[0]);
-      ctrl.listing.testingLab = {id: 1};
+      ctrl.listing.testingLab = { id: 1 };
       ctrl.attachModel();
       expect(ctrl.listing.testingLab).toEqual(mock.resources.testingLabs[0]);
     });
@@ -163,11 +173,11 @@
       });
 
       it('should add date longs from the date objects', () => {
-        var aDate = new Date('1/1/2009');
-        var dateValue = aDate.getTime();
+        const aDate = new Date('1/1/2009');
+        const dateValue = aDate.getTime();
         ctrl.listing.certificationEvents = [
           {
-            status: {name: 'Active'},
+            status: { name: 'Active' },
             statusDateObject: aDate,
           },
         ];
@@ -206,8 +216,8 @@
       });
 
       it('should leverage the util service to get the status', () => {
-        let count = utilService.certificationStatusWhenEditing.calls.count();
-        let status = ctrl.certificationStatusWhenEditing(mock.listing);
+        const count = utilService.certificationStatusWhenEditing.calls.count();
+        const status = ctrl.certificationStatusWhenEditing(mock.listing);
         expect(status).toBe('Active');
         expect(utilService.certificationStatusWhenEditing.calls.count()).toBe(count + 1);
       });
@@ -217,10 +227,10 @@
       it('should know when two status events were on the same day', () => {
         ctrl.listing.certificationEvents = [
           {
-            status: {name: 'Active'},
+            status: { name: 'Active' },
             statusDateObject: new Date('1/1/2009'),
-          },{
-            status: {name: 'Suspended by ONC'},
+          }, {
+            status: { name: 'Suspended by ONC' },
             statusDateObject: new Date('1/1/2009'),
           },
         ];
@@ -232,10 +242,10 @@
       it('should know when two status events are the same and consecutive', () => {
         ctrl.listing.certificationEvents = [
           {
-            status: {name: 'Active'},
+            status: { name: 'Active' },
             statusDateObject: new Date('1/1/2009'),
-          },{
-            status: {name: 'Active'},
+          }, {
+            status: { name: 'Active' },
             statusDateObject: new Date('2/2/2009'),
           },
         ];
@@ -251,46 +261,46 @@
         });
 
         it('should expect the code to be 1 if one parent and parent has ICS 00', () => {
-          ctrl.listing.ics.parents = [{chplProductNumber: '15.07.07.2713.CQ01.02.00.1.170331'}];
+          ctrl.listing.ics.parents = [{ chplProductNumber: '15.07.07.2713.CQ01.02.00.1.170331' }];
           expect(ctrl.requiredIcsCode()).toBe('01');
         });
 
         it('should expect the code to be 1 if two parents and parents have ICS 00', () => {
           ctrl.listing.ics.parents = [
-            {chplProductNumber: '15.07.07.2713.CQ01.02.00.1.170331'},
-            {chplProductNumber: '15.07.07.2713.CQ01.02.00.1.170331'},
+            { chplProductNumber: '15.07.07.2713.CQ01.02.00.1.170331' },
+            { chplProductNumber: '15.07.07.2713.CQ01.02.00.1.170331' },
           ];
           expect(ctrl.requiredIcsCode()).toBe('01');
         });
 
         it('should expect the code to be 2 if two parents and parents have ICS 01', () => {
           ctrl.listing.ics.parents = [
-            {chplProductNumber: '15.07.07.2713.CQ01.02.01.1.170331'},
-            {chplProductNumber: '15.07.07.2713.CQ01.02.01.1.170331'},
+            { chplProductNumber: '15.07.07.2713.CQ01.02.01.1.170331' },
+            { chplProductNumber: '15.07.07.2713.CQ01.02.01.1.170331' },
           ];
           expect(ctrl.requiredIcsCode()).toBe('02');
         });
 
         it('should expect the code to be 3 if two parents and parents have ICS 01,02', () => {
           ctrl.listing.ics.parents = [
-            {chplProductNumber: '15.07.07.2713.CQ01.02.01.1.170331'},
-            {chplProductNumber: '15.07.07.2713.CQ01.02.02.1.170331'},
+            { chplProductNumber: '15.07.07.2713.CQ01.02.01.1.170331' },
+            { chplProductNumber: '15.07.07.2713.CQ01.02.02.1.170331' },
           ];
           expect(ctrl.requiredIcsCode()).toBe('03');
         });
 
         it('should expect the code to be 10 if two parents and parents have ICS 03,09', () => {
           ctrl.listing.ics.parents = [
-            {chplProductNumber: '15.07.07.2713.CQ01.02.09.1.170331'},
-            {chplProductNumber: '15.07.07.2713.CQ01.02.03.1.170331'},
+            { chplProductNumber: '15.07.07.2713.CQ01.02.09.1.170331' },
+            { chplProductNumber: '15.07.07.2713.CQ01.02.03.1.170331' },
           ];
           expect(ctrl.requiredIcsCode()).toBe('10');
         });
 
         it('should expect the code to be 18 if two parents and parents have ICS 17,11', () => {
           ctrl.listing.ics.parents = [
-            {chplProductNumber: '15.07.07.2713.CQ01.02.17.1.170331'},
-            {chplProductNumber: '15.07.07.2713.CQ01.02.11.1.170331'},
+            { chplProductNumber: '15.07.07.2713.CQ01.02.17.1.170331' },
+            { chplProductNumber: '15.07.07.2713.CQ01.02.11.1.170331' },
           ];
           expect(ctrl.requiredIcsCode()).toBe('18');
         });
