cc5e158e2e4b21cddfce64695126d6d5e46635c2
Andrew Larned <andrew.larned@gmail.com>
2021-06-14 13:03:00 -0400

    test: expect new components to call their HOC
    [#OCD-3400]

---
+++ src/app/components/registration/user-create.test.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  cleanup, render, screen, waitFor,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+
+import ChplUserCreate from './user-create';
+
+const hocMock = {
+  dispatch: jest.fn(),
+};
+
+describe('the ChplUserCreate component', () => {
+  beforeEach(async () => {
+    render(
+      <ChplUserCreate
+        dispatch={hocMock.dispatch}
+      />,
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  describe('when adding permissions', () => {
+    it('should call the callback', async () => {
+      hocMock.dispatch.mockClear();
+      userEvent.type(screen.getByLabelText(/Full Name/), 'full name');
+      userEvent.type(screen.getByLabelText(/Email/), 'email@sample.com');
+      userEvent.type(screen.getByLabelText(/^Password/), 'hopefullyThisIsAValidPassword654');
+      userEvent.type(screen.getByLabelText(/Verification Password/), 'hopefullyThisIsAValidPassword654');
+      userEvent.click(screen.getByRole('button', { name: /Create account/i }));
+
+      await waitFor(() => {
+        expect(hocMock.dispatch).toHaveBeenCalledWith(
+          'create',
+          {
+            email: 'email@sample.com',
+            friendlyName: '',
+            fullName: 'full name',
+            password: 'hopefullyThisIsAValidPassword654',
+            passwordVerify: 'hopefullyThisIsAValidPassword654',
+            phoneNumber: '',
+            title: '',
+          },
+        );
+      });
+    });
+  });
+});
