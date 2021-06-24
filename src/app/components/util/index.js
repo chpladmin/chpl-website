7200934bbc1bdcf760dbdbdbe32aa66cf8829510
todd.young <tyoung@ainq.com>
2021-06-23 21:09:27 -0400

    refactor: remove unused import
    OCD-3656

--- src/app/components/util/index.js
+++ src/app/components/util/index.js
@@ -2,12 +2,10 @@ import ChplDialogTitle from './chpl-dialog-title';
 import { ChplEllipsis } from './ellipsis';
 import ChplNonProdIndicator from './non-prod-indicator';
 import ChplTextField from './chpl-text-field';
-import { getAngularService } from '../../services/angular-react-helper';
 
 export {
   ChplDialogTitle,
   ChplEllipsis,
   ChplNonProdIndicator,
   ChplTextField,
-  getAngularService,
 };
