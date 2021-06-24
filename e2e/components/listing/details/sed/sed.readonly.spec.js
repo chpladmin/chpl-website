6bdc42615977df9006101f0b9fcb56698e978529
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2021-06-24 14:28:17 -0400

    OCD 2835: AQA tests for view listing details and edit listing (#902)
    * test: Add AQA test for view listing details
    
    [#OCD-2835]

---
+++ e2e/components/listing/details/sed/sed.readonly.spec.js
@@ -0,0 +1,28 @@
+import Hooks from '../../../../utilities/hooks';
+import SedComponent from './sed.po';
+
+let hooks;
+let sed;
+
+describe('the Listing page', () => {
+  beforeEach(async () => {
+    hooks = new Hooks();
+    sed = new SedComponent();
+    hooks.open('#/listing/9833');
+    await hooks.waitForSpinnerToDisappear();
+  });
+
+  it('should show UCD process table and all criteria for UCS process under SED information', () => {
+    sed.sedHeader.scrollIntoView();
+    sed.expandSed();
+    expect(sed.ucdProcess.isDisplayed()).toBe(true);
+    expect(sed.criteriaUcdCount()).toBeGreaterThan(5);
+  });
+
+  it('should show all testings tasks under SED information', () => {
+    sed.sedHeader.scrollIntoView();
+    sed.expandSed();
+    expect(sed.tasksTable.isDisplayed()).toBe(true);
+    expect(sed.testingTasksCount()).toBeGreaterThan(40);
+  });
+});
