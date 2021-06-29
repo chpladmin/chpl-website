c31fd4a2bdfedc7fb0c93be184565dbdcbba10fa
todd.young <tyoung@ainq.com>
2021-06-29 09:40:45 -0400

    fix: use default export notation
    OCD-3637

--- src/app/components/util/index.js
+++ src/app/components/util/index.js
@@ -4,7 +4,7 @@ import { ChplEllipsis } from './chpl-ellipsis';
 import ChplHighlightCures from './highlight-cures';
 import ChplLink from './chpl-link';
 import ChplTextField from './chpl-text-field';
-import { ChplTooltip } from './chpl-tooltip';
+import ChplTooltip from './chpl-tooltip';
 
 export {
   ChplCriterionTitle,
