47034e568f42463a2b32663015875e30d7dfcf98
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2021-07-07 11:20:06 -0400

    OCD-3656 : AQA test for non prod environment indicator (#923)
    * test: Add AQA for non prod environment indicator
    
    [#OCD-3656]

--- e2e/pages/resources/overview/overview.readonly.spec.js
+++ e2e/pages/resources/overview/overview.readonly.spec.js
@@ -1,37 +1,44 @@
-import OverviewPage from './overview.po.js';
+import OverviewPage from './overview.po';
 import Hooks from '../../../utilities/hooks';
+import EnvironmentIndicatorComponent from '../../../components/environment-indicator/environment-indicator.po';
 
-let hooks, page;
+let hooks;
+let indicator;
+let page;
 
 beforeEach(async () => {
   page = new OverviewPage();
   hooks = new Hooks();
+  indicator = new EnvironmentIndicatorComponent();
   await hooks.open('#/resources/overview');
 });
 
 describe('the Overview page', () => {
+  it('should have an indicator to show non prod environment', () => {
+    expect(indicator.nonProdIndicator.isDisplayed()).toBe(true);
+  });
 
   it('should have an ONC ACB ATL table', () => {
-    assert.equal(page.acbatlTable.isDisplayed(),true);
+    expect(page.acbatlTable.isDisplayed()).toBe(true);
   });
 
   it('should have acb and atl data in the table', () => {
-    var rowcount = page.acbatlTableRow.length;
-    var colcount = page.acbatlTableCol.length;
-    var actualResult = [];
-    for (var i = 1; i <= rowcount; i ++) {
-      for (var j = 1; j <= colcount - 2; j ++) {
-        var cellvalue = $('#acbAtlTable tbody tr:nth-child(' + i + ') td:nth-child(' + j + ')').getText();
+    const rowcount = page.acbatlTableRow.length;
+    const colcount = page.acbatlTableCol.length;
+    const actualResult = [];
+    for (let i = 1; i <= rowcount; i++) {
+      for (let j = 1; j <= colcount - 2; j++) {
+        const cellvalue = $(`#acbAtlTable tbody tr:nth-child(${i}) td:nth-child(${j})`).getText();
         actualResult.push(cellvalue);
       }
     }
-    const expectedTableVlaues = ['ONC-ACB', 'Drummond Group', 'ONC-ACB', 'ICSA Labs', 'ONC-ACB','SLI Compliance', 'ONC-ACB', 'UL LLC', 'ONC-ATL', 'Drummond Group', 'ONC-ATL', 'ICSA Labs', 'ONC-ATL', 'National Committee for Quality Assurance (NCQA)', 'ONC-ATL', 'SLI Compliance','ONC-ATL', 'UL LLC'];
-    var isSame = false;
-    for ( var k = 0; k < actualResult.length; k ++) {
+    const expectedTableVlaues = ['ONC-ACB', 'Drummond Group', 'ONC-ACB', 'ICSA Labs', 'ONC-ACB', 'SLI Compliance', 'ONC-ACB', 'UL LLC', 'ONC-ATL', 'Drummond Group', 'ONC-ATL', 'ICSA Labs', 'ONC-ATL', 'National Committee for Quality Assurance (NCQA)', 'ONC-ATL', 'SLI Compliance', 'ONC-ATL', 'UL LLC'];
+    let isSame = false;
+    for (let k = 0; k < actualResult.length; k++) {
       if (actualResult[k] === expectedTableVlaues[k]) {
         isSame = true;
       }
     }
-    assert.equal(isSame,true);
+    expect(isSame).toBe(true);
   });
 });
