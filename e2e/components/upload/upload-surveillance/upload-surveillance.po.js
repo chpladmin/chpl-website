dd3eef8641972f518476dd0a35ddfe7c72b31c53
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2021-07-07 11:26:46 -0400

    OCD-3637: Add AQA test regarding permissions for surveillance (#912)
    * test: test about permissions for surveillance
    
    [#OCD-3637]

--- e2e/components/upload/upload-surveillance/upload-surveillance.po.js
+++ e2e/components/upload/upload-surveillance/upload-surveillance.po.js
@@ -1,28 +1,31 @@
-const uploadElements = {
-  root: 'chpl-upload-surveillance-bridge',
-  chooseUploadSurveillance: '//input[@id="upload-surveillance"]',
-  uploadButton: '.MuiButton-containedPrimary',
-};
-
 const path = require('path');
 
 class UploadSurveillanceComponent {
-  constructor () { }
-
-  get chooseUploadSurveillanceButton () {
-    return $(uploadElements.chooseUploadSurveillance);
+  constructor() {
+    this.elements = {
+      root: 'chpl-upload-surveillance-bridge',
+      chooseUploadSurveillance: '//input[@id="upload-surveillance"]',
+      uploadButton: '.MuiButton-containedPrimary',
+    };
   }
 
-  get uploadButton () {
-    return $(uploadElements.root).$(uploadElements.uploadButton);
+  get root() {
+    return $(this.elements.root);
   }
 
-  uploadSurveillance (uploadfilePath) {
+  get chooseUploadSurveillanceButton() {
+    return $(this.elements.chooseUploadSurveillance);
+  }
+
+  get uploadButton() {
+    return $(this.elements.root).$(this.elements.uploadButton);
+  }
+
+  uploadSurveillance(uploadfilePath) {
     const filePath = path.join(__dirname, uploadfilePath);
     this.chooseUploadSurveillanceButton.addValue(browser.uploadFile(filePath));
     this.uploadButton.waitAndClick();
   }
-
 }
 
 export default UploadSurveillanceComponent;
