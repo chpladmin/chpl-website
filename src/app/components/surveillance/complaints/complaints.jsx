341f486da53da0099e835cf2b147303d14170b19
Andrew Larned <andrew.larned@gmail.com>
2021-07-09 06:56:04 -0400

    style: fix eslinting errors
    [#OCD-3682]

--- src/app/components/surveillance/complaints/complaints.jsx
+++ src/app/components/surveillance/complaints/complaints.jsx
@@ -15,20 +15,19 @@ import {
   makeStyles,
 } from '@material-ui/core';
 import { arrayOf, bool, func } from 'prop-types';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
 import theme from '../../../themes/theme';
 import { getAngularService } from '../../../services/angular-react-helper';
 import { ChplSortableHeaders } from '../../util';
 import { complaint as complaintPropType } from '../../../shared/prop-types';
 
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
-import VisibilityIcon from '@material-ui/icons/Visibility';
 const useStyles = makeStyles(() => ({
   container: {
     maxHeight: '64vh',
   },
-
   deleteButton: {
     backgroundColor: '#c44f65',
     color: '#ffffff',
@@ -54,9 +53,7 @@ const headers = [
   { property: 'acbComplaintId', text: 'ONC-ACB Complaint ID', sortable: true },
   { property: 'oncComplaintId', text: 'ONC Complaint ID', sortable: true },
   { property: 'complainantTypeName', text: 'Complainant Type', sortable: true },
-  {
-    property: 'actions', text: 'Actions', invisible: true, sortable: false,
-  },
+  { property: 'actions', text: 'Actions', invisible: true, sortable: false },
 ];
 
 const sortComparator = (property) => {
@@ -119,7 +116,6 @@ function ChplComplaints(props) {
       <TableContainer className={classes.container} component={Paper}>
         <Table
           stickyHeader
-          size="large"
           aria-label="Complaints table"
         >
           <ChplSortableHeaders
@@ -147,57 +143,64 @@ function ChplComplaints(props) {
             )}
           <TableBody>
             {complaints
-             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-             .map((complaint) => (
-               <TableRow key={complaint.id}>
-                 <TableCell>{complaint.acbName}</TableCell>
-                 <TableCell>
-                   <Typography
-                     variant="subtitle1"
-                     className={complaint.complaintStatusTypeName === 'Open' ? classes.statusIndicatorOpen : classes.statusIndicatorClosed}
-                   >{complaint.complaintStatusTypeName}
-                   </Typography>
-                 </TableCell>
-                 <TableCell>{complaint.receivedDate}</TableCell>
-                 <TableCell>{complaint.acbComplaintId}</TableCell>
-                 <TableCell>{complaint.oncComplaintId}</TableCell>
-                 <TableCell>{complaint.complainantTypeName}</TableCell>
-                 <TableCell>
-                   <ButtonGroup
-                     color="primary"
-                   >
-                     { hasAnyRole(['ROLE_ONC', 'ROLE_ONC_STAFF'])
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((complaint) => (
+                <TableRow key={complaint.id}>
+                  <TableCell>{complaint.acbName}</TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="subtitle1"
+                      className={complaint.complaintStatusTypeName === 'Open' ? classes.statusIndicatorOpen : classes.statusIndicatorClosed}
+                    >
+                      {complaint.complaintStatusTypeName}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>{complaint.receivedDate}</TableCell>
+                  <TableCell>{complaint.acbComplaintId}</TableCell>
+                  <TableCell>{complaint.oncComplaintId}</TableCell>
+                  <TableCell>{complaint.complainantTypeName}</TableCell>
+                  <TableCell>
+                    <ButtonGroup
+                      color="primary"
+                    >
+                      { hasAnyRole(['ROLE_ONC', 'ROLE_ONC_STAFF'])
                        && (
                          <Button
                            onClick={() => handleAction('view', complaint)}
                          >
-                           View <VisibilityIcon className={classes.iconSpacing}/>
+                           View
+                           {' '}
+                           <VisibilityIcon className={classes.iconSpacing} />
                          </Button>
                        )}
-                     { hasAnyRole(['ROLE_ADMIN', 'ROLE_ACB'])
+                      { hasAnyRole(['ROLE_ADMIN', 'ROLE_ACB'])
                        && (
                          <Button
                            onClick={() => handleAction('edit', complaint)}
                            variant="contained"
                            color="primary"
                          >
-                           Edit <EditOutlinedIcon className={classes.iconSpacing}/>
+                           Edit
+                           {' '}
+                           <EditOutlinedIcon className={classes.iconSpacing} />
                          </Button>
                        )}
-                     { hasAnyRole(['ROLE_ADMIN', 'ROLE_ACB'])
+                      { hasAnyRole(['ROLE_ADMIN', 'ROLE_ACB'])
                        && (
                          <Button
                            onClick={() => handleAction('delete', complaint)}
                            variant="contained"
                            className={classes.deleteButton}
                          >
-                           Delete <DeleteOutlinedIcon className={classes.iconSpacing} />
+                           Delete
+                           {' '}
+                           <DeleteOutlinedIcon className={classes.iconSpacing} />
                          </Button>
                        )}
-                   </ButtonGroup>
-                 </TableCell>
-               </TableRow>
-             ))}
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              ))}
             {emptyRows > 0 && (
               <TableRow style={{ height: 33 * emptyRows }}>
                 <TableCell colSpan={headers.length} />
@@ -229,4 +232,4 @@ ChplComplaints.propTypes = {
 
 ChplComplaints.defaultProps = {
   displayAdd: false,
-}
+};
