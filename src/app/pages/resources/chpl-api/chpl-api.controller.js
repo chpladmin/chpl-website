c25baf8b0ef379257b02d30529b8d7387793c49f
Andrew Larned <andrew.larned@gmail.com>
2021-07-13 06:05:05 -0400

    feat: allow "try it out" on Swagger docs
    [#api-doc-update]

--- src/app/pages/resources/chpl-api/chpl-api.controller.js
+++ src/app/pages/resources/chpl-api/chpl-api.controller.js
@@ -5,7 +5,7 @@
     .controller('ChplApiController', ChplApiController);
 
   /** @ngInject */
-  function ChplApiController ($location, $log, API) {
+  function ChplApiController ($log, authService) {
     var vm = this;
 
     activate();
@@ -13,12 +13,7 @@
     ////////////////////////////////////////////////////////////////////
 
     function activate () {
-      vm.API = API;
-      if (vm.API === '/rest') {
-        vm.swaggerUrl = $location.absUrl().split('#')[0] + 'rest/api-docs';
-      } else {
-        vm.swaggerUrl = vm.API + '/api-docs';
-      }
+      vm.apiKey = authService.getApiKey();
     }
   }
 })();
--- src/app/pages/resources/chpl-api/swagger.jsx
+++ src/app/pages/resources/chpl-api/swagger.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+import { string } from 'prop-types';
 
-function ChplSwagger() {
+function ChplSwagger(props) {
+  const [apiKey] = props.apiKey;
+
+  const interceptor = (req) => {
+    req.url = req.url.replace('chpl-service', 'rest');
+    req.headers['Api-Key'] = apiKey;
+    console.log(req);
+    return req;
+  }
+
   return (
-    <SwaggerUI url="http://localhost:3000/rest/v3/api-docs" />
+    <SwaggerUI
+      url="http://localhost:3000/rest/v3/api-docs"
+      docExpansion="none"
+      requestInterceptor={interceptor}
+    />
   );
 }
 
 export default ChplSwagger;
 
 ChplSwagger.propTypes = {
+  apiKey: string.isRequired,
 };
