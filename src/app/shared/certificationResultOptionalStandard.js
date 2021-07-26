7eaefb6a578e82c4c2fe577cb4499eeb32d8ace7
Andrew Larned <andrew.larned@gmail.com>
2021-07-26 10:47:23 -0400

    style: fix eslinting errors
    [#OCD-3622]

--- src/app/shared/certificationResultOptionalStandard.js
+++ src/app/shared/certificationResultOptionalStandard.js
@@ -1,15 +1,11 @@
-(function () {
-  'use strict';
-
+(() => {
   angular.module('chpl.shared')
-    .factory('CertificationResultOptionalStandard', function () {
-      var CertificationResultOptionalStandard = function (optionalStandard) {
-        return {
-          optionalStandardId: optionalStandard.id,
-          citation: optionalStandard.citation,
-          description: optionalStandard.description,
-        };
-      };
+    .factory('CertificationResultOptionalStandard', () => {
+      const CertificationResultOptionalStandard = (optionalStandard) => ({
+        optionalStandardId: optionalStandard.id,
+        citation: optionalStandard.citation,
+        description: optionalStandard.description,
+      });
 
       // Return a reference to the function
       return CertificationResultOptionalStandard;
