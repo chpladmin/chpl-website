12e620c9c65d29c4f81a9d04d3d5160b5e52b9d2
mstank <mstank@ainq.com>
2021-08-09 11:18:10 -0400

    UI:Add update CHPL Login Text

--- src/app/pages/resources/style-guide/chpl-login.jsx
+++ src/app/pages/resources/style-guide/chpl-login.jsx
@@ -80,7 +80,9 @@ function ChplAdministratorLogin(props) {
                 label="Password"
                 required
                 />
-            <Typography variant="body2">This warning banner provides privacy and security notices consistent with applicable federal laws, directives, and other federal guidance for accessing this Government system, which includes all devices/storage media attached to this system. This system is provided for Government-authorized use only. Unauthorized or improper use of this system is prohibited and may result in disciplinary action and/or civil and criminal penalties. At any time, and for any lawful Government purpose, the government may monitor, record, and audit your system usage and/or intercept, search and seize any communication or data transiting or stored on this system. Therefore, you have no reasonable expectation of privacy. Any communication or data transiting or stored on this system may be disclosed or used for any lawful Government purpose.
+            <Typography variant="body2">This warning banner provides privacy and security notices consistent with applicable federal laws, directives, and other federal guidance for accessing this Government system, which includes all devices/storage media attached to this system. This system is provided for Government-authorized use only. Unauthorized or improper use of this system is prohibited and may result in disciplinary action and/or civil and criminal penalties. 
+            </Typography>
+            <Typography variant="body2">At any time, and for any lawful Government purpose, the government may monitor, record, and audit your system usage and/or intercept, search and seize any communication or data transiting or stored on this system. Therefore, you have no reasonable expectation of privacy. Any communication or data transiting or stored on this system may be disclosed or used for any lawful Government purpose.
             </Typography>
             </CardContent>
             <CardActions>
