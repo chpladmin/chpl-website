951ec5c2d9a778686edba8baa9928a027d5e8243
mstank <mstank@ainq.com>
2021-08-16 12:37:03 -0400

    UI: Added additional content, filters, search and toggle within advanced search popover

--- src/app/pages/resources/style-guide/sg-advanced-search-popover.jsx
+++ src/app/pages/resources/style-guide/sg-advanced-search-popover.jsx
@@ -4,32 +4,71 @@ import {
     ButtonGroup,
     Checkbox,
     Divider,
+    InputBase,
+    FormControlLabel,
     List,
     ListItem,
     ListItemText,
     ListItemIcon,
     ListSubheader,
     Popover,
-    makeStyles,
+    Switch,
     Typography,
+    makeStyles,
 } from '@material-ui/core';
+
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles({
     filterContainer: {
         background: '#E7F0F8',
         display: 'grid',
-        gridTemplateColumns: '6fr 2fr 4fr',
+        gridTemplateColumns: '5fr auto 4fr',
         padding: '16px',
     },
     filterGroupOneContainer: {
         display: 'grid',
         gridTemplateColumns: '1fr auto',
-        alignItems:'center',
+        alignItems: 'center',
     },
-    filterListContainer: {
+    filterGroupTwoContainer: {
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        alignItems: 'center',
+        borderLeft: '1px solid #599bde',
+    },
+    filterGroupThreeContainer: {
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        alignItems: 'center',
+    },
+    filterSubHeaderGroupThreeContainer: {
+        display: 'grid',
+        gridTemplateColumns: '10fr auto auto',
+        alignItems: 'center',
+        gap: '8px',
+    },
+    switchGroupThreeContainer: {
+        display: 'grid',
+        gridTemplateColumns: '10fr auto auto',
+        alignItems: 'center',
+        gap: '4px',
+    },
+    filterSubHeaderContainer: {
         display: 'grid',
         gridTemplateColumns: '1fr 1fr',
-        alignItems:'center',
+        alignItems: 'center',
+    },
+    searchInput: {
+        flexGrow: 1,
+        backgroundColor: '#ffffff',
+        padding: '4px',
+        borderRadius: '4px',
+        border: '1px solid #C6D5E5',
+        width: '100%',
+        alignItems: 'center',
+        display: 'grid',
+        gridTemplateColumns: 'auto 1fr',
     },
 });
 
@@ -91,79 +130,187 @@ function SgAdvancedSearchPopover(props) {
                         <div>
                             <List dense
                                 subheader={
-                                    <ListSubheader component="div" id="nested-list-subheader">
+                                    <ListSubheader disableSticky component="div" id="nested-list-subheader">
                                         <div className={classes.filterGroupOneContainer}>
-                                        <div>
-                                        <Typography variant="subtitle1"> Filter By </Typography>
-                                        </div>
-                                        <div>
-                                        <ButtonGroup
-                                            variant="text"
-                                            color="primary"
-                                            size="medium"
-                                            aria-label="apply to filter dropdown">
-                                            <Button>Reset</Button>
-                                            <Button>Select All</Button>
-                                        </ButtonGroup>
-                                        </div>
+                                            <div>
+                                                <Typography variant="subtitle1"> Filter By: </Typography>
+                                            </div>
+                                            <div>
+                                                <ButtonGroup
+                                                    variant="text"
+                                                    color="primary"
+                                                    size="medium"
+                                                    aria-label="apply to filter dropdown">
+                                                    <Button>Reset All Filters</Button>
+                                                </ButtonGroup>
+                                            </div>
                                         </div>
                                     </ListSubheader>
                                 }>
-                                <div className={classes.filterListContainer}>
+                                <div className={classes.filterSubHeaderContainer}>
                                     <div>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>Certification Criteria</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>Certification Status</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>Certification Edition</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>Clinical Quality Measures</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                </div>
-                                <div>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>Certification Date</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>ONC-ACBs</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>Patient Type</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                <ListItem>
-                                    <Checkbox color="primary" edge="start" />
-                                    <ListItemText>Surveillance Activity</ListItemText>
-                                    <ListItemIcon></ListItemIcon>
-                                </ListItem>
-                                </div>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>Certification Criteria</ListItemText>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>Certification Status</ListItemText>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>Certification Edition</ListItemText>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>Clinical Quality Measures</ListItemText>
+                                        </ListItem>
+                                    </div>
+                                    <div>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>Certification Date</ListItemText>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>ONC-ACBs</ListItemText>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>Patient Type</ListItemText>
+                                        </ListItem>
+                                        <ListItem>
+                                            <Checkbox color="primary" edge="start" />
+                                            <ListItemText>Surveillance Activity</ListItemText>
+                                        </ListItem>
+                                    </div>
                                 </div>
                             </List>
                         </div>
 
                     </div>
                     <div>
-                        Filter By 2
+                        <List dense
+                            subheader={
+                                <ListSubheader disableSticky component="div" id="nested-list-subheader">
+                                    <div className={classes.filterSubHeaderContainer}>
+                                        <div>
+                                            <ButtonGroup
+                                                variant="text"
+                                                color="primary"
+                                                size="medium"
+                                                aria-label="apply to filter dropdown">
+                                                <Button>Clear</Button>
+                                                <Button>Reset</Button>
+                                            </ButtonGroup>
+                                        </div>
+                                    </div>
+                                </ListSubheader>
+                            }>
+                            <div className={classes.filterGroupTwoContainer}>
+                                <div>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>2011</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>2014</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>2015</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>2015 Cures Update</ListItemText>
+                                    </ListItem>
+                                </div>
+                            </div>
+                        </List>
                     </div>
                     <div>
-                        Select Filter
+                        <List dense
+                            subheader={
+                                <ListSubheader disableSticky component="div" id="nested-list-subheader">
+                                    <div className={classes.filterSubHeaderGroupThreeContainer}>
+                                        <div className={classes.searchInput}>
+                                            <SearchIcon />
+                                            <InputBase
+                                                placeholder="Search for something..."
+
+                                            />
+                                        </div>
+                                        <div>
+                                            <Typography variant="subtitle1"> Matching: </Typography>
+                                        </div>
+                                        <div className={classes.switchGroupThreeContainer}>
+                                            <div>
+                                                <Typography variant="body1"> Any </Typography>
+                                            </div>
+                                            <div>
+                                                <Switch color="primary" />
+                                            </div>
+                                            <div>
+                                                <Typography variant="body1"> All </Typography>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </ListSubheader>
+                            }>
+                            <div>
+                                <div className={classes.filterGroupThreeContainer}>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>All</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.315 (a)(1): Computerized Provider Order Entry (CPOE) - Medications</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.315 (a)(3): CPOE - Diagnostic Imaging</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.315 (a)(5): Demographics</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(5): Demographics</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(6): Problem list</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(7): Medication list</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(8): Medication allergy list</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(9): Clinical decision support</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(10): Drug-formulary and preferred drug list checks</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(11): Smoking status</ListItemText>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Checkbox color="primary" edge="start" />
+                                        <ListItemText>170.302 (a)(12): Family health history</ListItemText>
+                                    </ListItem>
+                                </div>
+                            </div>
+                        </List>
                     </div>
                 </div>
             </Popover>
