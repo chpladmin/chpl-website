4f3172a5f0b469d5122b3bea0591eb239afa57ad
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2021-09-27 10:47:03 -0400

    OCD-3662: Add AQA tests for CQM errors on flexible upload and correct tools errors (#963)
    * test: Add AQA tests for CQM errors on flexible upload and correct test tools errors
    
    [#OCD-3662]

--- e2e/workflowTest/upload-confirm-listing-test/upload-listing-error-warning-validation.spec.js
+++ e2e/workflowTest/upload-confirm-listing-test/upload-listing-error-warning-validation.spec.js
@@ -9,36 +9,35 @@ let hooks;
 let loginComponent;
 let toast;
 let uploadListingComponent;
-const inputs = require('./dataProviders/chplId-data-validation-dp');
+const inputs = require('./dataProviders/upload-listing-error-warning-validation-dp');
 
 const validListingId = '15.04.04.1722.AQA4.03.01.1.200620';
 
-beforeEach(() => {
-  confirmPage = new ConfirmPage();
-  uploadListingComponent = new UploadListingComponent();
-  loginComponent = new LoginComponent();
-  hooks = new Hooks();
-  toast = new ToastComponent();
-  hooks.open('#/administration/upload');
-  loginComponent.logIn('admin');
-});
+if (process.env.ENV !== 'stage') {
+  beforeAll(() => {
+    confirmPage = new ConfirmPage();
+    uploadListingComponent = new UploadListingComponent();
+    loginComponent = new LoginComponent();
+    hooks = new Hooks();
+    toast = new ToastComponent();
+    hooks.open('#/administration/upload');
+    loginComponent.logIn('admin');
+    uploadListingComponent.uploadListingBeta('../../../resources/upload-listing-beta/2015_InvalidData.csv');
+    browser.waitUntil(() => toast.toastTitle.isDisplayed());
+    toast.clearAllToast();
+    uploadListingComponent.uploadListingBeta('../../../resources/listings/2015_v19_AQA4.csv');
+    browser.waitUntil(() => toast.toastTitle.isDisplayed());
+    toast.clearAllToast();
+  });
 
-inputs.forEach((input) => {
-  const { listingId } = input;
-  const { expectedErrors } = input;
-  const { expectedWarnings } = input;
+  inputs.forEach((input) => {
+    const { listingId } = input;
+    const { expectedErrors } = input;
+    const { expectedWarnings } = input;
 
-  describe(`User uploads a listing with CHPL ID ${listingId} that has invalid inputs in various fields or missing required criteria`, () => {
-    if (process.env.ENV !== 'stage') {
+    describe(`When admin uploads a listing with CHPL ID ${listingId} that has invalid inputs in various fields`, () => {
       beforeEach(() => {
-        uploadListingComponent.uploadListingBeta('../../../resources/upload-listing-beta/2015_InvalidData.csv');
-        browser.waitUntil(() => toast.toastTitle.isDisplayed());
         hooks.open('#/administration/confirm/listings');
-      });
-
-      afterEach(() => {
-        toast.clearAllToast();
-        loginComponent.logOut();
       });
 
       it('should show all expected error messages on pending listing inspect', () => {
@@ -74,26 +73,17 @@ inputs.forEach((input) => {
         });
         expect(count).toBe(expectedWarnings.length);
       });
-    }
-  });
-});
-
-describe('User inspects uploaded listing with valid CHPL ID', () => {
-  if (process.env.ENV !== 'stage') {
-    beforeEach(() => {
-      uploadListingComponent.uploadListingBeta('../../../resources/listings/2015_v19_AQA4.csv');
-      browser.waitUntil(() => toast.toastTitle.isDisplayed());
-      hooks.open('#/administration/confirm/listings');
     });
+  });
 
-    afterEach(() => {
-      toast.clearAllToast();
-      loginComponent.logOut();
+  describe('User inspects uploaded listing with valid CHPL ID', () => {
+    beforeEach(() => {
+      hooks.open('#/administration/confirm/listings');
     });
 
     it('should not show any error messages', () => {
       confirmPage.gotoPendingListingPage(validListingId);
       expect(confirmPage.errorOnInspect.length).toBe(0);
     });
-  }
-});
+  });
+}
