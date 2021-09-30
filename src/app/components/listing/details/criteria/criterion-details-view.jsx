f5fe144158e092cd7033ba926043340b974b8389
Todd Young <tyoung@ainq.com>
2021-09-30 14:46:48 -0400

    feat!: Revise list of test tools to select for a 2015 Edition listing
    OCD-2973

--- src/app/components/listing/details/criteria/criterion-details-view.jsx
+++ src/app/components/listing/details/criteria/criterion-details-view.jsx
@@ -355,7 +355,7 @@ function ChplCriterionDetailsView(props) {
                           <li key={tt.id || tt.key || index}>
                             Tool:
                             {' '}
-                            { tt.testToolName }
+                            {`${tt.testToolName}${tt.retired ? ' (Retired)' : ''}`}
                             ; Version:
                             {' '}
                             { tt.testToolVersion || 'N/A' }
