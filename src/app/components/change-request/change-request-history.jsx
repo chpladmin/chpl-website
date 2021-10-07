897b5b4205a1f90330d1dd5dd223ddde21618b39
Andrew Larned <andrew.larned@gmail.com>
2021-10-07 09:42:43 -0400

    ui: clean up history table
    [#OCD-3740]

--- src/app/components/change-request/change-request-history.jsx
+++ src/app/components/change-request/change-request-history.jsx
@@ -1,62 +1,29 @@
 import React, { useState } from 'react';
 import {
-  Button,
-  ButtonGroup,
   Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableRow,
-  TablePagination,
   makeStyles,
 } from '@material-ui/core';
-import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
-import GetAppIcon from '@material-ui/icons/GetApp';
-import SettingsIcon from '@material-ui/icons/Settings';
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
-import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
 import { arrayOf } from 'prop-types';
 
+import { getAngularService } from '../../services/angular-react-helper';
 import { changeRequest } from '../../shared/prop-types';
 import ChplSortableHeaders  from '../util/chpl-sortable-headers';
 
 const useStyles = makeStyles({
-  container: {
-    display: "grid",
-    gridTemplateColumns: "1fr",
-    gridTemplateRows: "1fr",
-    gap:'16px',
-  },
-  stickyColumn:{
-    position: 'sticky',
-    left: 0,
-    boxShadow: 'rgba(149, 157, 165, 0.1) 0px 4px 8px',
-    backgroundColor:'#ffffff',
-  },
   tableContainer: {
-    maxHeight: "800px",
-    overflowWrap:'normal',
     border:'.5px solid #c2c6ca',
-  },
-  tableActionContainer: {
-    display: "grid",
-    justifyContent:'end',
-  },
-  widgetContainer:{
-    gap:'8px',
-    display:'grid',
-    alignContent:'space-between',
-  },
-  iconSpacing: {
-    marginLeft: '4px',
   },
 });
 
 function ChplChangeRequestHistory(props) {
-  const classes = useStyles();
+  const DateUtil = getAngularService('DateUtil');
   const [changeRequests, setChangeRequests] = useState(props.changeRequests);
+  const classes = useStyles();
 
   const headers = [
     { text: 'Acting Organization', property: 'actingOrganization', sortable: true },
@@ -65,7 +32,7 @@ function ChplChangeRequestHistory(props) {
     { text: 'Comments', property: 'comment' },
   ];
 
-  const listingSortComparator = (property) => {
+  const sortComparator = (property) => {
     let sortOrder = 1;
     let key = property;
     if (key[0] === '-') {
@@ -79,41 +46,31 @@ function ChplChangeRequestHistory(props) {
   };
 
   const handleTableSort = (event, property, orderDirection) => {
-    setListings(listings.sort(listingSortComparator(orderDirection + property)).map((listing) => listing));
+    setChangeRequests(changeRequests.sort(sortComparator(orderDirection + property)).map((cr) => cr));
   };
 
-
   return (
-    <div className={classes.container}>
-      <TableContainer>
-        <div className={classes.tableActionContainer}>
-          <Button fullWidth color="secondary" variant="contained">Download
-            <GetAppIcon className={classes.iconSpacing}/>
-          </Button>
-        </div>
-      </TableContainer>
+    <div>
       <TableContainer className={classes.tableContainer} component={Paper}>
         <Table stickyHeader>
           <ChplSortableHeaders
             headers={headers}
             onTableSort={handleTableSort}
+            orderBy="statusChangeDate"
           />
           <TableBody>
             {changeRequests
              .map((changeRequest) => (
                <TableRow key={changeRequest.id}>
                  <TableCell>{changeRequest.actingOrganization}</TableCell>
-                 <TableCell>{changeRequest.statusChangeDate}</TableCell>
+                 <TableCell>{DateUtil.timestampToString(changeRequest.statusChangeDate)}</TableCell>
+                 <TableCell>{changeRequest.changeRequestStatusType.name}</TableCell>
                  <TableCell>{changeRequest.comment}</TableCell>
                </TableRow>
              ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[50, 100, 200, { label: 'All' }]}
-        component="div"
-      />
     </div>
   );
 }
