ab139489945ec767d5d979969e06e7ab6d9fdcab
todd.young <tyoung@ainq.com>
2021-10-15 09:32:06 -0400

    refactor: clean up code
    OCD-3757

--- src/app/pages/reports/listings/listings.component.js
+++ src/app/pages/reports/listings/listings.component.js
@@ -706,17 +706,16 @@ const ReportsListingsComponent = {
       if (!curr.optionalStandards) {
         curr.optionalStandards = [];
       }
-      const addedOptionalStandards = curr.optionalStandards
-        .filter((currOS) => prev.optionalStandards.filter((prevOS) => currOS.citation === prevOS.citation).length === 0);
-      addedOptionalStandards.forEach((addedOS) => {
-        ret.push(`<li>Optional Standard "${addedOS.citation}: ${addedOS.description}" was added</li>`);
-      });
-      const removedOptionalStandards = prev.optionalStandards
-        .filter((prevOS) => curr.optionalStandards.filter((currOS) => prevOS.citation === currOS.citation).length === 0);
-      removedOptionalStandards.forEach((removedOS) => {
-        ret.push(`<li>Optional Standard "${removedOS.citation}: ${removedOS.description}" was removed</li>`);
-      });
-
+      curr.optionalStandards.filter((currOS) => prev.optionalStandards.filter((prevOS) => currOS.citation === prevOS.citation).length === 0)
+        .forEach((addedOS) => {
+          ret.push(`<li>Optional Standard "${addedOS.citation}: ${addedOS.description}" was added</li>`);
+        }
+        );
+      prev.optionalStandards.filter((prevOS) => curr.optionalStandards.filter((currOS) => prevOS.citation === currOS.citation).length === 0)
+        .forEach((removedOS) => {
+          ret.push(`<li>Optional Standard "${removedOS.citation}: ${removedOS.description}" was removed</li>`);
+        }
+        );
       return ret;
     }
 
