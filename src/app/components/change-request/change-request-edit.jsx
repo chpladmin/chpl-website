b55ed9bc99b784391b817f787fcedaa778987caf
Andrew Larned <andrew.larned@gmail.com>
2021-10-29 07:48:21 -0400

    Revert "refactor: inline the editing switch for CR types"
    [#OCD-3740]
    
    This reverts commit 2ee40ee0e4ac7c09d3605785481cefb8d336d5bc.

--- src/app/components/change-request/change-request-edit.jsx
+++ src/app/components/change-request/change-request-edit.jsx
@@ -64,6 +64,38 @@ const validationSchema = yup.object({
     .required('Change Request status is required'),
 });
 
+const getChangeRequestDetails = (cr, handleDispatch) => {
+  switch (cr.changeRequestType.name) {
+    case 'Developer Attestation Change Request':
+      return (
+        <ChplChangeRequestAttestationEdit
+          changeRequest={cr}
+          dispatch={handleDispatch}
+        />
+      );
+    case 'Developer Details Change Request':
+      return (
+        <ChplChangeRequestDetailsEdit
+          changeRequest={cr}
+          dispatch={handleDispatch}
+        />
+      );
+    case 'Website Change Request':
+      return (
+        <ChplChangeRequestWebsiteEdit
+          changeRequest={cr}
+          dispatch={handleDispatch}
+        />
+      );
+    default:
+      return (
+        <>
+          No details found
+        </>
+      );
+  }
+};
+
 function ChplChangeRequestEdit(props) {
   /* eslint-disable react/destructuring-assignment */
   const { hasAnyRole } = useContext(UserContext);
@@ -172,27 +204,7 @@ function ChplChangeRequestEdit(props) {
       <CardContent>
         <div className={classes.container}>
           <div>
-            { changeRequest.changeRequestType.name === 'Developer Attestation Change Request'
-              && (
-                <ChplChangeRequestAttestationEdit
-                  changeRequest={changeRequest}
-                  dispatch={handleDispatch}
-                />
-              )}
-            { changeRequest.changeRequestType.name === 'Developer Details Change Request'
-              && (
-                <ChplChangeRequestDetailsEdit
-                  changeRequest={changeRequest}
-                  dispatch={handleDispatch}
-                />
-              )}
-            { changeRequest.changeRequestType.name === 'Website Change Request'
-              && (
-                <ChplChangeRequestWebsiteEdit
-                  changeRequest={changeRequest}
-                  dispatch={handleDispatch}
-                />
-              )}
+            {getChangeRequestDetails(changeRequest, handleDispatch)}
           </div>
           <div className={classes.actionContainer}>
             <Divider className={classes.actionDivider} orientation="vertical" />
