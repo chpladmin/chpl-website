cb5bb189bc9ec476c70753c145cf021cbdca2c96
Andrew Larned <andrew.larned@gmail.com>
2021-11-01 07:29:29 -0400

    feat: allow removal of filter item
    [#OCD-3771]

--- src/app/pages/collections/real-world-testing/filter-context.jsx
+++ src/app/pages/collections/real-world-testing/filter-context.jsx
@@ -6,10 +6,25 @@ function FilterProvider(props) {
   const [filters, setFilters] = useState(props.filters);
 
   const dispatch = (action, category, value) => {
-    console.log({action, category, value});
+    switch (action) {
+      case 'delete':
+        const filter = filters.find((f) => f.key === category.key);
+        const updatedFilter = {
+          ...filter,
+          values: filter.values.filter((v) => v !== value),
+        }
+        const updated = filters.filter((f) => f.key !== category.key).concat(updatedFilter);
+        setFilters(updated);
+        break;
+      default:
+        console.log({action, category, value});
+    }
   };
 
-  const queryString = () => filters.map((f) => `${f.key}=${f.values.join(',')}`).join('&');
+  const queryString = () => filters
+        .filter((f) => f.values.length > 0)
+        .map((f) => `${f.key}=${f.values.join(',')}`)
+        .join('&');
 
   const filterData = { dispatch, filters, queryString };
 
