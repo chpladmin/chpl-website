3086b5fddd979bd5cba5502488aca7652f52407b
Andrew Larned <andrew.larned@gmail.com>
2021-11-12 10:22:21 -0500

    refactor: convert second AQA upload test to new format
    [#OCD-3780-k]

--- e2e/workflowTest/upload-confirm-listing-test/suites/index.js
+++ e2e/workflowTest/upload-confirm-listing-test/suites/index.js
@@ -1,10 +1,13 @@
 import columnsErrorsAndWarnings from './columns-errors-and-warnings';
+import invalidFormatsErrorsAndWarnings from './invalid-formats-errors-and-warnings';
 
 const suites = [
   columnsErrorsAndWarnings,
+  invalidFormatsErrorsAndWarnings,
 ];
 
 export {
   columnsErrorsAndWarnings,
+  invalidFormatsErrorsAndWarnings,
   suites,
 };
--- e2e/workflowTest/upload-confirm-listing-test/suites/invalid-formats-errors-and-warnings.js
+++ e2e/workflowTest/upload-confirm-listing-test/suites/invalid-formats-errors-and-warnings.js
@@ -1,5 +1,7 @@
-module.exports = [
-  {
+const suite = {
+  description: 'with invalid or missing data',
+  file: '../../../resources/upload-listing-beta/2015_InvalidAndMissingData.csv',
+  listings: [{
     listingId: '13A!.AA.04.8990.FMT.0A@.AA.A.20230729',
     expectedErrors: [
       'The version code is required and must be 2 characters in length containing only the characters A-Z, a-z, 0-9, and _.',
@@ -22,10 +24,9 @@ module.exports = [
       'An unrecognized character was found for ACB Certification ID',
       'An unrecognized character was found for ACB Name'
     ],
-  },
-  {
-      listingId: '15.02.04.2701.REQ1.12.01.1.200620',
-      expectedErrors: [
+  }, {
+    listingId: '15.02.04.2701.REQ1.12.01.1.200620',
+    expectedErrors: [
       'The unique id indicates the product does have additional software but none is specified in the upload file.',
       'The ICS value is \'true\' which means this listing has inherited properties. It is required that at least one parent from which the listing inherits be provided.',
       'The \'Accessibility Certified\' value is \'true\' but 0 Accessibility Standard(s) were found.',
@@ -45,8 +46,7 @@ module.exports = [
     ],
     expectedWarnings: [
     ],
-  },
-  {
+  }, {
     listingId: '15.02.04.2701.REQ2.12.00.0.200620',
     expectedErrors: [
       'There was no version found for test tool Edge Testing Tool and certification 170.315 (g)(6) (Cures Update).',
@@ -66,21 +66,21 @@ module.exports = [
       'Certification 170.315 (b)(1) (Cures Update) contains duplicate Test Data: Name \'ONC Test Method\'.',
       'Certification 170.315 (a)(1) contains duplicate Test Procedure: \'ONC Test Method\'.',
       '170.315 (g)(3) is required but was not found.',
-      
     ],
     expectedWarnings: [
       'Certification 170.315 (a)(1) contains duplicate Test Functionality: Number \'(a)(1)(ii)\'. The duplicates have been removed.',
       'Test data \'test data\' is invalid for certification 170.315 (b)(1) (Cures Update). ONC Test Method will be used instead.',
       'Certification 170.315 (d)(13) has a Use Case but no Attestation Answer.'
     ]
-  },
-  {
-      listingId: '15.02.04.2701.ICS1.12.00.0.200620',
-      expectedErrors: [
+  }, {
+    listingId: '15.02.04.2701.ICS1.12.00.0.200620',
+    expectedErrors: [
       'The unique id indicates the product does not have ICS but the value for Inherited Certification Status is true.',
       'No listing was found with the unique ID \'CHP-00000\'. ICS parent listings must reference existing listings in the CHPL.',
     ],
     expectedWarnings: [
-    ],
-  },  
-];
+    ]
+  }],
+};
+
+export default suite;
