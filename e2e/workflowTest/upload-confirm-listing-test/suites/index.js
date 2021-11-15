f123a1ba7022532502d36828952d6a1fc0eae328
Andrew Larned <andrew.larned@gmail.com>
2021-11-15 07:34:36 -0500

    fix: use matching case for filenames
    [#OCD-3780-k]

--- e2e/workflowTest/upload-confirm-listing-test/suites/index.js
+++ e2e/workflowTest/upload-confirm-listing-test/suites/index.js
@@ -2,26 +2,26 @@ import columnsErrorsAndWarnings from './columns-errors-and-warnings';
 import cqmsErrorsAndWarnings from './cqms-errors-and-warnings';
 import criteriaRelationshipsErrorsAndWarnings from './criteria-relationships-errors-and-warnings';
 import developerErrorsAndWarnings from './developer-errors-and-warnings';
+import icsErrors from './ics-errors';
 import invalidFormatsErrorsAndWarnings from './invalid-formats-errors-and-warnings';
 import measuresErrorsAndWarnings from './measures-errors-and-warnings';
+import noErrors from './no-errors';
 import optionalStandardsErrorsAndWarnings from './optional-standards-errors-and-warnings';
 import sedErrorsAndWarnings from './sed-errors-and-warnings';
-import icsErrors from './ics-errors';
-import svapErrorsAndWarnings from './svap-errors-and-warnings'
-import noErrors from './no-errors';
+import svapErrorsAndWarnings from './svap-errors-and-warnings';
 
 const suites = [
   columnsErrorsAndWarnings,
   cqmsErrorsAndWarnings,
   criteriaRelationshipsErrorsAndWarnings,
   developerErrorsAndWarnings,
+  icsErrors,
   invalidFormatsErrorsAndWarnings,
   measuresErrorsAndWarnings,
+  noErrors,
   optionalStandardsErrorsAndWarnings,
   sedErrorsAndWarnings,
-  icsErrors,
   svapErrorsAndWarnings,
-  noErrors,
 ];
 
 export {
@@ -29,12 +29,12 @@ export {
   cqmsErrorsAndWarnings,
   criteriaRelationshipsErrorsAndWarnings,
   developerErrorsAndWarnings,
+  icsErrors,
   invalidFormatsErrorsAndWarnings,
   measuresErrorsAndWarnings,
+  noErrors,
   optionalStandardsErrorsAndWarnings,
   sedErrorsAndWarnings,
-  icsErrors,
   svapErrorsAndWarnings,
-  noErrors,
   suites,
 };
