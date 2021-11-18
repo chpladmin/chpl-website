ef4bdbbf236563e5a6baa1bc595481cedeaaa1b1
todd.young <tyoung@ainq.com>
2021-11-18 15:51:34 -0500

    test: add tests to ensure criteria title is displayed correctly
    OCD-3151

--- e2e/pages/compare/compare.readonly.spec.js
+++ e2e/pages/compare/compare.readonly.spec.js
@@ -1,5 +1,6 @@
-import ComparePage from './compare.po';
 import Hooks from '../../utilities/hooks';
+
+import ComparePage from './compare.po';
 
 let hooks;
 let page;
@@ -16,6 +17,22 @@ describe('on compare page after clicking show all-', () => {
     page.certificationCriteriaLink.scrollIntoView();
     page.certificationCriteriaLink.click();
     expect(page.allCCCQM.length).toBe(78);
+  });
+
+  describe('when viewing all criteria', () => {
+    beforeEach(async () => {
+      page.checkShowAllCheckbox();
+      page.certificationCriteriaLink.scrollIntoView();
+      page.certificationCriteriaLink.click();
+    });
+
+    it('should correctly display the criteria number/title', () => {
+      expect(page.getCellWithCriteriaNumber('170.315 (a)(1)').getText()).toBe('170.315 (a)(1): Computerized Provider Order Entry (CPOE) - Medications');
+    });
+
+    it('should correctly display the retired criteria number/title', () => {
+      expect(page.getCellWithCriteriaNumber('170.315 (a)(10)').getText()).toContain('Removed | 170.315 (a)(10): Drug-Formulary and Preferred Drug List Checks');
+    });
   });
 
   it('should display all CQMs', () => {
