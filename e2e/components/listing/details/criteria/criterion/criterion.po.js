43592d755a41d91ff6bbfce9d7be03f9bcde13cd
todd.young <tyoung@ainq.com>
2021-11-29 17:24:27 -0500

    refactor: update identifiers used to element references
    OCD-3151

--- e2e/components/listing/details/criteria/criterion/criterion.po.js
+++ e2e/components/listing/details/criteria/criterion/criterion.po.js
@@ -8,15 +8,15 @@ class CriterionComponent {
   criterionHeader(criteriaNumber, id) {
     const flag = this.isUiUpgradeFlagOn();
     if (flag) {
-      return $(`//*[@id="criterion-id-${id}-header"]`).$('h6');
+      return $(`#criterion-id-${id}-header`).$('h6');
     }
     return $(`//*[@id="criteria_${criteriaNumber}_details_header"]`).$('.criteria-title');
   }
 
   isUiUpgradeFlagOn() {
-    const canaryElelment = $(this.elements.uiFlagOnElement);
-    if (canaryElelment?.isExisting) {
-      return !canaryElelment.isExisting();
+    const canaryElement = $(this.elements.uiFlagOnElement);
+    if (canaryElement?.isExisting) {
+      return !canaryElement.isExisting();
     }
     return false;
   }
