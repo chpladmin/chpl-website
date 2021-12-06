0b79866db74ee15fce85ae8f262a123317dcc2ed
Katy Ekey <katy.ekey@gmail.com>
2021-12-06 13:38:08 -0500

    test: Add additional removed criteria to test, remove warnings no longer expected in the result
    [#OCD-3260]

--- e2e/workflowTest/upload-confirm-listing-test/suites/removed-criteria-errors-and-warnings.js
+++ e2e/workflowTest/upload-confirm-listing-test/suites/removed-criteria-errors-and-warnings.js
@@ -7,15 +7,6 @@ const suite = {
       'The criterion 170.315 (a)(6) has been removed and may not be added to the listing.',
     ],
     expectedWarnings: [
-      'Certification criterion 170.315 (a)(*) was found so 170.315 (d)(3) or 170.315 (d)(3) (Cures Update) is required but was not found.',
-      'Certification criterion 170.315 (a)(*) was found so 170.315 (d)(6) is required but was not found.',
-      'Certification criterion 170.315 (a)(*) was found so 170.315 (d)(4) is required but was not found.',
-      'Certification criterion 170.315 (a)(*) was found so 170.315 (d)(7) is required but was not found.',
-      'Certification criterion 170.315 (a)(*) was found so 170.315 (d)(5) is required but was not found.',
-      'The value for Documentation Url in criteria 170.315 (a)(6) is not a valid URL.',
-      'Certification criterion 170.315 (a)(*) was found so 170.315 (d)(2) or 170.315 (d)(2) (Cures Update) is required but was not found.',
-      'The value for Use Cases in criteria 170.315 (a)(6) is not a valid URL.',
-      'Certification criterion 170.315 (a)(*) was found so 170.315 (d)(1) is required but was not found.',
       'Listing has not attested to (g)(3), but at least one criteria was found attesting to SED.'
     ],
   }],
