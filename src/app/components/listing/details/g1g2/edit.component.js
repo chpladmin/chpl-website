88b6232c562423062b84c56e53302dc49d5431be
todd.young <tyoung@ainq.com>
2021-12-17 11:38:41 -0500

    feat: undo change made to move where the 'join' is being done
    OCD-3799

--- src/app/components/listing/details/g1g2/edit.component.js
+++ src/app/components/listing/details/g1g2/edit.component.js
@@ -35,8 +35,7 @@ export const G1G2EditComponent = {
             }
             m.displayCriteria = [... new Set(m.allowedCriteria
               .sort((a, b) => this.util.sortCert(a) - this.util.sortCert(b))
-              .map(c => this.wrapCriterionDisplayInSpan(c)))]
-              .join('; ');
+              .map(c => this.wrapCriterionDisplayInSpan(c)))];
             return m;
           })
           .sort((a, b) => this.measureSort(a, b));
