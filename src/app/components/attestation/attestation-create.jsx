8e07cd0f008a0cc3e1e30c79f841f2b9043f5664
Matt Stankiewicz <mstankiewicz@ainq.com>
2022-01-20 10:40:33 -0500

    ui:format step 1 with gutters, cards & adjust typography

--- src/app/components/attestation/attestation-create.jsx
+++ src/app/components/attestation/attestation-create.jsx
@@ -1,9 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
 import {
   Button,
+  Card,
+  CardContent,
+  Container,
+  Divider,
   FormControl,
   FormControlLabel,
   FormLabel,
+  Link,
   Radio,
   RadioGroup,
   ThemeProvider,
@@ -46,6 +51,12 @@ const interpretLink = (question) => {
 const useStyles = makeStyles({
   iconSpacing: {
     marginLeft: '4px',
+  },
+  listingInspectHeader: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '8px 32px',
   },
 });
 
@@ -141,7 +152,7 @@ function ChplAttestationCreate(props) {
           setStage(3);
         } else {
           const body = error.response.data?.error
-                || error.response.data?.errorMessages.join(' ');
+            || error.response.data?.errorMessages.join(' ');
           toaster.pop({
             type: 'error',
             title: 'Error',
@@ -155,45 +166,45 @@ function ChplAttestationCreate(props) {
 
   return (
     <ThemeProvider theme={theme}>
-      <Typography variant="h1">
-        Submit Attestation
-      </Typography>
+        <div className={classes.listingInspectHeader}>
+          <Typography variant="h1">
+            Submit Attestation
+          </Typography>
+          <Typography variant="h2">
+            Introduction
+          </Typography>
+        </div>
       <ChplAttestationProgress
         dispatch={handleProgressDispatch}
         value={stage}
         canNext={canNext()}
         canPrevious={canPrevious()}
       />
-      { stage === 0
+      {stage === 0
         && (
           <>
-            <Typography variant="h2">
-              Introduction
-            </Typography>
-            <Typography variant="body1">
-              The Conditions and Maintenance of Certification requirements express initial and ongoing requirements that a health IT developer of certified Health IT and its certified Health IT Module(s) must meet or adhere to in order to maintain their certification status in the ONC Health IT Certification Program (Program).
-            </Typography>
-            <Typography variant="body1">
-              The Attestations Condition and Maintenance of Certification (&quot;Attestations&quot;) at &sect; 170.406 requires that a health IT developer of certified health IT, or its authorized representative that is capable of binding the health IT developer, must provide the Secretary of Health and Human Services an attestation of compliance with the following Conditions and Maintenance of Certification requirements. Attestations are submitted to ONC-Authorized Certification Bodies (ONC-ACBs) for review and subsequent submission to ONC for public availability.
-            </Typography>
-            <Typography variant="body1">
-              Please proceed to review the Attestations and indicate your (health IT developer’s) compliance, noncompliance, or the inapplicability of each Condition and Maintenance of Certification requirement. Note, per &sect; 170.580, under certain circumstances, ONC may directly review a health IT developer&apos;s actions or practices or its certified health IT to determine whether it conforms to the requirements of the Program. This may result in corrective action as necessary.  In addition, you may be subject to investigation by the HHS Office of the Inspector General for submitting a false attestation as specified in the 21st Century Cures Act.
-            </Typography>
-            <Typography variant="body1">
-              For assistance with the Attestations submissions process, please visit the Health IT Feedback and Inquiry Portal to submit a ticket as applicable.
-            </Typography>
-            <Typography variant="body1">
-              For questions regarding the Attestations Condition and Maintenance of Certification requirement, please select the &quot;Attestations Condition&quot; category.
-            </Typography>
-            <Typography variant="body1">
-              For questions regarding a Condition and Maintenance of Certification requirement other than Attestations, please select the relevant Condition category.
-            </Typography>
-            <Typography variant="body1">
-              For technical assistance with this process, please select the &quot;CHPL&quot; category.
-            </Typography>
+            <Container>
+              <Card>
+                <CardContent>
+                  <Typography gutterBottom variant="body1">
+                    The Conditions and Maintenance of Certification requirements express initial and ongoing requirements that a health IT developer of certified Health IT and its certified Health IT Module(s) must meet or adhere to in order to maintain their certification status in the ONC Health IT Certification Program (Program).
+                  </Typography>
+                  <Typography gutterBottom variant="body1">
+                    The Attestations Condition and Maintenance of Certification (&quot;Attestations&quot;) at &sect; 170.406 requires that a health IT developer of certified health IT, or its authorized representative that is capable of binding the health IT developer, must provide the Secretary of Health and Human Services an attestation of compliance with the following Conditions and Maintenance of Certification requirements. Attestations are submitted to ONC-Authorized Certification Bodies (ONC-ACBs) for review and subsequent submission to ONC for public availability.
+                  </Typography>
+                  <Divider />
+                  <Typography gutterBottom variant="body1">
+                    Please proceed to review the Attestations and indicate your (health IT developer’s) compliance, noncompliance, or the inapplicability of each Condition and Maintenance of Certification requirement. Note, per &sect; 170.580, under certain circumstances, ONC may directly review a health IT developer&apos;s actions or practices or its certified health IT to determine whether it conforms to the requirements of the Program. This may result in corrective action as necessary.  In addition, you may be subject to investigation by the HHS Office of the Inspector General for submitting a false attestation as specified in the 21st Century Cures Act.
+                  </Typography>
+                  <Typography variant="body2">
+                    For assistance with the Attestations submissions process, please visit the <Link>Health IT Feedback and Inquiry Portal</Link> to submit a ticket as applicable. For questions regarding the Attestations Condition and Maintenance of Certification requirement, please select the &quot;Attestations Condition&quot; category. For questions regarding a Condition and Maintenance of Certification requirement other than Attestations, please select the relevant Condition category. For technical assistance with this process, please select the &quot;CHPL&quot; category.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Container>
           </>
         )}
-      { stage === 1
+      {stage === 1
         && (
           <>
             <Typography variant="h2">
@@ -203,7 +214,7 @@ function ChplAttestationCreate(props) {
               .map((response) => (
                 <div key={response.category.id}>
                   <Typography variant="h3">
-                    { response.category.name }
+                    {response.category.name}
                   </Typography>
                   <FormControl key={response.question.id} component="fieldset">
                     <FormLabel>{response.question.display}</FormLabel>
@@ -227,79 +238,79 @@ function ChplAttestationCreate(props) {
               ))}
           </>
         )}
-      { stage === 2
+      {stage === 2
         && (
-        <>
-          <Typography variant="h2">
-            Electronic Signature
-          </Typography>
-          <Typography variant="body1">
-            As a health IT developer of certified health IT, or as an authorized representative that is capable of binding the health IT developer, I certify the Attestations to the Secretary of Health and Human Services provided here are true and correct to the best of my knowledge and belief.
-          </Typography>
-          <Typography variant="body1">
-            I understand that under certain circumstances ONC may directly review the health IT developer&apos;s actions or practices or its certified health IT to determine whether it conforms to the requirements of the Program. This may result in corrective action as necessary.
-          </Typography>
-          <Typography variant="body1">
-            I also understand that I and my company may be subject to investigation by the HHS Office of the Inspector General for submitting a false attestation as specified in the 21st Century Cures Act.
-          </Typography>
-          <Typography variant="body1">
-            Typing your name below signifies you are completing the Attestations using an electronic signature. To continue with the electronic signature process, please enter your name and click the &quot;Sign Electronically&quot; button to confirm and submit the Attestations to your ONC-Authorized Certification Body (ONC-ACB) for review.
-          </Typography>
-          <Typography variant="body1">
-            Name:
-            {' '}
-            { user.fullName }
-          </Typography>
-          <Typography variant="body1">
-            Title:
-            {' '}
-            { user.title }
-          </Typography>
-          <Typography variant="body1">
-            Health IT Developer:
-            {' '}
-            { developer.name }
-          </Typography>
-          <ChplTextField
-            id="signature"
-            name="signature"
-            label="Electronic Signature"
-            required
-            value={signature}
-            onChange={handleSignature}
-          />
-          <Typography variant="body1">
-            Date:
-            {' '}
-            <Moment
-              date={Date.now()}
-              format="DD MMM yyyy"
+          <>
+            <Typography variant="h2">
+              Electronic Signature
+            </Typography>
+            <Typography variant="body1">
+              As a health IT developer of certified health IT, or as an authorized representative that is capable of binding the health IT developer, I certify the Attestations to the Secretary of Health and Human Services provided here are true and correct to the best of my knowledge and belief.
+            </Typography>
+            <Typography variant="body1">
+              I understand that under certain circumstances ONC may directly review the health IT developer&apos;s actions or practices or its certified health IT to determine whether it conforms to the requirements of the Program. This may result in corrective action as necessary.
+            </Typography>
+            <Typography variant="body1">
+              I also understand that I and my company may be subject to investigation by the HHS Office of the Inspector General for submitting a false attestation as specified in the 21st Century Cures Act.
+            </Typography>
+            <Typography variant="body1">
+              Typing your name below signifies you are completing the Attestations using an electronic signature. To continue with the electronic signature process, please enter your name and click the &quot;Sign Electronically&quot; button to confirm and submit the Attestations to your ONC-Authorized Certification Body (ONC-ACB) for review.
+            </Typography>
+            <Typography variant="body1">
+              Name:
+              {' '}
+              {user.fullName}
+            </Typography>
+            <Typography variant="body1">
+              Title:
+              {' '}
+              {user.title}
+            </Typography>
+            <Typography variant="body1">
+              Health IT Developer:
+              {' '}
+              {developer.name}
+            </Typography>
+            <ChplTextField
+              id="signature"
+              name="signature"
+              label="Electronic Signature"
+              required
+              value={signature}
+              onChange={handleSignature}
             />
-          </Typography>
-          <Button
-            id="sign-electronically"
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-            disabled={isSubmitDisabled()}
-          >
-            Sign Electronically
-            <SaveIcon
-              className={classes.iconSpacing}
-            />
-          </Button>
-        </>
+            <Typography variant="body1">
+              Date:
+              {' '}
+              <Moment
+                date={Date.now()}
+                format="DD MMM yyyy"
+              />
+            </Typography>
+            <Button
+              id="sign-electronically"
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+              disabled={isSubmitDisabled()}
+            >
+              Sign Electronically
+              <SaveIcon
+                className={classes.iconSpacing}
+              />
+            </Button>
+          </>
         )}
-      { stage === 3
+      {stage === 3
         && (
-        <>
-          <Typography variant="h2">
-            Confirmation
-          </Typography>
-          <Typography variant="body1">
-            Thank you for your Attestations Condition and Maintenance of Certification submission for the ONC Health IT Certification Program. Please direct any inquiries regarding your submission to your ONC-Authorized Certification Body (ONC-ACB).
-          </Typography>
-        </>
+          <>
+            <Typography variant="h2">
+              Confirmation
+            </Typography>
+            <Typography variant="body1">
+              Thank you for your Attestations Condition and Maintenance of Certification submission for the ONC Health IT Certification Program. Please direct any inquiries regarding your submission to your ONC-Authorized Certification Body (ONC-ACB).
+            </Typography>
+          </>
         )}
       <ChplActionBar
         dispatch={handleActionBarDispatch}
