fccd8e2755d9ccdc511c30a1c241029114b7b30c
Andrew Larned <andrew.larned@gmail.com>
2022-01-21 07:10:05 -0500

    feat: display pending Attestation in CR view mode
    [#OCD-3717]

--- src/app/components/change-request/types/attestation-view.jsx
+++ src/app/components/change-request/types/attestation-view.jsx
@@ -4,29 +4,45 @@ import {
   makeStyles,
 } from '@material-ui/core';
 
-import { changeRequest as changeRequestProp } from '../../../shared/prop-types';
+import interpretLink from 'components/attestation/attestation-util';
+import { getAngularService } from 'services/angular-react-helper';
+import { changeRequest as changeRequestProp } from 'shared/prop-types';
 
 const useStyles = makeStyles({
   container: {
     display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
+    gridTemplateColumns: '1fr 3fr',
     gap: '8px',
   },
 });
 
 function ChplChangeRequestAttestationView(props) {
+  const DateUtil = getAngularService('DateUtil');
   const { changeRequest } = props;
   const classes = useStyles();
 
   return (
     <div className={classes.container}>
       <div>
-        <Typography gutterBottom variant="subtitle2">Current attestation</Typography>
-        None
+        <Typography gutterBottom variant="subtitle2">Attestation Period</Typography>
+        { DateUtil.getDisplayDateFormat(changeRequest.details.attestationPeriod.periodStart) }
+        {' '}
+        -
+        {' '}
+        { DateUtil.getDisplayDateFormat(changeRequest.details.attestationPeriod.periodEnd) }
       </div>
       <div>
-        <Typography gutterBottom variant="subtitle2">Submitted attestation</Typography>
-        {changeRequest.details.attestation}
+        <Typography gutterBottom variant="subtitle2">Submitted attestations</Typography>
+        <ul>
+          { changeRequest.details.responses.map((response) => (
+            <li key={response.id}>
+              { interpretLink(response.question).display }
+              :
+              {' '}
+              { response.answer.answer }
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
