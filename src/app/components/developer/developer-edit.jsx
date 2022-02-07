e5ac88e1960faff45426341e0cf45e7c866c0edd
Andrew Larned <andrew.larned@gmail.com>
2022-02-07 14:05:26 -0500

    feat: enable editing of Developer address
    [#OCD-3824]

--- src/app/components/developer/developer-edit.jsx
+++ src/app/components/developer/developer-edit.jsx
@@ -47,6 +47,27 @@ const validationSchema = yup.object({
     .email('Bad format')
     .required('Email is required')
     .max(3, 'Email is too long'),
+  line1: yup.string()
+    .required('Address is required')
+    .max(3, 'Address is too long'),
+  line2: yup.string()
+    .max(3, 'Line 2 is too long'),
+  city: yup.string()
+    .required('City is required')
+    .max(3, 'City is too long'),
+  state: yup.string()
+    .required('State is required')
+    .max(3, 'State is too long'),
+  zipcode: yup.string()
+    .required('Zip is required')
+    .max(3, 'Zip is too long'),
+  country: yup.string()
+    .required('Country is required')
+    .max(3, 'Country is too long'),
+  website: yup.string()
+    .url('Bad format')
+    .required('Website is required')
+    .max(3, 'Website is too long'),
 });
 
 function ChplDeveloperEdit(props) {
@@ -69,6 +90,15 @@ function ChplDeveloperEdit(props) {
         phoneNumber: formik.values.phoneNumber,
         email: formik.values.email,
       },
+      address: {
+        line1: formik.values.line1,
+        line2: formik.values.line2,
+        city: formik.values.city,
+        state: formik.values.state,
+        zipcode: formik.values.zipcode,
+        country: formik.values.country,
+      },
+      website: formik.values.website,
     };
     dispatch('save', updatedDeveloper);
   };
@@ -93,6 +123,13 @@ function ChplDeveloperEdit(props) {
       title: developer.contact?.title || '',
       phoneNumber: developer.contact?.phoneNumber || '',
       email: developer.contact?.email || '',
+      line1: developer.address?.line1 || '',
+      line2: developer.address?.line2 || '',
+      city: developer.address?.city || '',
+      state: developer.address?.state || '',
+      zipcode: developer.address?.zipcode || '',
+      country: developer.address?.country || '',
+      website: developer.website || '',
     },
     onSubmit: () => {
       save();
@@ -172,6 +209,82 @@ function ChplDeveloperEdit(props) {
             onBlur={formik.handleBlur}
             error={formik.touched.email && !!formik.errors.email}
             helperText={formik.touched.email && formik.errors.email}
+          />
+          <ChplTextField
+            id="line1"
+            name="line1"
+            label="Address"
+            required
+            value={formik.values.line1}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.line1 && !!formik.errors.line1}
+            helperText={formik.touched.line1 && formik.errors.line1}
+          />
+          <ChplTextField
+            id="line2"
+            name="line2"
+            label="Line 2"
+            value={formik.values.line2}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.line2 && !!formik.errors.line2}
+            helperText={formik.touched.line2 && formik.errors.line2}
+          />
+          <ChplTextField
+            id="city"
+            name="city"
+            label="City"
+            required
+            value={formik.values.city}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.city && !!formik.errors.city}
+            helperText={formik.touched.city && formik.errors.city}
+          />
+          <ChplTextField
+            id="state"
+            name="state"
+            label="State"
+            required
+            value={formik.values.state}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.state && !!formik.errors.state}
+            helperText={formik.touched.state && formik.errors.state}
+          />
+          <ChplTextField
+            id="zipcode"
+            name="zipcode"
+            label="Zip"
+            required
+            value={formik.values.zipcode}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.zipcode && !!formik.errors.zipcode}
+            helperText={formik.touched.zipcode && formik.errors.zipcode}
+          />
+          <ChplTextField
+            id="country"
+            name="country"
+            label="Country"
+            required
+            value={formik.values.country}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.country && !!formik.errors.country}
+            helperText={formik.touched.country && formik.errors.country}
+          />
+          <ChplTextField
+            id="website"
+            name="website"
+            label="Website"
+            required
+            value={formik.values.website}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.website && !!formik.errors.website}
+            helperText={formik.touched.website && formik.errors.website}
           />
         </CardContent>
       </Card>
