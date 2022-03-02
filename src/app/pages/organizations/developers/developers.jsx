f6005c5ce1b2525aced9a1a5682add30926e5720
Andrew Larned <andrew.larned@gmail.com>
2022-03-02 12:57:25 -0500

    ui: show Developer table on load
    [#OCD-3880]

--- src/app/pages/organizations/developers/developers.jsx
+++ src/app/pages/organizations/developers/developers.jsx
@@ -1,42 +1,74 @@
-import React, { useContext, useEffect, useState } from 'react';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import React, { useEffect, useState } from 'react';
 import {
-  arrayOf, func, string,
-} from 'prop-types';
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import { useFetchDevelopers } from 'api/developer';
 import { ChplTextField } from 'components/util';
+import { getAngularService } from 'services/angular-react-helper';
 
-function ChplDevelopers(props) {
-  const {data, isLoading} = useFetchDevelopers();
-  const [developerToLoad, setDeveloperToLoad] = useState(null);
+function ChplDevelopers() {
+  const $state = getAngularService('$state');
+  const DateUtil = getAngularService('DateUtil');
+  const { data, isLoading } = useFetchDevelopers();
+  const [developers, setDevelopers] = useState([]);
   const [developerValueToLoad, setDeveloperValueToLoad] = useState('');
 
-  const goToDeveloper = (event, newValue) => {
-    console.log({event, newValue});
+  useEffect(() => {
+    if (isLoading) { return; }
+    setDevelopers(data.sort((a, b) => (a.name < b.name ? -1 : 1)));
+  }, [data, isLoading]);
+
+  const goToDeveloper = (_, { developerId }) => {
+    $state.go('.developer', { developerId });
   };
 
   return (
     <>
-      { !isLoading
+      { /* eslint-disable react/jsx-props-no-spreading */ }
+      <Autocomplete
+        id="developers"
+        name="developers"
+        options={developers}
+        onChange={goToDeveloper}
+        inputValue={developerValueToLoad}
+        onInputChange={(event, newValue) => {
+          setDeveloperValueToLoad(newValue);
+        }}
+        getOptionLabel={(item) => item.name}
+        renderInput={(params) => <ChplTextField {...params} label="Choose Developer" />}
+      />
+      { /* eslint-enable react/jsx-props-no-spreading */ }
+      { developers.length > 0
         && (
-          <>
-              { /* eslint-disable react/jsx-props-no-spreading */ }
-              <Autocomplete
-                id="developers"
-                name="developers"
-                options={data}
-                value={developerToLoad}
-                onChange={goToDeveloper}
-                inputValue={developerValueToLoad}
-                onInputChange={(event, newValue) => {
-                  setDeveloperValueToLoad(newValue);
-                }}
-                getOptionLabel={(item) => item.name}
-                renderInput={(params) => <ChplTextField {...params} label="Choose Developer" />}
-              />
-          { /* eslint-enable react/jsx-props-no-spreading */ }
-          </>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Code</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Status</TableCell>
+                  <TableCell>Last modified Date</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {developers.map((developer) => (
+                  <TableRow key={developer.developerId}>
+                    <TableCell>{developer.developerCode}</TableCell>
+                    <TableCell>{developer.name}</TableCell>
+                    <TableCell>{developer.status.status}</TableCell>
+                    <TableCell>{DateUtil.getDisplayDateFormat(parseInt(developer.lastModifiedDate, 10))}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         )}
     </>
   );
