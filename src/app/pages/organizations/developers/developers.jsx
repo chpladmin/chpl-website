d9c00b3c9ae02177eca586b5a64f12cfeeac97a9
mattystank <mstankiewicz@ainq.com>
2022-03-15 14:03:28 -0400

    ui: correct padding & formatting on developer search page. Add styling in theme file for autocomplete

--- src/app/pages/organizations/developers/developers.jsx
+++ src/app/pages/organizations/developers/developers.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {
+  Card,
+  CardContent,
   Table,
   TableBody,
   TableCell,
@@ -7,6 +9,8 @@ import {
   TableHead,
   TableRow,
   Typography,
+  ThemeProvider,
+  makeStyles,
 } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
@@ -14,13 +18,23 @@ import { useFetchDevelopers } from 'api/developer';
 import { ChplTextField } from 'components/util';
 import { getAngularService } from 'services/angular-react-helper';
 
+import { mergeClasses } from '@material-ui/styles';
+
+import theme from 'themes/theme';
+
+const useStyles = makeStyles({
+  headingPadding: {
+    padding: '16px 0',
+  },
+});
+
 function ChplDevelopers() {
   const $state = getAngularService('$state');
   const DateUtil = getAngularService('DateUtil');
   const { data, isLoading } = useFetchDevelopers();
   const [developers, setDevelopers] = useState([]);
   const [developerValueToLoad, setDeveloperValueToLoad] = useState('');
-
+  const classes = useStyles();
   useEffect(() => {
     if (isLoading) { return; }
     setDevelopers(data.sort((a, b) => (a.name < b.name ? -1 : 1)));
@@ -32,48 +46,59 @@ function ChplDevelopers() {
 
   return (
     <>
-      <Typography variant="h1">
-        View Developers
-      </Typography>
-      { /* eslint-disable react/jsx-props-no-spreading */ }
-      <Autocomplete
-        id="developers"
-        name="developers"
-        options={developers}
-        onChange={goToDeveloper}
-        inputValue={developerValueToLoad}
-        onInputChange={(event, newValue) => {
-          setDeveloperValueToLoad(newValue);
-        }}
-        getOptionLabel={(item) => `${item.name} (${item.developerCode})`}
-        renderInput={(params) => <ChplTextField {...params} label="Choose Developer" />}
-      />
-      { /* eslint-enable react/jsx-props-no-spreading */ }
-      { developers.length > 0
-        && (
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Code</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Status</TableCell>
-                  <TableCell>Last modified Date</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {developers.map((developer) => (
-                  <TableRow key={developer.developerId}>
-                    <TableCell>{developer.developerCode}</TableCell>
-                    <TableCell>{developer.name}</TableCell>
-                    <TableCell>{developer.status.status}</TableCell>
-                    <TableCell>{DateUtil.getDisplayDateFormat(parseInt(developer.lastModifiedDate, 10))}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
+      <ThemeProvider theme={theme}>
+        <Typography className={classes.headingPadding} variant="h1">
+          View Developers
+        </Typography>
+        { /* eslint-disable react/jsx-props-no-spreading */}
+        <Card>
+          <CardContent>
+            <Autocomplete
+              id="developers"
+              name="developers"
+              options={developers}
+              onChange={goToDeveloper}
+              inputValue={developerValueToLoad}
+              onInputChange={(event, newValue) => {
+                setDeveloperValueToLoad(newValue);
+              }}
+              getOptionLabel={(item) => `${item.name} (${item.developerCode})`}
+              renderInput={(params) => <ChplTextField {...params} label="Choose Developer" />}
+            />
+          </CardContent>
+        </Card>
+        <br />
+        { /* eslint-enable react/jsx-props-no-spreading */}
+        {developers.length > 0
+          && (
+            <Card>
+              <CardContent>
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Code</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Status</TableCell>
+                        <TableCell>Last modified Date</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {developers.map((developer) => (
+                        <TableRow key={developer.developerId}>
+                          <TableCell>{developer.developerCode}</TableCell>
+                          <TableCell>{developer.name}</TableCell>
+                          <TableCell>{developer.status.status}</TableCell>
+                          <TableCell>{DateUtil.getDisplayDateFormat(parseInt(developer.lastModifiedDate, 10))}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </CardContent>
+            </Card>
+          )}
+      </ThemeProvider>
     </>
   );
 }
