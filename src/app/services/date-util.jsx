d62b6a9356a325565b062858023dd052c2fd4340
Andrew Larned <andrew.larned@gmail.com>
2022-03-29 16:32:24 -0400

    feat: parse Announcement activity "generically"
    [#OCD-3855]

--- src/app/services/date-util.jsx
+++ src/app/services/date-util.jsx
@@ -1,3 +1,30 @@
+import '../../../node_modules/@js-joda/timezone';
+import { Locale } from '../../../node_modules/@js-joda/locale_en-us';
 import * as jsJoda from '../../../node_modules/@js-joda/core';
 
-export { jsJoda };
+const isLocalDateTime = (dateTimeToTest) => {
+  try {
+    jsJoda.LocalDateTime.parse(dateTimeToTest);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+const getDisplayDateFormat = (date) => {
+  const formatter = jsJoda.DateTimeFormatter.ofPattern('MMM d, y h:mm:ss a').withLocale(Locale.US);
+  if (typeof (date) === 'number') {
+    return jsJoda.ZonedDateTime
+      .ofInstant(jsJoda.Instant.ofEpochMilli(date),
+                 jsJoda.ZoneId.of('America/New_York'))
+      .format(formatter);
+  }
+  if (typeof (date) === 'string' && isLocalDateTime(date)) {
+    return jsJoda.LocalDateTime.parse(date).format(formatter);
+  }
+};
+
+export {
+  getDisplayDateFormat,
+  jsJoda,
+};
