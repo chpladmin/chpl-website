6019e079607fe3e4758f6cb94c9d0bb43baff39a
Andrew Larned <andrew.larned@gmail.com>
2022-03-30 12:38:17 -0400

    ui: add component to display announcements
    [#OCD-3855]

---
+++ src/app/components/announcement/announcements-display.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+import { useFetchAnnouncements } from 'api/announcements';
+
+function ChplAnnouncementsDisplay() {
+  const { data, isLoading, isSuccess } = useFetchAnnouncements({ getFuture: false });
+  const [announcements, setAnnouncements] = useState([]);
+
+  useEffect(() => {
+    if (isLoading || !isSuccess) {
+      return;
+    }
+    setAnnouncements(data.sort((a, b) => (a.startDateTime < b.startDateTime ? -1 : 1)));
+  }, [data, isLoading, isSuccess]);
+
+  if (announcements.length === 0) { return null; }
+
+  return (
+    <>
+      { announcements
+        .map((item) => (
+          <p key={item.id} className="announcement nav-text spaced-out badge">
+            { item.title }
+            {': '}
+            { item.text }
+          </p>
+        ))}
+    </>
+  );
+}
+
+export default ChplAnnouncementsDisplay;
+
+ChplAnnouncementsDisplay.propTypes = {
+};
--- src/app/components/announcement/index.js
+++ src/app/components/announcement/index.js
@@ -1,9 +1,11 @@
 import ChplAnnouncementEdit from './announcement-edit';
 import ChplAnnouncements from './announcements';
+import ChplAnnouncementsDisplay from './announcements-display';
 import ChplAnnouncementsWrapper from './announcements-wrapper';
 
 export {
   ChplAnnouncementEdit,
   ChplAnnouncements,
+  ChplAnnouncementsDisplay,
   ChplAnnouncementsWrapper,
 };
