9a76c2bca86dcc51cc740c75fe6a4b420ec88aa4
Andrew Larned <andrew.larned@gmail.com>
2022-04-22 10:22:22 -0400

    test: expect rejection with legacy UI to work
    [#OCD-3923]

--- e2e/workflowTest/upload-reject-listing-test/reject-listing-onInspect.spec.js
+++ e2e/workflowTest/upload-reject-listing-test/reject-listing-onInspect.spec.js
@@ -1,4 +1,4 @@
-import Upload from '../../pages/administration/upload/upload.po';
+import Upload from '../../components/upload/upload-listing/upload-listing.po';
 import Confirm from '../../pages/administration/confirm/confirm.po';
 import LoginComponent from '../../components/login/login.po';
 import Hooks from '../../utilities/hooks';
@@ -9,37 +9,26 @@ let loginComponent;
 let upload;
 const listingId = '15.04.04.1722.AQA3.03.01.1.200620';
 
-beforeAll(() => {
+beforeEach(() => {
   upload = new Upload();
   confirm = new Confirm();
   loginComponent = new LoginComponent();
   hooks = new Hooks();
   hooks.open('#/administration/upload');
   loginComponent.logIn('acb');
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
   upload.uploadListing('../../../resources/listings/2015_v19_AQA3.csv', true);
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
   hooks.open('#/administration/confirm/listings');
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
   browser.waitUntil(() => confirm.isLoaded());
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
 });
 
 describe('when user rejects a listing while inspecting uploaded listing', () => {
   it('should allow listing to get rejected', () => {
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
     confirm.gotoPendingListingPage(listingId, true);
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
     hooks.waitForSpinnerToDisappear();
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
     confirm.rejectButtonOnInspectListing.click();
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
     confirm.yesConfirmation.click();
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
     browser.waitUntil(() => !confirm.inspectLabel.isDisplayed());
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
     hooks.waitForSpinnerToDisappear();
-browser.saveScreenshot(`test_reports/e2e/screenshot/${Date.now()}.png`);
     expect(confirm.findListingToReject(listingId).isDisplayed()).toBe(false);
   });
 });
