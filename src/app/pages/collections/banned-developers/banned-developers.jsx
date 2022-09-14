75d85c51e3a73774fc2ee1aa898b25ec6a28f92b
Andrew Larned <andrew.larned@gmail.com>
2022-09-14 10:59:29 -0400

    ui!: Enable filtering by ONC-ACB on new-style Shortcut pages
    [#OCD-4017]

--- src/app/pages/collections/banned-developers/banned-developers.jsx
+++ src/app/pages/collections/banned-developers/banned-developers.jsx
@@ -30,6 +30,19 @@ function ChplBannedDevelopersCollectionPage() {
       .join('&'),
     getValueDisplay: getDateDisplay,
     getValueEntry: getDateEntry,
+  }, {
+    ...defaultFilter,
+    key: 'certificationBodies',
+    display: 'ONC-ACB',
+    values: [
+      { value: 'CCHIT', display: 'CCHIT (Retired)' },
+      { value: 'Drummond Group', default: true },
+      { value: 'ICSA Labs', default: true },
+      { value: 'Leidos', default: true },
+      { value: 'SLI Compliance', default: true },
+      { value: 'Surescripts LLC', display: 'Surescripts LLC (Retired)' },
+      { value: 'UL LLC', display: 'UL LLC (Retired)' },
+    ],
   }];
 
   return (
