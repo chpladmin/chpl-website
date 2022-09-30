5f9a954dffe370469f5a5c2bb470253f966b941b
Ashwini More <amore@ainq.com>
2022-09-30 14:26:23 -0400

    test: update test data in upload csvs to use current certification date and include P&S criteria when required as per the Cures rule

--- e2e/workflowTest/upload-confirm-listing-test/suites/cqms-errors-and-warnings.js
+++ e2e/workflowTest/upload-confirm-listing-test/suites/cqms-errors-and-warnings.js
@@ -2,7 +2,7 @@ const suite = {
   description: 'with issues with CQMs',
   file: '../../../resources/listings/2015_CQMs.csv',
   listings: [{
-    listingId: '15.02.04.2701.CQM1.12.00.1.200620',
+    listingId: '15.02.04.2701.CQM1.12.00.1.220920',
     expectedErrors: [
       'The CQM with CMS ID \'CMS2\' does not specify a version. A version is required.',
       'The CQM with CMS ID \'CMS72\' has an invalid version \'v19\'.',
@@ -15,7 +15,7 @@ const suite = {
       'CMS ID \'CMStest\' is not valid. The CQM has been removed.',
     ],
   }, {
-    listingId: '15.02.04.2701.CQM2.12.00.1.200620',
+    listingId: '15.02.04.2701.CQM2.12.00.1.220920',
     expectedErrors: [
       'Certification criterion \'170.315 (c)(1)\' was found but no matching Clinical Quality Measurement was found.',
       'Certification criterion \'170.315 (c)(2)\' was found but no matching Clinical Quality Measurement was found.',
@@ -25,7 +25,7 @@ const suite = {
     expectedWarnings: [
     ],
   }, {
-    listingId: '15.02.04.2701.CQM3.12.00.1.200620',
+    listingId: '15.02.04.2701.CQM3.12.00.1.220920',
     expectedErrors: [
       'Certification criterion \'170.315 (c)(3) (Cures Update)\' was found but no matching Clinical Quality Measurement was found.',
     ],
