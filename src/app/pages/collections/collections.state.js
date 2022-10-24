242803ad99c5070661ea9dcac0c3d492f3c84136
Andrew Larned <andrew.larned@gmail.com>
2022-10-24 14:16:26 -0400

    refactor: incorporate hybrid-router to start towards React UI-Router
    [#OCD-3837]

--- src/app/pages/collections/collections.state.js
+++ src/app/pages/collections/collections.state.js
@@ -1,3 +1,8 @@
+import ChplApiDocumentationCollectionWrapper from './api-documentation/api-documentation-wrapper';
+import ChplBannedDevelopersCollectionPage from './banned-developers/banned-developers';
+import ChplDecertifiedProductsCollectionWrapper from './decertified-products/decertified-products-wrapper';
+import ChplRealWorldTestingCollectionWrapper from './real-world-testing/real-world-testing-wrapper';
+
 /* eslint-disable global-require */
 const states = [
   {
@@ -8,7 +13,7 @@ const states = [
   }, {
     name: 'collections.api-documentation',
     url: '/api-documentation',
-    component: 'chplApiDocumentationCollectionWrapperBridge',
+    component: ChplApiDocumentationCollectionWrapper,
     data: { title: 'API Information for 2015 Edition Products' },
   }, {
     name: 'collections.corrective-action',
@@ -20,7 +25,7 @@ const states = [
   }, {
     name: 'collections.developers',
     url: '/developers',
-    component: 'chplBannedDevelopersCollectionPageBridge',
+    component: ChplBannedDevelopersCollectionPage,
     data: { title: 'Banned Developers' },
   }, {
     name: 'collections.inactive',
@@ -32,7 +37,7 @@ const states = [
   }, {
     name: 'collections.decertified-products',
     url: '/products',
-    component: 'chplDecertifiedProductsCollectionWrapperBridge',
+    component: ChplDecertifiedProductsCollectionWrapper,
     data: { title: 'Decertified Products' },
   }, {
     name: 'collections.sed',
@@ -44,7 +49,7 @@ const states = [
   }, {
     name: 'collections.real-world-testing',
     url: '/real-world-testing',
-    component: 'chplRealWorldTestingCollectionWrapperBridge',
+    component: ChplRealWorldTestingCollectionWrapper,
     data: { title: 'Real World Testing' },
   },
 ];
