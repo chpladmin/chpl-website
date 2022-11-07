1f9b34f8228911334f9082376483d9923120ad67
Andrew Larned <andrew.larned@gmail.com>
2022-11-07 09:59:43 -0500

    fix[aqa]: expect complaints tests to work
    [#OCD-4013]

--- e2e/pages/administration/complaints/edit-complaint.spec.js
+++ e2e/pages/administration/complaints/edit-complaint.spec.js
@@ -68,8 +68,8 @@ describe('when editing complaints', () => {
       await complaintsComponent.setActions(`Actions - ${timestamp}`);
       await complaintsComponent.saveComplaint();
       await (browser.waitUntil(async () => complaintsComponent.hasResults()));
+      await complaintsComponent.clearSearchTerm();
       await complaintsComponent.searchFilter(fields.acbId);
-      await (browser.waitUntil(async () => (await complaintsComponent.getResults()).length === 1));
       await (browser.waitUntil(async () => await (await complaintsComponent.getComplaintCell((await complaintsComponent.getTableComplaints())[0], STATUS_IDX)).getText() === 'CLOSED'));
       [complaint] = (await complaintsComponent.getTableComplaints());
       await expect(await (await complaintsComponent.getComplaintCell(complaint, STATUS_IDX)).getText()).toBe('CLOSED');
