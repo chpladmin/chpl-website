ffd406d6b9402f79b0698f1a0c78568c9fe68526
Andrew Larned <andrew.larned@gmail.com>
2022-11-29 13:39:50 -0500

    feat!: Manage "Accessibility Standards" in updated UI/UX
    [#OCD-4040]

---
+++ src/app/components/standards/accessibility-standard/accessibility-standards.jsx
@@ -0,0 +1,171 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CircularProgress,
+} from '@material-ui/core';
+import { useSnackbar } from 'notistack';
+
+import ChplAccessibilityStandardEdit from './accessibility-standard-edit';
+import ChplAccessibilityStandardsView from './accessibility-standards-view';
+
+import {
+  useDeleteAccessibilityStandard,
+  useFetchAccessibilityStandards,
+  usePostAccessibilityStandard,
+  usePutAccessibilityStandard,
+} from 'api/standards';
+import { BreadcrumbContext } from 'shared/contexts';
+
+function ChplAccessibilityStandards() {
+  const { append, display, hide } = useContext(BreadcrumbContext);
+  const { data, isLoading, isSuccess } = useFetchAccessibilityStandards();
+  const deleteAccessibilityStandard = useDeleteAccessibilityStandard();
+  const postAccessibilityStandard = usePostAccessibilityStandard();
+  const putAccessibilityStandard = usePutAccessibilityStandard();
+  const { enqueueSnackbar } = useSnackbar();
+  const [activeAccessibilityStandard, setActiveAccessibilityStandard] = useState(undefined);
+  const [errors, setErrors] = useState([]);
+  const [accessibilityStandards, setAccessibilityStandards] = useState([]);
+  let handleDispatch;
+
+  useEffect(() => {
+    append(
+      <Button
+        key="accessibilityStandards.viewall.disabled"
+        depth={1}
+        variant="text"
+        disabled
+      >
+        Accessibility Standard Maintenance
+      </Button>,
+    );
+    append(
+      <Button
+        key="accessibilityStandards.viewall"
+        depth={1}
+        variant="text"
+        onClick={() => handleDispatch({ action: 'cancel' })}
+      >
+        Accessibility Standard Maintenance
+      </Button>,
+    );
+    display('accessibilityStandards.viewall.disabled');
+  }, []);
+
+  useEffect(() => {
+    if (isLoading || !isSuccess) { return; }
+    setAccessibilityStandards(data);
+  }, [data, isLoading, isSuccess]);
+
+  handleDispatch = ({ action, payload }) => {
+    switch (action) {
+      case 'cancel':
+        setActiveAccessibilityStandard(undefined);
+        display('accessibilityStandards.viewall.disabled');
+        hide('accessibilityStandards.viewall');
+        hide('accessibilityStandards.add.disabled');
+        hide('accessibilityStandards.edit.disabled');
+        break;
+      case 'delete':
+        setErrors([]);
+        deleteAccessibilityStandard.mutate(payload, {
+          onSuccess: () => {
+            enqueueSnackbar('Accessibility Standard Deleted', {
+              variant: 'success',
+            });
+            setActiveAccessibilityStandard(undefined);
+            display('accessibilityStandards.viewall.disabled');
+            hide('accessibilityStandards.viewall');
+          },
+          onError: (error) => {
+            setErrors(error.response.data.errorMessages);
+          },
+        });
+        break;
+      case 'edit':
+        setActiveAccessibilityStandard(payload);
+        setErrors([]);
+        display('accessibilityStandards.viewall');
+        hide('accessibilityStandards.viewall.disabled');
+        break;
+      case 'save':
+        setErrors([]);
+        if (payload.id) {
+          putAccessibilityStandard.mutate(payload, {
+            onSuccess: () => {
+              enqueueSnackbar('Accessibility Standard Updated', {
+                variant: 'success',
+              });
+              setActiveAccessibilityStandard(undefined);
+              display('accessibilityStandards.viewall.disabled');
+              hide('accessibilityStandards.viewall');
+            },
+            onError: (error) => {
+              setErrors(error.response.data.errorMessages);
+            },
+          });
+        } else {
+          postAccessibilityStandard.mutate(payload, {
+            onSuccess: () => {
+              enqueueSnackbar('Accessibility Standard Created', {
+                variant: 'success',
+              });
+              setActiveAccessibilityStandard(undefined);
+              display('accessibilityStandards.viewall.disabled');
+              hide('accessibilityStandards.viewall');
+            },
+            onError: (error) => {
+              setErrors(error.response.data?.errorMessages);
+            },
+          });
+        }
+        break;
+        // no default
+    }
+  };
+
+  if (activeAccessibilityStandard) {
+    return (
+      <Card>
+        <CardHeader title={`${activeAccessibilityStandard.id ? 'Edit' : 'Add'} Accessibility Standard`} />
+        <CardContent>
+          <ChplAccessibilityStandardEdit
+            accessibilityStandard={activeAccessibilityStandard}
+            dispatch={handleDispatch}
+            errors={errors}
+          />
+        </CardContent>
+      </Card>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <CircularProgress />
+    );
+  }
+
+  return (
+    <Card>
+      <CardHeader title="Accessibility Standard Maintenance" />
+      <CardContent>
+        { (deleteAccessibilityStandard.isLoading || postAccessibilityStandard.isLoading || putAccessibilityStandard.isLoading)
+          && (
+            <CircularProgress />
+          )}
+        <ChplAccessibilityStandardsView
+          accessibilityStandards={accessibilityStandards}
+          dispatch={handleDispatch}
+        />
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ChplAccessibilityStandards;
+
+ChplAccessibilityStandards.propTypes = {
+};
