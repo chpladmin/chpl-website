155f852d3dfc0fb673ce923936341a613b009e2c
Andrew Larned <andrew.larned@gmail.com>
2022-12-01 12:00:42 -0500

    test[aqa]: expect CAP page to have relevant functionality
    [#OCD-3839]

---
+++ e2e/pages/collections/corrective-action/corrective-action.po.js
@@ -0,0 +1,19 @@
+import { open as openPage } from '../../../utilities/hooks.async';
+import CollectionPage from '../collection.po';
+
+class CorrectiveActionPage extends CollectionPage {
+  constructor() {
+    super();
+    this.elements = {
+      ...this.elements,
+      header: 'h1=Products: Corrective Action Status',
+    };
+  }
+
+  async open() {
+    await openPage('#/collections/corrective-action');
+    await (browser.waitUntil(async () => !(await this.isLoading())));
+  }
+}
+
+export default CorrectiveActionPage;
--- e2e/pages/collections/corrective-action/corrective-action.readonly.spec.js
+++ e2e/pages/collections/corrective-action/corrective-action.readonly.spec.js
@@ -1,130 +1,82 @@
-import CollectionsPage from '../collections.po';
-import Hooks from '../../../utilities/hooks';
+import CorrectiveActionPage from './corrective-action.po';
 
-let hooks; let page;
+let page;
 
 describe('the Corrective Action collection page', () => {
   beforeEach(async () => {
-    page = new CollectionsPage();
-    hooks = new Hooks();
-    hooks.open('#/collections/corrective-action');
-    await hooks.waitForSpinnerToDisappear();
+    page = new CorrectiveActionPage();
+    await page.open();
   });
 
-  it('should have body text', () => {
-    expect(page.bodyText.getText()).toContain('This is a list of all health IT products for which a non-conformity has been recorded. A certified product is non-conforming if, at any time, an ONC-Authorized Certification Body (ONC-ACB) or ONC determines that the product does not comply with a requirement of certification. Non-conformities reported as part of surveillance are noted as "Surveillance NCs", while non-conformities identified though an ONC Direct Review are noted as "Direct Review NCs". Not all non-conformities affect a product\'s functionality, and the existence of a non-conformity does not by itself mean that a product is "defective." Developers of certified products are required to notify customers of non-conformities and must take approved corrective actions to address such non-conformities in a timely and effective manner. Detailed information about non-conformities, and associated corrective action plans, can be accessed below by clicking on the product\'s CHPL ID.\nPlease note that by default, only listings that are active or suspended are shown in the search results.');
+  it('should have body text', async () => {
+    await expect(await page.getBodyText()).toContain('This is a list of all health IT products for which a non-conformity has been recorded. A certified product is non-conforming if, at any time, an ONC-Authorized Certification Body (ONC-ACB) or ONC determines that the product does not comply with a requirement of certification. Non-conformities reported as part of surveillance are noted as "Surveillance NCs", while non-conformities identified though an ONC Direct Review are noted as "Direct Review NCs". Not all non-conformities affect a product\'s functionality, and the existence of a non-conformity does not by itself mean that a product is "defective." Developers of certified products are required to notify customers of non-conformities and must take approved corrective actions to address such non-conformities in a timely and effective manner. Detailed information about non-conformities, and associated corrective action plans, can be accessed below by clicking on the product\'s CHPL ID.');
+    await expect(await page.getBodyText()).toContain('Please note that by default, only listings that are active or suspended are shown in the search results.');
   });
 
-  it('should have table headers in a defined order', () => {
-    const expectedHeaders = ['Edition', 'Developer', 'Product', 'Version', 'CHPL ID', 'ONC-ACB', '# Open Surveillance NCs', '# Closed Surveillance NCs', '# Open Direct Review NCs', '# Closed Direct Review NCs'];
-    const actualHeaders = page.getListingTableHeaders();
-    expect(actualHeaders.length).toBe(expectedHeaders.length, 'Found incorrect number of columns');
-    actualHeaders.forEach((header, idx) => {
-      expect(header.getText()).toBe(expectedHeaders[idx]);
-    });
+  it('should have table headers in a defined order', async () => {
+    const expectedHeaders = ['CHPL ID', 'Certification Edition', 'Developer', 'Product', 'Version', 'Certification Status', '# Open Surveillance NCs', '# Closed Surveillance NCs', '# Open Direct Review NCs', '# Closed Direct Review NCs'];
+    const actualHeaders = await page.getTableHeaders();
+    await expect(actualHeaders.length).toBe(expectedHeaders.length, 'Found incorrect number of columns');
+    await actualHeaders.forEach(async (header, idx) => expect(await header.getText()).toBe(expectedHeaders[idx]));
   });
 
   describe('when filtering', () => {
-    let countBefore;
-    let countAfter;
-    beforeEach(() => {
-      countBefore = page.listingTotalCount();
-    });
+    describe('using predefined filters', () => {
+      let countBefore;
+      let countAfter;
 
-    afterEach(() => {
-      page.clearFilters.click();
-    });
+      beforeEach(async () => {
+        countBefore = await page.getTotalResultCount();
+      });
 
-    describe('using acb filter to de select drummond group', () => {
+      afterEach(async () => {
+        await page.resetFilters();
+      });
 
-      it('should filter listing results', () => {
-        page.selectFilter('acb', 'Drummond_Group');
-        page.waitForUpdatedListingResultsCount();
-        countAfter = page.listingTotalCount();
-        expect(countAfter).toBeLessThan(countBefore);
+      it('should filter on edition', async () => {
+        await page.removeFilter('Certification Edition', '2015');
+        countAfter = await page.getTotalResultCount();
+        await expect(countAfter).toBeLessThan(countBefore);
+      });
+
+      it('should filter on status', async () => {
+        await page.setListFilter('certificationStatuses', 'Withdrawn_by_Developer');
+        countAfter = await page.getTotalResultCount();
+        await expect(countAfter).toBeGreaterThan(countBefore);
+      });
+
+      it('should filter on open nonconformities', async () => {
+        await page.setListFilter('nonConformityOptions', 'open_nonconformity');
+        countAfter = await page.getTotalResultCount();
+        await expect(countAfter).toBeLessThan(countBefore);
       });
     });
 
-    describe('using certification edition filter to de select 2015 cures update', () => {
-
-      it('should filter listing results', () => {
-        page.selectFilter('edition', '2015_Cures_Update');
-        page.waitForUpdatedListingResultsCount();
-        countAfter = page.listingTotalCount();
-        expect(countAfter).toBeLessThan(countBefore);
+    describe('by text', () => {
+      afterEach(async () => {
+        await page.clearSearchTerm();
       });
-    });
 
-    describe('using certification status filter to select withdrawn by developer', () => {
-
-      it('should filter listing results', () => {
-        page.selectFilter('certificationStatus', 'Withdrawn_by_Developer');
-        page.waitForUpdatedListingResultsCount();
-        countAfter = page.listingTotalCount();
-        expect(countAfter).toBeGreaterThan(countBefore);
+      it('should show only listings that match the CHPL ID', async () => {
+        const searchTerm = '15.99';
+        const columnIndex = 0;
+        await page.searchForText(searchTerm);
+        await expect(await page.getCellInRow(0, columnIndex)).toContain(searchTerm);
       });
-    });
 
-    describe('using certification status filter to select open non conformity', () => {
-
-      it('should filter listing results', () => {
-        page.selectFilter('nonconformities', 'open-nonconformity');
-        page.waitForUpdatedListingResultsCount();
-        countAfter = page.listingTotalCount();
-        expect(countAfter).toBeLessThan(countBefore);
+      it('should show only listings that match the developer', async () => {
+        const searchTerm = 'Iatric';
+        const columnIndex = 2;
+        await page.searchForText(searchTerm);
+        await expect(await page.getCellInRow(0, columnIndex)).toContain(searchTerm);
       });
-    });
-  });
 
-  describe('when searching listing by developer', () => {
-    const DEVELOPER_COL_IDX = 2;
-    const developerName = 'Greenway';
-    it('should only show listings that match the developer', () => {
-      page.searchForListing(developerName);
-      page.waitForUpdatedListingResultsCount();
-      const count = page.listingTotalCount();
-      for (let i = 1; i <= count; i++) {
-        expect(page.getColumnText(i, DEVELOPER_COL_IDX)).toContain(developerName);
-      }
-    });
-  });
-
-  describe('when searching listing by version', () => {
-    const VERSION_COL_IDX = 4;
-    const versionName = 'v.12';
-    it('should only show listings that match the version', () => {
-      page.searchForListing(versionName);
-      page.waitForUpdatedListingResultsCount();
-      const count = page.listingTotalCount();
-      for (let i = 1; i <= count; i++) {
-        expect(page.getColumnText(i, VERSION_COL_IDX)).toContain(versionName);
-      }
-    });
-  });
-
-  describe('when searching listing by product', () => {
-    const PRODUCT_COL_IDX = 3;
-    const productName = 'Intergy EHR';
-    it('should only show listings that match the product', () => {
-      page.searchForListing(productName);
-      page.waitForUpdatedListingResultsCount();
-      const count = page.listingTotalCount();
-      for (let i = 1; i <= count; i++) {
-        expect(page.getColumnText(i, PRODUCT_COL_IDX)).toContain(productName);
-      }
-    });
-  });
-
-  describe('when searching listing by CHPL ID', () => {
-    const CHPLID_COL_IDX = 5;
-    const chplIdName = '15.04.04.2913.Gree.11.01.1.180919';
-    it('should only show listings that match the product', () => {
-      page.searchForListing(chplIdName);
-      page.waitForUpdatedListingResultsCount();
-      const count = page.listingTotalCount();
-      for (let i = 1; i <= count; i++) {
-        expect(page.getColumnText(i, CHPLID_COL_IDX)).toContain(chplIdName);
-      }
+      it('should show only listings that match the product', async () => {
+        const searchTerm = 'RDEE';
+        const columnIndex = 3;
+        await page.searchForText(searchTerm);
+        await expect(await page.getCellInRow(0, columnIndex)).toContain(searchTerm);
+      });
     });
   });
 });
