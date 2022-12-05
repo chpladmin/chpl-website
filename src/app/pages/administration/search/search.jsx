9b6aaf0c9eac7b8983381788c79363adebe642e4
Andrew Larned <andrew.larned@gmail.com>
2022-12-05 15:44:52 -0500

    ui!: Provide new search UI/UX page for UAT
    [#OCD-3918]

---
+++ src/app/pages/administration/search/search.jsx
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from 'react';
+
+import ChplSearchView from './search-view';
+
+import { useFetchCqms, useFetchCriteria } from 'api/data';
+import {
+  FilterProvider,
+  defaultFilter,
+  getDateDisplay,
+  getDateEntry,
+} from 'components/filter';
+import { sortCqms } from 'services/cqms.service';
+import { sortCriteria } from 'services/criteria.service';
+
+const staticFilters = [{
+  ...defaultFilter,
+  key: 'derivedCertificationEditions',
+  display: 'Certification Edition',
+  values: [
+    { value: '2011' },
+    { value: '2014' },
+    { value: '2015', default: true },
+    { value: '2015 Cures Update', default: true },
+  ],
+}, {
+  ...defaultFilter,
+  key: 'certificationStatuses',
+  display: 'Certification Status',
+  values: [
+    { value: 'Active', default: true },
+    { value: 'Suspended by ONC', default: true },
+    { value: 'Suspended by ONC-ACB', default: true },
+    { value: 'Terminated by ONC' },
+    { value: 'Withdrawn by Developer Under Surveillance/Review' },
+    { value: 'Withdrawn by ONC-ACB' },
+    { value: 'Withdrawn by Developer' },
+    { value: 'Retired' },
+  ],
+}, {
+  ...defaultFilter,
+  key: 'certificationDate',
+  display: 'Certification Date',
+  values: [
+    { value: 'Before', default: '' },
+    { value: 'After', default: '' },
+  ],
+  getQuery: (value) => value.values
+    .sort((a, b) => (a.value < b.value ? -1 : 1))
+    .map((v) => `${v.value === 'After' ? 'certificationDateStart' : 'certificationDateEnd'}=${v.selected}`)
+    .join('&'),
+  getValueDisplay: getDateDisplay,
+  getValueEntry: getDateEntry,
+}, {
+  ...defaultFilter,
+  key: 'certificationBodies',
+  display: 'ONC-ACB',
+  values: [
+    { value: 'CCHIT', display: 'CCHIT (Retired)' },
+    { value: 'Drummond Group', default: true },
+    { value: 'ICSA Labs', default: true },
+    { value: 'Leidos', default: true },
+    { value: 'SLI Compliance', default: true },
+    { value: 'Surescripts LLC', display: 'Surescripts LLC (Retired)' },
+    { value: 'UL LLC', display: 'UL LLC (Retired)' },
+  ],
+}, {
+  ...defaultFilter,
+  key: 'nonConformityOptions',
+  display: 'Compliance',
+  operatorKey: 'nonConformityOptionsOperator',
+  values: [
+    { value: 'open_nonconformity', display: 'Open Non-conformity' },
+    { value: 'closed_nonconformity', display: 'Closed Non-conformity' },
+    { value: 'never_nonconformity', display: 'Never had a Non-conformity' },
+    { value: 'not_open_nonconformity', display: 'Has no open Non-conformities' },
+    { value: 'not_closed_nonconformity', display: 'Has no closed Non-conformities' },
+    { value: 'not_never_nonconformit', display: 'Has had a Non-conformity' },
+  ],
+}];
+
+function ChplSearchPage() {
+  const [filters, setFilters] = useState(staticFilters);
+  const ccQuery = useFetchCriteria();
+  const cqmQuery = useFetchCqms();
+
+  useEffect(() => {
+    if (ccQuery.isLoading || !ccQuery.isSuccess) {
+      return;
+    }
+    const values = ccQuery.data.criteria
+      .map((cc) => ({
+        ...cc,
+        value: cc.id,
+        display: `${cc.removed ? 'Removed | ' : ''}${cc.number}${cc.title.includes('Cures Update') ? ' (Cures Update)' : ''}`,
+      }));
+    setFilters((f) => f
+      .filter((filter) => filter.key !== 'certificationCriteriaIds')
+      .concat({
+        ...defaultFilter,
+        key: 'certificationCriteriaIds',
+        display: 'Certification Criteria',
+        operatorKey: 'certificationCriteriaOperator',
+        sortValues: (filter, a, b) => sortCriteria(a, b),
+        values,
+      }));
+  }, [ccQuery.data, ccQuery.isLoading, ccQuery.isSuccess]);
+
+  useEffect(() => {
+    if (cqmQuery.isLoading || !cqmQuery.isSuccess) {
+      return;
+    }
+    const values = cqmQuery.data
+      .map((cqm) => ({
+        ...cqm,
+        value: cqm.name,
+        display: cqm.name.substring(0, 3) === 'CMS' ? `${cqm.name}` : `Retired | NQF-${cqm.name}`,
+      }));
+    setFilters((f) => f
+      .filter((filter) => filter.key !== 'cqms')
+      .concat({
+        ...defaultFilter,
+        key: 'cqms',
+        display: 'Clinical Quality Measures',
+        operatorKey: 'cqmsOperator',
+        sortValues: (filter, a, b) => sortCqms(a, b),
+        values,
+      }));
+  }, [cqmQuery.data, cqmQuery.isLoading, cqmQuery.isSuccess]);
+
+  const analytics = {
+    category: 'Search',
+  };
+
+  return (
+    <FilterProvider
+      analytics={analytics}
+      filters={filters}
+      storageKey="storageKey-searchPage"
+    >
+      <ChplSearchView
+        analytics={analytics}
+      />
+    </FilterProvider>
+  );
+}
+
+export default ChplSearchPage;
+
+ChplSearchPage.propTypes = {
+};
