8657a3d50fdefc1b85a1821ff5f0a44e21d3937a
Andrew Larned <andrew.larned@gmail.com>
2022-12-08 09:17:15 -0500

    refactor: extract some basic filters for reuse
    [#OCD-3918]

---
+++ src/app/components/filter/filters/certification-bodies.jsx
@@ -0,0 +1,21 @@
+import getAcbValueEntry from './acb-value-entry';
+
+import { defaultFilter } from 'components/filter';
+
+const filter = {
+  ...defaultFilter,
+  key: 'certificationBodies',
+  display: 'ONC-ACB',
+  getValueEntry: getAcbValueEntry,
+  values: [
+    { value: 'CCHIT', display: 'CCHIT (Retired)' },
+    { value: 'Drummond Group', default: true },
+    { value: 'ICSA Labs', default: true },
+    { value: 'Leidos', default: true },
+    { value: 'SLI Compliance', default: true },
+    { value: 'Surescripts LLC', display: 'Surescripts LLC (Retired)' },
+    { value: 'UL LLC', display: 'UL LLC (Retired)' },
+  ],
+};
+
+export default filter;
---
+++ src/app/components/filter/filters/index.js
@@ -0,0 +1,11 @@
+import certificationBodies from './certification-bodies';
+import certificationDate from './certification-date';
+import certificationStatuses from './certification-statuses';
+import derivedCertificationEditions from './derived-certification-editions';
+
+export {
+  certificationBodies,
+  certificationDate,
+  certificationStatuses,
+  derivedCertificationEditions,
+};
