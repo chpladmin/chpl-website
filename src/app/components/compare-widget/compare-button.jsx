5b008ccaa92663427ab6780d692633f932939454
Andrew Larned <andrew.larned@gmail.com>
2023-02-01 08:41:16 -0500

    refactor: remove unused functionality
    [#OCD-4124]

--- src/app/components/compare-widget/compare-button.jsx
+++ src/app/components/compare-widget/compare-button.jsx
@@ -1,35 +1,30 @@
 import React, { useContext } from 'react';
 import {
   Button,
-  ButtonGroup,
 } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import { node } from 'prop-types';
 
 import { CompareContext } from 'shared/contexts';
 import { listing as listingPropType } from 'shared/prop-types';
 
 function ChplCompareButton(props) {
-  const { children, listing } = props;
+  const { listing } = props;
   const { addListing, isInWidget, removeListing } = useContext(CompareContext);
 
   const handleClick = () => (isInWidget(listing) ? removeListing(listing) : addListing(listing));
 
   return (
-    <ButtonGroup>
-      { children }
-      <Button
-        variant="contained"
-        size="small"
-        color="secondary"
-        id={`toggle-compare-${listing.id}`}
-        onClick={handleClick}
-        endIcon={isInWidget(listing) ? <RemoveIcon /> : <AddIcon />}
-      >
-        Compare
-      </Button>
-    </ButtonGroup>
+    <Button
+      variant="contained"
+      size="small"
+      color="secondary"
+      id={`toggle-compare-${listing.id}`}
+      onClick={handleClick}
+      endIcon={isInWidget(listing) ? <RemoveIcon /> : <AddIcon />}
+    >
+      Compare
+    </Button>
   );
 }
 
@@ -37,9 +32,4 @@ export default ChplCompareButton;
 
 ChplCompareButton.propTypes = {
   listing: listingPropType.isRequired,
-  children: node,
-};
-
-ChplCompareButton.defaultProps = {
-  children: undefined,
 };
