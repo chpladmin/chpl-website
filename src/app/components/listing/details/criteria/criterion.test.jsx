2311bbc8695b3851496c16c59a6ea3e0362a9d45
Andrew Larned <andrew.larned@gmail.com>
2023-02-07 07:21:26 -0500

    refactor: remove text expecting unused service to be used
    [#OCD-4137]

--- src/app/components/listing/details/criteria/criterion.test.jsx
+++ src/app/components/listing/details/criteria/criterion.test.jsx
@@ -7,17 +7,14 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 
 import * as angularReactHelper from '../../../../services/angular-react-helper';
+
 import ChplCriterion from './criterion';
 
 const $analyticsMock = {
   eventTrack: jest.fn(),
 };
-const utilServiceMock = {
-  isCures: jest.fn(),
-};
 angularReactHelper.getAngularService = jest.fn();
 when(angularReactHelper.getAngularService).calledWith('$analytics').mockReturnValue($analyticsMock);
-when(angularReactHelper.getAngularService).calledWith('utilService').mockReturnValue(utilServiceMock);
 
 jest.mock('./criterion-details-view', () => ({
   __esModule: true,
@@ -48,26 +45,12 @@ describe('the ChplCriterion component', () => {
 
     it('should track analytics', async () => {
       $analyticsMock.eventTrack.mockClear();
-      utilServiceMock.isCures.mockReturnValueOnce(false);
       userEvent.click(screen.getByText('170.315 (z)(1)'));
 
       await waitFor(() => {
         expect($analyticsMock.eventTrack).toHaveBeenCalledWith(
           'Viewed criteria details',
           { category: 'Listing Details', label: '170.315 (z)(1)' },
-        );
-      });
-    });
-
-    it('should track analytics for cures criteria', async () => {
-      $analyticsMock.eventTrack.mockClear();
-      utilServiceMock.isCures.mockReturnValueOnce(true);
-      userEvent.click(screen.getByText('170.315 (z)(1)'));
-
-      await waitFor(() => {
-        expect($analyticsMock.eventTrack).toHaveBeenCalledWith(
-          'Viewed criteria details',
-          { category: 'Listing Details', label: '170.315 (z)(1) (Cures Update)' },
         );
       });
     });
