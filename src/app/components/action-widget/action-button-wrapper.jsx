b43763ed11bdebf30722808786a56a36ed22367e
Andrew Larned <andrew.larned@gmail.com>
2023-02-10 15:17:31 -0500

    ui: let users download CSV from anywhere "ActionButton" is enabled
    [#OCD-4145]

--- src/app/components/action-widget/action-button-wrapper.jsx
+++ src/app/components/action-widget/action-button-wrapper.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   ThemeProvider,
 } from '@material-ui/core';
+import { node } from 'prop-types';
 
 import ChplActionButton from './action-button';
 
@@ -9,23 +10,24 @@ import ApiWrapper from 'api/api-wrapper';
 import FlagWrapper from 'api/flag-wrapper';
 import CmsWrapper from 'components/cms-widget/cms-wrapper';
 import CompareWrapper from 'components/compare-widget/compare-wrapper';
+import { UserWrapper } from 'components/login';
 import { listing as listingPropType } from 'shared/prop-types';
 import theme from 'themes/theme';
 
 function ChplActionButtonWrapper(props) {
-  const { listing } = props;
-
   return (
     <ThemeProvider theme={theme}>
-      <ApiWrapper showQueryTools={false}>
-        <FlagWrapper>
-          <CmsWrapper>
-            <CompareWrapper>
-              <ChplActionButton listing={listing} />
-            </CompareWrapper>
-          </CmsWrapper>
-        </FlagWrapper>
-      </ApiWrapper>
+      <UserWrapper>
+        <ApiWrapper showQueryTools={false}>
+          <FlagWrapper>
+            <CmsWrapper>
+              <CompareWrapper>
+                <ChplActionButton {...props} />
+              </CompareWrapper>
+            </CmsWrapper>
+          </FlagWrapper>
+        </ApiWrapper>
+      </UserWrapper>
     </ThemeProvider>
   );
 }
@@ -34,4 +36,9 @@ export default ChplActionButtonWrapper;
 
 ChplActionButtonWrapper.propTypes = {
   listing: listingPropType.isRequired,
+  children: node,
+};
+
+ChplActionButtonWrapper.defaultProps = {
+  children: undefined,
 };
--- src/app/components/action-widget/action-button.jsx
+++ src/app/components/action-widget/action-button.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import {
+  Button,
+} from '@material-ui/core';
 import { node } from 'prop-types';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 
 import CompareButton from 'components/compare-widget/compare-button';
 import CmsButton from 'components/cms-widget/cms-button';
+import { UserContext } from 'shared/contexts';
 import { listing as listingPropType } from 'shared/prop-types';
+import { getAngularService } from 'services/angular-react-helper';
 
 function ChplActionButton(props) {
   const { children, listing } = props;
+  const { hasAnyRole } = useContext(UserContext);
+  const API = getAngularService('API');
+  const authService = getAngularService('authService');
+
+  const handleClick = () => {
+    const downloadLink = `${API}/listings/${listing.id}/uploaded-file?api_key=${authService.getApiKey()}`;
+    window.open(downloadLink);
+  };
 
   return (
     <>
       { children }
       <CompareButton listing={listing} />
       <CmsButton listing={listing} />
+      { hasAnyRole(['ROLE_ADMIN'])
+        && (
+          <Button
+            color="secondary"
+            variant="contained"
+            size="small"
+            id={`download-csv-${listing.id}`}
+            onClick={handleClick}
+            endIcon={<CloudDownloadIcon />}
+            fullWidth
+          >
+            Download CSV
+          </Button>
+        )}
     </>
   );
 }
