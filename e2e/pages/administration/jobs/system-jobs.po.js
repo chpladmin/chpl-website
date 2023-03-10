81cca3916ee0397311ac1841b9d5e44e1c983a59
Anitha Thomas <thomaan@pointclickcare.com>
2023-03-10 15:47:20 -0500

    OCD-3673: AQA tests for System Jobs page

---
+++ e2e/pages/administration/jobs/system-jobs.po.js
@@ -0,0 +1,33 @@
+class SystemJobsComponent {
+  constructor() {
+    this.elements = {
+      systemjobsTable: 'table[aria-label="Types of Jobs table"]',
+      scheduledsystemjobsTable: 'table[aria-label="Scheduled System Jobs table"]',
+    };
+  }
+
+  async getSystemJobsTableHeaders() {
+    return (await
+            (await
+             $(this.elements.systemjobsTable)
+            ).$('thead')
+           ).$$('th');
+  }
+
+  async getScheduledSystemJobsTableHeaders() {
+    return (await
+            (await
+             $(this.elements.scheduledsystemjobsTable)
+            ).$('thead')
+           ).$$('th');
+  }
+
+  getAvailableJobs() {
+    return $(this.elements.systemjobsTable)
+      .$('tbody')
+      .$$('tr')
+      .map((row) => row.$$('td')[0].getText());
+  }
+}
+
+export default SystemJobsComponent;
