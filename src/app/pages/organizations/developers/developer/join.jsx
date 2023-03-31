7ce61c1c6fb46de4adb30237745a79f8c8389891
Matthew Stankiewicz <stankm@pointclickcare.com>
2023-03-31 13:23:27 -0400

    style: linting & take out unneeded code

--- src/app/pages/organizations/developers/developer/join.jsx
+++ src/app/pages/organizations/developers/developer/join.jsx
@@ -51,7 +51,6 @@ const useStyles = makeStyles({
   },
   stickyCardContainer: {
     width: '100%',
-    position: 'static',
     [theme.breakpoints.up('md')]: {
       width: '50vw',
       position: 'sticky',
@@ -150,18 +149,18 @@ function ChplJoinDevelopers({ id }) {
             <CardContent>
               {developersToJoin.length === 0
                && (
-                   <Box display="flex" flexDirection="column" gridGap="8px">
-                     <Typography variant="h6" gutterBottom><strong>Instructions</strong></Typography>
-                     <Typography variant="body1" gutterBottom>
-                       &quot;Join Developers&quot; allows you to merge the content of one developer into another. This means that certain information about the selected &quot;old&quot; developer will no longer be present, including any demographic data, Attestations previously submitted, Direct Reviews associated with the developer or any of their listings, and any users that may have been associated with the developer.
-                     </Typography>
-                     <Typography variant="body1" gutterBottom>
-                       To use the &quot;Join Developers&quot; feature, please select one or more developers that you want to join with the retained developer. This action will combine all the products, versions, and listings with the selected developer(s) under the retained developer.
-                     </Typography>
-                     <Typography variant="body1">
-                       Once the action is completed, the retained developer will be updated with the appropriate data from the selected developer(s), and the selected developer(s) will be deleted. Please note that this action cannot be undone, so be sure to carefully review your selections before proceeding with the Join Developers action.
-                     </Typography>
-                   </Box>
+               <Box display="flex" flexDirection="column" gridGap="8px">
+                 <Typography variant="h6" gutterBottom><strong>Instructions</strong></Typography>
+                 <Typography variant="body1" gutterBottom>
+                   &quot;Join Developers&quot; allows you to merge the content of one developer into another. This means that certain information about the selected &quot;old&quot; developer will no longer be present, including any demographic data, Attestations previously submitted, Direct Reviews associated with the developer or any of their listings, and any users that may have been associated with the developer.
+                 </Typography>
+                 <Typography variant="body1" gutterBottom>
+                   To use the &quot;Join Developers&quot; feature, please select one or more developers that you want to join with the retained developer. This action will combine all the products, versions, and listings with the selected developer(s) under the retained developer.
+                 </Typography>
+                 <Typography variant="body1">
+                   Once the action is completed, the retained developer will be updated with the appropriate data from the selected developer(s), and the selected developer(s) will be deleted. Please note that this action cannot be undone, so be sure to carefully review your selections before proceeding with the Join Developers action.
+                 </Typography>
+               </Box>
                )}
               {developersToJoin.length > 0
                 && (
