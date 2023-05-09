c7652421ec1b1ed47b118b93567fe070ea666e7c
Andrew Larned <andrew.larned@gmail.com>
2023-05-09 14:09:03 -0400

    fix: get DR data into dr component
    [#OCD-4188]

--- src/app/components/listing/details/compliance/direct-reviews.jsx
+++ src/app/components/listing/details/compliance/direct-reviews.jsx
@@ -10,9 +10,13 @@ import {
   makeStyles,
 } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
-import { palette, utilStyles } from 'themes';
-import { ChplTooltip } from 'components/util';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { arrayOf } from 'prop-types';
+
+import ChplDirectReviewsView from 'components/direct-reviews/direct-reviews-view';
+import { ChplTooltip } from 'components/util';
+import { directReview as directReviewPropType } from 'shared/prop-types';
+import { palette, utilStyles } from 'themes';
 
 const useStyles = makeStyles({
   ...utilStyles,
@@ -55,127 +59,139 @@ const useStyles = makeStyles({
   },
 });
 
-
 function ChplDirectReviews(props) {
+  const { directReviews } = props;
   const classes = useStyles();
-  return (
-    <div>
-           <Accordion className={classes.NestedAccordionLevelOne}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                className={classes.NestedAccordionLevelOneSummary}
-                color="secondary"
-              >
-                <Box display="flex" flexDirection="row" justifyContent="space-between" width="100%">
-                  <Typography>
-                    Direct Review
-                  </Typography>
-                  <Typography variant="body2">
-                    1 open / 2 non conformities found
-                  </Typography>
-                </Box>
-              </AccordionSummary>
-              <CardContent>
-                <Accordion className={classes.NestedAccordionLevelOne}>
-                  <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    className={classes.NestedAccordionLevelTwoSummary}
-                    color="secondary"
-                  > <Typography>
-                      Open Direct Review
-                    </Typography>
-                  </AccordionSummary>
-                  <CardContent>
-                    <Card>
-                      <CardHeader
-                        titleTypographyProps={{ variant: 'h6' }}
-                        className={classes.subCard}
-                        title="Non Conformity Type: 170.315(d)(2) Example Text (Cures Update)">
-                      </CardHeader>
-                      <CardContent>
-                        <Box display="flex" gridGap="8px" flexWrap="wrap" flexDirection="row" justifyContent="space-between">
-                          <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
-                            <Box display="flex" flexDirection="column" >
-                              <Typography variant='subtitle2'>
-                                Developer Associated Listings
-                              </Typography>
-                              <Typography>
-                                MM/DD/YYYY
-                              </Typography>
-                            </Box>
-                            <Box>
-                              <ChplTooltip
-                                placement="top"
-                                title="Placement text"
-                              >
-                                <InfoIcon color="primary"></InfoIcon>
-                              </ChplTooltip>
-                            </Box>
-                          </Box>
-                          <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
-                            <Box display="flex" flexDirection="column" >
-                              <Typography variant='subtitle2'>
-                                Corrective Action Plan Approval date
-                              </Typography>
-                              <Typography>
-                                MM/DD/YYYY
-                              </Typography>
-                            </Box>
-                            <Box>
-                              <ChplTooltip
-                                placement="top"
-                                title="Placement text"
-                              >
-                                <InfoIcon color="primary"></InfoIcon>
-                              </ChplTooltip>
-                            </Box>
-                          </Box>
-                          <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
-                            <Box display="flex" flexDirection="column" >
-                              <Typography variant='subtitle2'>
-                                Corrective Action Plan Must Be Completed
-                              </Typography>
-                              <Typography>
-                                MM/DD/YYYY
-                              </Typography>
-                            </Box>
-                            <Box>
-                              <ChplTooltip
-                                placement="top"
-                                title="Placement text"
-                              >
-                                <InfoIcon color="primary"></InfoIcon>
-                              </ChplTooltip>
-                            </Box>
-                          </Box>
-                          <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
-                            <Box display="flex" flexDirection="column" >
-                              <Typography variant='subtitle2'>
-                                Corrective Action Plan was completed
-                              </Typography>
-                              <Typography>
-                                MM/DD/YYYY
-                              </Typography>
-                            </Box>
-                            <Box>
-                              <ChplTooltip
-                                placement="top"
-                                title="Placement text"
-                              >
-                                <InfoIcon color="primary"></InfoIcon>
-                              </ChplTooltip>
-                            </Box>
-                          </Box>
-                        </Box>
-                      </CardContent>
-                    </Card>
-                  </CardContent>
-                </Accordion>
-              </CardContent>
-            </Accordion>
 
-    </div >
+  return (
+    <Accordion className={classes.NestedAccordionLevelOne}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        className={classes.NestedAccordionLevelOneSummary}
+        color="secondary"
+      >
+        <Box display="flex" flexDirection="row" justifyContent="space-between" width="100%">
+          <Typography>
+            Direct Review Activities
+          </Typography>
+          <Typography variant="body2">
+            (
+            { directReviews.length }
+            {' '}
+            found)
+          </Typography>
+        </Box>
+      </AccordionSummary>
+      <CardContent>
+        <Typography gutterBottom>
+          Direct Review information is displayed here if a Direct Review has been opened by ONC that either affects this listing directly or applies to the developer of this listing
+</Typography>
+        <ChplDirectReviewsView directReviews={directReviews} />
+        { directReviews.map((dr) => (
+          <Accordion className={classes.NestedAccordionLevelOne}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              className={classes.NestedAccordionLevelTwoSummary}
+              color="secondary"
+            >
+              <Typography>
+                Open Direct Review
+              </Typography>
+            </AccordionSummary>
+            <CardContent>
+              <Card>
+                <CardHeader
+                  titleTypographyProps={{ variant: 'h6' }}
+                  className={classes.subCard}
+                  title="Non Conformity Type: 170.315(d)(2) Example Text (Cures Update)">
+                </CardHeader>
+                <CardContent>
+                  <Box display="flex" gridGap="8px" flexWrap="wrap" flexDirection="row" justifyContent="space-between">
+                    <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
+                      <Box display="flex" flexDirection="column" >
+                        <Typography variant='subtitle2'>
+                          Developer Associated Listings
+                        </Typography>
+                        <Typography>
+                          MM/DD/YYYY
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <ChplTooltip
+                          placement="top"
+                          title="Placement text"
+                        >
+                          <InfoIcon color="primary"></InfoIcon>
+                        </ChplTooltip>
+                      </Box>
+                    </Box>
+                    <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
+                      <Box display="flex" flexDirection="column" >
+                        <Typography variant='subtitle2'>
+                          Corrective Action Plan Approval date
+                        </Typography>
+                        <Typography>
+                          MM/DD/YYYY
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <ChplTooltip
+                          placement="top"
+                          title="Placement text"
+                        >
+                          <InfoIcon color="primary"></InfoIcon>
+                        </ChplTooltip>
+                      </Box>
+                    </Box>
+                    <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
+                      <Box display="flex" flexDirection="column" >
+                        <Typography variant='subtitle2'>
+                          Corrective Action Plan Must Be Completed
+                        </Typography>
+                        <Typography>
+                          MM/DD/YYYY
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <ChplTooltip
+                          placement="top"
+                          title="Placement text"
+                        >
+                          <InfoIcon color="primary"></InfoIcon>
+                        </ChplTooltip>
+                      </Box>
+                    </Box>
+                    <Box width="48%" gridGap="8px" alignItems="center" display="flex" justifyContent="space-between">
+                      <Box display="flex" flexDirection="column" >
+                        <Typography variant='subtitle2'>
+                          Corrective Action Plan was completed
+                        </Typography>
+                        <Typography>
+                          MM/DD/YYYY
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <ChplTooltip
+                          placement="top"
+                          title="Placement text"
+                        >
+                          <InfoIcon color="primary"></InfoIcon>
+                        </ChplTooltip>
+                      </Box>
+                    </Box>
+                  </Box>
+                </CardContent>
+              </Card>
+            </CardContent>
+          </Accordion>
+        ))}
+      </CardContent>
+    </Accordion>
   );
 }
 
 export default ChplDirectReviews;
+
+ChplDirectReviews.propTypes = {
+  directReviews: arrayOf(directReviewPropType).isRequired,
+};
