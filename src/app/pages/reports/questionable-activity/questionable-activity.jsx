40b8ae2d42b40dd2c691bcb34c60d3f7365e0e82
Andrew Larned <andrew.larned@gmail.com>
2023-05-12 14:01:11 -0400

    feat!: Enable view/search of Questionable Activity for admins
    [#OCD-4174]

---
+++ src/app/pages/reports/questionable-activity/questionable-activity.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+
+import ChplQuestionableActivityView from './questionable-activity-view';
+
+import { useFetchAcbs } from 'api/acbs';
+import { FilterProvider } from 'components/filter';
+import {
+  certificationBodies,
+  certificationDate,
+  certificationStatuses,
+  derivedCertificationEditions,
+} from 'components/filter/filters';
+
+const staticFilters = [
+  certificationDate,
+  certificationStatuses, {
+    ...derivedCertificationEditions,
+    required: true,
+    values: [
+      { value: '2015', default: true },
+      { value: '2015 Cures Update', default: true },
+    ],
+  }];
+
+function ChplQuestionableActivityPage() {
+  const [filters, setFilters] = useState(staticFilters);
+  const acbQuery = useFetchAcbs();
+
+  useEffect(() => {
+    if (acbQuery.isLoading || !acbQuery.isSuccess) {
+      return;
+    }
+    const values = acbQuery.data.acbs
+      .map((acb) => ({
+        ...acb,
+        value: acb.name,
+        display: `${acb.retired ? 'Retired | ' : ''}${acb.name}`,
+        default: !acb.retired || ((Date.now() - acb.retirementDate) < (1000 * 60 * 60 * 24 * 30 * 4)), // approx 4 months
+      }));
+    setFilters((f) => f
+      .filter((filter) => filter.key !== 'certificationBodies')
+      .concat({
+        ...certificationBodies,
+        values,
+      }));
+  }, [acbQuery.data, acbQuery.isLoading, acbQuery.isSuccess]);
+
+  const analytics = {
+    category: 'SED Information for 2015 Edition Products',
+  };
+
+  return (
+    <FilterProvider
+      analytics={analytics}
+      filters={filters}
+      storageKey="storageKey-sedPage"
+    >
+      <ChplQuestionableActivityView
+        analytics={analytics}
+      />
+    </FilterProvider>
+  );
+}
+
+export default ChplQuestionableActivityPage;
+
+ChplQuestionableActivityPage.propTypes = {
+};
