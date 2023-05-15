6b0f298ef33eeebc0bb6995c842a52460c605a74
Matthew Stankiewicz <stankm@pointclickcare.com>
2023-05-15 12:12:13 -0400

    style: linting

--- src/app/components/listing/details/compliance/surveillance.jsx
+++ src/app/components/listing/details/compliance/surveillance.jsx
@@ -113,9 +113,11 @@ const getSurveillanceResult = (surveillance) => {
       { getSurveillanceResultsSummary(surveillance).map((result) => (
         <ListItem key={result.id}>
           <Box display="flex" flexDirection="column" justifyContent="space-between">
-          <Typography variant='body1'>
-            { `${result.statusName} Non-Conformity Found for ` } <span className={result.removed ? 'removed' : ''}>{ result.display }</span>
-          </Typography>
+            <Typography variant="body1">
+              { `${result.statusName} Non-Conformity Found for ` }
+              {' '}
+              <span className={result.removed ? 'removed' : ''}>{ result.display }</span>
+            </Typography>
           </Box>
         </ListItem>
       ))}
@@ -204,55 +206,55 @@ function ChplSurveillance({ surveillance: initialSurveillance }) {
                 { getDataDisplay('Certification Criteria and Program Requirements Surveilled', getItemsSurveilled(surv), 'The ONC Health IT Certification Program requirement that was surveilled. For example, this may be a specific certification criteria (e.g. 170.315(a)(1)), disclosure requirement (e.g. 170.523(k)(1)), another requirement with a regulatory reference (e.g. 170.523(l)), or a brief description of the surveilled requirement.', true) }
                 { getDataDisplay('Surveillance Result', getSurveillanceResult(surv), 'Whether or not a non-conformity was found for the conducted surveillance.', true) }
               </Box>
-              <Divider/>
+              <Divider />
               <Box pt={2}>
-              { surv.requirements.map((req) => req.nonconformities.map((nc) => (
-                <Accordion className={classes.surveillance} key={nc.id}>
-                  <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    className={classes.surveillanceDetailsSummary}
-                    color="secondary"
-                  >
-                    <Typography>
-                      Details for
-                      {' '}
-                      { getRequirementDisplay(req) }
-                    </Typography>
-                  </AccordionSummary>
-                  <CardContent>
-                    <Box display="flex" gridGap="8px" flexWrap="wrap" flexDirection="row" justifyContent="space-between">
-                      { getDataDisplay('Date of Determination of Non-Conformity', <Typography>{ getDisplayDateFormat(nc.dateOfDeterminationDay) }</Typography>, 'The date that the ONC-ACB determined that a non-conformity was present.') }
-                      { getDataDisplay('Corrective Action Plan Approval Date', <Typography>{ getDisplayDateFormat(nc.capApprovalDay) }</Typography>, 'The date that the ONC-ACB approved the corrective action plan proposed by the developer.') }
-                      { getDataDisplay('Date Corrective Action Began', <Typography>{ getDisplayDateFormat(nc.capStartDay) }</Typography>, 'The date that the corrective action was started.') }
-                      { getDataDisplay('Date Corrective Action Must Be Completed', <Typography>{ getDisplayDateFormat(nc.capMustCompleteDay) }</Typography>, 'The date that the corrective action must be completed in order to avoid termination of the certified product’s certification status.') }
-                      { getDataDisplay('Date Corrective Action Was Completed', <Typography>{ getDisplayDateFormat(nc.capEndDay) }</Typography>, 'The date that the corrective action was completed.') }
-                      { getDataDisplay('Non-Conformity Type',
-                        <Typography className={nc.type.removed ? 'removed' : ''}>
-                          {nc.type.removed ? 'Removed | ' : ''}
-                          {' '}
-                          {nc.type.number ? (`${nc.type.number}: `) : ''}
-                          {' '}
-                          {nc.type.title}
-                        </Typography>,
-                        'For non-conformities related to specific regulatory references (e.g. certified capabilities, disclosure requirements, or use of the Certification Mark), the regulation reference is used (e.g. 170.315(a)(2) or 170.523(l)). If the non-conformity type is designated as "Other Non-Conformity", then the associated non-conformity does not have a relevant regulatory reference.') }
-                      { getDataDisplay('Non-Conformity Status', <Typography>{ nc.nonconformityStatus }</Typography>, 'Whether the non-conformity is open or closed (has been resolved).') }
-                      { surv.type.name === 'Randomized' && getDataDisplay('Pass Rate',
-                        <Typography>
-                          { nc.sitesPassed }
-                          {' '}
-                          /
-                          {' '}
-                          { nc.totalSites }
-                        </Typography>,
-                        'Pass rates only apply to non-conformities found as a result of random surveillance. The numerator for the pass rate is the number of sites for each criterion that passed randomized surveillance for the Health IT module being evaluated. The denominator is the total number of sites for which randomized surveillance was conducted on the Health IT module.') }
-                      { getDataDisplay('Non-Conformity Summary', <Typography>{ nc.summary }</Typography>, 'A brief summary describing why the certified product was found to be non-conformant.') }
-                      { getDataDisplay('Findings', <Typography>{ nc.findings }</Typography>, 'A detailed description of the ONC-ACB’s findings related to the non-conformity. This provides a full picture of the potential non-conformities or other deficiencies the ONC-ACB identified, how they were evaluated, and how the ONC-ACB reached its non-conformity determination.', true) }
-                      { getDataDisplay('Developer Explanation', <Typography>{ nc.developerExplanation }</Typography>, 'If available, the developer’s explanation of why it agrees or disagrees with the ONC-ACB’s assessment of the non-conformity and an explanation of why the non-conformity occurred.', true) }
-                      { getDataDisplay('Resolution', <Typography>{ nc.resolution }</Typography>, 'A detailed description of how the non-conformity was resolved.', true) }
-                    </Box>
-                  </CardContent>
-                </Accordion>
-              )))}
+                { surv.requirements.map((req) => req.nonconformities.map((nc) => (
+                  <Accordion className={classes.surveillance} key={nc.id}>
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      className={classes.surveillanceDetailsSummary}
+                      color="secondary"
+                    >
+                      <Typography>
+                        Details for
+                        {' '}
+                        { getRequirementDisplay(req) }
+                      </Typography>
+                    </AccordionSummary>
+                    <CardContent>
+                      <Box display="flex" gridGap="8px" flexWrap="wrap" flexDirection="row" justifyContent="space-between">
+                        { getDataDisplay('Date of Determination of Non-Conformity', <Typography>{ getDisplayDateFormat(nc.dateOfDeterminationDay) }</Typography>, 'The date that the ONC-ACB determined that a non-conformity was present.') }
+                        { getDataDisplay('Corrective Action Plan Approval Date', <Typography>{ getDisplayDateFormat(nc.capApprovalDay) }</Typography>, 'The date that the ONC-ACB approved the corrective action plan proposed by the developer.') }
+                        { getDataDisplay('Date Corrective Action Began', <Typography>{ getDisplayDateFormat(nc.capStartDay) }</Typography>, 'The date that the corrective action was started.') }
+                        { getDataDisplay('Date Corrective Action Must Be Completed', <Typography>{ getDisplayDateFormat(nc.capMustCompleteDay) }</Typography>, 'The date that the corrective action must be completed in order to avoid termination of the certified product’s certification status.') }
+                        { getDataDisplay('Date Corrective Action Was Completed', <Typography>{ getDisplayDateFormat(nc.capEndDay) }</Typography>, 'The date that the corrective action was completed.') }
+                        { getDataDisplay('Non-Conformity Type',
+                          <Typography className={nc.type.removed ? 'removed' : ''}>
+                            {nc.type.removed ? 'Removed | ' : ''}
+                            {' '}
+                            {nc.type.number ? (`${nc.type.number}: `) : ''}
+                            {' '}
+                            {nc.type.title}
+                          </Typography>,
+                          'For non-conformities related to specific regulatory references (e.g. certified capabilities, disclosure requirements, or use of the Certification Mark), the regulation reference is used (e.g. 170.315(a)(2) or 170.523(l)). If the non-conformity type is designated as "Other Non-Conformity", then the associated non-conformity does not have a relevant regulatory reference.') }
+                        { getDataDisplay('Non-Conformity Status', <Typography>{ nc.nonconformityStatus }</Typography>, 'Whether the non-conformity is open or closed (has been resolved).') }
+                        { surv.type.name === 'Randomized' && getDataDisplay('Pass Rate',
+                          <Typography>
+                            { nc.sitesPassed }
+                            {' '}
+                            /
+                            {' '}
+                            { nc.totalSites }
+                          </Typography>,
+                          'Pass rates only apply to non-conformities found as a result of random surveillance. The numerator for the pass rate is the number of sites for each criterion that passed randomized surveillance for the Health IT module being evaluated. The denominator is the total number of sites for which randomized surveillance was conducted on the Health IT module.') }
+                        { getDataDisplay('Non-Conformity Summary', <Typography>{ nc.summary }</Typography>, 'A brief summary describing why the certified product was found to be non-conformant.') }
+                        { getDataDisplay('Findings', <Typography>{ nc.findings }</Typography>, 'A detailed description of the ONC-ACB’s findings related to the non-conformity. This provides a full picture of the potential non-conformities or other deficiencies the ONC-ACB identified, how they were evaluated, and how the ONC-ACB reached its non-conformity determination.', true) }
+                        { getDataDisplay('Developer Explanation', <Typography>{ nc.developerExplanation }</Typography>, 'If available, the developer’s explanation of why it agrees or disagrees with the ONC-ACB’s assessment of the non-conformity and an explanation of why the non-conformity occurred.', true) }
+                        { getDataDisplay('Resolution', <Typography>{ nc.resolution }</Typography>, 'A detailed description of how the non-conformity was resolved.', true) }
+                      </Box>
+                    </CardContent>
+                  </Accordion>
+                )))}
               </Box>
             </CardContent>
           </Accordion>
