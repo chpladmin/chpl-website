cf082dc47f80ea1462312b7e3e8d4bfa91e4cc4f
Andrew Larned <andrew.larned@gmail.com>
2023-06-07 14:13:54 -0400

    feat: use expanded Users component in other places
    [#OCD-4055]

--- src/app/components/user/users-wrapper.jsx
+++ src/app/components/user/users-wrapper.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { arrayOf, func, string } from 'prop-types';
 
 import ChplUsers from './users';
 
 import AppWrapper from 'app-wrapper';
+import { user as userPropType } from 'shared/prop-types';
 
-function ChplUsersWrapper() {
+function ChplUsersWrapper({ users, dispatch, roles }) {
   return (
     <AppWrapper>
-      <ChplUsers />
+      <ChplUsers users={users} dispatch={dispatch} roles={roles} />
     </AppWrapper>
   );
 }
@@ -15,4 +17,7 @@ function ChplUsersWrapper() {
 export default ChplUsersWrapper;
 
 ChplUsersWrapper.propTypes = {
+  users: arrayOf(userPropType).isRequired,
+  dispatch: func.isRequired,
+  roles: arrayOf(string).isRequired,
 };
