a5b12a3e8ca30276a76b1821abf4dbe31d4ab354
Andrew Larned <andrew.larned@gmail.com>
2023-06-07 08:59:48 -0400

    test: expect ATL readonly functionality to work
    [#OCD-4055]

--- e2e/pages/organizations/onc-acbs-atls/atls.readonly.spec.js
+++ e2e/pages/organizations/onc-acbs-atls/atls.readonly.spec.js
@@ -1,8 +1,7 @@
 import Hooks from '../../../utilities/hooks';
 import AddressComponent from '../../../components/address/address.po';
 import LoginComponent from '../../../components/login/login.sync.po';
-import UsersComponent from '../../../components/users/users.po';
-import ToastComponent from '../../../components/toast/toast.po';
+import { open } from '../../../utilities/hooks.async';
 
 import OrganizationPage from './organization.po';
 
@@ -10,96 +9,46 @@ let address;
 let hooks;
 let login;
 let page;
-let toast;
-let user;
 
 describe('the ONC-ATL Management page', () => {
-  const timestamp = Date.now();
-  const websiteUrl = `http://www.example${timestamp}.com`;
-  const today = new Date().toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
-  const atlAddress = {
-    address: `address${timestamp}`,
-    city: `city${timestamp}`,
-    state: `state${timestamp}`,
-    zip: `11111${timestamp}`,
-    country: `country${timestamp}`,
-  };
-
   beforeEach(async () => {
     page = new OrganizationPage();
     hooks = new Hooks();
-    login = new LoginComponent();
     address = new AddressComponent();
-    user = new UsersComponent();
-    toast = new ToastComponent();
-    await hooks.open('#/organizations/onc-atls');
+    login = new LoginComponent();
+    await open('#/resources/overview');
   });
 
-  it('should have at least 7 ATL organizations', () => {
-    expect(page.organizationListCount()).toBeGreaterThanOrEqual(7);
-  });
-
-  it('should display ATL organizations in alphabetical order', () => {
-    const atlCount = page.organizationListCount();
-    let i;
-    for (i = 1; i < atlCount; i += 1) {
-      expect(page.organizationListValue(i - 1).getText()).toBeLessThan(page.organizationListValue(i).getText());
-    }
-  });
-
-  describe('when impersonating as ROLE_ATL for drummond group', () => {
-    beforeEach(() => {
-      login.logIn('onc');
-      hooks.open('#/organizations/onc-atls');
-      hooks.waitForSpinnerToDisappear();
-      page.openOrganizationDetails('Drummond Group');
-      user.impersonateUser('Jim Dow');
-      hooks.open('#/organizations/onc-atls'); // added to avoid 404 page not found error
-    });
-
-    afterEach(() => {
-      login.logOut();
-    });
-
-    it('should display registered users under Drummond', () => {
-      const atl = 'Drummond Group';
-      page.openOrganizationDetails(atl);
-      hooks.waitForSpinnerToDisappear();
-      expect(page.manageUsersPanelHeader).toBeDisplayed();
-      expect(page.manageUsersPanel.getText()).toContain('ROLE_ATL');
-      expect(page.manageUsersPanel.getText()).toContain('Drummond Group');
-    });
-
-    it('should not display the option to edit ATL details for ICSA Labs', () => {
-      const atl = 'ICSA Labs';
-      const atlId = '5';
-      page.openOrganizationDetails(atl);
-      browser.waitUntil(() => page.generalInformation('ATL', atlId).isDisplayed());
-      expect(page.organizationEditButton.isDisplayed()).toBe(false);
-    });
-
-    it('should not display registered users under ICSA Labs', () => {
-      const atl = 'ICSA Labs';
-      const atlId = '5';
-      page.openOrganizationDetails(atl);
-      browser.waitUntil(() => page.generalInformation('ATL', atlId).isDisplayed());
-      expect(page.manageUsersPanelHeader.isDisplayed()).toBe(false);
-    });
+  afterEach(() => {
+    login.logOut();
   });
 
   describe('when logged in as ONC', () => {
-    beforeEach(() => {
-      login.logIn('onc');
+    beforeEach(async () => {
+      await login.logIn('onc');
+      await page.open('onc-acbs');
+      await (browser.waitUntil(async () => (await page.organizationListCount() > 0)));
     });
 
-    afterEach(() => {
-      login.logOut();
+    it('should have at least 7 ATL organizations', () => {
+      expect(page.organizationListCount()).toBeGreaterThanOrEqual(7);
     });
 
-    it('should allow user to unretire and retire existing ATL', () => {
+    xit('should allow user to unretire and retire existing ATL', () => {
       const atl = 'CCHIT';
       const organizationType = 'ATL';
       const atlId = '2';
+      const timestamp = Date.now();
+      const websiteUrl = `http://www.example${timestamp}.com`;
+      const today = new Date().toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+      const atlAddress = {
+        address: `address${timestamp}`,
+        city: `city${timestamp}`,
+        state: `state${timestamp}`,
+        zip: `11111${timestamp}`,
+        country: `country${timestamp}`,
+      };
+
       hooks.open('#/organizations/onc-atls');
       hooks.waitForSpinnerToDisappear();
       page.openOrganizationDetails(atl);
@@ -110,7 +59,6 @@ describe('the ONC-ATL Management page', () => {
       address.set(atlAddress);
       page.saveOrganizationButton.click();
       hooks.waitForSpinnerToDisappear();
-      toast.clearAllToast();
       expect(page.generalInformation(organizationType, atlId).getText()).toContain('Retired: No');
       hooks.open('#/organizations/onc-atls');
       hooks.waitForSpinnerToDisappear();
@@ -121,8 +69,6 @@ describe('the ONC-ATL Management page', () => {
       page.retirementDate.setValue(today);
       page.saveOrganizationButton.click();
       hooks.waitForSpinnerToDisappear();
-      browser.waitUntil(() => toast.toastContainer.isDisplayed());
-      toast.clearAllToast();
       expect(page.generalInformation(organizationType, atlId).getText()).toContain('Retired: Yes');
     });
   });
