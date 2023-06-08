6681782422815f10ca61dca8fca69706dbed5c33
Andrew Larned <andrew.larned@gmail.com>
2023-06-08 14:35:27 -0400

    test: expect ATLs to be editable
    [#OCD-4055]

--- e2e/pages/organizations/onc-acbs-atls/atls.spec.js
+++ e2e/pages/organizations/onc-acbs-atls/atls.spec.js
@@ -1,12 +1,10 @@
-import Hooks from '../../../utilities/hooks';
-import AddressComponent from '../../../components/address/address.po';
-import LoginComponent from '../../../components/login/login.sync.po';
+import LoginComponent from '../../../components/login/login.po';
+import AddressComponent from '../../../components/address/address.async.po';
 import { open } from '../../../utilities/hooks.async';
 
 import OrganizationPage from './organization.po';
 
 let address;
-let hooks;
 let login;
 let page;
 
@@ -25,7 +23,6 @@ describe('the ONC-ATL Management page', () => {
     await browser.setWindowSize(1600, 1024); // demo of a bigger screen (esp. useful for screenshots)
     await browser.setWindowRect(0, 0, 1600, 1024); // not sure if both are required
     page = new OrganizationPage();
-    hooks = new Hooks();
     login = new LoginComponent();
     address = new AddressComponent();
     await open('#/resources/overview');
@@ -43,21 +40,22 @@ describe('the ONC-ATL Management page', () => {
     });
 
     it('should allow user to Create a new ATL', async () => {
+      const initialCount = await page.organizationListCount();
       const newAtlName = `${'Zatl-'}${timestamp}`;
       await page.createOrganization('ATL');
       await (await page.getOrganizationName()).addValue(newAtlName);
       await (await page.getOrganizationWebsite()).addValue(websiteUrl);
       await address.set(atlAddress);
       await (await page.getSaveOrganizationButton()).click();
-      await hooks.waitForSpinnerToDisappear();
+      await (browser.waitUntil(async () => ((await page.organizationListCount()) > initialCount)));
       await page.openOrganizationDetails(newAtlName);
-      await expect(await (await page.getNewOrganizationGeneralInfo()).getText()).toContain(newAtlName);
-      await expect(await (await page.getNewOrganizationGeneralInfo()).getText()).toContain(websiteUrl);
-      await expect(await (await page.getNewOrganizationGeneralInfo()).getText()).toContain(atlAddress.address);
-      await expect(await (await page.getNewOrganizationGeneralInfo()).getText()).toContain(atlAddress.city);
-      await expect(await (await page.getNewOrganizationGeneralInfo()).getText()).toContain(atlAddress.state);
-      await expect(await (await page.getNewOrganizationGeneralInfo()).getText()).toContain(atlAddress.zip);
-      await expect(await (await page.getNewOrganizationGeneralInfo()).getText()).toContain(atlAddress.country);
+      await expect(await (await page.getOrganizationDetails(newAtlName)).getText()).toContain(newAtlName);
+      await expect(await (await page.getOrganizationDetails(newAtlName)).getText()).toContain(websiteUrl);
+      await expect(await (await page.getOrganizationDetails(newAtlName)).getText()).toContain(atlAddress.address);
+      await expect(await (await page.getOrganizationDetails(newAtlName)).getText()).toContain(atlAddress.city);
+      await expect(await (await page.getOrganizationDetails(newAtlName)).getText()).toContain(atlAddress.state);
+      await expect(await (await page.getOrganizationDetails(newAtlName)).getText()).toContain(atlAddress.zip);
+      await expect(await (await page.getOrganizationDetails(newAtlName)).getText()).toContain(atlAddress.country);
     });
   });
 });
