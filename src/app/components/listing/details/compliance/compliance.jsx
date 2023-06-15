71464e6ae2462098a450559293ffd248172fac7d
Andrew Larned <andrew.larned@gmail.com>
2023-06-15 09:06:45 -0400

    ui: get compliance section to look more like the rest of them
    [#OCD-4136]

--- src/app/components/listing/details/compliance/compliance.jsx
+++ src/app/components/listing/details/compliance/compliance.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
-  Accordion,
-  AccordionSummary,
-  Box,
+  Card,
   CardContent,
   Typography,
   makeStyles,
@@ -18,66 +16,21 @@ import { palette, utilStyles } from 'themes';
 
 const useStyles = makeStyles({
   ...utilStyles,
-  infoIcon: {
-    color: `${palette.primary}`,
-  },
-  root: {
-    width: '100%',
-    padding: '0 8px!important',
-  },
-  subCard: {
-    backgroundColor: `${palette.white}`,
-    borderBottom: '.5px solid #c2c6ca',
-  },
-  NestedAccordionLevelOne: {
-    borderRadius: '4px',
-    display: 'grid',
-    borderColor: ' #c2c6ca',
-    borderWidth: '.5px',
-    borderStyle: 'solid',
-    padding: '0px',
-    backgroundColor: `${palette.white}`,
-  },
-  NestedAccordionLevelOneSummary: {
-    backgroundColor: `${palette.secondary}!important`,
-    borderRadius: '4px',
-    borderBottom: '.5px solid #c2c6ca',
-    width: '100%',
-    padding: '0 8px!important',
-  },
-  NestedAccordionLevelTwoSummary: {
-    backgroundColor: `${palette.white}!important`,
-    borderRadius: '4px',
-    borderBottom: '.5px solid #c2c6ca',
-    width: '100%',
-    padding: '0 8px!important',
-  },
-  '& span.MuiTypography-root.MuiCardHeader-title.MuiTypography-h6.MuiTypography-displayBlock': {
-    fontWeight: '300',
-  },
 });
 
 function ChplCompliance({ directReviews, directReviewsAvailable, surveillance }) {
   const classes = useStyles();
 
   return (
-    <Accordion className={classes.NestedAccordionLevelOne}>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        className={classes.NestedAccordionLevelOneSummary}
-        color="secondary"
-      >
-        <Typography>
+    <Card>
+      <CardContent>
+        <Typography variant="subtitle">
           Compliance Activities
         </Typography>
-      </AccordionSummary>
-      <Box display="flex" flexDirection="column">
-        <CardContent>
-          <ChplSurveillance surveillance={surveillance} />
-          <ChplDirectReviews directReviews={directReviews} directReviewsAvailable={directReviewsAvailable} />
-        </CardContent>
-      </Box>
-    </Accordion>
+        <ChplSurveillance surveillance={surveillance} />
+        <ChplDirectReviews directReviews={directReviews} directReviewsAvailable={directReviewsAvailable} />
+      </CardContent>
+    </Card>
   );
 }
 
