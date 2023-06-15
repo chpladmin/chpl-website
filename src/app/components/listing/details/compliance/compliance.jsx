380f70bf0848be5dd9df7caa72e9bd4a422d2606
Andrew Larned <andrew.larned@gmail.com>
2023-06-15 09:10:46 -0400

    refactor: remove unused flag options
    [#OCD-4136]

--- src/app/components/listing/details/compliance/compliance.jsx
+++ src/app/components/listing/details/compliance/compliance.jsx
@@ -3,28 +3,19 @@ import {
   Card,
   CardContent,
   Typography,
-  makeStyles,
 } from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { arrayOf, bool } from 'prop-types';
 
 import ChplDirectReviews from './direct-reviews';
 import ChplSurveillance from './surveillance';
 
 import { directReview as directReviewPropType, surveillance as surveillancePropType } from 'shared/prop-types';
-import { palette, utilStyles } from 'themes';
-
-const useStyles = makeStyles({
-  ...utilStyles,
-});
 
 function ChplCompliance({ directReviews, directReviewsAvailable, surveillance }) {
-  const classes = useStyles();
-
   return (
     <Card>
       <CardContent>
-        <Typography variant="subtitle">
+        <Typography variant="subtitle1">
           Compliance Activities
         </Typography>
         <ChplSurveillance surveillance={surveillance} />
