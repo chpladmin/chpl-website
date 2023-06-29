2a9d21bd5210acbeea61e3d9473acd11f9e50829
Matthew Stankiewicz <stankm@pointclickcare.com>
2023-06-29 16:44:17 -0400

    ui: modified SED Task View format & breakpoints for mobile

--- src/app/components/listing/details/sed/sed-task-view.jsx
+++ src/app/components/listing/details/sed/sed-task-view.jsx
@@ -5,8 +5,8 @@ import {
   AccordionSummary,
   Box,
   Card,
+  CardContent,
   CardHeader,
-  Container,
   List,
   ListItem,
   Table,
@@ -24,12 +24,12 @@ import ChplSedTaskParticipantsView from './sed-task-participants-view';
 
 import { ChplHighlightCures } from 'components/util';
 import { sortCriteria } from 'services/criteria.service';
-import { palette, utilStyles } from 'themes';
+import { palette, utilStyles, theme } from 'themes';
 
 const useStyles = makeStyles({
   ...utilStyles,
   accordion: {
-    borderRadius: '8px',
+    borderRadius: '4px',
     display: 'grid',
     borderColor: palette.divider,
     borderWidth: '.5px',
@@ -50,8 +50,11 @@ const useStyles = makeStyles({
   taskData: {
     display: 'grid',
     flexDirection: 'column',
-    gridTemplateColumns: '1fr 1fr',
+    gridTemplateColumns: '1fr',
     gridGap: '16px',
+    [theme.breakpoints.up('sm')]: {
+      gridTemplateColumns: '1fr 1fr',
+    },
   },
   rotate: {
     transform: 'rotate(180deg)',
@@ -134,200 +137,202 @@ function ChplSedTaskView({ task: initialTask }) {
         className={classes.accordionDetails}
         id={`task-id-${task.id}-details`}
       >
-        <Container className={classes.taskData}>
-          <Card className={classes.fullWidthGridRow} id="summary">
-            <CardHeader title="Summary" />
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Task Description</TableCell>
-                  <TableCell>{ task.description }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Associated Certification Criteria</TableCell>
-                  <TableCell>
-                    <List>
-                      {task.criteria
-                        .sort(sortCriteria)
-                        .map((criterion) => (
-                          <ListItem key={criterion.id}>
-                            { criterion.removed && 'Removed | ' }
-                            { criterion.number }
-                            :
-                            {' '}
-                            <ChplHighlightCures text={criterion.title} />
-                          </ListItem>
-                        ))}
-                    </List>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Card>
-          <Card id="rating">
-            <CardHeader title="Rating" />
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Rating Scale</TableCell>
-                  <TableCell>{ task.taskRatingScale }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Task Rating</TableCell>
-                  <TableCell>{ task.taskRating }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Task Rating - Standard Deviation</TableCell>
-                  <TableCell>{ task.taskRatingStddev }</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Card>
-          <Card>
-            <CardHeader title="Task Time" />
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Task Time - Mean (s) </TableCell>
-                  <TableCell>{ task.taskTimeAvg }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Task Time - Standard Deviation (s)</TableCell>
-                  <TableCell>{ task.taskTimeStddev }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Task Time Deviation - Observed/Optimal (s)</TableCell>
-                  <TableCell>
-                    { task.taskTimeDeviationObservedAvg }
-                    {' '}
-                    /
-                    {' '}
-                    { task.taskTimeDeviationOptimalAvg }
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Card>
-          <Card id="success">
-            <CardHeader title="Task Success" />
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Task Success - Mean (%) </TableCell>
-                  <TableCell>{ task.taskSuccessAverage }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Task Success - Standard Deviation (%)</TableCell>
-                  <TableCell>{ task.taskSuccessStddev }</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Card>
-          <Card>
-            <CardHeader title="Task Errors" />
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Task Errors - Mean (%) </TableCell>
-                  <TableCell>{ task.taskErrors }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Task Errors - Standard Deviation (%)</TableCell>
-                  <TableCell>{ task.taskErrorsStddev }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Task Path Deviation - Observed/Optimal (# of Steps)</TableCell>
-                  <TableCell>
-                    { task.taskPathDeviationObserved }
-                    {' '}
-                    /
-                    {' '}
-                    { task.taskPathDeviationOptimal }
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Card>
-          <Card className={classes.fullWidthGridRow} id="participants">
-            <CardHeader title="Participants" />
-            <Box display="flex" flexDirection="row" justifyContent="flex-end" p={4}>
-              <ChplSedTaskParticipantsView
-                participants={task.testParticipants}
-              />
-            </Box>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>Total Number of Participants</TableCell>
-                  <TableCell>{ task.testParticipants.length }</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Occupation Breakdown</TableCell>
-                  <TableCell>
-                    <List>
-                      {occupations
-                        .map((occupation) => (
-                          <ListItem key={occupation.name}>
-                            { occupation.name }
-                            :
-                            {' '}
-                            { occupation.count}
-                            {' '}
-                            /
-                            { task.testParticipants.length }
-                            {' '}
-                            (
-                            { occupation.percentage }
-                            )
-                          </ListItem>
-                        ))}
-                    </List>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Product Experience - Mean (Months)</TableCell>
-                  <TableCell>{ meanExperience }</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Card>
-        </Container>
+        <CardContent>
+          <Box className={classes.taskData}>
+            <Card className={classes.fullWidthGridRow} id="summary">
+              <CardHeader title="Summary" />
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Task Description</TableCell>
+                    <TableCell>{ task.description }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Associated Certification Criteria</TableCell>
+                    <TableCell>
+                      <List>
+                        {task.criteria
+                          .sort(sortCriteria)
+                          .map((criterion) => (
+                            <ListItem key={criterion.id}>
+                              { criterion.removed && 'Removed | ' }
+                              { criterion.number }
+                              :
+                              {' '}
+                              <ChplHighlightCures text={criterion.title} />
+                            </ListItem>
+                          ))}
+                      </List>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Card>
+            <Card id="rating">
+              <CardHeader title="Rating" />
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Rating Scale</TableCell>
+                    <TableCell>{ task.taskRatingScale }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Task Rating</TableCell>
+                    <TableCell>{ task.taskRating }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Task Rating - Standard Deviation</TableCell>
+                    <TableCell>{ task.taskRatingStddev }</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Card>
+            <Card>
+              <CardHeader title="Task Time" />
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Task Time - Mean (s) </TableCell>
+                    <TableCell>{ task.taskTimeAvg }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Task Time - Standard Deviation (s)</TableCell>
+                    <TableCell>{ task.taskTimeStddev }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Task Time Deviation - Observed/Optimal (s)</TableCell>
+                    <TableCell>
+                      { task.taskTimeDeviationObservedAvg }
+                      {' '}
+                      /
+                      {' '}
+                      { task.taskTimeDeviationOptimalAvg }
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Card>
+            <Card id="success">
+              <CardHeader title="Task Success" />
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Task Success - Mean (%) </TableCell>
+                    <TableCell>{ task.taskSuccessAverage }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Task Success - Standard Deviation (%)</TableCell>
+                    <TableCell>{ task.taskSuccessStddev }</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Card>
+            <Card>
+              <CardHeader title="Task Errors" />
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Task Errors - Mean (%) </TableCell>
+                    <TableCell>{ task.taskErrors }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Task Errors - Standard Deviation (%)</TableCell>
+                    <TableCell>{ task.taskErrorsStddev }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Task Path Deviation - Observed/Optimal (# of Steps)</TableCell>
+                    <TableCell>
+                      { task.taskPathDeviationObserved }
+                      {' '}
+                      /
+                      {' '}
+                      { task.taskPathDeviationOptimal }
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Card>
+            <Card className={classes.fullWidthGridRow} id="participants">
+              <CardHeader title="Participants" />
+              <Box display="flex" flexDirection="row" justifyContent="flex-end" p={4}>
+                <ChplSedTaskParticipantsView
+                  participants={task.testParticipants}
+                />
+              </Box>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Description</TableCell>
+                    <TableCell>Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Total Number of Participants</TableCell>
+                    <TableCell>{ task.testParticipants.length }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Occupation Breakdown</TableCell>
+                    <TableCell>
+                      <List>
+                        {occupations
+                          .map((occupation) => (
+                            <ListItem key={occupation.name}>
+                              { occupation.name }
+                              :
+                              {' '}
+                              { occupation.count}
+                              {' '}
+                              /
+                              { task.testParticipants.length }
+                              {' '}
+                              (
+                              { occupation.percentage }
+                              )
+                            </ListItem>
+                          ))}
+                      </List>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Product Experience - Mean (Months)</TableCell>
+                    <TableCell>{ meanExperience }</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Card>
+          </Box>
+        </CardContent>
       </AccordionDetails>
     </Accordion>
   );
