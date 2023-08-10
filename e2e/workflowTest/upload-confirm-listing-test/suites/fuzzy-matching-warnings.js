a858130927118d8a94ac91ea9bbc761dbececd16
Katy Ekey <katy.ekey@gmail.com>
2023-08-10 10:28:00 -0400

    refactor: Use unique CHPL ID for fuzzy matching upload
    [#OCD-4269]

--- e2e/workflowTest/upload-confirm-listing-test/suites/fuzzy-matching-warnings.js
+++ e2e/workflowTest/upload-confirm-listing-test/suites/fuzzy-matching-warnings.js
@@ -2,7 +2,7 @@ const suite = {
   description: 'when doing fuzzy matching on QMS Standards',
   file: '../../../resources/listings/2015_FuzzyMatching.csv',
   listings: [{
-    listingId: '15.02.04.2701.MVL1.12.00.1.200620',
+    listingId: '15.02.04.2701.FUZZ.12.00.1.200620',
     expectedErrors: [],
     expectedWarnings: [
       'The QMS Standard value was changed from Home Grown mapped to ISO 9002 to Home Grown mapped to ISO 9001.',
