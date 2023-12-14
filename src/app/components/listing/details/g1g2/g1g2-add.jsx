c11c2c2d8c002d8d87060f8592aadbbd32b184e7
Andrew Larned <andrew.larned@gmail.com>
2023-12-14 07:36:24 -0500

    feat: allow selection of measures
    [#OCD-2839]

--- src/app/components/listing/details/g1g2/g1g2-add.jsx
+++ src/app/components/listing/details/g1g2/g1g2-add.jsx
@@ -16,6 +16,7 @@ import * as yup from 'yup';
 
 import { useFetchMeasures, useFetchMeasureTypes } from 'api/data';
 import { ChplTextField } from 'components/util';
+import { sortCriteria } from 'services/criteria.service';
 import { ListingContext } from 'shared/contexts';
 import { utilStyles } from 'themes';
 
@@ -46,12 +47,41 @@ const useStyles = makeStyles({
 const validationSchema = yup.object({
   newMeasureTest: yup.string()
     .required('Field is required'),
-  newMeasureDomain: yup.string()
+  newMeasureDomain: yup.object()
     .required('Field is required'),
 });
 
+const sortTests = (first, second) => {
+  const a = parseInt(first.substring(2), 10);
+  const b = parseInt(second.substring(2), 10);
+  return a - b;
+}
+
+const sortMeasures = (a, b) => {
+  if (!a.measureType) {
+    a = { measureType: 0, measure: a };
+  }
+  if (!b.measureType) {
+    b = { measureType: 0, measure: b };
+  }
+  if (!a.measure.id || !b.measure.id) {
+    return a.measure.id ? 1 : -1;
+  }
+  const getNum = test => parseInt(test.substring(2), 10);
+  return a.measure.removed !== b.measure.removed ? (a.measure.removed ? 1 : -1) :
+    a.measureType.name < b.measureType.name ? -1 : a.measureType.name > b.measureType.name ? 1 :
+    getNum(a.measure.abbreviation) < getNum(b.measure.abbreviation) ? -1 : getNum(a.measure.abbreviation) > getNum(b.measure.abbreviation) ? 1 :
+    a.measure.domain.name < b.measure.domain.name ? -1 : a.measure.domain.name > b.measure.domain.name ? 1 :
+    a.measure.name < b.measure.name ? -1 : a.measure.name > b.measure.name ? 1 :
+    0;
+}
+
 function ChplG1G2Add() {
   const { listing, setListing } = useContext(ListingContext);
+  const [measures, setMeasures] = useState([]);
+  const [tests, setTests] = useState([]);
+  const [types, setTypes] = useState([]);
+  const [isG1, setIsG1] = useState(true);
   const measuresQuery = useFetchMeasures();
   const measureTypesQuery = useFetchMeasureTypes();
   const classes = useStyles();
@@ -61,16 +91,25 @@ function ChplG1G2Add() {
     if (measuresQuery.isLoading || !measuresQuery.isSuccess) {
       return;
     }
-    console.log(measuresQuery.data);
-    //setCertificationStatuses(measuresQuery.data.sort((a, b) => (a.name < b.name ? -1 : 1)));
+    setMeasures(measuresQuery.data.data
+                .map((m) => ({
+                  ...m,
+                  displayName: `${m.removed ? 'Removed | ' : ''}${m.domain.name}`,
+                  displayCriteria: [... new Set(m.allowedCriteria.map((c) => c.number))].sort(sortCriteria),
+                }))
+                .sort(sortMeasures));
+    setTests([... new Set(measuresQuery.data.data.map((m) => m.abbreviation))]
+              .sort(sortTests));
   }, [measuresQuery.data, measuresQuery.isLoading, measuresQuery.isSuccess]);
 
   useEffect(() => {
     if (measureTypesQuery.isLoading || !measureTypesQuery.isSuccess) {
       return;
     }
-    console.log(measureTypesQuery.data);
-    //setCertificationStatuses(measureTypesQuery.data.sort((a, b) => (a.name < b.name ? -1 : 1)));
+    setTypes(measureTypesQuery.data.data
+             .map((t) => t)
+             .sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+            );
   }, [measureTypesQuery.data, measureTypesQuery.isLoading, measureTypesQuery.isSuccess]);
 
   const add = (v) => {
@@ -87,9 +126,8 @@ function ChplG1G2Add() {
           measure,
         }),
     });
-    formik.setFieldValue('newStatusType', '');
-    formik.setFieldValue('newStatusDay', '');
-    formik.setFieldValue('newStatusReason', '');
+    formik.setFieldValue('newMeasureTest', '');
+    formik.setFieldValue('newMeasureDomain', '');
     //setAddingStatus(false);
   };
 
@@ -97,8 +135,8 @@ function ChplG1G2Add() {
     console.log(event.target.value);
   };
 
-  const toggleType = (event) => {
-    console.log(event.target.value);
+  const toggleType = () => {
+    setIsG1((prev) => !prev);
   };
 
   formik = useFormik({
@@ -129,8 +167,8 @@ function ChplG1G2Add() {
         error={formik.touched.newMeasureTest && !!formik.errors.newMeasureTest}
         helperText={formik.touched.newMeasureTest && formik.errors.newMeasureTest}
       >
-        { [].map((item) => (
-          <MenuItem value={item} key={item.id}>{item.name}</MenuItem>
+        { tests.map((item) => (
+          <MenuItem value={item} key={item}>{item}</MenuItem>
         ))}
       </ChplTextField>
       <ChplTextField
@@ -139,27 +177,35 @@ function ChplG1G2Add() {
         name="newMeasureDomain"
         label="New Test Domain"
         required
+        disabled={!formik.values.newMeasureTest}
         value={formik.values.newMeasureDomain}
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
         error={formik.touched.newMeasureDomain && !!formik.errors.newMeasureDomain}
         helperText={formik.touched.newMeasureDomain && formik.errors.newMeasureDomain}
       >
-        { [].map((item) => (
-          <MenuItem value={item} key={item.id}>{item.name}</MenuItem>
-        ))}
+        { measures
+          .filter((m) => m.abbreviation === formik.values.newMeasureTest)
+          .map((item) => (
+            <MenuItem value={item} key={item.id}>{item.displayName}</MenuItem>
+          ))}
       </ChplTextField>
-      <Switch
-        id="new-measure-type"
-        color="primary"
-        size="small"
-        onChange={toggleType}
-        checked={false}
+      <FormControlLabel
+        control={(
+          <Switch
+            id="is-g1"
+            color="primary"
+            size="small"
+            onChange={toggleType}
+            checked={isG1}
+          />
+        )}
+        label={`${isG1 ? 'G1' : 'G2'}`}
       />
       <FormControlLabel
         label="criteria"
         key="criteria"
-        control={
+        control={(
           <Checkbox
             color="primary"
             name="criteria"
@@ -167,7 +213,7 @@ function ChplG1G2Add() {
             onChange={toggleCriteria}
             checked={false}
           />
-        }
+        )}
       />
     </>
   );
