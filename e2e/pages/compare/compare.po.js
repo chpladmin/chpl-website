2c6845f7a14fc32d9e96d595d79ab8012add6401
Andrew Larned <andrew.larned@gmail.com>
2023-12-18 10:21:32 -0500

    fix: use enough async/await stuff to get the tests to pass
    [#OCD-4414]

--- e2e/pages/compare/compare.po.js
+++ e2e/pages/compare/compare.po.js
@@ -1,3 +1,5 @@
+import { open } from '../../utilities/hooks.async';
+
 class ComparePage {
   constructor() {
     this.elements = {
@@ -5,6 +7,19 @@ class ComparePage {
       cqmRow: (id) => `#cqm-${id}`,
       chplProductNumber: (number) => `td*=${number}`,
     };
+  }
+
+  async open(ids) {
+    await open(`#/compare/${ids.join('&')}`);
+    await browser.waitUntil(async () => {
+      const table = await $('table');
+      const head = await table.$('thead');
+      const rows = await head.$$('tr');
+      const row = await rows[0];
+      const cells = await row.$$('th');
+      const isReady = cells.length === (ids.length + 1);
+      return isReady;
+    });
   }
 
   async getCellWithCriteriaId(id) {
@@ -15,24 +30,34 @@ class ComparePage {
     return $(this.elements.cqmRow(id)).$$('td')[0];
   }
 
-  async isListingLoaded(chplProductNumber) {
-    return $(this.elements.chplProductNumber(chplProductNumber)).isDisplayed();
-  }
-
   async findColumnIndex(chplProductNumber) {
-    return $(this.elements.chplProductNumber(chplProductNumber))
-      .parentElement()
-      .$$('td')
-      .findIndex(async (ele) => await(ele.getText()) === chplProductNumber);
+    const startCell = await $(this.elements.chplProductNumber(chplProductNumber));
+    const row = await startCell.parentElement();
+    const cells = await row.$$('td');
+    let index, text, isFound;
+    for (let i = 0; i < cells.length; i++) {
+      text = await cells[i].getText();
+      isFound = text === chplProductNumber;
+      if (isFound) { index = i; }
+    };
+    return index;
   }
 
   async getDecertificationDate(chplProductNumber) {
-    return $('table')
-      .$('tbody')
-      .$$('tr')
-      .find(async (row) => await (row.getText()).includes('Inactive/Decertified Date'))
-      .$$('td')[this.findColumnIndex(chplProductNumber)]
-      .getText();
+    const table = await $('table');
+    const body = await table.$('tbody');
+    const rows = await body.$$('tr');
+    let row, test, isFound;
+    for (let i = 0; i < rows.length; i++) {
+      test = await rows[i].getText();
+      isFound = test.includes('Inactive/Decertified Date');
+      if (isFound) { row = rows[i] };
+    };
+    const cells = await row.$$('td');
+    const index = await this.findColumnIndex(chplProductNumber);
+    const cell = cells[index];
+    const text = await cell.getText();
+    return text;
   }
 }
 
