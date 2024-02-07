7072154692f48a287904827a99f06b472167c521
Ashwini More <morea@pointclickcare.com>
2024-02-06 21:29:40 -0500

    test: convert criteria tests to async & update ignored tests to be functional

--- e2e/components/listing/details/criteria/criteria.readonly.spec.js
+++ e2e/components/listing/details/criteria/criteria.readonly.spec.js
@@ -1,55 +1,56 @@
 import Hooks from '../../../../utilities/hooks';
+import ListingPage from '../../../../pages/listing/listing.po';
 
 import CriteriaComponent from './criteria.po';
 
 let criteria;
+let page;
 let hooks;
 
 beforeEach(async () => {
   criteria = new CriteriaComponent();
+  page = new ListingPage();
   hooks = new Hooks();
 });
 
 describe('the 2015 listing page', () => {
   beforeEach(async () => {
-    hooks.open('#/listing/9833');
+    await hooks.open('#/listing/9833');
     await hooks.waitForSpinnerToDisappear();
+    await page.criteriaLeftNavButton();
   });
 
-  it('should display attested criteria by default', () => {
-    browser.waitUntil(() => criteria.criteriaCount() > 1);
-    expect(criteria.criteriaCount()).toBeGreaterThan(35);
+  it('should display attested criteria by default', async () => {
+    await browser.waitUntil(async () => (await criteria.criteriaCount()) > 1);
+    await expect(await criteria.criteriaCount()).toBeGreaterThan(35);
   });
 
-  it.skip('should display removed criteria header', () => {
-    expect(criteria.removedCriteriaHeader.isDisplayed()).toBe(true);
+  it('should display removed criteria header', async () => {
+    await expect(await criteria.removedCriteriaHeader.isDisplayed()).toBe(true);
   });
 
-  it('should display view only a1 criteria details', () => {
-    criteria.expandCriteria('1');
-    expect(criteria.criteriaDetailTable('1').isDisplayed()).toBe(true);
-  });
-
-  it('should display view only removed a6 criteria details', () => {
-    criteria.expandRemovedCriteria();
-    criteria.expandCriteria('6');
-    expect(criteria.criteriaDetailTable('6').isDisplayed()).toBe(true);
+  it('should display view only a1 criteria details', async () => {
+    await criteria.expandCriteria('1');
+    await hooks.waitForSpinnerToDisappear();
+    await expect(await (await criteria.criteriaDetails('1')).isDisplayed()).toBe(true);
   });
 });
 
 describe('the 2014 listing page', () => {
   beforeEach(async () => {
-    hooks.open('#/listing/8490');
+    await hooks.open('#/listing/8490');
     await hooks.waitForSpinnerToDisappear();
+    await page.criteriaLeftNavButton();
   });
 
-  it('should display attested criteria by default', () => {
-    browser.waitUntil(() => criteria.criteriaCount() > 1);
-    expect(criteria.criteriaCount()).toBeGreaterThan(35);
+  it('should display attested criteria by default', async () => {
+    await browser.waitUntil(async () => (await criteria.criteriaCount()) > 1);
+    await expect(await criteria.criteriaCount()).toBeGreaterThan(35);
   });
 
-  it('should display view only a1 criteria details', () => {
-    criteria.expandCriteria('61');
-    expect(criteria.criteriaDetailTable('61').isDisplayed()).toBe(true);
+  it('should display view only a1 criteria details', async () => {
+    await criteria.expandCriteria('61');
+    await hooks.waitForSpinnerToDisappear();
+    await expect(await (await criteria.criteriaDetails('61')).isDisplayed()).toBe(true);
   });
 });
