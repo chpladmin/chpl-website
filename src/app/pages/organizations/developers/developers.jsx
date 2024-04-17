19bdb5e2d0898180141ddf2a1176d2a301c36a44
Andrew Larned <andrew.larned@gmail.com>
2024-04-17 11:41:38 -0400

    feat: enable searching by active listing status
    [#OCD-3907]

--- src/app/pages/organizations/developers/developers.jsx
+++ src/app/pages/organizations/developers/developers.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import ChplDevelopersView from './developers-view';
 
 import { useFetchAcbs } from 'api/acbs';
-import { FilterProvider } from 'components/filter';
+import { FilterProvider, defaultFilter } from 'components/filter';
 import {
   certificationBodies,
   decertificationDate,
@@ -12,7 +12,17 @@ import {
 
 const staticFilters = [
   decertificationDate,
-  developerStatus,
+  developerStatus, {
+    ...defaultFilter,
+    key: 'activeListingsOptions',
+    display: 'Active Listings',
+    operatorKey: 'activeListingsOptionsOperator',
+    values: [
+      { value: 'has_any_active', display: 'Has Any Active' },
+      { value: 'has_no_active', display: 'Has No Active' },
+      { value: 'had_any_active_during_most_recent_past_attestation_period', display: 'Had Any Active During Most Recent Past Attestation Period' },
+    ],
+  },
 ];
 
 function ChplDevelopersPage() {
