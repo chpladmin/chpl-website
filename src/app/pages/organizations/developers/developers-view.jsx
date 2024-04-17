b4ce2186f965711a11bb068f36f35fc54153f360
Andrew Larned <andrew.larned@gmail.com>
2024-04-17 14:17:58 -0400

    ui: remove unnecessary text
    [#OCD-3907]

--- src/app/pages/organizations/developers/developers-view.jsx
+++ src/app/pages/organizations/developers/developers-view.jsx
@@ -157,30 +157,13 @@ function ChplDevelopersView(props) {
     <>
       <div className={classes.pageHeader}>
         <Typography variant="h1">
-          Developers Under Certification Ban
+          Developers
         </Typography>
       </div>
       <div className={classes.pageBody} id="main-content" tabIndex="-1">
         <div>
-          <Typography variant="body1" gutterBottom>
-            This is a list of health IT developers currently precluded from certifying any health IT products under the ONC Health IT Certification Program - including new products as well as upgraded versions of current products. ONC may lift these statuses if it determines that the developer has taken appropriate steps to remedy problems or issues for all affected products and users and prevent their recurrence. A developer may be precluded from certifying products for two reasons:
-          </Typography>
-          <ol>
-            <li>
-              <strong>Developer Failure to Take Appropriate Corrective Action</strong>
-              {' '}
-              A developer may be precluded from the Program if the developer or one of its products fails to comply with any requirements of certification and the developer fails to take appropriate actions to correct the non-compliance.
-            </li>
-            <li>
-              <strong>Product Withdrawn While Under Surveillance</strong>
-              {' '}
-              A developer may also be precluded if it fails to cooperate with the surveillance or other oversight of its certified products. ONC may lift the ban if it determines that the developer has taken appropriate steps to remedy problems or issues for all affected products and users and prevent their recurrence.
-            </li>
-          </ol>
           <Typography variant="body1">
-            Health IT products currently listed on the CHPL will maintain their listed certification status regardless of whether their developer is precluded from the program. Please consult your health IT product’s details page to confirm its certification status by
-            {' '}
-            <a href="https://chpl.healthit.gov/#/search">searching for the product</a>
+            Text here?
           </Typography>
         </div>
       </div>
