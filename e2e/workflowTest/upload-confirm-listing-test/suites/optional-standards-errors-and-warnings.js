607b74afcb09057e9afc6454ae952462c20a8c5c
Katy Ekey <katy.ekey@gmail.com>
2024-04-25 17:03:37 -0400

    test[aqa]: Add aqa for optional standard fuzzy match

--- e2e/workflowTest/upload-confirm-listing-test/suites/optional-standards-errors-and-warnings.js
+++ e2e/workflowTest/upload-confirm-listing-test/suites/optional-standards-errors-and-warnings.js
@@ -5,13 +5,14 @@ const suite = {
     listingId: '15.04.04.1722.OPTS.V1.00.1.210607',
     expectedErrors: [
       'Criteria 170.315 (b)(1) contains an optional standard \'BAD OS\' which does not exist.',
-      'Optional Standard 170.204(a)(2) is not valid for criteria 170.315 (b)(1).',
+      'Optional Standard WCAG 2.0, Level AA Conformance is not valid for criteria 170.315 (b)(1).',
       '170.315 (g)(3) is required but was not found.',
       'Listing has not attested to (g)(3), but at least one criteria was found attesting to SED.',
     ],
     expectedWarnings: [
       'Optional Standards are not applicable for the criterion 170.315 (a)(1). They have been removed.',
-      'Certification 170.315 (b)(1) contains duplicate Optional Standard: \'170.207(a)(4)\'. The duplicates have been removed.',
+      'Optional Standard \'SNOMED CT\' was changed to \'SNOMED CT®\' for criteria 170.315 (b)(1).',
+      'Certification 170.315 (b)(1) contains duplicate Optional Standard: \'SNOMED CT®\'. The duplicates have been removed.',
     ],
   }],
 };
