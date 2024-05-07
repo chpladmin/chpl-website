08753e0aee23152723295cbe29241abe9a650e0b
Katy Ekey <katy.ekey@gmail.com>
2024-05-07 15:05:19 -0400

    feat!: Add new subscription subjects to filters

--- src/app/components/filter/filters/subscription-subjects.jsx
+++ src/app/components/filter/filters/subscription-subjects.jsx
@@ -8,6 +8,9 @@ const filter = {
     { value: 'Certification Status Changed' },
     { value: 'Certification Criterion Added' },
     { value: 'Certification Criterion Removed' },
+    { value: 'RWT Plans URL Changed' },
+    { value: 'RWT Results URL Changed' },
+    { value: 'Service Base URL List Changed' },
   ],
 };
 
