0a574692cedc259fc6058b7c4e49486d4555f461
Andrew Larned <andrew.larned@gmail.com>
2024-05-13 09:09:29 -0400

    test: default screen to a "normal" sized screen
    [#OCD-4522]

--- test/pageobjects/page.es6.js
+++ test/pageobjects/page.es6.js
@@ -8,12 +8,16 @@ class Page {
   constructor() {
     this.name = 'Page';
     this.elements = {};
+    this.width = 1600;
+    this.height = 1024;
   }
+
   /**
    * Opens a sub page of the page
    * @param path path of the sub page (e.g. /path/to/page.html)
    */
-  open (path) {
+  async open(path) {
+    await browser.setWindowSize(this.width, this.height);
     return browser.url(`#/${path}`);
   }
 }
