1fbfef023d217e668dcf855c3a0b368ce1e30960
Andrew Larned <andrew.larned@gmail.com>
2024-06-05 14:41:30 -0400

    fix: use listingType parameter
    [#OCD-4600]

--- src/app/pages/resources/download/download.jsx
+++ src/app/pages/resources/download/download.jsx
@@ -107,9 +107,9 @@ function ChplResourcesDownload() {
 
   useEffect(() => {
     const data = {
-      'Active products summary': { data: `${API}/listings/download/active?api_key=${getApiKey()}&format=csv`, definition: `${API}/listings/download/active?api_key=${getApiKey()}&format=csv&definition=true`, label: 'Active products' },
-      'Inactive products summary': { data: `${API}/listings/download/inactive?api_key=${getApiKey()}&format=csv`, definition: `${API}/listings/download/inactive?api_key=${getApiKey()}&format=csv&definition=true`, label: 'Inactive products' },
-      '2014 edition summary': { data: `${API}/listings/download/2014?api_key=${getApiKey()}&format=csv`, definition: `${API}/listings/download/2014?api_key=${getApiKey()}&format=csv&definition=true`, label: '2014 products' },
+      'Active products summary': { data: `${API}/listings/download?listingType=active&api_key=${getApiKey()}&format=csv`, definition: `${API}/listings/download?listingType=active&api_key=${getApiKey()}&format=csv&definition=true`, label: 'Active products' },
+      'Inactive products summary': { data: `${API}/listings/download?listingType=inactive&api_key=${getApiKey()}&format=csv`, definition: `${API}/listings/download?listingType=inactive&api_key=${getApiKey()}&format=csv&definition=true`, label: 'Inactive products' },
+      '2014 edition summary': { data: `${API}/listings/download?listingType=2014&api_key=${getApiKey()}&format=csv`, definition: `${API}/listings/download?listingType=2014&api_key=${getApiKey()}&format=csv&definition=true`, label: '2014 products' },
       'SVAP Summary': { data: `${API}/svap/download?api_key=${getApiKey()}`, definition: `${API}/svap/download?api_key=${getApiKey()}&definition=true`, label: 'SVAP Summary' },
       'Surveillance (Basic)': { data: `${API}/surveillance/download?api_key=${getApiKey()}&type=basic&authorization=Bearer%20${getToken()}`, definition: `${API}/surveillance/download?api_key=${getApiKey()}&type=basic&definition=true&authorization=Bearer%20${getToken()}`, label: 'Surveillance (Basic)' },
       'Surveillance Activity': { data: `${API}/surveillance/download?api_key=${getApiKey()}&type=all`, definition: `${API}/surveillance/download?api_key=${getApiKey()}&type=all&definition=true`, label: 'Surveillance' },
