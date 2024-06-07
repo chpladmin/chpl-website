fea799025a1323605f4a972310fadf8014603265
Andrew Larned <andrew.larned@gmail.com>
2024-06-07 07:32:54 -0400

    feat: require password on Cognito user create
    [#OCD-4602]

--- src/app/components/registration/cognito-user-create.jsx
+++ src/app/components/registration/cognito-user-create.jsx
@@ -11,7 +11,10 @@ import {
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 
+import { PasswordStrengthMeter } from 'components/login';
 import { ChplTextField } from 'components/util';
+
+const zxcvbn = require('zxcvbn');
 
 const useStyles = makeStyles(() => ({
   content: {
@@ -32,9 +35,25 @@ const validationSchema = yup.object({
   phoneNumber: yup.string()
     .required('Phone Number is required')
     .matches(phoneRegExp, 'Phone number is not valid'),
+  password: yup.string()
+    .required('Password is required')
+    .test(
+      'password-strength',
+      'Password is not strong enough',
+      (value, context) => context.parent.passwordStrength >= 3,
+    ),
+  passwordVerify: yup.string()
+    .required('Verify Password is required')
+    .test(
+      'password-matches',
+      'Verify Password does not match Password',
+      (value, context) => value === context.parent.password,
+    ),
 });
 
 function ChplCognitoUserCreate(props) {
+  const [passwordMessages, setPasswordMessages] = useState([]);
+  const [strength, setStrength] = useState(0);
   const classes = useStyles();
   
   let formik;
@@ -44,12 +63,28 @@ function ChplCognitoUserCreate(props) {
       email: formik.values.email,
       friendlyName: formik.values.friendlyName,
       fullName: formik.values.fullName,
-      password: formik.values.newPassword,
-      passwordVerify: formik.values.verificationPassword,
+      password: formik.values.password,
+      passwordVerify: formik.values.passwordVerify,
       phoneNumber: formik.values.phoneNumber,
       title: formik.values.title,
     };
     props.dispatch('cognito-create', user);
+  };
+
+  const updatePassword = (event) => {
+    const vals = ['chpl'];
+    if (formik.values.fullName) { vals.push(formik.values.fullName); }
+    if (formik.values.email) { vals.push(formik.values.email); }
+    if (formik.values.phoneNumber) { vals.push(formik.values.phoneNumber); }
+    const passwordStrength = zxcvbn(event.target.value, vals);
+    formik.values.passwordStrength = passwordStrength.score;
+    setStrength(passwordStrength.score);
+    setPasswordMessages(
+      [passwordStrength.feedback?.warning]
+        .concat(passwordStrength.feedback?.suggestions)
+        .filter((msg) => msg),
+    );
+    formik.handleChange(event);
   };
 
   formik = useFormik({
@@ -59,6 +94,9 @@ function ChplCognitoUserCreate(props) {
       title: '',
       phoneNumber: '',
       email: '',
+      password: '',
+      passwordVerify: '',
+      passwordStrength: 0,
     },
     onSubmit: () => {
       create();
@@ -74,7 +112,7 @@ function ChplCognitoUserCreate(props) {
         Welcome to ONC’s Certified Health IT Product List (CHPL). You have been invited to be an Administrator, which will allow you to manage your organization’s information on the CHPL. Please log in to your existing account to add any permissions and/or organizations, or create a new account by completing the form and selecting the ‘create account’ button below.
       </Typography>
       <Typography>
-        Create a new account.
+        Create a new Cognito account.
       </Typography>
       <ChplTextField
         id="full-name"
@@ -128,6 +166,41 @@ function ChplCognitoUserCreate(props) {
         onBlur={formik.handleBlur}
         error={formik.touched.email && !!formik.errors.email}
         helperText={formik.touched.email && formik.errors.email}
+      />
+      <ChplTextField
+        type="password"
+        id="password"
+        name="password"
+        label="Password"
+        required
+        value={formik.values.password}
+        onChange={updatePassword}
+        onBlur={formik.handleBlur}
+        error={formik.touched.password && !!formik.errors.password}
+        helperText={formik.touched.password && formik.errors.password}
+      />
+      <PasswordStrengthMeter
+        value={strength}
+      />
+      { passwordMessages.length > 0
+        && (
+          <ul>
+            { passwordMessages.map((msg) => (
+              <li key={msg}>{ msg }</li>
+            ))}
+          </ul>
+        )}
+      <ChplTextField
+        type="password"
+        id="password-verify"
+        name="passwordVerify"
+        label="Password Verify"
+        required
+        value={formik.values.passwordVerify}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.passwordVerify && !!formik.errors.passwordVerify}
+        helperText={formik.touched.passwordVerify && formik.errors.passwordVerify}
       />
       <Button
         color="primary"
