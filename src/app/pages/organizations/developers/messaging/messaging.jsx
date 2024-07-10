b44d0f0d520efeb3894ce08d486ec06c73e48cf7
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-07-10 15:16:45 -0400

    feat: reformat message view with sticky nav and two column approach
    [#OCD-4569]

--- src/app/pages/organizations/developers/messaging/messaging.jsx
+++ src/app/pages/organizations/developers/messaging/messaging.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {
+  Box,
   Button,
-  ButtonGroup,
   Card,
   CardContent,
   Container,
+  Divider,
   Table,
   TableBody,
   TableCell,
@@ -13,35 +14,53 @@ import {
   Typography,
   makeStyles,
 } from '@material-ui/core';
-import { func } from 'prop-types';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import { useSnackbar } from 'notistack';
 import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 import SendIcon from '@material-ui/icons/Send';
-
-import { useFetchDevelopersBySearch, usePostMessage, usePostMessagePreview } from 'api/developer';
+import SendOutlined from '@material-ui/icons/SendOutlined';
+import { useFormik } from 'formik';
+import { useSnackbar } from 'notistack';
+import { func } from 'prop-types';
+import * as yup from 'yup';
+import {
+  useFetchDevelopersBySearch,
+  usePostMessage,
+  usePostMessagePreview,
+} from 'api/developer';
 import { useFilterContext } from 'components/filter';
 import { ChplLink, ChplTextField } from 'components/util';
-import { utilStyles } from 'themes';
+import { palette, theme, utilStyles } from 'themes';
 
 const useStyles = makeStyles({
   ...utilStyles,
+  pre: {
+    wordBreak: 'break-all',
+    whiteSpace: 'break-spaces',
+  },
   pageHeader: {
     padding: '32px',
     backgroundColor: '#ffffff',
   },
   pageBody: {
-    display: 'grid',
-    gridTemplateColumns: '1fr',
+    display: 'flex',
+    flexDirection: 'row',
     gap: '16px',
-    padding: '16px 32px',
+    padding: '16px',
     backgroundColor: '#f9f9f9',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+    },
   },
   content: {
     display: 'flex',
     flexDirection: 'column',
     gap: '16px',
+  },
+  code: {
+    textWrap: 'pretty',
+  },
+  cancelButton: {
+    color: palette.error,
+    border: `1px solid ${palette.error}`,
   },
   actionBarButton: {
     minWidth: '15vw',
@@ -51,13 +70,35 @@ const useStyles = makeStyles({
     justifyContent: 'center',
     padding: '16px 0',
   },
+  stickyBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'sticky',
+    gap: '32px',
+    top: '110px',
+    height: 'min-content',
+    width: '70%',
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+      position: 'relative',
+      top: 'auto',
+    },
+  },
+  rightColumn:{
+    width: '30%,',
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+    },
+  },
+  rowHeader:{
+    paddingTop: '8px',
+    padddingBottom: '8px',
+  },
 });
 
 const validationSchema = yup.object({
-  subject: yup.string()
-    .required('Subject is required'),
-  body: yup.string()
-    .required('Message body is required'),
+  subject: yup.string().required('Subject is required'),
+  body: yup.string().required('Message body is required'),
 });
 
 function ChplMessaging({ dispatch }) {
@@ -79,45 +120,52 @@ function ChplMessaging({ dispatch }) {
   });
 
   useEffect(() => {
-    if (isLoading) { return; }
-    if (isError || !data.results) {
-      return;
-    }
-    if (isLoading || !data.results) { return; }
+    if (isLoading) return;
+    if (isError || !data.results) return;
+    if (isLoading || !data.results) return;
     setRecordCount(data.recordCount);
   }, [data?.results, data?.recordCount, isError, isLoading]);
 
   const sendMessage = () => {
-    postMessage.mutate({
-      subject: formik.values.subject,
-      body: formik.values.body,
-      query: queryParams(),
-    }, {
-      onSuccess: () => {
-        enqueueSnackbar('Message queued', { variant: 'success' });
-        dispatch();
+    postMessage.mutate(
+      {
+        subject: formik.values.subject,
+        body: formik.values.body,
+        query: queryParams(),
       },
-      onError: (error) => {
-        const body = `An error occurred: ${error.response?.data?.error}`;
-        enqueueSnackbar(body, { variant: 'error' });
-      },
-    });
+      {
+        onSuccess: () => {
+          enqueueSnackbar('Message queued', { variant: 'success' });
+          dispatch();
+        },
+        onError: (error) => {
+          const body = `An error occurred: ${error.response?.data?.error}`;
+          enqueueSnackbar(body, { variant: 'error' });
+        },
+      }
+    );
   };
 
   const sendMessagePreview = () => {
-    postMessagePreview.mutate({
-      subject: formik.values.subject,
-      body: formik.values.body,
-      query: queryParams(),
-    }, {
-      onSuccess: () => {
-        enqueueSnackbar('Message preview has been queued. Please check your email to verify formatting', { variant: 'success' });
+    postMessagePreview.mutate(
+      {
+        subject: formik.values.subject,
+        body: formik.values.body,
+        query: queryParams(),
       },
-      onError: (error) => {
-        const body = `An error occurred: ${error.response?.data?.error}`;
-        enqueueSnackbar(body, { variant: 'error' });
-      },
-    });
+      {
+        onSuccess: () => {
+          enqueueSnackbar(
+            'Message preview has been queued. Please check your email to verify formatting',
+            { variant: 'success' }
+          );
+        },
+        onError: (error) => {
+          const body = `An error occurred: ${error.response?.data?.error}`;
+          enqueueSnackbar(body, { variant: 'error' });
+        },
+      }
+    );
   };
 
   formik = useFormik({
@@ -131,26 +179,30 @@ function ChplMessaging({ dispatch }) {
     },
   });
 
+  const minRows = window.outerWidth >= 1200 ? 16 : 8;
+
   return (
-    <>
+    <div>
       <div className={classes.pageHeader}>
-        <Typography variant="h1">
-          Messaging
-        </Typography>
+        <Typography variant="h1">Messaging</Typography>
       </div>
-      <div className={classes.pageBody} id="main-content" tabIndex="-1">
-        <Container maxWidth="md">
+      <Container
+        maxWidth={'xl'}
+        className={classes.pageBody}
+        id="main-content"
+        tabIndex="-1"
+      >
+        <Box
+          className={classes.stickyBox}
+        >
           <Card>
-            <CardContent
-              className={classes.content}
-            >
-              <Typography variant="body1">
-                Messaging
-                {' '}
-                { recordCount }
-                {' '}
-                developers
+            <CardContent className={classes.content}>
+              <Typography variant="h3" component={'h2'}>
+                <strong>
+                  Messaging {recordCount} developers
+                </strong>
               </Typography>
+              <Divider />
               <ChplTextField
                 id="subject"
                 name="subject"
@@ -174,109 +226,195 @@ function ChplMessaging({ dispatch }) {
                 onBlur={formik.handleBlur}
                 error={formik.touched.body && !!formik.errors.body}
                 helperText={formik.touched.body && formik.errors.body}
-                minRows={4}
+                minRows={minRows}
               />
             </CardContent>
           </Card>
-        </Container>
-        <Container maxWidth="md">
-          <Card>
-            <CardContent
-              className={classes.content}
+          <Card bgcolor={'white'}>
+            <Box
+              padding={'16px'}
+              display={'flex'}
+              flexDirection={'row'}
+              justifyContent={'space-between'}
             >
-              <Typography variant="h2">
-                Markdown reference
+              <Box display={'flex'} flexDirection={'row'} gridGap={'16px'}>
+                <Button
+                  onClick={dispatch}
+                  variant="outlined"
+                  className={classes.cancelButton}
+                  endIcon={<CloseOutlinedIcon />}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  onClick={sendMessagePreview}
+                  disabled={!formik.isValid}
+                  variant="outlined"
+                  color="primary"
+                  endIcon={<SendOutlined />}
+                >
+                  Send Message Preview
+                </Button>
+              </Box>
+              <Button
+                onClick={formik.handleSubmit}
+                disabled={!formik.isValid}
+                variant="contained"
+                color="primary"
+                endIcon={<SendIcon />}
+              >
+                Send Message
+              </Button>
+            </Box>
+          </Card>
+        </Box>
+        <Box className={classes.rightColumn}>
+          <Card>
+            <CardContent className={classes.content}>
+              <Typography sx={{ mt: 0.5 }} variant="h4" component={'h3'}>
+                <strong>Markdown reference</strong>
               </Typography>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Type ...</TableCell>
-                    <TableCell>... to get</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableRow>
-                    <TableCell><pre>_Italic_</pre></TableCell>
-                    <TableCell><i>Italic</i></TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre>**Bold**</pre></TableCell>
-                    <TableCell><b>Bold</b></TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre># Heading 1</pre></TableCell>
-                    <TableCell><h1>Heading 1</h1></TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre>## Heading 2</pre></TableCell>
-                    <TableCell><h2>Heading 2</h2></TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre>[Link](http://www.example.com)</pre></TableCell>
-                    <TableCell><a href="http://www.example.com">Link</a></TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre>* List<br />* List<br />    * Put four spaces before the "*" to make a sub-bullet<br />* List</pre></TableCell>
-                    <TableCell><ul><li>List</li><li>List</li><ul><li>Put four spaces before the "*" to make a sub-bullet</li></ul><li>List</li></ul></TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre>1. One<br />2. Two<br />3. Three</pre></TableCell>
-                    <TableCell><ol><li>One</li><li>Two</li><li>Three</li></ol></TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre>A paragraph of text<br /><br />Followed by a blank line<br/><br/>To get multiple paragraphs</pre></TableCell>
-                    <TableCell>A paragraph of text<br /><br />Followed by a blank line<br/><br/>To get multiple paragraphs</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell><pre>From: (with two spaces at the end of the line)  <br />To put a newline in a paragraph</pre></TableCell>
-                    <TableCell>From:<br />To put a newline in a paragraph</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
+              <Divider />
+              <Card>
+                <Table size="small">
+                  <TableHead sx={{ py: 4 }}>
+                    <TableRow className={classes.rowHeader}>
+                      <TableCell>Type ...</TableCell>
+                      <TableCell>... to get</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}>_Italic_</pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        <i>Italic</i>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}>**Bold**</pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        <b>Bold</b>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}># Heading 1</pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        <h1>Heading 1</h1>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}>## Heading 2</pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        <h2>Heading 2</h2>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}>
+                          [Link](http://www.example.com)
+                        </pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        <a href="http://www.example.com">Link</a>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}>
+                          * List
+                          <br />* List
+                          <br /> * Put four spaces before the "*" to make a
+                          sub-bullet
+                          <br />* List
+                        </pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        <ul>
+                          <li>List</li>
+                          <li>List</li>
+                          <ul>
+                            <li>
+                              Put four spaces before the "*" to make a
+                              sub-bullet
+                            </li>
+                          </ul>
+                          <li>List</li>
+                        </ul>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}>
+                          1. One
+                          <br />
+                          2. Two
+                          <br />
+                          3. Three
+                        </pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        <ol>
+                          <li>One</li>
+                          <li>Two</li>
+                          <li>Three</li>
+                        </ol>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>
+                        <pre className={classes.code}>
+                          A paragraph of text
+                          <br />
+                          <br />
+                          Followed by a blank line
+                          <br />
+                          <br />
+                          To get multiple paragraphs
+                        </pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        A paragraph of text
+                        <br />
+                        <br />
+                        Followed by a blank line
+                        <br />
+                        <br />
+                        To get multiple paragraphs
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell width={'45%'}>
+                        <pre className={classes.code}>
+                          From: (with two spaces at the end of the line) <br />
+                          To put a newline in a paragraph
+                        </pre>
+                      </TableCell>
+                      <TableCell size="small">
+                        From:
+                        <br />
+                        To put a newline in a paragraph
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </Card>
               <Typography>
-                For more information about formatting, please see:
-                {' '}
-                <ChplLink
-                  href="https://commonmark.org/help/"
-                />
+                For more information about formatting, please see:{' '}
+                <ChplLink href="https://commonmark.org/help/" />
               </Typography>
             </CardContent>
           </Card>
-        </Container>
-        <div className={classes.actionBarButtons}>
-          <ButtonGroup
-            color="primary"
-          >
-            <Button
-              onClick={dispatch}
-              variant="outlined"
-              className={classes.actionBarButton}
-              endIcon={<CloseOutlinedIcon />}
-            >
-              Cancel
-            </Button>
-            <Button
-              onClick={sendMessagePreview}
-              disabled={!formik.isValid}
-              variant="outlined"
-              className={classes.actionBarButton}
-              endIcon={<SendIcon />}
-            >
-              Send Message Preview
-            </Button>
-            <Button
-              onClick={formik.handleSubmit}
-              disabled={!formik.isValid}
-              variant="contained"
-              className={classes.actionBarButton}
-              endIcon={<SendIcon />}
-            >
-              Send Message
-            </Button>
-          </ButtonGroup>
-        </div>
-      </div>
-    </>
+        </Box>
+      </Container>
+    </div>
   );
 }
 
