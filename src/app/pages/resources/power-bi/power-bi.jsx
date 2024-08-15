f9be20545530c46c793a3c33f6cf3f296a20722a
Todd Young <tyoung@ainq.com>
2024-08-15 11:46:21 -0400

    feat: add temporary page to display developer statistics report
    OCD-4653

---
+++ src/app/pages/resources/power-bi/power-bi.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PowerBI() {
+  return (
+    <iframe title="DeveloperStatistics" width="100%" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiMzkzNmEwODMtMDAyZC00ZDMwLWI4MjYtZDZkY2M3NDM1N2RiIiwidCI6IjMwN2QyMTJhLWZiODYtNDgwNy04NGRkLTg2Nzc2OWI4MDQyYSIsImMiOjF9&pageName=b0a55bff2fe744b9463f&navContentPaneEnabled=false&filterPaneEnabled=false" frameborder="0" allowFullScreen="true" />
+  );
+}
+
+export default PowerBI;
--- src/app/pages/resources/resources.state.js
+++ src/app/pages/resources/resources.state.js
@@ -63,6 +63,14 @@ const states = [
       title: 'CHPL Style Guide',
       roles: ['chpl-admin'],
     },
+  }, {
+    name: 'power-bi',
+    url: '/power-bi',
+    component: 'powerBIBridge',
+    data: {
+      title: 'Power BI Report',
+      roles: ['chpl-admin'],
+    },
   },
 ];
 
