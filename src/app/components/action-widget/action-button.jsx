67385a63bf388f7efc4bb97779b5ccdf710c8726
Andrew Larned <andrew.larned@gmail.com>
2024-08-28 08:03:11 -0400

    ui: show "download" buttons only on listing details pages
    [#OCD-4637]

--- src/app/components/action-widget/action-button.jsx
+++ src/app/components/action-widget/action-button.jsx
@@ -1,19 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import {
   Box,
-  Button,
   makeStyles,
 } from '@material-ui/core';
 import { bool, node } from 'prop-types';
-import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 
 import CompareButton from 'components/compare-widget/compare-button';
 import CmsButton from 'components/cms-widget/cms-button';
-import { eventTrack } from 'services/analytics.service';
-import { UserContext } from 'shared/contexts';
-import { isListingActive } from 'services/listing.service';
 import { listing as listingPropType } from 'shared/prop-types';
-import { getAngularService } from 'services/angular-react-helper';
 
 const useStyles = makeStyles({
   tableActions: {
@@ -34,72 +28,14 @@ const useStyles = makeStyles({
 
 function ChplActionButton(props) {
   const { children, horizontal, listing } = props;
-  const API = getAngularService('API');
-  const {
-    getApiKey,
-    getToken,
-  } = getAngularService('authService');
-  const { hasAnyRole, user } = useContext(UserContext);
   const classes = useStyles();
 
-  const downloadOriginalCsv = () => {
-    eventTrack({
-      event: 'Download Original CSV',
-      category: 'Listing Details',
-      label: listing.chplProductNumber,
-      aggregationName: listing.product.name,
-      group: user?.role,
-    });
-    const downloadLink = `${API}/listings/${listing.id}/uploaded-file?api_key=${getApiKey()}&authorization=Bearer%20${getToken()}`;
-    window.open(downloadLink);
-  };
-
-  const downloadCurrentCsv = () => {
-    eventTrack({
-      event: 'Download Current CSV',
-      category: 'Listing Details',
-      label: listing.chplProductNumber,
-      aggregationName: listing.product.name,
-      group: user?.role,
-    });
-    const downloadLink = `${API}/certified_products/${listing.id}/download?api_key=${getApiKey()}&authorization=Bearer%20${getToken()}`;
-    window.open(downloadLink);
-  };
-
   return (
-    <>
-      <Box className={horizontal ? classes.tableActionsHorizontal : classes.tableActions}>
-        {children}
-        <CompareButton listing={listing} />
-        <CmsButton listing={listing} />
-        { hasAnyRole(['chpl-admin']) && listing.id >= 10912
-          && (
-            <Button
-              color="secondary"
-              variant="contained"
-              size="small"
-              id={`download-original-csv-${listing.id}`}
-              onClick={downloadOriginalCsv}
-              endIcon={<CloudDownloadIcon />}
-            >
-              Original CSV
-            </Button>
-          )}
-        { hasAnyRole(['chpl-admin', 'chpl-onc', 'chpl-onc-acb']) && (isListingActive(listing))
-          && (
-            <Button
-              color="secondary"
-              variant="contained"
-              size="small"
-              id={`download-current-csv-${listing.id}`}
-              onClick={downloadCurrentCsv}
-              endIcon={<CloudDownloadIcon />}
-            >
-              Current CSV
-            </Button>
-          )}
-      </Box>
-    </>
+    <Box className={horizontal ? classes.tableActionsHorizontal : classes.tableActions}>
+      {children}
+      <CompareButton listing={listing} />
+      <CmsButton listing={listing} />
+    </Box>
   );
 }
 
