a5934a89fb59b9475b4d81e95da54ce1e2940eeb
Todd Young <tyoung@ainq.com>
2024-09-17 09:19:43 -0400

    feat!: Create dashboard with Developer Statistics report
    OCD-4653

---
+++ src/app/api/reports.jsx
@@ -0,0 +1,20 @@
+import { useQuery } from 'react-query';
+
+import { useAxios } from './axios';
+
+const useFetchReportUrl = (reportName) => {
+  const axios = useAxios();
+  return useQuery(['report-url', reportName], async () => {
+    if (reportName) {
+      const response = await axios.get(`report-data/${reportName}/url`);
+      return response.data;
+    }
+    return {};
+  }, {
+    enabled: !!reportName,
+  });
+};
+
+/* eslint-disable import/prefer-default-export */
+// remove eslint disable line when new api methods are added
+export { useFetchReportUrl };
