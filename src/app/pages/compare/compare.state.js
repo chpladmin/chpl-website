382565148235500d32977643724df6442c4f3e76
Andrew Larned <andrew.larned@gmail.com>
2024-09-25 09:13:11 -0400

    refactor: remove intermediary compare component
    [#OCD-4672]

--- src/app/pages/compare/compare.state.js
+++ src/app/pages/compare/compare.state.js
@@ -1,17 +1,28 @@
-let states = [
+const states = [
   {
     name: 'compare',
-    url: '/compare/{compareIds}',
-    component: 'chplCompare',
+    url: '/compare/{ids}',
+    component: 'chplComparePageBridge',
+    params: {
+      ids: { squash: true, value: null },
+    },
+    resolve: {
+      ids: ($transition$) => {
+        'ngInject';
+
+        return $transition$.params().ids;
+      },
+    },
     data: { title: 'CHPL Product Comparison' },
   },
 ];
 
-function compareStatesConfig ($stateProvider) {
+function compareStatesConfig($stateProvider) {
   'ngInject';
-  states.forEach(state => {
+
+  states.forEach((state) => {
     $stateProvider.state(state);
   });
 }
 
-export { compareStatesConfig };
+export default compareStatesConfig;
