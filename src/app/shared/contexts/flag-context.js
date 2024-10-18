6ab4f4993066315f57673c5e455e46d125e848a9
Andrew Larned <andrew.larned@gmail.com>
2024-10-18 10:29:47 -0400

    refactor: put flag state into flag context
    [#OCD-4660]

--- src/app/shared/contexts/flag-context.js
+++ src/app/shared/contexts/flag-context.js
@@ -1,7 +1,11 @@
 import { createContext } from 'react';
 
 const FlagContext = createContext({
+  cmsA9GracePeriodEndIsOn: () => {},
+  demographicChangeRequestIsOn: () => {},
   isOn: () => {},
+  ssoIsOn: () => {},
+  uploadToUpdateIsOn: () => {},
 });
 FlagContext.displayName = 'ff4j-flags';
 
