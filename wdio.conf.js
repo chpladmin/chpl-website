ce716de6e57c8c8a17b257d927cfe43bb66d59f5
Katy Ekey <katy.ekey@gmail.com>
2024-10-21 14:57:08 -0400

    test[aqa]: Undo global wdio timeout adjustment

--- wdio.conf.js
+++ wdio.conf.js
@@ -109,7 +109,7 @@ exports.config = {
   baseUrl,
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 60000,
+  waitforTimeout: 10000,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
