330b6898bbb1c11e3e47e3acca817b7f842f02e8
Andrew Larned <andrew.larned@gmail.com>
2024-11-18 15:42:21 -0500

    fix: indicate when no changes could be found
    [#OCD-4705]

--- src/app/pages/reports/activity/activity-details.jsx
+++ src/app/pages/reports/activity/activity-details.jsx
@@ -46,6 +46,9 @@ const getDetails = (activity) => {
       .map((item) => `<li>${item}</li>`)
       .join(''));
   }
+  if (details.length === 0) {
+    details = '<li>No changes</li>';
+  }
   return <ul dangerouslySetInnerHTML={{ __html: details }} />;
 };
 
