3ec16b15b6fcf4a0d223fbb18395dc275e8cd330
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-12-16 15:56:09 -0500

    fix: edit user to full screen

--- src/app/pages/organizations/developers/developer/developer-view.jsx
+++ src/app/pages/organizations/developers/developer/developer-view.jsx
@@ -19,6 +19,11 @@ import theme from 'themes/theme';
 
 const useStyles = makeStyles({
   ...utilStyles,
+  editUser:{
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    paddingTop: '16px',
+  },
   lefthandContainer: {
     width: '33%',
     minWidth: '33%',
@@ -118,8 +123,8 @@ function ChplDeveloperView({ dispatch }) {
   };
 
   return (
-    <Box className={classes.mainContent}>
-      <Box className={`${classes.lefthandContainer} ${state === 'editUser' ? classes.fullWidthGridRow : ''}`}>
+    <Box className={`${state === 'editUser' ? classes.editUser : classes.mainContent}`}>
+      <Box className={classes.lefthandContainer}>
         { state === 'view'
           && (
             <Box className={classes.lefthandColumn}>
@@ -143,36 +148,26 @@ function ChplDeveloperView({ dispatch }) {
 
       </Box>
       <Box className={classes.righthandColumn}>
-        { state === 'view'
-        && (
+        {state === 'view' && (
           <>
-            { can('manageTracking')
-              && (
-                <ChplChangeRequests
-                  disallowedFilters={['submittedDateTime', 'searchTerm']}
-                  bonusQuery={`&developerId=${developer.id}`}
-                />
-              )}
-            <ChplDirectReviews
-              developer={developer}
-            />
-            <ChplProducts
-              developer={developer}
-              dispatch={handleProductDispatch}
-            />
+            {can('manageTracking') && (
+              <ChplChangeRequests
+                disallowedFilters={['submittedDateTime', 'searchTerm']}
+                bonusQuery={`&developerId=${developer.id}`}
+              />
+            )}
+            <ChplDirectReviews developer={developer} />
+            <ChplProducts developer={developer} dispatch={handleProductDispatch} />
           </>
         )}
-        { (state === 'view' || state === 'editUser')
-          && (
-            <Box>
-              <ChplUsers
-                users={users}
-                dispatch={handleUserDispatch}
-                roles={['ROLE_DEVELOPER']}
-                groupNames={['chpl-developer']}
-              />
-            </Box>
-          )}
+        {(state === 'view' || state === 'editUser') && (
+          <ChplUsers
+            users={users}
+            dispatch={handleUserDispatch}
+            roles={['ROLE_DEVELOPER']}
+            groupNames={['chpl-developer']}
+          />
+        )}
       </Box>
     </Box>
   );
