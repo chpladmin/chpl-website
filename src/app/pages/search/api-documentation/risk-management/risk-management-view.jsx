ad9d79c293f1e92465184dcb7de7702a055fadca
Todd Young <tyoung@ainq.com>
2025-01-22 10:45:21 -0500

    wip: add risk management summary info collection page
    OCD-4791

---
+++ src/app/pages/search/api-documentation/risk-management/risk-management-view.jsx
@@ -0,0 +1,354 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import Moment from 'react-moment';
+
+import {
+  useFetchApiDocumentationData,
+  useFetchListings,
+} from 'api/search';
+import ChplActionButton from 'components/action-widget/action-button';
+import ChplCertificationStatusLegend from 'components/certification-status/certification-status';
+import ChplDownloadListings from 'components/download-listings/download-listings';
+import {
+  ChplLink,
+  ChplPagination,
+} from 'components/util';
+import { ChplSortableHeaders } from 'components/util/sortable-headers';
+import {
+  ChplFilterChips,
+  ChplFilterSearchBar,
+  useFilterContext,
+} from 'components/filter';
+import { eventTrack } from 'services/analytics.service';
+import { getAngularService } from 'services/angular-react-helper';
+import { getStatusIcon } from 'services/listing.service';
+import { useSessionStorage as useStorage } from 'services/storage.service';
+import { useAnalyticsContext } from 'shared/contexts';
+import { theme, utilStyles } from 'themes';
+
+const useStyles = makeStyles({
+  ...utilStyles,
+  pageHeader: {
+    padding: '32px',
+    backgroundColor: '#ffffff',
+  },
+  pageBody: {
+    display: 'grid',
+    gridTemplateColumns: ' 1fr',
+    gap: '16px',
+    padding: '16px 32px',
+    backgroundColor: '#f9f9f9',
+    [theme.breakpoints.up('md')]: {
+      gridTemplateColumns: '2fr 1fr',
+    },
+  },
+  pageContent: {
+    display: 'grid',
+    gridTemplateRows: '3fr 1fr',
+  },
+  stickyColumn: {
+    position: 'sticky',
+    left: 0,
+    boxShadow: 'rgba(149, 157, 165, 0.1) 0px 4px 8px',
+    backgroundColor: '#ffffff',
+    overflowWrap: 'anywhere',
+    [theme.breakpoints.up('sm')]: {
+      minWidth: '275px',
+    },
+  },
+  tableContainer: {
+    overflowWrap: 'normal',
+    border: '.5px solid #c2c6ca',
+    margin: '0px 32px',
+    width: 'auto',
+  },
+  tableResultsHeaderContainer: {
+    display: 'grid',
+    gap: '8px',
+    margin: '16px 32px',
+    gridTemplateColumns: '1fr',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    [theme.breakpoints.up('sm')]: {
+      gridTemplateColumns: 'auto auto',
+    },
+  },
+  resultsContainer: {
+    display: 'grid',
+    gap: '8px',
+    justifyContent: 'start',
+    gridTemplateColumns: 'auto auto',
+    alignItems: 'center',
+  },
+  wrap: {
+    flexFlow: 'wrap',
+  },
+});
+
+const criteriaLookup = {
+  210: { display: '170.315 (b)(11)', sort: 0 },
+};
+
+/* eslint object-curly-newline: ["error", { "minProperties": 5, "consistent": true }] */
+const headers = [
+  { property: 'chpl_id', text: 'CHPL ID', sortable: true },
+  { property: 'developer', text: 'Developer', sortable: true },
+  { property: 'product', text: 'Product', sortable: true },
+  { property: 'version', text: 'Version', sortable: true },
+  { text: 'Status', extra: <ChplCertificationStatusLegend /> },
+  { text: 'Risk Management Summary Information' },
+  { text: 'Actions', invisible: true },
+];
+  return (
+    <dl>
+      {items.map(({ url, criteria }) => (
+        <React.Fragment key={url}>
+          <dt>{ criteria }</dt>
+          <dd>
+            <ChplLink
+              key={url}
+              href={url}
+              analytics={{
+                ...analytics,
+                event: 'Go to API Documentation',
+                label: chplProductNumber,
+                aggregationName: product.name,
+              }}
+            />
+          </dd>
+        </React.Fragment>
+      ))}
+    </dl>
+  );
+};
+
+function ChplRiskManagementSearchView() {
+  const storageKey = 'storageKey-riskManagementView';
+  const API = getAngularService('API');
+  const authService = getAngularService('authService');
+  const { analytics } = useAnalyticsContext();
+  const [listings, setListings] = useState([]);
+  const [orderBy, setOrderBy] = useStorage(`${storageKey}-orderBy`, 'developer');
+  const [pageNumber, setPageNumber] = useStorage(`${storageKey}-pageNumber`, 0);
+  const [pageSize, setPageSize] = useStorage(`${storageKey}-pageSize`, 25);
+  const [sortDescending, setSortDescending] = useStorage(`${storageKey}-sortDescending`, false);
+  const [recordCount, setRecordCount] = useState(0);
+  const classes = useStyles();
+  const toggledCsvDefaults = ['riskManagement'];
+
+  const filterContext = useFilterContext();
+  const { data, isError, isLoading } = useFetchListings({
+    orderBy,
+    pageNumber,
+    pageSize,
+    sortDescending,
+    query: filterContext.queryString(),
+  });
+  const { data: documentation } = useFetchApiDocumentationData();
+
+  useEffect(() => {
+    if (isLoading) { return; }
+    if (isError || !data.results) {
+      setListings([]);
+      return;
+    }
+    setListings(data.results.map((listing) => ({
+      ...listing,
+      riskManagementSummaryInformationValue: listing.riskManagementSummaryInformation?.value || '',
+    })));
+    setRecordCount(data.recordCount);
+  }, [data?.results, data?.recordCount, isError, isLoading, analytics]);
+
+  useEffect(() => {
+    if (data?.recordCount > 0 && pageNumber > 0 && data?.results?.length === 0) {
+      setPageNumber(0);
+    }
+  }, [data?.recordCount, pageNumber, data?.results?.length]);
+
+  useEffect(() => {
+    setDownloadLink(`${API}/files/risk_management?api_key=${authService.getApiKey()}`);
+  }, [API, authService]);
+
+  useEffect(() => {
+    if (!documentation?.associatedDate) { return; }
+    setDocumentationDate(documentation.associatedDate);
+  }, [documentation?.associatedDate]);
+
+  const handleTableSort = (event, property, orderDirection) => {
+    eventTrack({
+      ...analytics,
+      event: 'Sort Column',
+      label: `${property} - ${orderDirection === 'desc' ? 'DESC' : 'ASC'}`,
+    });
+    setOrderBy(property);
+    setSortDescending(orderDirection === 'desc');
+  };
+
+  const pageStart = (pageNumber * pageSize) + 1;
+  const pageEnd = Math.min((pageNumber + 1) * pageSize, recordCount);
+
+  return (
+    <>
+      <div className={classes.pageHeader}>
+        <Typography variant="h1">Risk Management Summary Information</Typography>
+      </div>
+      <div className={classes.pageBody} id="main-content" tabIndex="-1">
+        <div>
+          <Typography variant="body1">
+            This list includes all health IT products that have been certified to following API Criterion:
+          </Typography>
+          <ul>
+            <li>&sect;170.315 (b)(11): Decision Support Interventions</li>
+          </ul>
+          <Typography variant="body1">
+            Please note that by default, only listings that are active or suspended are shown in the search results.
+          </Typography>
+        </div>
+      </div>
+      <ChplFilterSearchBar />
+      <div>
+        <ChplFilterChips />
+      </div>
+      { isLoading
+        && (
+          <>Loading</>
+        )}
+      { !isLoading
+        && (
+          <>
+            <div className={classes.tableResultsHeaderContainer}>
+              <div className={`${classes.resultsContainer} ${classes.wrap}`}>
+                <Typography variant="subtitle2">Search Results:</Typography>
+                { listings.length === 0
+                  && (
+                    <Typography>
+                      No results found
+                    </Typography>
+                  )}
+                { listings.length > 0
+                  && (
+                    <Typography variant="body2">
+                      {`(${pageStart}-${pageEnd} of ${recordCount} Results)`}
+                    </Typography>
+                  )}
+              </div>
+              { listings.length > 0
+                && (
+                  <ChplDownloadListings
+                    listings={listings}
+                    toggled={toggledCsvDefaults}
+                  />
+                )}
+            </div>
+            { listings.length > 0
+              && (
+                <>
+                  <TableContainer className={classes.tableContainer} component={Paper}>
+                    <Table
+                      stickyHeader
+                      aria-label="Risk Management Summary Information Search table"
+                    >
+                      <ChplSortableHeaders
+                        headers={headers}
+                        onTableSort={handleTableSort}
+                        orderBy={orderBy}
+                        order={sortDescending ? 'desc' : 'asc'}
+                        stickyHeader
+                      />
+                      <TableBody>
+                        { listings
+                          .map((item) => (
+                            <TableRow key={item.id}>
+                              <TableCell className={classes.stickyColumn}>
+                                <strong>
+                                  <ChplLink
+                                    href={`#/listing/${item.id}`}
+                                    text={item.chplProductNumber}
+                                    analytics={{
+                                      ...analytics,
+                                      event: 'Navigate to Listing Details Page',
+                                      label: item.chplProductNumber,
+                                      aggregationName: item.product.name,
+                                    }}
+                                    external={false}
+                                    router={{ sref: 'listing', options: { id: item.id } }}
+                                  />
+                                </strong>
+                              </TableCell>
+                              <TableCell>
+                                <ChplLink
+                                  href={`#/organizations/developers/${item.developer.id}`}
+                                  text={item.developer.name}
+                                  analytics={{
+                                    ...analytics,
+                                    event: 'Navigate to Developer Page',
+                                    label: item.developer.name,
+                                  }}
+                                  external={false}
+                                  router={{ sref: 'organizations.developers.developer', options: { id: item.developer.id } }}
+                                />
+                              </TableCell>
+                              <TableCell>{item.product.name}</TableCell>
+                              <TableCell>{item.version.name}</TableCell>
+                              <TableCell>{ getStatusIcon(item.certificationStatus) }</TableCell>
+                              <TableCell className={classes.linkWrap}>
+                                { item.apiDocumentationNode }
+                              </TableCell>
+                              <TableCell className={classes.linkWrap}>
+                                { item.riskManagementSummaryInformationValue
+                                  ? (
+                                    <dl>
+                                      <dt>170.315 (g)(10)</dt>
+                                      <dd>
+                                        <ChplLink
+                                          href={item.riskManagementSummaryInformationValue}
+                                          analytics={{
+                                            ...analytics,
+                                            event: 'Go to Risk Management Summary Information',
+                                            label: item.chplProductNumber,
+                                            aggregationName: item.product.name,
+                                          }}
+                                        />
+                                      </dd>
+                                    </dl>
+                                  ) : (
+                                    <>N/A</>
+                                  )}
+                              </TableCell>
+                              <TableCell>
+                                <ChplActionButton listing={item} />
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                  <ChplPagination
+                    count={recordCount}
+                    page={pageNumber}
+                    rowsPerPage={pageSize}
+                    rowsPerPageOptions={[25, 50, 100]}
+                    setPage={setPageNumber}
+                    setRowsPerPage={setPageSize}
+                  />
+                </>
+              )}
+          </>
+        )}
+    </>
+  );
+}
+
+export default ChplRiskManagementSearchView;
+
+ChplRiskManagementSearchView.propTypes = {
+};
