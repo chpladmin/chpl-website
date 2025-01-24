1f2bbff7b010dcecfde1b09768a0038b9ce33a72
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-01-24 16:33:23 -0500

    ui: reformat non conformity card

--- src/app/components/listing/details/compliance/non-conformity-edit.jsx
+++ src/app/components/listing/details/compliance/non-conformity-edit.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {
   Box,
-  Button,
   Card,
   CardContent,
   CardHeader,
   CircularProgress,
+  IconButton,
   MenuItem,
   makeStyles,
 } from '@material-ui/core';
@@ -14,14 +14,18 @@ import {
 } from 'prop-types';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import { useFetchNonConformityTypes } from 'api/data';
-import { ChplTextField } from 'components/util';
+import { ChplTextField, ChplTooltip } from 'components/util';
 import { sortNonconformityTypes } from 'services/surveillance.service';
-import { palette, utilStyles } from 'themes';
+import { utilStyles } from 'themes';
 
 const useStyles = makeStyles({
   ...utilStyles,
+  nonConformityCard: {
+    marginBottom: '8px',
+  },
 });
 
 const validationSchema = yup.object({
@@ -133,175 +137,194 @@ function ChplNonConformityEdit({
 
   return (
     <>
-      <Card>
-        <CardHeader title="Non-Conformity" />
+      <Card className={classes.nonConformityCard}>
+        <CardHeader
+          title="Non-Conformity"
+          action={(
+            <ChplTooltip placement="left" title="Remove Non-Conformity">
+              <IconButton onClick={remove}>
+                <DeleteIcon color="error" />
+              </IconButton>
+            </ChplTooltip>
+        )}
+        />
         <CardContent>
-          <Button
-            onClick={remove}
-          >
-            Remove Non-Conformity
-          </Button>
-          <Box display="flex" gridGap="8px" flexWrap="wrap" flexDirection="row" justifyContent="space-between" pb={2}>
-            <ChplTextField
-              select
-              id="type"
-              name="type"
-              label="Non-Conformity Type"
-              required
-              value={formik.values.type}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.type && !!formik.errors.type}
-              helperText={formik.touched.type && formik.errors.type}
-            >
-              { nonConformityTypes.map((type) => (
-                <MenuItem key={type.id} value={type.id}>
-                  {type.status === 'REMOVED' ? 'Removed | ' : ''}
-                  {type.status === 'RETIRED' ? 'Retired | ' : ''}
-                  {type.number ? (`${type.number}: `) : ''}
-                  {type.title}
-                </MenuItem>
-              ))}
-            </ChplTextField>
-            <ChplTextField
-              type="date"
-              id="date-of-determination"
-              name="dateOfDeterminationDay"
-              label="Date of Determination"
-              required
-              value={formik.values.dateOfDeterminationDay}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.dateOfDeterminationDay && !!formik.errors.dateOfDeterminationDay}
-              helperText={formik.touched.dateOfDeterminationDay && formik.errors.dateOfDeterminationDay}
-            />
-            <ChplTextField
-              type="date"
-              id="cap-approval-day"
-              name="capApprovalDay"
-              label="Corrective Action Plan Approval Date"
-              value={formik.values.capApprovalDay}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.capApprovalDay && !!formik.errors.capApprovalDay}
-              helperText={formik.touched.capApprovalDay && formik.errors.capApprovalDay}
-            />
-            <ChplTextField
-              type="date"
-              id="cap-start-day"
-              name="capStartDay"
-              label="Corrective Action Plan Start Date"
-              value={formik.values.capStartDay}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.capStartDay && !!formik.errors.capStartDay}
-              helperText={formik.touched.capStartDay && formik.errors.capStartDay}
-            />
-            <ChplTextField
-              type="date"
-              id="cap-end-day"
-              name="capEndDay"
-              label="Corrective Action Plan End Date"
-              value={formik.values.capEndDay}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.capEndDay && !!formik.errors.capEndDay}
-              helperText={formik.touched.capEndDay && formik.errors.capEndDay}
-            />
-            <ChplTextField
-              type="date"
-              id="cap-must-complete-day"
-              name="capMustCompleteDay"
-              label="Corrective Action Plan Must Complete Date"
-              value={formik.values.capMustCompleteDay}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.capMustCompleteDay && !!formik.errors.capMustCompleteDay}
-              helperText={formik.touched.capMustCompleteDay && formik.errors.capMustCompleteDay}
-            />
-            <ChplTextField
-              type="date"
-              id="non-conformity-close-day"
-              name="nonconformityCloseDay"
-              label="Non-Conformity Close Date"
-              value={formik.values.nonconformityCloseDay}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.nonconformityCloseDay && !!formik.errors.nonconformityCloseDay}
-              helperText={formik.touched.nonconformityCloseDay && formik.errors.nonconformityCloseDay}
-            />
-            <ChplTextField
-              type="number"
-              id="sites-passed"
-              name="sitesPassed"
-              label="Number of Sites Passed"
-              required={formik.values.randomizedSitesUsed > 0}
-              disabled={formik.values.randomizedSitesUsed === 0}
-              value={formik.values.sitesPassed}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.sitesPassed && !!formik.errors.sitesPassed}
-              helperText={formik.touched.sitesPassed && formik.errors.sitesPassed}
-            />
-            <ChplTextField
-              type="number"
-              id="total-sites"
-              name="totalSites"
-              label="Total Sites"
-              required={formik.values.randomizedSitesUsed > 0}
-              disabled={formik.values.randomizedSitesUsed === 0}
-              value={formik.values.totalSites}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.totalSites && !!formik.errors.totalSites}
-              helperText={formik.touched.totalSites && formik.errors.totalSites}
-            />
-            <ChplTextField
-              id="summary"
-              name="summary"
-              label="Summary"
-              required
-              value={formik.values.summary}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.summary && !!formik.errors.summary}
-              helperText={formik.touched.summary && formik.errors.summary}
-            />
-            <ChplTextField
-              id="findings"
-              name="findings"
-              label="Findings"
-              multiline
-              required
-              value={formik.values.findings}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.findings && !!formik.errors.findings}
-              helperText={formik.touched.findings && formik.errors.findings}
-            />
-            <ChplTextField
-              id="developer-explanation"
-              name="developerExplanation"
-              label="Developer Explanation"
-              multiline
-              value={formik.values.developerExplanation}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.developerExplanation && !!formik.errors.developerExplanation}
-              helperText={formik.touched.developerExplanation && formik.errors.developerExplanation}
-            />
-            <ChplTextField
-              id="resolution"
-              name="resolution"
-              label="Resolution"
-              multiline
-              required={!!formik.values.nonconformityCloseDay}
-              value={formik.values.resolution}
-              onChange={handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.resolution && !!formik.errors.resolution}
-              helperText={formik.touched.resolution && formik.errors.resolution}
-            />
+          <Box display="flex" gridGap="16px" flexDirection="column" justifyContent="space-between" pb={2}>
+            <Box display="flex" gridGap="16px" flexDirection="row">
+              <ChplTextField
+                select
+                id="type"
+                name="type"
+                label="Non-Conformity Type"
+                required
+                value={formik.values.type}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.type && !!formik.errors.type}
+                helperText={formik.touched.type && formik.errors.type}
+              >
+                { nonConformityTypes.map((type) => (
+                  <MenuItem key={type.id} value={type.id}>
+                    {type.status === 'REMOVED' ? 'Removed | ' : ''}
+                    {type.status === 'RETIRED' ? 'Retired | ' : ''}
+                    {type.number ? (`${type.number}: `) : ''}
+                    {type.title}
+                  </MenuItem>
+                ))}
+              </ChplTextField>
+            </Box>
+            <Box display="flex" gridGap="16px" flexDirection="row">
+              <ChplTextField
+                type="date"
+                id="date-of-determination"
+                name="dateOfDeterminationDay"
+                label="Date of Determination"
+                required
+                value={formik.values.dateOfDeterminationDay}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.dateOfDeterminationDay && !!formik.errors.dateOfDeterminationDay}
+                helperText={formik.touched.dateOfDeterminationDay && formik.errors.dateOfDeterminationDay}
+              />
+
+              <ChplTextField
+                type="date"
+                id="cap-approval-day"
+                name="capApprovalDay"
+                label="Corrective Action Plan Approval Date"
+                value={formik.values.capApprovalDay}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.capApprovalDay && !!formik.errors.capApprovalDay}
+                helperText={formik.touched.capApprovalDay && formik.errors.capApprovalDay}
+              />
+            </Box>
+            <Box display="flex" gridGap="16px" flexDirection="row">
+              <ChplTextField
+                type="date"
+                id="cap-start-day"
+                name="capStartDay"
+                label="Corrective Action Plan Start Date"
+                value={formik.values.capStartDay}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.capStartDay && !!formik.errors.capStartDay}
+                helperText={formik.touched.capStartDay && formik.errors.capStartDay}
+              />
+              <ChplTextField
+                type="date"
+                id="cap-end-day"
+                name="capEndDay"
+                label="Corrective Action Plan End Date"
+                value={formik.values.capEndDay}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.capEndDay && !!formik.errors.capEndDay}
+                helperText={formik.touched.capEndDay && formik.errors.capEndDay}
+              />
+            </Box>
+            <Box display="flex" gridGap="16px" flexDirection="row">
+              <ChplTextField
+                type="date"
+                id="cap-must-complete-day"
+                name="capMustCompleteDay"
+                label="Corrective Action Plan Must Complete Date"
+                value={formik.values.capMustCompleteDay}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.capMustCompleteDay && !!formik.errors.capMustCompleteDay}
+                helperText={formik.touched.capMustCompleteDay && formik.errors.capMustCompleteDay}
+              />
+              <ChplTextField
+                type="date"
+                id="non-conformity-close-day"
+                name="nonconformityCloseDay"
+                label="Non-Conformity Close Date"
+                value={formik.values.nonconformityCloseDay}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.nonconformityCloseDay && !!formik.errors.nonconformityCloseDay}
+                helperText={formik.touched.nonconformityCloseDay && formik.errors.nonconformityCloseDay}
+              />
+            </Box>
+            <Box display="flex" gridGap="16px" flexDirection="row">
+              <ChplTextField
+                type="number"
+                id="sites-passed"
+                name="sitesPassed"
+                label="Number of Sites Passed"
+                required={formik.values.randomizedSitesUsed > 0}
+                disabled={formik.values.randomizedSitesUsed === 0}
+                value={formik.values.sitesPassed}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.sitesPassed && !!formik.errors.sitesPassed}
+                helperText={formik.touched.sitesPassed && formik.errors.sitesPassed}
+              />
+              <ChplTextField
+                type="number"
+                id="total-sites"
+                name="totalSites"
+                label="Total Sites"
+                required={formik.values.randomizedSitesUsed > 0}
+                disabled={formik.values.randomizedSitesUsed === 0}
+                value={formik.values.totalSites}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.totalSites && !!formik.errors.totalSites}
+                helperText={formik.touched.totalSites && formik.errors.totalSites}
+              />
+            </Box>
+            <Box display="flex" gridGap="16px" flexDirection="column">
+              <ChplTextField
+                id="summary"
+                name="summary"
+                label="Summary"
+                required
+                value={formik.values.summary}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.summary && !!formik.errors.summary}
+                helperText={formik.touched.summary && formik.errors.summary}
+              />
+              <ChplTextField
+                id="findings"
+                name="findings"
+                label="Findings"
+                multiline
+                required
+                value={formik.values.findings}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.findings && !!formik.errors.findings}
+                helperText={formik.touched.findings && formik.errors.findings}
+              />
+            </Box>
+            <Box display="flex" gridGap="16px" flexDirection="column">
+              <ChplTextField
+                id="developer-explanation"
+                name="developerExplanation"
+                label="Developer Explanation"
+                multiline
+                value={formik.values.developerExplanation}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.developerExplanation && !!formik.errors.developerExplanation}
+                helperText={formik.touched.developerExplanation && formik.errors.developerExplanation}
+              />
+              <ChplTextField
+                id="resolution"
+                name="resolution"
+                label="Resolution"
+                multiline
+                required={!!formik.values.nonconformityCloseDay}
+                value={formik.values.resolution}
+                onChange={handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.resolution && !!formik.errors.resolution}
+                helperText={formik.touched.resolution && formik.errors.resolution}
+              />
+            </Box>
           </Box>
         </CardContent>
       </Card>
