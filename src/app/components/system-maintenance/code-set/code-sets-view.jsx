64596b64c873e754eeb45da377be810bc65a66d3
Andrew Larned <andrew.larned@gmail.com>
2025-02-24 15:05:41 -0500

    feat: handle g1/g2 measures and code sets
    [#OCD-4783]

---
+++ src/app/components/system-maintenance/code-set/code-sets-view.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  makeStyles,
+} from '@material-ui/core';
+import { arrayOf, object } from 'prop-types';
+
+import { ChplSortableHeaders, sortComparator } from 'components/util/sortable-headers';
+import { sortCriteria } from 'services/criteria.service';
+import { getDisplayDateFormat } from 'services/date-util';
+import { utilStyles } from 'themes';
+
+const headers = [
+  { property: 'name', text: 'Name', sortable: true },
+  { property: 'startDay', text: 'Start Date', sortable: true },
+  { property: 'requiredDay', text: 'Required Date', sortable: true },
+  { text: 'Applicable Criteria' },
+];
+
+const useStyles = makeStyles({
+  ...utilStyles,
+});
+
+function ChplCodeSetsView({ codeSets: initialCodeSets }) {
+  const [codeSets, setCodeSets] = useState([]);
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('name');
+  const classes = useStyles();
+
+  useEffect(() => {
+    setCodeSets(initialCodeSets
+      .map((item) => ({
+        ...item,
+        criteriaDisplay: item.criteria
+          .sort(sortCriteria)
+          .map((c) => c.number)
+          .join(', '),
+      }))
+      .sort(sortComparator('name')));
+  }, [initialCodeSets]);
+
+  const handleTableSort = (event, property, orderDirection) => {
+    const descending = orderDirection === 'desc';
+    const updated = codeSets.sort(sortComparator(property, descending));
+    setOrderBy(property);
+    setOrder(orderDirection);
+    setCodeSets(updated);
+  };
+
+  return (
+    <>
+      <TableContainer className={classes.container} component={Paper}>
+        <Table
+          aria-label="Code Set table"
+        >
+          <ChplSortableHeaders
+            headers={headers}
+            onTableSort={handleTableSort}
+            orderBy={orderBy}
+            order={order}
+            stickyHeader
+          />
+          <TableBody>
+            { codeSets
+              .map((item) => (
+                <TableRow key={`${item.id}`}>
+                  <TableCell className={classes.firstColumn}>
+                    { item.name }
+                  </TableCell>
+                  <TableCell>
+                    { getDisplayDateFormat(item.startDay) }
+                  </TableCell>
+                  <TableCell>
+                    { getDisplayDateFormat(item.requiredDay) }
+                  </TableCell>
+                  <TableCell>
+                    { item.criteriaDisplay }
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
+
+export default ChplCodeSetsView;
+
+ChplCodeSetsView.propTypes = {
+  codeSets: arrayOf(object).isRequired,
+};
--- src/app/pages/administration/system-maintenance/system-maintenance.jsx
+++ src/app/pages/administration/system-maintenance/system-maintenance.jsx
@@ -28,9 +28,11 @@ import ChplAccessibilityStandards from 'components/system-maintenance/accessibil
 import ChplAnnouncements from 'components/system-maintenance/announcement/announcements';
 import ChplApiKeys from 'components/system-maintenance/api-key/api-keys';
 import ChplCertificationCriteria from 'components/system-maintenance/certification-criterion/certification-criteria';
+import ChplCodeSets from 'components/system-maintenance/code-set/code-sets';
 import ChplConformanceMethods from 'components/system-maintenance/conformance-method/conformance-methods';
 import ChplCqms from 'components/system-maintenance/cqm/cqms';
 import ChplFunctionalitiesTested from 'components/system-maintenance/functionality-tested/functionalities-tested';
+import ChplG1g2 from 'components/system-maintenance/g1g2/g1g2';
 import ChplManageSubscriptions from 'pages/subscriptions/manage-subscriptions';
 import ChplQmsStandards from 'components/system-maintenance/qms-standard/qms-standards';
 import ChplStandards from 'components/system-maintenance/standard/standards';
@@ -128,6 +130,11 @@ const maintenanceItems = [{
   secondary: 'Table of the Certification Criteria values',
   icon: <BookOutlinedIcon />,
 }, {
+  id: 'codeSets',
+  primary: 'Code Sets',
+  secondary: 'Table of Code Sets',
+  icon: <BookOutlinedIcon />,
+}, {
   id: 'conformanceMethods',
   primary: 'Conformance Methods',
   secondary: 'Table of Conformance Methods',
@@ -142,6 +149,11 @@ const maintenanceItems = [{
   primary: 'Functionalities Tested',
   secondary: 'Table of the Functionality Tested values used during testing of certification criterion functionality',
   icon: <BeenhereOutlinedIcon />,
+}, {
+  id: 'g1g2',
+  primary: 'G1/G2 Measures',
+  secondary: 'Table of G1/G2 Measures',
+  icon: <BookOutlinedIcon />,
 }, {
   id: 'qmsStandards',
   primary: 'QMS Standards',
@@ -246,12 +258,14 @@ function ChplSystemMaintenance() {
     hide('announcements.edit.disabled');
     hide('apiKeys.viewall.disabled');
     hide('certificationCriteria.viewall.disabled');
+    hide('codeSets.viewall.disabled');
     hide('conformanceMethods.viewall.disabled');
     hide('cqms.viewall.disabled');
     hide('functionalitiesTested.viewall.disabled');
     hide('functionalitiesTested.viewall');
     hide('functionalitiesTested.add.disabled');
     hide('functionalitiesTested.edit.disabled');
+    hide('g1g2.viewall.disabled');
     hide('manageSubscriptions.viewall.disabled');
     hide('qmsStandards.viewall.disabled');
     hide('qmsStandards.viewall');
@@ -336,12 +350,14 @@ function ChplSystemMaintenance() {
           { active === 'announcements' && <ChplAnnouncements /> }
           { active === 'apiKeys' && <ChplApiKeys /> }
           { active === 'certificationCriteria' && <ChplCertificationCriteria /> }
+          { active === 'codeSets' && <ChplCodeSets /> }
           { active === 'conformanceMethods' && <ChplConformanceMethods /> }
           { active === 'cqms' && <ChplCqms /> }
-          { active === 'qmsStandards' && <ChplQmsStandards /> }
           { active === 'functionalitiesTested' && <ChplFunctionalitiesTested /> }
-          { active === 'subscriptions' && <ChplManageSubscriptions /> }
+          { active === 'g1g2' && <ChplG1g2 /> }
+          { active === 'qmsStandards' && <ChplQmsStandards /> }
           { active === 'standards' && <ChplStandards /> }
+          { active === 'subscriptions' && <ChplManageSubscriptions /> }
           { active === 'svaps' && <ChplSvaps /> }
           { active === 'systemJobs' && <ChplSystemJobs /> }
           { active === 'testData' && <ChplTestData /> }
