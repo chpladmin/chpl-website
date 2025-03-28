f04f5f95191534a11c9910813c730a1a0ef7eb2b
Andrew Larned <andrew.larned@gmail.com>
2025-03-28 10:51:35 -0400

    feat!: Enable search/filter on system maintenance pages
    [#OCD-4816]

--- src/app/components/system-maintenance/standard/standards-view.jsx
+++ src/app/components/system-maintenance/standard/standards-view.jsx
@@ -7,6 +7,7 @@ import {
   TableCell,
   TableContainer,
   TableRow,
+  Typography,
   makeStyles,
 } from '@material-ui/core';
 import { arrayOf, func } from 'prop-types';
@@ -15,6 +16,11 @@ import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 
 import { useFetchStandardsActivity } from 'api/activity';
 import ChplSystemMaintenanceActivity from 'components/activity/system-maintenance-activity';
+import {
+  ChplFilterChips,
+  ChplFilterSearchBar,
+  useFilterContext,
+} from 'components/filter';
 import { ChplUpdateIndicator } from 'components/util';
 import { ChplSortableHeaders, sortComparator } from 'components/util/sortable-headers';
 import { sortCriteria } from 'services/criteria.service';
@@ -48,10 +54,17 @@ function ChplStandardsView({ dispatch, standards: initialStandards }) {
   const { hasAnyRole } = useContext(UserContext);
   const [order, setOrder] = useState('asc');
   const [orderBy, setOrderBy] = useState('value');
+  const filterContext = useFilterContext();
   const classes = useStyles();
 
   useEffect(() => {
     setStandards(initialStandards
+      .filter((item) => filterContext.filters.reduce((acc, f) => f.filterFn(item, f) && acc, true))
+      .filter((item) => filterContext.searchTerm === ''
+              || item.value.toLowerCase().includes(filterContext.searchTerm.toLowerCase())
+              || item.regulatoryTextCitation.toLowerCase().includes(filterContext.searchTerm.toLowerCase())
+              || item.rule?.name.toLowerCase().includes(filterContext.searchTerm.toLowerCase())
+              || item.groupName?.toLowerCase().includes(filterContext.searchTerm.toLowerCase()))
       .map((item) => ({
         ...item,
         criteriaDisplay: item.criteria
@@ -60,7 +73,7 @@ function ChplStandardsView({ dispatch, standards: initialStandards }) {
           .join(', '),
       }))
       .sort(sortComparator('value')));
-  }, [initialStandards]);
+  }, [initialStandards, filterContext.filters, filterContext.searchTerm]);
 
   const handleTableSort = (event, property, orderDirection) => {
     const descending = orderDirection === 'desc';
@@ -72,6 +85,15 @@ function ChplStandardsView({ dispatch, standards: initialStandards }) {
 
   return (
     <>
+      <ChplFilterSearchBar
+        placeholder="Search by Value, Citation, Rule, or Group..."
+      />
+      <div>
+        <ChplFilterChips />
+      </div>
+      <Typography variant="body2">
+        {`(${standards.length} Result${standards.length !== 1 ? 's' : ''})`}
+      </Typography>
       <div className={classes.tableResultsHeaderContainer}>
         <ChplSystemMaintenanceActivity
           fetch={useFetchStandardsActivity}
