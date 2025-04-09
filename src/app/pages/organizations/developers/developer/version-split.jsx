ef050bb212dd8bfc87ae0e5d2a4fd4c6ad72ee1e
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-04-09 15:24:16 -0400

    ui: adjust page container class in version split
    [#OCD-4828]

--- src/app/pages/organizations/developers/developer/version-split.jsx
+++ src/app/pages/organizations/developers/developer/version-split.jsx
@@ -44,7 +44,7 @@ const useStyles = makeStyles({
     justifyContent: 'space-between',
     gridGap: '32px',
     alignItems: 'stretch',
-    minHeight: 'calc(100vh - 289px)',
+    paddingBottom: '60vh',
     [theme.breakpoints.up('md')]: {
       display: 'grid',
       gridTemplateColumns: '1fr 1fr',
