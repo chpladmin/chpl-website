97764aadd1b7bcd64ebe65ec149a5d1906be880a
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-04-28 16:34:21 -0400

    ui: reduce code and type needed for view listing in quarter view
    [#OCD-4797]

--- src/app/components/surveillance/reporting/quarter-view-listing.jsx
+++ src/app/components/surveillance/reporting/quarter-view-listing.jsx
@@ -1,55 +1,26 @@
 import React from 'react';
 import {
-  Typography,
-  makeStyles,
+  Box,
+  Divider,
 } from '@material-ui/core';
 import { object } from 'prop-types';
 
 import ChplQuarterViewListingSurveillance from './quarter-view-listing-surveillance';
 
-import { getDisplayDateFormat } from 'services/date-util';
-import { theme, utilStyles } from 'themes';
-
-const useStyles = makeStyles({
-  ...utilStyles,
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'stretch',
-    gap: '16px',
-    [theme.breakpoints.up('md')]: {
-      display: 'grid',
-      gridTemplateColumns: '1fr 3fr',
-      alignItems: 'start',
-    },
-  },
-  menuItems: {
-    padding: '8px',
-    justifyContent: 'space-between',
-    '&.Mui-disabled': {
-      color: '#000',
-      backgroundColor: '#f9f9f9',
-      fontWeight: 600,
-    },
-  },
-});
-
 function ChplQuarterViewListing({
   listing,
 }) {
-  const classes = useStyles();
-
   return (
     <>
-      <Typography>{ listing.chplProductNumber }</Typography>
-      <Typography>{ getDisplayDateFormat(listing.certificationDay) }</Typography>
-      <Typography>{ listing.certificationStatus }</Typography>
-      { listing.surveillances.map((surv) => (
-        <ChplQuarterViewListingSurveillance
-          key={surv.id}
-          surveillance={surv}
-        />
-      ))}
+      <Box display="flex" width="100%" gridGap="32px" flexDirection="row" justifyContent="space-between">
+        {listing.surveillances.map((surv) => (
+          <ChplQuarterViewListingSurveillance
+            key={surv.id}
+            surveillance={surv}
+          />
+        ))}
+      </Box>
+      <Divider />
     </>
   );
 }
