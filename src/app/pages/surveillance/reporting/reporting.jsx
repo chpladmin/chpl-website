90411e3b1a5074e486b151530ba144d2e5d3a03d
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-04-28 16:28:50 -0400

    ui: add explainer text and grid for reports to live in
    [#OCD-4797]

--- src/app/pages/surveillance/reporting/reporting.jsx
+++ src/app/pages/surveillance/reporting/reporting.jsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Card,
+  CardContent,
   Chip,
   Container,
   Typography,
@@ -20,12 +21,16 @@ const useStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'stretch',
+    paddingTop: '16px',
     gap: '16px',
     [theme.breakpoints.up('md')]: {
       display: 'grid',
       gridTemplateColumns: '1fr 3fr',
       alignItems: 'start',
     },
+  },
+  fixFooterSpacing: {
+    minHeight: 'calc(100vh - 100px)',
   },
   menuItems: {
     padding: '8px',
@@ -72,44 +77,54 @@ function ChplSurveillanceReporting() {
   };
 
   return (
-    <Container maxWidth="lg">
+    <Container className={classes.fixFooterSpacing} maxWidth="lg">
       <div className={(acbs.length > 1 && state === '') ? classes.container : ''}>
         { acbs.length > 1 && state === ''
-          && (
-            <Card>
-              { acbs.map((acb) => (
-                <Button
-                  key={acb.name}
-                  onClick={() => navigate(acb)}
-                  disabled={acbs.find((o) => o.id === activeAcb?.id)?.name === acb.name}
-                  id={`acb-navigation-${acb.name}`}
-                  fullWidth
-                  variant="text"
-                  color="primary"
-                  endIcon={<ArrowForwardIcon />}
-                  className={classes.menuItems}
-                >
-                  <Box display="flex" flexDirection="row" gridGap={4}>
-                    { acb.retired ? <Chip size="small" color="default" variant="outlined" label="Retired" /> : '' }
-                    { acb.name }
-                  </Box>
-                </Button>
-              ))}
-            </Card>
-          )}
+            && (
+              <Card>
+                { acbs.map((acb) => (
+                  <Button
+                    key={acb.name}
+                    onClick={() => navigate(acb)}
+                    disabled={acbs.find((o) => o.id === activeAcb?.id)?.name === acb.name}
+                    id={`acb-navigation-${acb.name}`}
+                    fullWidth
+                    variant="text"
+                    color="primary"
+                    endIcon={<ArrowForwardIcon />}
+                    className={classes.menuItems}
+                  >
+                    <Box display="flex" flexDirection="row" gridGap={4}>
+                      { acb.retired ? <Chip size="small" color="default" variant="outlined" label="Retired" /> : '' }
+                      { acb.name }
+                    </Box>
+                  </Button>
+                ))}
+              </Card>
+            )}
         { !activeAcb
-          && (
-            <Typography>
-              Reports go here
-            </Typography>
-          )}
+            && (
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" gutterBottom component="h2">
+                    <strong>Quarterly and Annual Surveillance Reports</strong>
+                  </Typography>
+                  <Typography gutterBottom>
+                    To view detailed quarterly and annual reports, start by selecting an ABC from the menu on the left.
+                  </Typography>
+                  <Typography>
+                    Once selected, the corresponding reports will appear here, giving you access to key performance data and year-end summaries.
+                  </Typography>
+                </CardContent>
+              </Card>
+            )}
         { activeAcb
-          && (
-            <ChplReport
-              acb={activeAcb}
-              dispatch={handleDispatch}
-            />
-          )}
+            && (
+              <ChplReport
+                acb={activeAcb}
+                dispatch={handleDispatch}
+              />
+            )}
       </div>
     </Container>
   );
