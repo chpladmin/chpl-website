c13170ecd9d00f57837f4c8b9d24db1e3654ded7
Andrew Larned <andrew.larned@gmail.com>
2025-08-13 13:44:50 -0400

    feat: start annual reporting edit component
    [#OCD-4886]

---
+++ src/app/components/surveillance/reporting/annual-edit.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
+import {
+  func,
+  object,
+} from 'prop-types';
+
+import { ChplActionBar } from 'components/action-bar';
+import { theme, utilStyles } from 'themes';
+
+const useStyles = makeStyles({
+  ...utilStyles,
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    gap: '16px',
+    marginBottom: '32px',
+    [theme.breakpoints.up('md')]: {
+      display: 'grid',
+      gridTemplateColumns: '350px 1fr',
+      alignItems: 'start',
+    },
+  },
+  reportInfoCard: {
+    padding: '8px',
+  },
+  responseBox: {
+    padding: '16px',
+    backgroundColor: '#eee',
+    border: '1px solid #afafaf',
+    borderRadius: '4px',
+  },
+  stickyColumn: {
+    position: 'sticky',
+    top: '124px',
+    zIndex: 1,
+    boxShadow: 'rgba(149, 157, 165, 0.1) 0 4px 8px',
+  },
+  summaryGroup: {
+    margin: '8px 0',
+    whiteSpace: 'pre-line',
+  },
+});
+
+function ChplAnnualEdit({
+  dispatch,
+  report,
+}) {
+  const classes = useStyles();
+
+  const handleDispatch = (action) => {
+    dispatch({ action });
+  };
+
+  return (
+    <div className={classes.container}>
+      <Box className={classes.stickyColumn}>
+        <Card className={classes.reportInfoCard}>
+          <CardContent>
+            <Typography variant="h6" component="h2">
+              <strong>{`${report.acb?.name} Annual Surveillance Reporting`}</strong>
+            </Typography>
+            <Typography variant="body1">
+              { report.year }
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Card>
+        <CardContent>
+          <Box>
+            <Typography variant="h6" component="h2">
+              <strong>Obstacle Summary</strong>
+            </Typography>
+            <Typography style={{ paddingBottom: '4px', color: '#373737' }} variant="body2" gutterBottom>
+              Please list any obstacles encountered during surveillance, including those related to resources/technical capabilities, developers, and providers/end-users.
+            </Typography>
+            <Typography className={classes.responseBox}>
+              { report.obstacleSummary }
+            </Typography>
+          </Box>
+          <Box className={classes.summaryGroup}>
+            <Typography variant="h6" component="h2">
+              <strong>Priority Changes From Findings Summary</strong>
+            </Typography>
+          </Box>
+          <Typography className={classes.responseBox}>
+            { report.priorityChangesFromFindingsSummary }
+          </Typography>
+          <Divider />
+          <Typography variant="body2">
+            The titles and descriptions used in this module&apos;s user interface reflect the most recent version of the report and may appear differently for historical reports in the downloads
+          </Typography>
+        </CardContent>
+      </Card>
+      <ChplActionBar
+        dispatch={handleDispatch}
+      />
+    </div>
+  );
+}
+
+export default ChplAnnualEdit;
+
+ChplAnnualEdit.propTypes = {
+  dispatch: func.isRequired,
+  report: object.isRequired,
+};
--- src/app/components/surveillance/reporting/annual.jsx
+++ src/app/components/surveillance/reporting/annual.jsx
@@ -12,6 +12,7 @@ import CloudDownloadOutlinedIcon from '@material-ui/icons/CloudDownloadOutlined'
 import Edit from '@material-ui/icons/Edit';
 import RemoveRedEye from '@material-ui/icons/RemoveRedEye';
 
+import ChplAnnualEdit from './annual-edit';
 import ChplAnnualView from './annual-view';
 
 import { usePostAnnualReportRequest } from 'api/surveillance';
@@ -50,73 +51,90 @@ function ChplAnnual({ year, dispatch, report }) {
     }
   };
 
+  const edit = () => {
+    setState('edit');
+    dispatch({ action: 'focus-annual' });
+  };
+
   const view = () => {
     setState('view');
     dispatch({ action: 'focus-annual' });
   };
 
+  if (state === 'edit') {
+    return (
+      <ChplAnnualEdit
+        report={report}
+        dispatch={handleDispatch}
+      />
+    );
+  }
+
+  if (state === 'view') {
+    return (
+      <ChplAnnualView
+        report={report}
+        dispatch={handleDispatch}
+      />
+    );
+  }
+
   return (
-    <>
-      {state === 'summary' ? (
-        <Card>
-          <CardContent>
-            <Typography variant="h4" component="h2">
-              <strong>Annual Summary</strong>
-            </Typography>
-            <Typography variant="body2" style={{ padding: '4px' }}>
-              {year}
-            </Typography>
-            <Box>
-              { report.id
-                && (
-                  <Box sx={{ display: 'flex', flexDirection: 'row', mt: 2 }}>
-                    { hasAnyRole(['chpl-admin', 'chpl-onc-acb'])
-                      && (
-                        <Button
-                          color="primary"
-                          size="small"
-                          endIcon={<Edit />}
-                        >
-                          Edit
-                        </Button>
-                      )}
-                    { hasAnyRole(['chpl-onc'])
-                      && (
-                        <Button
-                          color="primary"
-                          variant="outlined"
-                          size="small"
-                          style={{ marginRight: '4px' }}
-                          onClick={view}
-                          endIcon={<RemoveRedEye />}
-                        >
-                          View
-                        </Button>
-                      )}
+    <Card>
+      <CardContent>
+        <Typography variant="h4" component="h2">
+          <strong>Annual Summary</strong>
+        </Typography>
+        <Typography variant="body2" style={{ padding: '4px' }}>
+          {year}
+        </Typography>
+        <Box>
+          { report.id
+            && (
+              <Box sx={{ display: 'flex', flexDirection: 'row', mt: 2 }}>
+                { hasAnyRole(['chpl-admin', 'chpl-onc-acb'])
+                  && (
                     <Button
                       color="primary"
+                      variant="outlined"
                       size="small"
-                      onClick={download}
-                      endIcon={<CloudDownloadOutlinedIcon />}
+                      style={{ marginRight: '4px' }}
+                      onClick={edit}
+                      endIcon={<Edit />}
                     >
-                      Download
+                      Edit
                     </Button>
-                  </Box>
-                )}
-              { !report.id && hasAnyRole(['chpl-admin', 'chpl-onc-acb'])
-                && (
-                  <Button>Initiate</Button>
-                )}
-            </Box>
-          </CardContent>
-        </Card>
-      ) : (
-        <ChplAnnualView
-          report={report}
-          dispatch={handleDispatch}
-        />
-      )}
-    </>
+                  )}
+                { hasAnyRole(['chpl-onc'])
+                  && (
+                    <Button
+                      color="primary"
+                      variant="outlined"
+                      size="small"
+                      style={{ marginRight: '4px' }}
+                      onClick={view}
+                      endIcon={<RemoveRedEye />}
+                    >
+                      View
+                    </Button>
+                  )}
+                <Button
+                  color="primary"
+                  size="small"
+                  onClick={download}
+                  endIcon={<CloudDownloadOutlinedIcon />}
+                >
+                  Download
+                </Button>
+              </Box>
+            )}
+          { !report.id && hasAnyRole(['chpl-admin', 'chpl-onc-acb'])
+            && (
+              <Button>Initiate</Button>
+            )}
+        </Box>
+      </CardContent>
+    </Card>
   );
 }
 
