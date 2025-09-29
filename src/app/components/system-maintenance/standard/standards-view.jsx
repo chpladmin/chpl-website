de850930b1743acae78de8b90c39ed1048bba0c8
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-09-29 13:58:17 -0400

    ui: enhance layout for code sets, functionalities tested, standards, and svaps views
    [#OCD-5042]

--- src/app/components/system-maintenance/standard/standards-view.jsx
+++ src/app/components/system-maintenance/standard/standards-view.jsx
@@ -45,6 +45,10 @@ const headers = [
 
 const useStyles = makeStyles({
   ...utilStyles,
+    tableResultsHeaderContainer:{
+      display: 'flex',
+      justifyContent: 'flex-end',
+  },
 });
 
 function ChplStandardsView({ dispatch, standards: initialStandards }) {
@@ -90,29 +94,31 @@ function ChplStandardsView({ dispatch, standards: initialStandards }) {
       <div>
         <ChplFilterChips />
       </div>
-      <div className={classes.tableResultsHeaderContainer}>
+      <Box display="flex" justifyContent="space-between" alignItems="center" mx={8} my={2}>
         <Box display="flex" flexDirection="row" gridGap={1}>
           <Typography variant="subtitle2">Search Results:</Typography>
           <Typography variant="body2">
             {`(${standards.length} Result${standards.length !== 1 ? 's' : ''})`}
           </Typography>
         </Box>
-        <ChplSystemMaintenanceActivity
-          fetch={useFetchStandardsActivity}
-          title="Standards"
-        />
-        { hasAnyRole(['chpl-admin', 'chpl-onc']) && (
-          <Button
-            onClick={() => dispatch({ action: 'edit', payload: {} })}
-            id="add-new-standard"
-            variant="contained"
-            color="primary"
-            endIcon={<AddIcon />}
-          >
-            Add
-          </Button>
-        )}
-      </div>
+        <div className={classes.tableResultsHeaderContainer}> 
+          <ChplSystemMaintenanceActivity
+            fetch={useFetchStandardsActivity}
+            title="Standards"
+          />
+          { hasAnyRole(['chpl-admin', 'chpl-onc']) && (
+            <Button
+              onClick={() => dispatch({ action: 'edit', payload: {} })}
+              id="add-new-standard"
+              variant="contained"
+              color="primary"
+              endIcon={<AddIcon />}
+            >
+              Add
+            </Button>
+          )}
+        </div>
+      </Box>
       <TableContainer className={classes.container} component={Paper}>
         <Table
           aria-label="Standards table"
