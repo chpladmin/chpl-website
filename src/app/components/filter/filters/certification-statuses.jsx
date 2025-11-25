11866bd11c7679c81a47f64d681305ce90762910
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-11-25 15:41:32 -0500

    feat: enhance certification status display with icons and improve filter functionality
    [#OCD-5067]

--- src/app/components/filter/filters/certification-statuses.jsx
+++ src/app/components/filter/filters/certification-statuses.jsx
@@ -1,19 +1,70 @@
+import React from 'react';
+import {
+  Box,
+  Checkbox,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+
 import { defaultFilter } from 'components/filter';
+import { getStatusIcon } from 'services/listing.service';
+
+const getCertificationStatusValueEntry = ({ filter, handleFilterToggle }) => filter.values.map((value) => {
+  const labelId = `filter-panel-secondary-items-${(`${value.value}`).replace(/ /g, '_')}`;
+  return (
+    <ListItem
+      key={value.value}
+      button
+      onClick={() => handleFilterToggle(value)}
+      disabled={filter.required && value.selected && filter.values.filter((a) => a.selected).length === 1}
+    >
+      <ListItemIcon>
+        <Checkbox
+          color="primary"
+          edge="start"
+          checked={value?.selected || false}
+          tabIndex={-1}
+          inputProps={{ 'aria-labelledby': labelId }}
+        />
+      </ListItemIcon>
+      <Box
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        {getStatusIcon({ name: filter.getLongValueDisplay(value) })}
+      </Box>
+      <ListItemText style={{ display: 'flex', flex: 'none', flexDirection: 'row' }} id={labelId}>
+        {filter.getLongValueDisplay(value)}
+      </ListItemText>
+    </ListItem>
+  );
+});
+
+const values = [
+  { value: 'Active', default: true },
+  { value: 'Suspended by ONC', default: true },
+  { value: 'Suspended by ONC-ACB', default: true },
+  { value: 'Withdrawn by Developer' },
+  { value: 'Retired' },
+  { value: 'Terminated by ONC' },
+  { value: 'Withdrawn by Developer Under Surveillance/Review' },
+  { value: 'Withdrawn by ONC-ACB' },
+];
 
 const filter = {
   ...defaultFilter,
   key: 'certificationStatuses',
   display: 'Certification Status',
-  values: [
-    { value: 'Active', default: true },
-    { value: 'Suspended by ONC', default: true },
-    { value: 'Suspended by ONC-ACB', default: true },
-    { value: 'Terminated by ONC' },
-    { value: 'Withdrawn by Developer Under Surveillance/Review' },
-    { value: 'Withdrawn by ONC-ACB' },
-    { value: 'Withdrawn by Developer' },
-    { value: 'Retired' },
-  ],
+  getValueEntry: getCertificationStatusValueEntry,
+  sortValues: (f, a, b) => {
+    const aIndex = values.findIndex((v) => v.value === a.value);
+    const bIndex = values.findIndex((v) => v.value === b.value);
+    return aIndex - bIndex;
+  },
+  values,
 };
 
 export default filter;
