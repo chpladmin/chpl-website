e2c9471be3cd9518a7b547927089f9ba5ea982f0
Andrew Larned <andrew.larned@gmail.com>
2025-12-17 14:34:38 -0500

    refactor: remove old components
    [#OCD-5124]

--- src/app/services/network.service.js
+++ src/app/services/network.service.js
@@ -61,17 +61,6 @@ export default class NetworkService {
     return this.apiGET(`/acbs?editable=${editable}`, { forceReload: true });
   }
 
-  getAllCriteria(props) {
-    const params = Object
-      .entries(props)
-      .filter(([, value]) => value !== null && value !== undefined)
-      .map(([key, value]) => `${key}=${value}`)
-      .join('&');
-    let query = '/certification-criteria';
-    if (params.length > 0) { query += `?${params}`; }
-    return this.apiGET(query);
-  }
-
   getAnnualSurveillanceReports() {
     return this.apiGET('/surveillance-report/annual');
   }
