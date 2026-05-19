8257f56642dbc4ff0010ee986dc98fd0fde213fd
Andrew Larned <andrew.larned@gmail.com>
2026-05-19 13:14:13 -0400

    feat: add editing fields for demo CR
    [#OCD-5249]

--- src/app/components/demographics/demographics-wizard-section-2.jsx
+++ src/app/components/demographics/demographics-wizard-section-2.jsx
@@ -5,16 +5,21 @@ import {
   Card,
   CardContent,
   Container,
+  Divider,
+  FormControlLabel,
+  Switch,
   Typography,
   makeStyles,
 } from '@material-ui/core';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import Moment from 'react-moment';
 import { bool, func } from 'prop-types';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
-import ChplDeveloperEdit from 'components/developer/developer-edit';
 import ChplUrlChecker from 'components/url-checker/url-checker';
 import UrlCheckerContext from 'components/url-checker/url-checker-context';
+import { ChplTextField } from 'components/util';
 import { eventTrack } from 'services/analytics.service';
 import { DeveloperContext, UserContext, useAnalyticsContext } from 'shared/contexts';
 import { utilStyles } from 'themes';
@@ -55,7 +60,69 @@ const useStyles = makeStyles({
   editContainer: {
     gridColumn: '1 / 7',
   },
+  editFields: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gap: '16px',
+    alignItems: 'start',
+  },
 });
+
+const validationSchema = yup.object({
+  fullName: yup.string()
+    .required('Full Name is required')
+    .max(500, 'Full Name is too long'),
+  email: yup.string()
+    .email('Improper format (sample@example.com)')
+    .required('Email is required')
+    .max(250, 'Email is too long'),
+  phoneNumber: yup.string()
+    .required('Phone is required')
+    .max(100, 'Phone is too long'),
+  line1: yup.string()
+    .required('Address is required')
+    .max(250, 'Address is too long'),
+  line2: yup.string()
+    .max(250, 'Line 2 is too long'),
+  city: yup.string()
+    .required('City is required')
+    .max(250, 'City is too long'),
+  state: yup.string()
+    .required('State is required')
+    .max(250, 'State is too long'),
+  zipcode: yup.string()
+    .required('Zip is required')
+    .max(25, 'Zip is too long'),
+  country: yup.string()
+    .required('Country is required')
+    .max(250, 'Country is too long'),
+  website: yup.string()
+    .url('Improper format (http://www.example.com)')
+    .required('Website is required')
+    .max(300, 'Website is too long'),
+});
+
+const getEditField = ({
+  key,
+  display,
+  formik,
+  required = true,
+  className,
+}) => (
+  <div className={className}>
+    <ChplTextField
+      id={key}
+      name={key}
+      label={display}
+      required={required}
+      value={formik.values[key]}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      error={formik.touched[key] && !!formik.errors[key]}
+      helperText={formik.touched[key] && formik.errors[key]}
+    />
+  </div>
+);
 
 function ChplDemographicsWizardSection2({ isSubmitting = false, dispatch }) {
   const { developer } = useContext(DeveloperContext);
@@ -63,8 +130,12 @@ function ChplDemographicsWizardSection2({ isSubmitting = false, dispatch }) {
   const { user } = useContext(UserContext);
   const { url, setUrl } = useContext(UrlCheckerContext);
   const classes = useStyles();
+  let formik;
 
-  const isSubmitDisabled = () => (!url || url.length === 0 || isSubmitting);
+  const getEnhancedEditField = (editProps) => getEditField({
+    ...editProps,
+    formik,
+  });
 
   const handleDispatch = ({ action, url: submittedUrl }) => {
     switch (action) {
@@ -85,6 +156,28 @@ function ChplDemographicsWizardSection2({ isSubmitting = false, dispatch }) {
     });
     dispatch(url);
   };
+
+  const isSubmitDisabled = () => (!url || url.length === 0 || isSubmitting);
+
+  formik = useFormik({
+    initialValues: {
+      selfDeveloper: !!developer.selfDeveloper,
+      fullName: developer.contact?.fullName || '',
+      email: developer.contact?.email || '',
+      phoneNumber: developer.contact?.phoneNumber || '',
+      line1: developer.address?.line1 || '',
+      line2: developer.address?.line2 || '',
+      city: developer.address?.city || '',
+      state: developer.address?.state || '',
+      zipcode: developer.address?.zipcode || '',
+      country: developer.address?.country || '',
+      website: developer.website || '',
+    },
+    onSubmit: () => {
+      save();
+    },
+    validationSchema,
+  });
 
   return (
     <div className={classes.fixFooterSpacing}>
@@ -142,20 +235,38 @@ function ChplDemographicsWizardSection2({ isSubmitting = false, dispatch }) {
           </CardContent>
         </Card>
         <Card className={classes.editContainer}>
-          <CardContent>
-            <ChplDeveloperEdit
-              dispatch={handleDispatch}
-              errorMessages={[]}
-              isInvalid={false}
-              isSplitting={false}
+          <CardContent className={classes.editFields}>
+            <FormControlLabel
+              control={(
+                <Switch
+                  id="self-developer"
+                  name="selfDeveloper"
+                  color="primary"
+                  checked={formik.values.selfDeveloper}
+                  onChange={formik.handleChange}
+                  className={classes.fullWidthGridRow}
+                />
+              )}
+              label="Self-Developer"
             />
-          </CardContent>
-        </Card>
-        <Card className={classes.urlContainer}>
-          <CardContent>
-            <ChplUrlChecker
-              dispatch={handleDispatch}
-            />
+            <Divider className={classes.fullWidthGridRow} />
+            { getEnhancedEditField({ key: 'fullName', display: 'Full Name', className: classes.fullWidthGridRow }) }
+            { getEnhancedEditField({ key: 'email', display: 'Email' }) }
+            { getEnhancedEditField({ key: 'phoneNumber', display: 'Phone' }) }
+            <Divider className={classes.fullWidthGridRow} />
+            { getEnhancedEditField({ key: 'line1', display: 'Address' }) }
+            { getEnhancedEditField({ key: 'line2', display: 'Line 2', required: false }) }
+            { getEnhancedEditField({ key: 'city', display: 'City' }) }
+            { getEnhancedEditField({ key: 'state', display: 'State' }) }
+            { getEnhancedEditField({ key: 'zipcode', display: 'Zip' }) }
+            { getEnhancedEditField({ key: 'country', display: 'Country' }) }
+            <Divider className={classes.fullWidthGridRow} />
+            <div className={classes.fullWidthGridRow}>
+              <ChplUrlChecker
+                dispatch={handleDispatch}
+                url={formik.values.website}
+              />
+            </div>
           </CardContent>
         </Card>
         <div className={classes.fullWidthGridRow}>
