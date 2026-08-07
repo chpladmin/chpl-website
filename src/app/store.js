bc4a0e1acdbe21b327fb2d1a0758515e2d8b23f7
Andrew Larned <andrew.larned@gmail.com>
2026-08-07 12:15:37 -0400

    feat: save store in localStorage
    [#OCD-5392]

--- src/app/store.js
+++ src/app/store.js
@@ -2,8 +2,38 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import userInfoReducer from 'components/login/userInfo.slice';
 
-export default configureStore({
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('reduxState');
+    if (serializedState === null) return undefined; // Let reducers initialize state
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('reduxState', serializedState);
+  } catch (err) {
+    // Ignore write errors or log them
+  }
+};
+
+const preloadedState = loadState();
+
+const store = configureStore({
   reducer: {
     userInfo: userInfoReducer,
   },
+  preloadedState,
 });
+
+store.subscribe(() => {
+  saveState({
+    userInfo: store.getState().userInfo,
+  });
+});
+
+export default store;
