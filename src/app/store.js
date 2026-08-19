884b06895a23f90e95227affa7cc8841f65c677d
Andrew Larned <andrew.larned@gmail.com>
2026-08-19 12:35:02 -0400

    fix: revert attempt to save redux state in localstorage
    [#OCD-5392]

--- src/app/store.js
+++ src/app/store.js
@@ -3,40 +3,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import browserInfoReducer from 'components/browser/browserInfo.slice';
 import userInfoReducer from 'components/login/userInfo.slice';
 
-const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem('reduxState');
-    if (serializedState === null) return undefined; // Let reducers initialize state
-    return JSON.parse(serializedState);
-  } catch (err) {
-    return undefined;
-  }
-};
-
-const saveState = (state) => {
-  try {
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('reduxState', serializedState);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const preloadedState = loadState();
-
 const store = configureStore({
   reducer: {
     browserInfo: browserInfoReducer,
     userInfo: userInfoReducer,
   },
-  preloadedState,
-});
-
-store.subscribe(() => {
-  saveState({
-    browserInfo: store.getState().browserInfo,
-    userInfo: store.getState().userInfo,
-  });
 });
 
 export default store;
