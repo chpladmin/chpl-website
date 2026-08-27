029a1ae634cdede2b1e90e026ad7d63784e36b79
Andrew Larned <andrew.larned@gmail.com>
2026-08-27 09:36:29 -0400

    fix!: Handle case where "up to date" field is null
    [#OCD-5400]

--- src/app/components/listing/details/criteria/criterion.jsx
+++ src/app/components/listing/details/criteria/criterion.jsx
@@ -175,7 +175,7 @@ function ChplCriterion({
                 { criterion.criterion.title }
               </Typography>
             </Box>
-              { !criterion.upToDate && !isDisabled
+            { criterion.upToDate === false && !isDisabled
               && (
                 <Box>
                   <ChplTooltip title="Requirement not met">
